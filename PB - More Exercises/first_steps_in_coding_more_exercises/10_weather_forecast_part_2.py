ffcc9770db78fb2ecb46c1154a578f7885e5d9e9
ceo-trash <87097003+ceo-trash@users.noreply.github.com>
2022-03-03 15:58:14 +0200

    Update 10_weather_forecast_part_2.py

--- PB - More Exercises/first_steps_in_coding_more_exercises/10_weather_forecast_part_2.py
+++ PB - More Exercises/first_steps_in_coding_more_exercises/10_weather_forecast_part_2.py
@@ -1,8 +1,8 @@
 weather_celcius = float(input())
 
-if weather_celcius <= 5.00:
+if weather_celcius <= 5.00 or weather_celcius > 35:
     print("unknown")
-elif 5.00 <= weather_celcius <= 11.9:
+elif weather_celcius <= 11.9:
     print("Cold")
 elif weather_celcius <= 14.90:
     print("Cool")
@@ -12,8 +12,7 @@ elif weather_celcius <= 25.90:
     print("Warm")
 elif weather_celcius <= 35.00:
     print("Hot")
-else:
-    print("unknown")
+
 
     
 
