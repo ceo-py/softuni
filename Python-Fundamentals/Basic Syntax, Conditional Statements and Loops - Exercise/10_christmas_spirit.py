645d62dbb50beb6dc71ff6ae197ff25d3b328c44
ceo-py <sfmf@abv.bg>
2022-05-21 00:34:09 +0200

    clearing unnecessary boolean properties

--- Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/10_christmas_spirit.py
+++ Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/10_christmas_spirit.py
@@ -11,10 +11,10 @@ for day in range(1, days + 1):
     if day % 11 == 0:
         quantity += 2
     if day % 2 == 0:
-        total_cost += (ornament_set * quantity)
+        total_cost += ornament_set * quantity
         gained_spirit += 5
     if day % 3 == 0:
-        total_cost += (tree_skirt * quantity) + (tree_garlands * quantity)
+        total_cost += (tree_skirt + tree_garlands) * quantity
         tree_set = True
         gained_spirit += 13
     if day % 5 == 0:
@@ -23,9 +23,9 @@ for day in range(1, days + 1):
         if tree_set:
             gained_spirit += 30
     if day % 10 == 0:
-        total_cost += (tree_skirt + tree_garlands + tree_lights)
+        total_cost += tree_skirt + tree_garlands + tree_lights
         gained_spirit -= 20
-if days % 10 == 0 :
+if days % 10 == 0:
     gained_spirit -= 30
 
 print(f"Total cost: {total_cost}")
