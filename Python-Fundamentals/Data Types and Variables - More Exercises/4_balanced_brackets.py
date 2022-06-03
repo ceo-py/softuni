0521887132cd812663bd3e5521d96bd8762a0de8
ceo-py <sfmf@abv.bg>
2022-06-03 14:57:09 +0200

    another solution

--- Python-Fundamentals/Data Types and Variables - More Exercises/4_balanced_brackets.py
+++ Python-Fundamentals/Data Types and Variables - More Exercises/4_balanced_brackets.py
@@ -1,25 +1,41 @@
 number = int(input())
-
-open = 0
-close = 0
-check_for_next_one = False
-close_next = 0
+counter = 0
 for _ in range(number):
-
     check = input()
-
-    if check == "(":
-        open += 1
-        check_for_next_one = True
-
-    elif check == ")":
-        close += 1
-        if check_for_next_one:
-            close_next += 1
-            check_for_next_one = False
-
-if open == close and close == close_next:
+    if "(" in check:
+        counter += 1
+    elif ")" in check:
+        counter -= 1
+    if 0 != counter != 1:
+        print("UNBALANCED")
+        break
+else:
     print("BALANCED")
 
-else:
-    print("UNBALANCED")
+
+
+# number = int(input())
+#
+# open = 0
+# close = 0
+# check_for_next_one = False
+# close_next = 0
+# for _ in range(number):
+#
+#     check = input()
+#
+#     if check == "(":
+#         open += 1
+#         check_for_next_one = True
+#
+#     elif check == ")":
+#         close += 1
+#         if check_for_next_one:
+#             close_next += 1
+#             check_for_next_one = False
+#
+# if open == close and close == close_next:
+#     print("BALANCED")
+#
+# else:
+#     print("UNBALANCED")
