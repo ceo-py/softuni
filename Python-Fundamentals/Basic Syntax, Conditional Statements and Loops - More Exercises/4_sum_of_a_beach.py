37a03efb7095f2036df327f7b1cad7619eef468d
ceo-py <sfmf@abv.bg>
2022-06-03 14:55:39 +0200

    comprehend solution

--- Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercises/4_sum_of_a_beach.py
+++ Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercises/4_sum_of_a_beach.py
@@ -1,39 +1,50 @@
-text = input()
-counter = 0
-convert_text_to_lower = list(text.lower())
-i = 1
-check_len = len(convert_text_to_lower)
-for word in convert_text_to_lower:
-    if check_len - 2 == i:
-        break
-    check_word = word + convert_text_to_lower[i] + convert_text_to_lower[i + 1] + convert_text_to_lower[i + 2]
-    i += 1
-    if "sand" in check_word:
-        counter += 1
+text = input().lower()
+words = ["sand", "water", "fish", "sun"]
+print(sum([text.count(word) for word in words]))
 
-    if "fish" in check_word:
-        counter += 1
 
-check_word = ""
-i = 1
-for word in convert_text_to_lower:
-    if check_len - 1 == i:
-        break
-    check_word = word + convert_text_to_lower[i] + convert_text_to_lower[i + 1]
-    i += 1
-    if "sun" in check_word:
-        counter += 1
 
-check_word = ""
-i = 1
-for word in convert_text_to_lower:
-    if check_len - 3 == i:
-        break
 
-    check_word = word + convert_text_to_lower[i] + convert_text_to_lower[i + 1] + convert_text_to_lower[i + 2] + \
-                 convert_text_to_lower[i + 3]
-    i += 1
-    if "water" in check_word:
-        counter += 1
 
-print(counter)
+
+
+#
+# text = input()
+# counter = 0
+# convert_text_to_lower = list(text.lower())
+# i = 1
+# check_len = len(convert_text_to_lower)
+# for word in convert_text_to_lower:
+#     if check_len - 2 == i:
+#         break
+#     check_word = word + convert_text_to_lower[i] + convert_text_to_lower[i + 1] + convert_text_to_lower[i + 2]
+#     i += 1
+#     if "sand" in check_word:
+#         counter += 1
+#
+#     if "fish" in check_word:
+#         counter += 1
+#
+# check_word = ""
+# i = 1
+# for word in convert_text_to_lower:
+#     if check_len - 1 == i:
+#         break
+#     check_word = word + convert_text_to_lower[i] + convert_text_to_lower[i + 1]
+#     i += 1
+#     if "sun" in check_word:
+#         counter += 1
+#
+# check_word = ""
+# i = 1
+# for word in convert_text_to_lower:
+#     if check_len - 3 == i:
+#         break
+#
+#     check_word = word + convert_text_to_lower[i] + convert_text_to_lower[i + 1] + convert_text_to_lower[i + 2] + \
+#                  convert_text_to_lower[i + 3]
+#     i += 1
+#     if "water" in check_word:
+#         counter += 1
+#
+# print(counter)
