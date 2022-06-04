bf189d74237bc34db62dba40bf5e48dd2474a72f
ceo-py <sfmf@abv.bg>
2022-06-04 23:41:37 +0200

    ref

--- Python-Fundamentals/Data Types and Variables - Exercise/4_sum_of_chars.py
+++ Python-Fundamentals/Data Types and Variables - Exercise/4_sum_of_chars.py
@@ -1,8 +1,11 @@
-number_of_letters = int(input())
+print(f"The sum equals: {sum(ord(input()) for _ in range(int(input())))}")
 
-total_sum = 0
-for _ in range(number_of_letters):
-    text = input()
-    total_sum += ord(text)
 
-print(f"The sum equals: {total_sum}")
+# number_of_letters = int(input())
+#
+# total_sum = 0
+# for _ in range(number_of_letters):
+#     text = input()
+#     total_sum += ord(text)
+#
+# print(f"The sum equals: {total_sum}")
