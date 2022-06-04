d7802fc5ad4ad17eb1a1072d9d788a46cb9ad513
ceo-py <sfmf@abv.bg>
2022-06-04 18:37:54 +0200

    ref

--- Python-Fundamentals/Lists Basics - Exercise/8_seize_the_fire.py
+++ Python-Fundamentals/Lists Basics - Exercise/8_seize_the_fire.py
@@ -6,37 +6,19 @@ effort = 0
 total_fire = 0
 water_left = water
 for clean_text in fire_levels:
-    if "High = " in clean_text:
-        text = int(clean_text.replace("High = ", ""))
-
-        if text in range(81, 126) and water_left >= text:
-            put_out_cells.append(text)
-            effort += text * 0.25
-            total_fire += text
-            water_left -= text
-
-    elif "Low = " in clean_text:
-        text = int(clean_text.replace("Low = ", ""))
-
-        if text in range(1, 51) and water_left >= text:
-            put_out_cells.append(text)
-            effort += text * 0.25
-            total_fire += text
-            water_left -= text
-
-    elif "Medium = " in clean_text:
-        text = int(clean_text.replace("Medium = ", ""))
-
-        if text in range(51, 81) and water_left >= text:
-            put_out_cells.append(int(text))
-            effort += text * 0.25
-            total_fire += text
-            water_left -= text
+    type_of_fire, cell_value = clean_text.split(" = ")
+    cell_value = int(cell_value)
+    if water_left >= cell_value:
+        if any(["High" in type_of_fire and cell_value in range(81, 126),
+                "Low" in type_of_fire and cell_value in range(1, 51),
+                "Medium" in type_of_fire and cell_value in range(51, 81)]):
+            put_out_cells.append(cell_value)
+            effort += cell_value * 0.25
+            total_fire += cell_value
+            water_left -= cell_value
 
 print("Cells:")
 for n in put_out_cells:
     print(f" - {n}")
-
 print(f"Effort: {effort:.2f}")
-
 print(f"Total Fire: {total_fire}")
