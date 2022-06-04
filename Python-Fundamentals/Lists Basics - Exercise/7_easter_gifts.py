5df117729cecc83ab4844aee40645159db0d8fa0
ceo-py <sfmf@abv.bg>
2022-06-04 18:25:48 +0200

    ref

--- Python-Fundamentals/Lists Basics - Exercise/7_easter_gifts.py
+++ Python-Fundamentals/Lists Basics - Exercise/7_easter_gifts.py
@@ -3,21 +3,17 @@ names_of_gifts = input().split(" ")
 
 command = input()
 while command != "No Money":
-    if "OutOfStock" in command:
-        out_of_stock = command.split()
+    command_type, *other_info = command.split()
+    if "OutOfStock" in command_type:
         for i, name in enumerate(names_of_gifts):
-            if out_of_stock[-1] == name:
+            if other_info[-1] == name:
                 names_of_gifts[i] = "None"
-
-    elif "Required" in command:
-        required_gift = command.split()
+    elif "Required" in command_type:
         length = len(names_of_gifts)
-        if length > int(required_gift[-1]) >= 0:
-            names_of_gifts[int(required_gift[-1])] = required_gift[1]
-
-    elif "JustInCase" in command:
-        just_in_case = command.split()
-        names_of_gifts[-1] = just_in_case[-1]
+        if length > int(other_info[-1]) >= 0:
+            names_of_gifts[int(other_info[-1])] = other_info[0]
+    elif "JustInCase" in command_type:
+        names_of_gifts[-1] = other_info[-1]
     command = input()
 
 print(" ".join(x for x in names_of_gifts if x != "None"))
