a4a1653512608543bc548e88899192402357a60a
ceo-py <sfmf@abv.bg>
2022-06-04 19:36:10 +0200

    ref

--- Python-Fundamentals/Lists Basics - Exercise/6_survival_of_the_biggest.py
+++ Python-Fundamentals/Lists Basics - Exercise/6_survival_of_the_biggest.py
@@ -1,9 +1,18 @@
-numbers = list(map(int,input().strip().split(" ")))
-how_many_numbers_to_remove = int(input())
-
-for n in range(how_many_numbers_to_remove):
-    numbers.remove(min(numbers))
+numbers = list(map(int, input().strip().split(" ")))
+[numbers.remove(min(numbers)) for _ in range(int(input()))]
 print(", ".join(str(x) for x in numbers))
+
+
+
+
+
+
+# numbers = list(map(int,input().strip().split(" ")))
+# how_many_numbers_to_remove = int(input())
+#
+# for n in range(how_many_numbers_to_remove):
+#     numbers.remove(min(numbers))
+# print(", ".join(str(x) for x in numbers))
 
 
 
