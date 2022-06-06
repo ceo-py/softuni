0d443f015d2b3a40711624bffe8c9ea7c96a676c
ceo-py <sfmf@abv.bg>
2022-06-06 20:29:04 +0200

    ref

--- Python-Fundamentals/Lists Basics - Exercise/5_faro_shuffle.py
+++ Python-Fundamentals/Lists Basics - Exercise/5_faro_shuffle.py
@@ -1,16 +1,34 @@
 cards = input().split()
 shuffle = int(input())
+middle_of_deck = len(cards) // 2
 
-lenght = len(cards)
-mid = int(lenght / 2)
+for number_of_shuffle in range(shuffle):
+    result_after_shuffle = []
+    for mid_card, front_card in enumerate(range(middle_of_deck), middle_of_deck):
+        result_after_shuffle.append(cards[front_card])
+        result_after_shuffle.append(cards[mid_card])
+    cards = result_after_shuffle.copy()
 
-for i in range(shuffle):
-    list = []
-    for p in range(0, mid):
-        list.append(cards[p])
-        list.append(cards[mid])
-        mid += 1
-    cards = list
-    mid = int(lenght / 2)
+print(cards)
 
-print(list)
+
+
+
+
+
+# cards = input().split()
+# shuffle = int(input())
+#
+# lenght = len(cards)
+# mid = int(lenght / 2)
+#
+# for i in range(shuffle):
+#     list = []
+#     for p in range(0, mid):
+#         list.append(cards[p])
+#         list.append(cards[mid])
+#         mid += 1
+#     cards = list
+#     mid = int(lenght / 2)
+#
+# print(list)
