07d1ead67e4a40221197072eb0fdb2dc0888e2b1
ceo-py <sfmf@abv.bg>
2022-06-06 15:55:54 +0200

    ref

--- Python-Fundamentals/Lists Basics - More Exercises/3._car_race.py
+++ Python-Fundamentals/Lists Basics - More Exercises/3._car_race.py
@@ -1,28 +1,48 @@
-sequence_of_numbers = input().split(" ")
+sequence_of_numbers = [int(num) for num in input().split()]
 
-car_one_total = 0
-car_two_total = 0
-s_car = sequence_of_numbers
-where_is_split = len(sequence_of_numbers) // 2
+first_car = len(sequence_of_numbers) // 2
 
-for car_one, car_two in zip(sequence_of_numbers[:where_is_split], s_car[::-1]):
-    car_one = int(car_one)
-    car_two = int(car_two)
-    if car_one == 0:
-        car_one_total = car_one_total * 0.8
-    else:
-        car_one_total += car_one
+first_car_score = sum([num if num != 0 else -sum(sequence_of_numbers[:first_car][:pos]) * 0.2 for pos, num in
+                       enumerate(sequence_of_numbers[:first_car])])
+second_car_score = sum([num if num != 0 else -sum(sequence_of_numbers[::-1][:first_car][:pos]) * 0.2 for pos, num in
+                    enumerate(sequence_of_numbers[::-1][:first_car])])
 
-    if car_two == 0:
-        car_two_total = car_two_total * 0.8
-    else:
-        car_two_total += car_two
-
-if car_one_total < car_two_total:
-    print(f"The winner is left with total time: {car_one_total:.1f}")
-
+if first_car_score < second_car_score:
+    print(f"The winner is left with total time: {first_car_score:.1f}")
 else:
-    print(f"The winner is right with total time: {car_two_total:.1f}")
+    print(f"The winner is right with total time: {second_car_score:.1f}")
+
+
+
+#
+# sequence_of_numbers = input().split(" ")
+#
+# car_one_total = 0
+# car_two_total = 0
+# s_car = sequence_of_numbers
+# where_is_split = len(sequence_of_numbers) // 2
+#
+# for car_one, car_two in zip(sequence_of_numbers[:where_is_split], s_car[::-1]):
+#     car_one = int(car_one)
+#     car_two = int(car_two)
+#     if car_one == 0:
+#         car_one_total = car_one_total * 0.8
+#     else:
+#         car_one_total += car_one
+#
+#     if car_two == 0:
+#         car_two_total = car_two_total * 0.8
+#     else:
+#         car_two_total += car_two
+#
+# if car_one_total < car_two_total:
+#     print(f"The winner is left with total time: {car_one_total:.1f}")
+#
+# else:
+#     print(f"The winner is right with total time: {car_two_total:.1f}")
+#
+#
+
 
 
 # sequence_of_numbers = input().split(" ")
