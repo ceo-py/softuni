f86ef9a82acf32d6ed4db6d81272a2c9fffc7673
ceo-py <sfmf@abv.bg>
2022-06-08 17:08:55 +0200

    initial solution

---
+++ Python Advanced/Python Advanced - Exercises/Tuples and Sets - Lab/6_summation_pairs.py
@@ -0,0 +1,15 @@
+pairs_of_numbers = [int(num) for num in input().split()]
+
+target_number = int(input())
+result_print = []
+total_iterations = 0
+for pos, number_one in enumerate(pairs_of_numbers, 1):
+    for number_two in pairs_of_numbers[pos:]:
+        total_iterations += 1
+        if number_one + number_two == target_number:
+            print(f"{number_one} + {number_two} = {target_number}")
+            result_print.append(f"({number_one}, {number_two})")
+
+print(f"Iterations done: {total_iterations}")
+if result_print:
+    print('\n'.join(set(result_print)))
