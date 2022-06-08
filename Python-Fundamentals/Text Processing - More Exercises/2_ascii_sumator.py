ec39017f7c281c490f75b26cbfc00a9c596b7938
ceo-py <sfmf@abv.bg>
2022-06-08 11:48:21 +0200

    ref

--- Python-Fundamentals/Text Processing - More Exercises/2_ascii_sumator.py
+++ Python-Fundamentals/Text Processing - More Exercises/2_ascii_sumator.py
@@ -1,8 +1,16 @@
 start_char, end_char, random_string = input(), input(), input()
 
-show_result = 0
-for character in random_string:
-    if ord(start_char) < ord(character) < ord(end_char):
-        show_result += ord(character)
+print(sum([ord(char) for char in random_string if ord(start_char) < ord(char) < ord(end_char)]))
 
-print(show_result)
+
+
+
+
+# start_char, end_char, random_string = input(), input(), input()
+#
+# show_result = 0
+# for character in random_string:
+#     if ord(start_char) < ord(character) < ord(end_char):
+#         show_result += ord(character)
+#
+# print(show_result)
