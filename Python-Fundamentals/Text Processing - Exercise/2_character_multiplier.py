495162d7b730d48c0b1e1aa0c9b1bee95c31f8e9
ceo-py <sfmf@abv.bg>
2022-06-08 12:24:30 +0200

    ref

--- Python-Fundamentals/Text Processing - Exercise/2_character_multiplier.py
+++ Python-Fundamentals/Text Processing - Exercise/2_character_multiplier.py
@@ -1,14 +1,29 @@
-from itertools import zip_longest
-
 first_string, second_string = input().split()
-result_ = []
 
-for ltr_one, ltr_two in zip_longest(first_string, second_string, fillvalue="Skip"):
-    if "Skip" != ltr_one and "Skip" != ltr_two:
-        result_.append(ord(ltr_one) * ord(ltr_two))
-    elif "Skip" != ltr_one:
-        result_.append(ord(ltr_one))
-    elif "Skip" != ltr_two:
-        result_.append(ord(ltr_two))
+first_string = [ord(num) for num in first_string]
+second_string = [ord(num) for num in second_string]
+first_string_len = len(first_string)
+second_string_len = len(second_string)
+if first_string_len > second_string_len:
+    for _ in range(first_string_len - second_string_len):
+        second_string.append(1)
+elif first_string_len < second_string_len:
+    for _ in range(second_string_len - first_string_len):
+        first_string.append(1)
+print(sum([first_string[i] * second_string[i] for i in range(len(first_string))]))
 
-print(sum(result_))
+
+# from itertools import zip_longest
+
+# first_string, second_string = input().split()
+# result_ = []
+#
+# for ltr_one, ltr_two in zip_longest(first_string, second_string, fillvalue="Skip"):
+#     if ltr_one != "Skip" != ltr_two:
+#         result_.append(ord(ltr_one) * ord(ltr_two))
+#     elif "Skip" != ltr_one:
+#         result_.append(ord(ltr_one))
+#     elif "Skip" != ltr_two:
+#         result_.append(ord(ltr_two))
+#
+# print(sum(result_))
