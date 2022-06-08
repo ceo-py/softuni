32561379716fda72514f730dc124822d8c8b0add
ceo-py <sfmf@abv.bg>
2022-06-08 16:52:24 +0200

    initial solution

---
+++ Python Advanced/Python Advanced - Exercises/Tuples and Sets - Lab/5_softuni_party.py
@@ -0,0 +1,16 @@
+number_of_guests = int(input())
+
+reservation = [input() for _ in range(number_of_guests)]
+guests_who_came = input()
+while guests_who_came != "END":
+    if guests_who_came in reservation:
+        reservation.remove(guests_who_came)
+    guests_who_came = input()
+
+print(len(reservation))
+vip, regular = [], []
+[vip.append(res) if res[0].isdigit() else regular.append(res) for res in reservation]
+if vip:
+    print('\n'.join(sorted(vip)))
+if regular:
+    print('\n'.join(sorted(regular)))
