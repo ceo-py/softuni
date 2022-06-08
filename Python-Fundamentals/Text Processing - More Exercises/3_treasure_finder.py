a3bc82be4ff0b06a7ec6a505ac7587b4a9b84192
ceo-py <sfmf@abv.bg>
2022-06-08 11:39:31 +0200

    ref

--- Python-Fundamentals/Text Processing - More Exercises/3_treasure_finder.py
+++ Python-Fundamentals/Text Processing - More Exercises/3_treasure_finder.py
@@ -1,18 +1,39 @@
-import re
 secret_key = [int(x) for x in input().split()]
 secret_msg = input()
 
 how_long = len(secret_key)
-pattern = re.compile(r"&(.+)&.+<(.+)>")
 while secret_msg != "find":
-    decode_msg = ""
-    index_key = 0
-    for x in secret_msg:
-        decode_msg += chr(ord(x) - secret_key[index_key])
-        index_key += 1
-        if index_key >= how_long:
-            index_key = 0
-    matches = pattern.finditer(decode_msg)
-    for show in matches:
-        print(f"Found {show[1]} at {show[2]}")
+    aa = "".join([chr(ord(secret_msg[i]) - secret_key[i % len(secret_key)]) for i in range(len(secret_msg))])
+    item = aa.split("&")[-2]
+    location = aa.split("<")[-1][:-1]
+    print(f"Found {item} at {location}")
     secret_msg = input()
+
+
+
+
+
+
+
+#
+#
+#
+#
+# import re
+# secret_key = [int(x) for x in input().split()]
+# secret_msg = input()
+#
+# how_long = len(secret_key)
+# pattern = re.compile(r"&(.+)&.+<(.+)>")
+# while secret_msg != "find":
+#     decode_msg = ""
+#     index_key = 0
+#     for x in secret_msg:
+#         decode_msg += chr(ord(x) - secret_key[index_key])
+#         index_key += 1
+#         if index_key >= how_long:
+#             index_key = 0
+#     matches = pattern.finditer(decode_msg)
+#     for show in matches:
+#         print(f"Found {show[1]} at {show[2]}")
+#     secret_msg = input()
