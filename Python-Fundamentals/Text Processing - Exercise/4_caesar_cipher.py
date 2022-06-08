f45cdbc36818af4630151c3c83531e3dd9182069
ceo-py <sfmf@abv.bg>
2022-06-08 12:34:55 +0200

    ref

--- Python-Fundamentals/Text Processing - Exercise/4_caesar_cipher.py
+++ Python-Fundamentals/Text Processing - Exercise/4_caesar_cipher.py
@@ -1,7 +1,10 @@
-main_text = input()
-result = ""
-for letter in main_text:
-    cripted_letter = ord(letter)
-    result += chr(cripted_letter + 3)
+print("".join([chr(ord(letter) + 3) for letter in input()]))
 
-print(result)
+
+# main_text = input()
+# result = ""
+# for letter in main_text:
+#     cripted_letter = ord(letter)
+#     result += chr(cripted_letter + 3)
+#
+# print(result)
