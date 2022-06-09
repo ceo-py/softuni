55171996d1b72225b6fda81e6f079f2f40c2fe3d
ceo-py <sfmf@abv.bg>
2022-06-09 11:35:46 +0200

    ref

--- PB - More Exercises/while_loop_more_exercises/3_stream_of_letters.py
+++ PB - More Exercises/while_loop_more_exercises/3_stream_of_letters.py
@@ -1,36 +1,66 @@
-is_loop_exit = False
-is_c = False
-is_o = False
-is_n = False
+skip_letters = "con"
 
-cuttent_string = ""
-result_string = ""
-
-while (not is_loop_exit):
-    input_string = input()
-
-    if input_string.lower() == "end":
-        is_loop_exit = True
+letter = input()
+complete_word = ""
+skip_letters_count = ""
+guess_word = ""
+while letter != "End":
+    if letter in skip_letters:
+        skip_letters_count += letter
     else:
-        if input_string == "c" and not is_c:
-            is_c = True
-        elif input_string == "o" and not is_o:
-            is_o = True
-        elif input_string == "n" and not is_n:
-            is_n = True
-        else:
-            if input_string.isalpha():
-                cuttent_string = f"{cuttent_string}{input_string}"
+        if letter.islower() or letter.isupper():
+            guess_word += letter
+    if "c" in skip_letters_count and "o" in skip_letters_count and "n" in skip_letters_count:
+        skip_letters_count = ""
+        complete_word += f"{guess_word} "
+        guess_word = ""
+    for char in skip_letters_count:
+        if skip_letters_count.count(letter) > 1:
+            guess_word += letter
+            break
+    letter = input()
 
-        is_code_complete = (is_n and is_o and is_c)
-        if (is_code_complete):
-            result_string = f"{result_string}{cuttent_string} "
-            cuttent_string = ""
-            is_c = False
-            is_o = False
-            is_n = False
+print(complete_word)
 
-print(result_string)
+
+
+
+
+
+
+# is_loop_exit = False
+# is_c = False
+# is_o = False
+# is_n = False
+#
+# cuttent_string = ""
+# result_string = ""
+#
+# while (not is_loop_exit):
+#     input_string = input()
+#
+#     if input_string.lower() == "end":
+#         is_loop_exit = True
+#     else:
+#         if input_string == "c" and not is_c:
+#             is_c = True
+#         elif input_string == "o" and not is_o:
+#             is_o = True
+#         elif input_string == "n" and not is_n:
+#             is_n = True
+#         else:
+#             if input_string.isalpha():
+#                 cuttent_string = f"{cuttent_string}{input_string}"
+#
+#         is_code_complete = (is_n and is_o and is_c)
+#         if (is_code_complete):
+#             result_string = f"{result_string}{cuttent_string} "
+#             cuttent_string = ""
+#             is_c = False
+#             is_o = False
+#             is_n = False
+#
+# print(result_string)
 
 
 # import string
