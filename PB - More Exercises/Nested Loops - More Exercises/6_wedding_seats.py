9748aefa1797d73d121bad11684e932e4b82fdd7
ceo-py <sfmf@abv.bg>
2022-06-14 20:25:52 +0200

    ref

--- PB - More Exercises/Nested Loops - More Exercises/6_wedding_seats.py
+++ PB - More Exercises/Nested Loops - More Exercises/6_wedding_seats.py
@@ -4,14 +4,11 @@ sector_simbols = input()
 number_rows_sector_one = int(input())
 number_space_odd_row = int(input())
 
-alphabet_string_low = string.ascii_lowercase
-alphabet_list_lower = list(alphabet_string_low)
-alphabet_string_upper = string.ascii_uppercase
-alphabet_list_up = list(alphabet_string_upper)
+alphabet_list_lower = string.ascii_lowercase
+alphabet_list_up = string.ascii_uppercase
 sector_one = alphabet_list_up.index(sector_simbols)
 checking_letters = alphabet_list_up[:sector_one + 1]
 even_rows = number_space_odd_row + 1
-letter_count = 0
 counter_operation = 0
 add_row = 0
 
@@ -24,14 +21,11 @@ for letter in checking_letters:
         even_rows_count = 0
 
         for possition in alphabet_list_lower:
+
             if added_rows > even_rows:
                 break
 
             if letter == "A":
-
-                if row % 2 != 0 and number_space_odd_row > added_rows:
-                    print(f"{letter}{row}{possition}")
-                    counter_operation += 1
 
                 if row % 2 == 0 and even_rows_count < even_rows:
                     print(f"{letter}{row}{possition}")
@@ -39,17 +33,14 @@ for letter in checking_letters:
                     counter_operation += 1
 
             else:
-
-                if row % 2 != 0 and number_space_odd_row > added_rows:
-                    print(f"{letter}{row}{possition}")
-                    counter_operation += 1
-
                 if row % 2 == 0 and even_rows_count < even_rows - add_row + 1:
                     print(f"{letter}{row}{possition}")
                     even_rows_count += 1
                     counter_operation += 1
 
+            if row % 2 != 0 and number_space_odd_row > added_rows:
+                print(f"{letter}{row}{possition}")
+                counter_operation += 1
             added_rows += 1
-    letter_count += 1
 
 print(counter_operation)
