1d2430e32e4a34dbee3a77e9b2d9faaabafab17d
ceo-py <sfmf@abv.bg>
2022-06-14 08:46:11 +0200

    ref

--- Python-Fundamentals/Functions - Exercise/4_odd_and_even_sum.py
+++ Python-Fundamentals/Functions - Exercise/4_odd_and_even_sum.py
@@ -2,8 +2,8 @@ numbers = input()
 
 
 def all_sum(num):
-    return f"Odd sum = {sum([int(n) for n in num if int(n) % 2 !=0])}," \
-           f" Even sum = {sum([int(n) for n in num if int(n) % 2 ==0])}"
+    return f"Odd sum = {sum(int(n) for n in num if int(n) % 2 !=0)}," \
+           f" Even sum = {sum(int(n) for n in num if int(n) % 2 ==0)}"
 
 
 print(all_sum(numbers))
