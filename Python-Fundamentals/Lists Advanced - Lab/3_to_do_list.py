b76bde0576563782bf95270bf572201856988416
ceo-py <sfmf@abv.bg>
2022-06-17 19:57:32 +0200

    ref

--- Python-Fundamentals/Lists Advanced - Lab/3_to_do_list.py
+++ Python-Fundamentals/Lists Advanced - Lab/3_to_do_list.py
@@ -1,16 +1,39 @@
-main_list = [0] * 10
+command = input()
 
-while True:
+result = []
+
+while command != "End":
+    pos, task = command.split("-")
+    result.append([int(pos), task])
     command = input()
 
-    if command == "End":
-        break
+print([to_do[1] for to_do in sorted(result, key=lambda x: x[0])])
 
-    tokents = command.split("-")
-    priority = int(tokents[0]) - 1
-    note = tokents[1]
-    main_list.pop(priority)
-    main_list.insert(priority, note)
 
-result = [n for n in main_list if n != 0]
-print(result)
+
+
+
+
+
+
+
+
+
+#
+#
+# main_list = [0] * 10
+#
+# while True:
+#     command = input()
+#
+#     if command == "End":
+#         break
+#
+#     tokents = command.split("-")
+#     priority = int(tokents[0]) - 1
+#     note = tokents[1]
+#     main_list.pop(priority)
+#     main_list.insert(priority, note)
+#
+# result = [n for n in main_list if n != 0]
+# print(result)
