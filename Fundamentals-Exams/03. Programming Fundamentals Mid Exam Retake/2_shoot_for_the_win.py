3ca72fe83da5c66388bd07059b9bf70d3812e5f0
ceo-py <sfmf@abv.bg>
2022-06-21 16:53:00 +0200

    ref

--- Fundamentals-Exams/03. Programming Fundamentals Mid Exam Retake/2_shoot_for_the_win.py
+++ Fundamentals-Exams/03. Programming Fundamentals Mid Exam Retake/2_shoot_for_the_win.py
@@ -11,14 +11,48 @@ while command != "End":
         target_value = main_target[command]
         for index, value in enumerate(main_target):
             if value != -1:
+                result_between_targets = value - target_value
                 if value <= target_value:
                     result_between_targets = value + target_value
-                    main_target[index] = result_between_targets
-                else:
-                    result_between_targets = value - target_value
-                    main_target[index] = result_between_targets
+                main_target[index] = result_between_targets
         main_target[command] = -1
 
     command = input()
 
 print(f"Shot targets: {made_shots} ->", *main_target, sep=" ")
+
+
+
+
+
+
+
+
+
+
+#
+#
+# main_target = [int(n) for n in input().split()]
+#
+# made_shots = 0
+# command = input()
+# targets_number = len(main_target) - 1
+#
+# while command != "End":
+#     command = int(command)
+#     if targets_number >= command >= 0 and main_target[command] != -1:
+#         made_shots += 1
+#         target_value = main_target[command]
+#         for index, value in enumerate(main_target):
+#             if value != -1:
+#                 if value <= target_value:
+#                     result_between_targets = value + target_value
+#                     main_target[index] = result_between_targets
+#                 else:
+#                     result_between_targets = value - target_value
+#                     main_target[index] = result_between_targets
+#         main_target[command] = -1
+#
+#     command = input()
+#
+# print(f"Shot targets: {made_shots} ->", *main_target, sep=" ")
