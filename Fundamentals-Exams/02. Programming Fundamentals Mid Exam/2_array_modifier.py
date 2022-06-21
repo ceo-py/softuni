204b67001222ca9c2f230dbc10d7fb7de54d49d5
ceo-py <sfmf@abv.bg>
2022-06-21 17:17:52 +0200

    ref

--- Fundamentals-Exams/02. Programming Fundamentals Mid Exam/2_array_modifier.py
+++ Fundamentals-Exams/02. Programming Fundamentals Mid Exam/2_array_modifier.py
@@ -4,10 +4,8 @@ command = input()
 
 def swap_command(number_one, number_two):
     global main_array_values
-    first_element = main_array_values[number_one]
-    second_element = main_array_values[number_two]
-    main_array_values[number_one] = second_element
-    main_array_values[number_two] = first_element
+    main_array_values[number_one], main_array_values[number_two] = main_array_values[number_two], main_array_values[number_one]
+
 
 
 def multiply_command(number_one, number_two):
@@ -38,3 +36,29 @@ while command != "end":
     command = input()
 
 print(*main_array_values, sep=", ")
+
+
+
+# numbers = list(map(int, input().split()))
+# command = input()
+#
+# while command != 'end':
+#     command_list = command.split()
+#     action = command_list[0]
+#     if action == 'swap':
+#         swap_one = int(command_list[1])
+#         swap_two = int(command_list[2])
+#         numbers[swap_one], numbers[swap_two] = numbers[swap_two], numbers[swap_one]
+#     elif action == 'multiply':
+#         multiplier_one = int(numbers[int(command_list[1])])
+#         multiplier_two = int(numbers[int(command_list[2])])
+#         multiplication = multiplier_one * multiplier_two
+#         numbers[int(command_list[1])] = multiplication
+#     elif command == 'decrease':
+#         for pos in range(len(numbers)):
+#             numbers[pos] -= 1
+#     command = input()
+#
+# final_list = [str(i) for i in numbers]
+#
+# print(', '.join(final_list))
