95bfa56e7aac7b105064fc35666bce61d4751d09
ceo-py <sfmf@abv.bg>
2022-06-21 16:17:18 +0200

    ref

--- Fundamentals-Exams/02. Programming Fundamentals Mid Exam/1_softuni_reception.py
+++ Fundamentals-Exams/02. Programming Fundamentals Mid Exam/1_softuni_reception.py
@@ -1,16 +1,35 @@
-employee_one_efficiency = int(input())
-employee_two_efficiency = int(input())
-employee_three_efficiency = int(input())
-students_in_q = int(input())
-
-total_answers_per_hour = employee_one_efficiency + employee_two_efficiency + employee_three_efficiency
-
-time_needed = 0
-for hour in range(1, students_in_q + 1):
-    if hour % 4 != 0 and students_in_q > 0:
-        students_in_q += -total_answers_per_hour
-    elif students_in_q <= 0:
-        break
+employee_info = [int(input()) for num in range(4)]
+total_answers_per_hour = sum(employee_info[:3])
+students_in_q = employee_info[-1]
+time_needed = 1
+while students_in_q > 0:
+    if time_needed % 4 != 0:
+        students_in_q -= total_answers_per_hour
     time_needed += 1
 
-print(f"Time needed: {time_needed}h.")
+print(f"Time needed: {time_needed - 1}h.")
+
+
+
+
+
+
+#
+#
+#
+# employee_one_efficiency = int(input())
+# employee_two_efficiency = int(input())
+# employee_three_efficiency = int(input())
+# students_in_q = int(input())
+#
+# total_answers_per_hour = employee_one_efficiency + employee_two_efficiency + employee_three_efficiency
+#
+# time_needed = 0
+# for hour in range(1, students_in_q + 1):
+#     if hour % 4 != 0 and students_in_q > 0:
+#         students_in_q += -total_answers_per_hour
+#     elif students_in_q <= 0:
+#         break
+#     time_needed += 1
+#
+# print(f"Time needed: {time_needed}h.")
