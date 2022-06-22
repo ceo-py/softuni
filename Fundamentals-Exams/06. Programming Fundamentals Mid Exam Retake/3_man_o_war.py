0d1da603b3e2c5b1d2e7c8481bd0c9d9da9fbac0
ceo-py <sfmf@abv.bg>
2022-06-22 23:17:22 +0200

    ref

--- Fundamentals-Exams/06. Programming Fundamentals Mid Exam Retake/3_man_o_war.py
+++ Fundamentals-Exams/06. Programming Fundamentals Mid Exam Retake/3_man_o_war.py
@@ -1,8 +1,6 @@
 pirate_ship, warship, maximum_health = [int(n) for n in input().split(">")], [int(n) for n in input().split(">")], int(
     input())
-
 command = input()
-# ship_alive = True
 
 
 def status_ship():
@@ -15,25 +13,21 @@ def status_ship():
 
 
 def fire_ship(index, damage):
-    global ship_alive
     if 0 <= index < len(warship):
         warship[index] -= damage
         if warship[index] <= 0:
             print(f"You won! The enemy ship has sunken.")
-            # ship_alive = False
             exit()
 
 
 def defend_ship(start_index, end_index, damage):
-    global ship_alive
     if 0 <= start_index < len(pirate_ship) and 0 <= end_index < len(pirate_ship):
         for index in range(start_index, end_index + 1):
             pirate_ship[index] -= damage
             if pirate_ship[index] <= 0:
                 print(f"You lost! The pirate ship has sunken.")
-                # ship_alive = False
-                # break
                 exit()
+
 
 def repair_ship(index, health):
     if 0 <= index < len(pirate_ship):
@@ -48,20 +42,15 @@ while command != "Retire":
     if type_command == "Status":
         status_ship()
     else:
-        index_ship = int(command[1])
-        damage_or_health_ship = int(command[2])
+        index_ship, damage_or_health_ship = int(command[1]), int(command[2])
     if type_command == "Fire":
         fire_ship(index_ship, damage_or_health_ship)
     elif type_command == "Defend":
         defend_ship(index_ship, damage_or_health_ship, int(command[-1]))
     elif type_command == "Repair":
         repair_ship(index_ship, damage_or_health_ship)
-    # if not ship_alive:
-    #     break
     command = input()
 
-
-# if ship_alive:
 if len(pirate_ship) and len(warship) > 0:
     print(f"Pirate ship status: {sum(pirate_ship)}")
     print(f"Warship status: {sum(warship)}")
