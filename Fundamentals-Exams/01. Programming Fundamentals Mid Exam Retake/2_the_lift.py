bb6128d970f351d2dc3cf804ffce642b5737c467
ceo-py <sfmf@abv.bg>
2022-06-24 17:05:28 +0200

    ref

--- Fundamentals-Exams/01. Programming Fundamentals Mid Exam Retake/2_the_lift.py
+++ Fundamentals-Exams/01. Programming Fundamentals Mid Exam Retake/2_the_lift.py
@@ -33,3 +33,21 @@ if not enough_spots and equal_spots:
     print(f"There isn't enough space! {passengers} people in a queue!")
 
 print(*floors_lift, sep=" ")
+
+
+
+
+#
+# people = int(input())
+# current_wagons = list(map(int, input().split()))
+# for i in range(len(current_wagons)):
+#     max_people = min(4 - current_wagons[i], people)
+#     current_wagons[i] += max_people
+#     people -= max_people
+#
+# if people > 0:
+#     print(f"There isn't enough space! {people} people in a queue!")
+# elif len([wagon for wagon in current_wagons if wagon < 4]) > 0:
+#     print("The lift has empty spots!")
+#
+# print(" ".join([str(wagon) for wagon in current_wagons]))
