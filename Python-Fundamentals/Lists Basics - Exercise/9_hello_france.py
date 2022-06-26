bba1d643ce7a26731a96231a72fa88b60992c2e8
ceo-py <sfmf@abv.bg>
2022-06-26 17:42:58 +0200

    ref

--- Python-Fundamentals/Lists Basics - Exercise/9_hello_france.py
+++ Python-Fundamentals/Lists Basics - Exercise/9_hello_france.py
@@ -1,29 +1,75 @@
-items_accessories = input().split("|")
+collection_of_items = input().split("|")
 budget = int(input())
-
-items_price, selling_items = list(), list()
 budget_left = budget
 train_ticket = 150
+total_money = 0
+items_bought = []
 
-for clean_text in items_accessories:
-    type_item, price = clean_text.split("->")
-    price = float(price)
-    if budget_left >= price:
-        if any(["Clothes" in type_item and price <= 50, "Shoes" in type_item and price <= 35,
-                "Accessories" in type_item and price <= 20.50]):
-            items_price.append(price)
-            budget_left -= price
-            selling_items.append(price * 1.40)
 
-difference = sum(selling_items) - sum(items_price)
-for n in selling_items:
-    print(f"{n:.2f}", end=" ")
+def add_money(current_price):
+    global total_money, budget_left
+    total_money += current_price
+    budget_left -= current_price
 
-print(f"\nProfit: {difference:.2f}")
+
+for index in collection_of_items:
+    current_item, current_price = [float(x) if x[-1].isdigit() else x for x in index.split('->')]
+    if budget_left >= current_price:
+        if current_item == 'Clothes' and current_price <= 50:
+            add_money(current_price)
+            items_bought.append(current_price + (current_price * 0.40))
+        elif current_item == 'Shoes' and current_price <= 35:
+            add_money(current_price)
+            items_bought.append(current_price + (current_price * 0.40))
+        elif current_item == 'Accessories' and current_price <= 20.50:
+            add_money(current_price)
+            items_bought.append(current_price + (current_price * 0.40))
+
+difference = sum(items_bought) - total_money
+print(' '.join(f"{n:.2f}" for n in items_bought))
+print(f'Profit: {difference:.2f}')
 if budget + difference > train_ticket:
-    print(f"Hello, France!")
+    print('Hello, France!')
 else:
-    print("Not enough money.")
+    print('Not enough money.')
+
+
+
+
+
+
+
+# items_accessories = input().split("|")
+# budget = int(input())
+#
+# items_price, selling_items = list(), list()
+# budget_left = budget
+# train_ticket = 150
+#
+# for clean_text in items_accessories:
+#     type_item, price = clean_text.split("->")
+#     price = float(price)
+#     if budget_left >= price:
+#         if any(["Clothes" in type_item and price <= 50, "Shoes" in type_item and price <= 35,
+#                 "Accessories" in type_item and price <= 20.50]):
+#             items_price.append(price)
+#             budget_left -= price
+#             selling_items.append(price * 1.40)
+#
+# difference = sum(selling_items) - sum(items_price)
+# for n in selling_items:
+#     print(f"{n:.2f}", end=" ")
+#
+# print(f"\nProfit: {difference:.2f}")
+# if budget + difference > train_ticket:
+#     print(f"Hello, France!")
+# else:
+#     print("Not enough money.")
+
+
+
+
+
 
 
 # items_accessories = input().split("|")
