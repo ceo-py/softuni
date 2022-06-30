5c418001c9e0a44870974de0b34fc91e4ce819f1
ceo-py <sfmf@abv.bg>
2022-06-30 10:46:32 +0200

    ref

---
+++ Python Advanced/Lists as Stacks and Queues - Lab/1_reverse_strings.py
@@ -0,0 +1,14 @@
+stack_list = list(input())
+while stack_list:
+    print(stack_list.pop(), sep= "")
+
+
+# from collections import deque
+# stack = deque(input())
+# stack.reverse()
+# print(*stack, sep="")
+
+
+
+
+# print(input()[::-1])
