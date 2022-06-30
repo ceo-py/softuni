c7461326b7764898d5e2cc7b2f2f66b22b8a84e4
ceo-py <sfmf@abv.bg>
2022-06-30 17:18:45 +0200

    ref

--- Python Basics/Conditional Statements Advanced - Exercise/6_operations_between_numbers.py
+++ Python Basics/Conditional Statements Advanced - Exercise/6_operations_between_numbers.py
@@ -2,34 +2,26 @@ number_one = int(input())
 number_two = int(input())
 operator = input()
 
-if number_one == 0:
+if number_one == 0 == number_two:
     print(f"Cannot divide {number_one} by zero")
-elif number_two == 0:
-    print(f"Cannot divide {number_one} by zero")
-elif operator == "+":
-    total = number_one + number_two
-    if total % 2 == 0:
-        print(f"{number_one} + {number_two} = {total} - even")
-    else:
-        print(f"{number_one} + {number_two} = {total} - odd")
-elif operator == "-":
-    total = number_one - number_two
-    if total % 2 == 0:
-        print(f"{number_one} - {number_two} = {total} - even")
-    else:
-        print(f"{number_one} - {number_two} = {total} - odd")
-elif operator == "*":
-    total = number_one * number_two
-    if total % 2 == 0:
-        print(f"{number_one} * {number_two} = {total} - even")
-    else:
-        print(f"{number_one} * {number_two} = {total} - odd")
 elif operator == "/":
     total = number_one / number_two
     print(f"{number_one} / {number_two} = {total:.2f}")
 elif operator == "%":
     total = number_one % number_two
     print(f"{number_one} % {number_two} = {total}")
+else:
+    if operator == "+":
+        total = number_one + number_two
+    elif operator == "-":
+        total = number_one - number_two
+    elif operator == "*":
+        total = number_one * number_two
+    if total % 2 == 0:
+        print(f"{number_one} {operator} {number_two} = {total} - even")
+    else:
+        print(f"{number_one} {operator} {number_two} = {total} - odd")
+
 
 
 
