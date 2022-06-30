2ecdaa6140dbb1e814b7d8a48709b6718e0c2d70
ceo-py <sfmf@abv.bg>
2022-06-30 12:49:32 +0200

    ref

--- Python Advanced/Lists as Stacks and Queues - Lab/5_hot_potato.py
+++ Python Advanced/Lists as Stacks and Queues - Lab/5_hot_potato.py
@@ -8,3 +8,19 @@ while len(kids) > 1:
     print(f"Removed {kids.pop()}")
 
 print(f"Last is {kids.popleft()}")
+
+
+
+
+# from collections import deque
+# kids = deque(input().split())
+# tosses = int(input())
+#
+# while len(kids) > 1:
+#     for num in range(1, tosses + 1):
+#         if num % tosses == 0:
+#             print(f"Removed {kids.popleft()}")
+#             break
+#         kids.append(kids.popleft())
+#
+# print(f"Last is {kids.pop()}")
