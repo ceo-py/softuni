ed1dacbd97f91bbed9a7d0a6cdc136c6b7bfef50
ceo-py <sfmf@abv.bg>
2022-07-02 13:10:29 +0200

    ref

--- Python Basics/For Loop - Lab/10_odd_even_sum.py
+++ Python Basics/For Loop - Lab/10_odd_even_sum.py
@@ -1,18 +1,15 @@
 number_enter = int(input())
 
-numbers = list()
 even_number = 0
 odd_number = 0
 count = 2
 
-for _ in range(number_enter):
+for position in range(number_enter):
     number = int(input())
-    numbers.append(number)
-for number_one in numbers:
-    if (count % 2) != 0:
-        odd_number += number_one
+    if (position % 2) != 0:
+        odd_number += number
     else:
-        even_number += number_one
+        even_number += number
     count += 1
 
 if even_number == odd_number:
@@ -20,3 +17,37 @@ if even_number == odd_number:
 else:
     total = odd_number - even_number
     print(f"No\nDiff = {abs(total)}")
+
+
+
+
+
+
+#
+#
+#
+#
+#
+#
+# number_enter = int(input())
+#
+# numbers = list()
+# even_number = 0
+# odd_number = 0
+# count = 2
+#
+# for _ in range(number_enter):
+#     number = int(input())
+#     numbers.append(number)
+# for number_one in numbers:
+#     if (count % 2) != 0:
+#         odd_number += number_one
+#     else:
+#         even_number += number_one
+#     count += 1
+#
+# if even_number == odd_number:
+#     print(f"Yes\nSum = {abs(even_number)}")
+# else:
+#     total = odd_number - even_number
+#     print(f"No\nDiff = {abs(total)}")
