913c75d0b334b815bcae9bce4f19b484a99caba2
ceo-py <sfmf@abv.bg>
2022-07-03 14:23:36 +0200

    ref

--- Python Advanced/Python Advanced - Exercises/Tuples and Sets - Exercise/2_sets_of_elements.py
+++ Python Advanced/Python Advanced - Exercises/Tuples and Sets - Exercise/2_sets_of_elements.py
@@ -1,4 +1,24 @@
 first_set_sep, second_set_sep = [int(num) for num in input().split()]
 
-all_numbers = [input() for _ in range(first_set_sep+second_set_sep)]
-print("\n".join(list(set(all_numbers[:first_set_sep]) & set(all_numbers[first_set_sep:]))))
+first_set = set()
+second_set = set()
+
+for num in range(first_set_sep):
+    first_set.add(input())
+
+for num in range(second_set_sep):
+    second_set.add(input())
+
+print("\n".join(first_set.intersection(second_set)))
+
+
+
+
+
+
+#
+#
+# first_set_sep, second_set_sep = [int(num) for num in input().split()]
+#
+# all_numbers = [input() for _ in range(first_set_sep+second_set_sep)]
+# print("\n".join(list(set(all_numbers[:first_set_sep]) & set(all_numbers[first_set_sep:]))))
