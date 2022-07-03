56bcbc1bddcb3bdf2dd5f900dd78fab04e86a886
ceo-py <sfmf@abv.bg>
2022-07-03 13:25:02 +0200

    ref

--- Python Advanced/Python Advanced - Exercises/Tuples and Sets - Lab/4_parking_lot.py
+++ Python Advanced/Python Advanced - Exercises/Tuples and Sets - Lab/4_parking_lot.py
@@ -1,14 +1,39 @@
 car_numbers = int(input())
 
-parking_lot = {}
+parking_lot = set()
 for car in range(car_numbers):
     direction, car_plate_number = input().split(", ")
-    if "IN" == direction:
-        parking_lot[car_plate_number] = direction
-        continue
-    del parking_lot[car_plate_number]
 
-if parking_lot.keys():
-    print('\n'.join(parking_lot.keys()))
+    if "IN" == direction:
+        parking_lot.add(car_plate_number)
+
+    elif "OUT" == direction:
+        parking_lot.remove(car_plate_number)
+
+
+if parking_lot:
+    print('\n'.join(parking_lot))
 else:
     print("Parking Lot is Empty")
+
+
+
+
+
+
+
+
+# car_numbers = int(input())
+#
+# parking_lot = {}
+# for car in range(car_numbers):
+#     direction, car_plate_number = input().split(", ")
+#     if "IN" == direction:
+#         parking_lot[car_plate_number] = direction
+#         continue
+#     del parking_lot[car_plate_number]
+#
+# if parking_lot.keys():
+#     print('\n'.join(parking_lot.keys()))
+# else:
+#     print("Parking Lot is Empty")
