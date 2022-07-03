e5157788a178dbff4f9a2c82b1e767dc87cf9ec1
ceo-py <sfmf@abv.bg>
2022-07-03 13:14:50 +0200

    ref

--- Python Advanced/Python Advanced - Exercises/Tuples and Sets - Lab/3_record_unique_names.py
+++ Python Advanced/Python Advanced - Exercises/Tuples and Sets - Lab/3_record_unique_names.py
@@ -1,4 +1,16 @@
 number_of_names = int(input())
 
-names = [input() for _ in range(number_of_names)]
-print("\n".join(set(names)))
+names = set()
+
+for _ in range(number_of_names):
+    names.add(input())
+
+print("\n".join(names))
+
+
+
+#
+# number_of_names = int(input())
+#
+# names = [input() for _ in range(number_of_names)]
+# print("\n".join(set(names)))
