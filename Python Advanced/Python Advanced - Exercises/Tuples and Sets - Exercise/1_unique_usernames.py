8669850c60f9110153cf90fc0212aff9c7f5f686
ceo-py <sfmf@abv.bg>
2022-07-03 14:11:53 +0200

    ref

--- Python Advanced/Python Advanced - Exercises/Tuples and Sets - Exercise/1_unique_usernames.py
+++ Python Advanced/Python Advanced - Exercises/Tuples and Sets - Exercise/1_unique_usernames.py
@@ -1,3 +1,21 @@
 number_of_user_names = int(input())
-names = [input() for _ in range(number_of_user_names)]
-print("\n".join(set(names)))
+names = set()
+for name in range(number_of_user_names):
+    names.add(input())
+
+print("\n".join(names))
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# number_of_user_names = int(input())
+# names = [input() for _ in range(number_of_user_names)]
+# print("\n".join(set(names)))
