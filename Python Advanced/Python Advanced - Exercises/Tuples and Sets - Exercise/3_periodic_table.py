985c367d4a75844064020add53cd676073ffed7a
ceo-py <sfmf@abv.bg>
2022-07-03 14:28:46 +0200

    ref

--- Python Advanced/Python Advanced - Exercises/Tuples and Sets - Exercise/3_periodic_table.py
+++ Python Advanced/Python Advanced - Exercises/Tuples and Sets - Exercise/3_periodic_table.py
@@ -1,8 +1,27 @@
 elements_count = int(input())
 
-elements = []
+elements = set()
 for _ in range(elements_count):
-    for element in input().split():
-        elements.append(element)
+    how_many_elements = input().split()
+    for element in how_many_elements:
+        elements.add(element)
 
-print("\n".join(set(elements)))
+print("\n".join(elements))
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+# elements_count = int(input())
+#
+# elements = []
+# for _ in range(elements_count):
+#     for element in input().split():
+#         elements.append(element)
+#
+# print("\n".join(set(elements)))
