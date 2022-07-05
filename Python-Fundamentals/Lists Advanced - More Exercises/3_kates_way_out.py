b3694521dfc6b0fff69a8988aedadf699fbdb453
ceo-py <sfmf@abv.bg>
2022-07-05 13:37:37 +0200

    ref

--- Python-Fundamentals/Lists Advanced - More Exercises/3_kates_way_out.py
+++ Python-Fundamentals/Lists Advanced - More Exercises/3_kates_way_out.py
@@ -1,3 +1,77 @@
+def correct_lab_bounds(row, col):
+    if row < 0 or col < 0 or row >= len(lab) or col >= len(lab[0]):
+        return True
+
+
+def check_wall(row, col):
+    if lab[row][col] == "#":
+        return True
+
+
+def check_already_visit(row, col):
+    if lab[row][col] == "v":
+        return True
+
+
+def find_exit(row, col):
+    if row == 0 or row == len(lab) - 1 or col == 0 or col == len(lab[0]):
+        return True
+
+
+def find_starting_point():
+    for pos_row, row in enumerate(lab):
+        for pos_col, col in enumerate(row):
+            if col == "k":
+                return pos_row, pos_col
+
+
+def find_the_lab_path(row, col, lab):
+    if correct_lab_bounds(row, col):
+        return
+    if check_wall(row, col):
+        return
+    if check_already_visit(row, col):
+        return
+
+    path_steps.append(1)
+
+    if find_exit(row, col):
+        max_len_path.append(sum(path_steps))
+
+    lab[row][col] = "v"
+    find_the_lab_path(row, col + 1, lab)  # check right
+    find_the_lab_path(row, col - 1, lab)  # check left
+    find_the_lab_path(row + 1, col, lab)  # check up
+    find_the_lab_path(row - 1, col, lab)  # check down
+    lab[row][col] = " "
+
+    path_steps.pop()
+
+
+row = int(input())
+lab = []
+path_steps = []
+max_len_path = []
+for _ in range(row):
+    lab.append(list(input()))
+col = len(lab[0])
+start_row, start_col = find_starting_point()
+
+find_the_lab_path(start_row, start_col, lab)
+
+if max_len_path:
+    print(f"Kate got out in {max(max_len_path)} moves")
+else:
+    print("Kate cannot get out")
+
+
+
+
+
+
+
+
+
 # def find_position(maze):
 #     position = []
 #     for row in range(len(maze)):
@@ -80,81 +154,83 @@
 #
 
 
-import queue
 
-maze_rows = int(input())
-maze = []
-way_out = []
-for _ in range(maze_rows):
-    maze.append([x for x in input()])
-
-
-def find_start_point(maze, point_start):
-    for i, row in enumerate(maze):
-        for j, value in enumerate(row):
-            if point_start == value:
-                return i, j
-
-    return None
-
-
-def find_end_point(maze, point_end):
-    for i, row in enumerate(maze):
-        for j, value in enumerate(row):
-            if (i == 0 or i == maze_rows - 1) and point_end == value:
-                return i, j
-
-    return None
-
-
-def find_path(maze):
-    global way_out
-    start = "k"
-    end = find_end_point(maze, " ")
-    start_pos = find_start_point(maze, start)
-    q = queue.Queue()
-    q.put((start_pos, [start_pos]))
-    visited = set()
-
-    while not q.empty():
-        current_pos, path = q.get()
-        row, col = current_pos
-        if current_pos == end:
-            way_out = path
-            return path
-
-        neighbors = find_neighbors(maze, row, col)
-        for neighbor in neighbors:
-            if neighbor in visited:
-                continue
-
-            r, c = neighbor
-            if maze[r][c] == "#":
-                continue
-
-            new_path = path + [neighbor]
-            q.put((neighbor, new_path))
-            visited.add(neighbor)
-
-
-def find_neighbors(maze, row, col):
-    neighbors = []
-    if row > 0:  # UP
-        neighbors.append((row - 1, col))
-    if row + 1 < len(maze):  # DOWN
-        neighbors.append((row + 1, col))
-    if col > 0:  # LEFT
-        neighbors.append((row, col - 1))
-    if col + 1 < len(maze[0]):  # RIGHT
-        neighbors.append((row, col + 1))
-
-    return neighbors
-
-
-find_path(maze)
-if way_out:
-    print(f"Kate got out in {len(way_out)} moves")
-else:
-    print("Kate cannot get out")
-
-
+#
+# import queue
+#
+# maze_rows = int(input())
+# maze = []
+# way_out = []
+# for _ in range(maze_rows):
+#     maze.append([x for x in input()])
+#
+#
+# def find_start_point(maze, point_start):
+#     for i, row in enumerate(maze):
+#         for j, value in enumerate(row):
+#             if point_start == value:
+#                 return i, j
+#
+#     return None
+#
+#
+# def find_end_point(maze, point_end):
+#     for i, row in enumerate(maze):
+#         for j, value in enumerate(row):
+#             if (i == 0 or i == maze_rows - 1) and point_end == value:
+#                 return i, j
+#
+#     return None
+#
+#
+# def find_path(maze):
+#     global way_out
+#     start = "k"
+#     end = find_end_point(maze, " ")
+#     start_pos = find_start_point(maze, start)
+#     q = queue.Queue()
+#     q.put((start_pos, [start_pos]))
+#     visited = set()
+#
+#     while not q.empty():
+#         current_pos, path = q.get()
+#         row, col = current_pos
+#         if current_pos == end:
+#             way_out = path
+#             return path
+#
+#         neighbors = find_neighbors(maze, row, col)
+#         for neighbor in neighbors:
+#             if neighbor in visited:
+#                 continue
+#
+#             r, c = neighbor
+#             if maze[r][c] == "#":
+#                 continue
+#
+#             new_path = path + [neighbor]
+#             q.put((neighbor, new_path))
+#             visited.add(neighbor)
+#
+#
+# def find_neighbors(maze, row, col):
+#     neighbors = []
+#     if row > 0:  # UP
+#         neighbors.append((row - 1, col))
+#     if row + 1 < len(maze):  # DOWN
+#         neighbors.append((row + 1, col))
+#     if col > 0:  # LEFT
+#         neighbors.append((row, col - 1))
+#     if col + 1 < len(maze[0]):  # RIGHT
+#         neighbors.append((row, col + 1))
+#
+#     return neighbors
+#
+#
+# find_path(maze)
+# if way_out:
+#     print(f"Kate got out in {len(way_out)} moves")
+# else:
+#     print("Kate cannot get out")
+#
+#
