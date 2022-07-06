3147c66d8bf2ad02c2a22c1b165200f07f1dcddd
ceo-py <sfmf@abv.bg>
2022-07-06 21:37:04 +0200

    ref

--- PB - More Exercises/For-Loop - More Exercises/8_Equal_pairs.py
+++ PB - More Exercises/For-Loop - More Exercises/8_Equal_pairs.py
@@ -9,7 +9,9 @@ for i in range(2 * n):
     curr_pair_sum += curr
 
     if i % 2 != 0 and i >= 3:
-        max_diff_pair = max(max_diff_pair, abs(curr_pair_sum - prev_pair_sum))
+        pairs = abs(curr_pair_sum - prev_pair_sum)
+        if max_diff_pair < pairs:
+            max_diff_pair = pairs
         prev_pair_sum = curr_pair_sum
         curr_pair_sum = 0
 
@@ -21,6 +23,37 @@ if max_diff_pair == 0:
     print(f"Yes, value={prev_pair_sum}")
 else:
     print(f"No, maxdiff={max_diff_pair}")
+
+
+
+
+
+
+
+
+# n = int(input())
+#
+# curr_pair_sum = 0
+# prev_pair_sum = 0
+# max_diff_pair = 0
+#
+# for i in range(2 * n):
+#     curr = int(input())
+#     curr_pair_sum += curr
+#
+#     if i % 2 != 0 and i >= 3:
+#         max_diff_pair = max(max_diff_pair, abs(curr_pair_sum - prev_pair_sum))
+#         prev_pair_sum = curr_pair_sum
+#         curr_pair_sum = 0
+#
+#     elif i % 2 != 0 and i >= 1:
+#         prev_pair_sum = curr_pair_sum
+#         curr_pair_sum = 0
+#
+# if max_diff_pair == 0:
+#     print(f"Yes, value={prev_pair_sum}")
+# else:
+#     print(f"No, maxdiff={max_diff_pair}")
 
 
 
