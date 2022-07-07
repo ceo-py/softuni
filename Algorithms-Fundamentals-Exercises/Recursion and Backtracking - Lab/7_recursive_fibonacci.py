7737520eb9d3b33fcccba0ff5a5264ad4c6f7d1b
ceo-py <sfmf@abv.bg>
2022-07-07 14:50:39 +0200

    ref

--- Algorithms-Fundamentals-Exercises/Recursion and Backtracking - Lab/7_recursive_fibonacci.py
+++ Algorithms-Fundamentals-Exercises/Recursion and Backtracking - Lab/7_recursive_fibonacci.py
@@ -1,11 +1,28 @@
-number = int(input())
-FibArray = [0, 1]
+n = int(input())
 
 
-def fibonacci_of(n):
-    if n in {0, 1}:
-        return n
-    return fibonacci_of(n - 1) + fibonacci_of(n - 2)
+def fib_of(n):
+    if n <= 1:
+        return 1
+    return fib_of(n - 1) + fib_of(n - 2)
 
 
-print(sum([fibonacci_of(n) for n in range(number)]) + 1)
+print(fib_of(n))
+
+
+
+
+#
+# number = int(input())
+# FibArray = [0, 1]
+#
+#
+# def fibonacci_of(n):
+#     if n in {0, 1}:
+#         return n
+#     return fibonacci_of(n - 1) + fibonacci_of(n - 2)
+#
+#
+# print(sum([fibonacci_of(n) for n in range(number)]) + 1)
+#
+#
