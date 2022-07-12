1e81d0deeb72df82a4f81fe014147204e01f906f
ceo-py <sfmf@abv.bg>
2022-07-12 21:19:54 +0200

    ref

--- Python Basics/Conditional Statements Advanced - Lab/7_working_hours.py
+++ Python Basics/Conditional Statements Advanced - Lab/7_working_hours.py
@@ -1,17 +1,30 @@
 hour = int(input())
-day_of_the_week = input()
+day = input()
 
-week = {
-    "Monday": "open",
-    "Tuesday": "open",
-    "Wednesday": "open",
-    "Thursday": "open",
-    "Friday": "open",
-    "Saturday": "open",
-    "Sunday": "closed"
-}
-
-if hour in range(10, 19):
-    print(week[day_of_the_week])
+if 10 <= hour <= 18 and day != "Sunday":
+    print("open")
 else:
-    print(week["Sunday"])
+    print("closed")
+
+
+
+
+#
+#
+# hour = int(input())
+# day_of_the_week = input()
+#
+# week = {
+#     "Monday": "open",
+#     "Tuesday": "open",
+#     "Wednesday": "open",
+#     "Thursday": "open",
+#     "Friday": "open",
+#     "Saturday": "open",
+#     "Sunday": "closed"
+# }
+#
+# if hour in range(10, 19):
+#     print(week[day_of_the_week])
+# else:
+#     print(week["Sunday"])
