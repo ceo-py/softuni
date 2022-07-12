74501af609e33e684d96ef808c4a854b914de1cb
ceo-py <sfmf@abv.bg>
2022-07-12 15:40:46 +0200

    ref

--- PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/6_the_most_powerful_word.py
+++ PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/6_the_most_powerful_word.py
@@ -1,38 +1,78 @@
 import math
 
-end_of_last_world = True
-most_powerfull_word = ""
+word_enter = input()
+most_powerful_word = ""
 points = 0
 how_long_is_the_word = 0
 
-
-while end_of_last_world:
-    word_enter = input()
+while word_enter != "End of words":
     counter = 0
     total_word_points = 0
     bonus_points = False
 
-    if word_enter == "End of words":
-        print(f"The most powerful word is {most_powerfull_word} - {points}")
-        end_of_last_world = False
+    how_long_is_the_word = len(word_enter)
+    for letter in word_enter:
+        counter += 1
+        letters_ = "aeiouy"
+        if (letter in letters_ or letter in letters_.upper()) and counter == 1:
+            bonus_points = True
+
+        total_word_points += int(ord(letter))
+
+    if bonus_points:
+        total_word_points *= how_long_is_the_word
 
     else:
-        how_long_is_the_word = len(word_enter)
-        for letter in word_enter:
-            counter += 1
-            if (letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u" or letter == "y" or \
-                    letter == "A" or letter == "E" or letter == "I" or letter == "O" or letter == "U" or letter == "Y") and counter == 1:
-                bonus_points = True
+        total_word_points = math.floor(total_word_points / how_long_is_the_word)
 
-            total_word_points += int(ord(letter))
+    if total_word_points > points:
+        points = total_word_points
+        most_powerful_word = word_enter
 
-        if bonus_points:
-            total_word_points *= how_long_is_the_word
+    word_enter = input()
 
-        else:
-            total_word_points = math.floor(total_word_points / how_long_is_the_word)
+print(f"The most powerful word is {most_powerful_word} - {points}")
 
-        if total_word_points > points:
-            points = total_word_points
-            most_powerfull_word = word_enter
 
+
+
+#
+#
+# import math
+#
+# end_of_last_world = True
+# most_powerfull_word = ""
+# points = 0
+# how_long_is_the_word = 0
+#
+#
+# while end_of_last_world:
+#     word_enter = input()
+#     counter = 0
+#     total_word_points = 0
+#     bonus_points = False
+#
+#     if word_enter == "End of words":
+#         print(f"The most powerful word is {most_powerfull_word} - {points}")
+#         end_of_last_world = False
+#
+#     else:
+#         how_long_is_the_word = len(word_enter)
+#         for letter in word_enter:
+#             counter += 1
+#             if (letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u" or letter == "y" or \
+#                     letter == "A" or letter == "E" or letter == "I" or letter == "O" or letter == "U" or letter == "Y") and counter == 1:
+#                 bonus_points = True
+#
+#             total_word_points += int(ord(letter))
+#
+#         if bonus_points:
+#             total_word_points *= how_long_is_the_word
+#
+#         else:
+#             total_word_points = math.floor(total_word_points / how_long_is_the_word)
+#
+#         if total_word_points > points:
+#             points = total_word_points
+#             most_powerfull_word = word_enter
+#
