838ea50dd0a9bd491c40d6d0526639ee8e4a749e
ceo-py <sfmf@abv.bg>
2022-07-14 10:42:35 +0200

    ref

--- Python-Fundamentals/Dictionaries - Exercise/7_softuni_parking.py
+++ Python-Fundamentals/Dictionaries - Exercise/7_softuni_parking.py
@@ -5,17 +5,17 @@ parking_info = {}
 def register_car(name, number):
     if name in parking_info:
         print(f"ERROR: already registered with plate number {parking_info[name]}")
-    else:
-        parking_info[name] = number
-        print(f"{name} registered {number} successfully")
+        return
+    parking_info[name] = number
+    print(f"{name} registered {number} successfully")
 
 
 def unregister_car(name):
     if name not in parking_info:
         print(f"ERROR: user {name} not found")
-    else:
-        del parking_info[name]
-        print(f"{name} unregistered successfully")
+        return
+    del parking_info[name]
+    print(f"{name} unregistered successfully")
 
 
 def all_cars_in_praking():
@@ -24,14 +24,53 @@ def all_cars_in_praking():
 
 
 for _ in range(number_cars):
-    car = input()
-    car = car.split()
-    command = car[0]
-    name = car[1]
+    command, name, *number = input().split()
     if command == "register":
-        number = car[-1]
+        number = number[-1]
         register_car(name, number)
     else:
         unregister_car(name)
 
 all_cars_in_praking()
+
+
+
+#
+#
+# number_cars = int(input())
+# parking_info = {}
+#
+#
+# def register_car(name, number):
+#     if name in parking_info:
+#         print(f"ERROR: already registered with plate number {parking_info[name]}")
+#     else:
+#         parking_info[name] = number
+#         print(f"{name} registered {number} successfully")
+#
+#
+# def unregister_car(name):
+#     if name not in parking_info:
+#         print(f"ERROR: user {name} not found")
+#     else:
+#         del parking_info[name]
+#         print(f"{name} unregistered successfully")
+#
+#
+# def all_cars_in_praking():
+#     for key, value in parking_info.items():
+#         print(f"{key} => {value}")
+#
+#
+# for _ in range(number_cars):
+#     car = input()
+#     car = car.split()
+#     command = car[0]
+#     name = car[1]
+#     if command == "register":
+#         number = car[-1]
+#         register_car(name, number)
+#     else:
+#         unregister_car(name)
+#
+# all_cars_in_praking()
