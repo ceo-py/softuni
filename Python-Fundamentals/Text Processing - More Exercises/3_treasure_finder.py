7b468e01f1c0cb902883dec5efdd23b249553443
ceo-py <sfmf@abv.bg>
2022-07-16 23:23:28 +0200

    ref

--- Python-Fundamentals/Text Processing - More Exercises/3_treasure_finder.py
+++ Python-Fundamentals/Text Processing - More Exercises/3_treasure_finder.py
@@ -3,7 +3,7 @@ secret_msg = input()
 
 how_long = len(secret_key)
 while secret_msg != "find":
-    secret_text = "".join([chr(ord(secret_msg[i]) - secret_key[i % len(secret_key)]) for i in range(len(secret_msg))])
+    secret_text = "".join([chr(ord(secret_msg[i]) - secret_key[i % how_long]) for i in range(len(secret_msg))])
     item = secret_text.split("&")[-2]
     location = secret_text.split("<")[-1][:-1]
     print(f"Found {item} at {location}")
