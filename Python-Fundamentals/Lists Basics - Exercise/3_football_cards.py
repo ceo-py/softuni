f0f2287a065bf06b622f3d4217d83989541ffa62
ceo-py <sfmf@abv.bg>
2022-07-18 11:56:44 +0200

    ref

--- Python-Fundamentals/Lists Basics - Exercise/3_football_cards.py
+++ Python-Fundamentals/Lists Basics - Exercise/3_football_cards.py
@@ -8,13 +8,48 @@ for team_num in range(1, 12):
 for player in team_information:
     if player in teams_info:
         teams_info.remove(player)
-    if any([sum([x.count("A") for x in teams_info]) < 7, sum([x.count("A") for x in teams_info]) < 7]):
-        terminated = True
-        break
+        if any([sum([x.count("A") for x in teams_info]) < 7, sum([x.count("A") for x in teams_info]) < 7]):
+            terminated = True
+            break
 
 print(f"Team A - {sum([x.count('A') for x in teams_info])}; Team B - {sum([x.count('B') for x in teams_info])}")
 if terminated:
     print("Game was terminated")
+
+
+
+
+#
+#
+# team_information = input().split()
+# terminated = False
+# team_a = set()
+# team_b = set()
+# for team_num in range(1, 12):
+#     team_a.add(f"A-{team_num}")
+#     team_b.add(f"B-{team_num}")
+#
+# for player in range(1, len(team_information) + 1):
+#     if team_information[player - 1] in team_a:
+#         if len(team_a.difference(set(team_information[:player]))) < 7:
+#             terminated = True
+#             break
+#     elif team_information[player - 1] in team_b:
+#         if len(team_b.difference(set(team_information[:player]))) < 7:
+#             terminated = True
+#             break
+#
+#
+# print(f"Team A - {len(team_a.difference(set(team_information[:player])))}; Team B - {len(team_b.difference(set(team_information[:player])))}")
+# if terminated:
+#     print("Game was terminated")
+#
+#
+#
+#
+#
+
+
 
 
 
