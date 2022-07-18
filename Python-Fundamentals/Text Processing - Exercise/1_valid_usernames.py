cd67f4936824c41595ba744a18d54c040b8e9c13
ceo-py <sfmf@abv.bg>
2022-07-18 18:27:44 +0200

    ref

--- Python-Fundamentals/Text Processing - Exercise/1_valid_usernames.py
+++ Python-Fundamentals/Text Processing - Exercise/1_valid_usernames.py
@@ -1,9 +1,28 @@
-test_string = input().split(", ")
+import string
 
-for test in test_string:
-    if 3 <= len(test) <= 16:
-        for check in test:
-            if not any([check.isdigit(), check.isalpha(), check == "-", check == "_"]):
+usernames = input().split(", ")
+for username in usernames:
+    if 3 <= len(username) <= 16:
+        for char in username:
+            if char not in "-_" + string.ascii_letters + string.digits:
                 break
+
         else:
-            print(test)
+            print(username)
+
+
+
+
+
+
+#
+#
+# test_string = input().split(", ")
+#
+# for test in test_string:
+#     if 3 <= len(test) <= 16:
+#         for check in test:
+#             if not any([check.isdigit(), check.isalpha(), check == "-", check == "_"]):
+#                 break
+#         else:
+#             print(test)
