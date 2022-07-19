a7a0d06224f27bca1a4c9af41c7e14f439152776
ceo-py <sfmf@abv.bg>
2022-07-19 16:33:58 +0200

    ref

--- Fundamentals-Exams/05. Programming Fundamentals Final Exam/2_emoji_detector.py
+++ Fundamentals-Exams/05. Programming Fundamentals Final Exam/2_emoji_detector.py
@@ -1,35 +1,66 @@
 import re
 
 main_string = input()
+all_digits = ''.join(x for x in main_string if x.isdigit())
+cool_threshold = 1
+for num in all_digits:
+    cool_threshold *= int(num)
 
-find_numbers = ''.join([num for num in main_string if num.isdigit()])
-total_sum_numbers = 1
-for num in find_numbers:
-    total_sum_numbers *= int(num)
 
-patterns = re.compile(r"\:{2}([A-Z]{1}[a-z]{2,})\:{2}|\*{2}([A-Z]{1}[a-z]{2,})\*{2}")
-find_emoji = re.finditer(patterns, main_string)
-resul_to_print = []
-for find_number, show in enumerate(find_emoji):
-    asci_value = 0
-    group = 1
-    if show[1] is None:
-        group = 2
-    asci_value = sum([ord(letter) for letter in show[group]])
-    if asci_value > total_sum_numbers:
-        resul_to_print.append(show[0])
+print(f"Cool threshold: {cool_threshold}")
+patterns = re.compile(r"(::)(?P<emoji>[A-Z][a-z]{2,})(::)|(\*\*)(?P<emoji2>[A-Z][a-z]{2,})(\*\*)")
+main_string = list(re.finditer(patterns, main_string))
+print(f"{len(main_string)} emojis found in the text. The cool ones are:")
+for found in main_string:
+    if found["emoji"]:
+        result = found["emoji"]
+    elif found["emoji2"]:
+        result = found["emoji2"]
 
-print(f"Cool threshold: {total_sum_numbers}")
-print(f"{find_number + 1} emojis found in the text. The cool ones are:")
-print(*resul_to_print, sep="\n")
+    find_cool = sum([ord(letter) for letter in result])
+    if find_cool >= cool_threshold:
+        print(found[0])
 
 
 
 
 
 
-
-
+#
+#
+#
+# import re
+#
+# main_string = input()
+#
+# find_numbers = ''.join([num for num in main_string if num.isdigit()])
+# total_sum_numbers = 1
+# for num in find_numbers:
+#     total_sum_numbers *= int(num)
+#
+# patterns = re.compile(r"\:{2}([A-Z]{1}[a-z]{2,})\:{2}|\*{2}([A-Z]{1}[a-z]{2,})\*{2}")
+# find_emoji = re.finditer(patterns, main_string)
+# resul_to_print = []
+# for find_number, show in enumerate(find_emoji):
+#     asci_value = 0
+#     group = 1
+#     if show[1] is None:
+#         group = 2
+#     asci_value = sum([ord(letter) for letter in show[group]])
+#     if asci_value > total_sum_numbers:
+#         resul_to_print.append(show[0])
+#
+# print(f"Cool threshold: {total_sum_numbers}")
+# print(f"{find_number + 1} emojis found in the text. The cool ones are:")
+# print(*resul_to_print, sep="\n")
+#
+#
+#
+#
+#
+#
+#
+#
 
 
 #
