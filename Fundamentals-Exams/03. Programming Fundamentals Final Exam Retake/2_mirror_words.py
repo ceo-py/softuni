36ef7f3300dc5423558adf66d3383c1909b615aa
ceo-py <sfmf@abv.bg>
2022-07-23 12:51:34 +0200

    ref

--- Fundamentals-Exams/03. Programming Fundamentals Final Exam Retake/2_mirror_words.py
+++ Fundamentals-Exams/03. Programming Fundamentals Final Exam Retake/2_mirror_words.py
@@ -6,25 +6,20 @@ patterns = re.compile(r"([@#])(?P<word>[A-Za-z]{3,})\1\1(?P<word2>[A-Za-z]{3,})\
 list_result = list()
 result = list(re.finditer(patterns, main_string))
 
-
 for show in result:
     if show["word"] == show["word2"][::-1]:
         list_result.append(f"{show['word']} <=> {show['word2']}")
-
 
 if result:
     print(f"{len(result)} word pairs found!")
     if list_result:
         print("The mirror words are:")
-        print(*list_result, sep = ", ")
+        print(*list_result, sep=", ")
     else:
         print("No mirror words!")
 else:
     print("No word pairs found!")
     print("No mirror words!")
-
-
-
 
 #
 #
