bad3de06994e0ac237bedf8714f92abb7b2d034c
ceo-py <sfmf@abv.bg>
2022-07-26 08:55:20 +0200

    ref

--- Python-Fundamentals/Regular Expressions - Exercise/3_find_occurrences_of_word_in_sentence.py
+++ Python-Fundamentals/Regular Expressions - Exercise/3_find_occurrences_of_word_in_sentence.py
@@ -1,10 +1,23 @@
 import re
-main_string = input().lower()
-find_string = input().lower()
-pattern = re.compile(r"\b"+find_string+r"\b")
-matches = pattern.finditer(main_string)
-show_result = 0
-for show in matches:
-    show_result += 1
 
-print(show_result)
+main_string = input().lower()
+word_to_look_for = input().lower()
+
+patterns = re.compile(r"\b" + word_to_look_for + r"\b")
+print(len(list(re.finditer(patterns, main_string))))
+
+
+
+
+
+#
+# import re
+# main_string = input().lower()
+# find_string = input().lower()
+# pattern = re.compile(r"\b"+find_string+r"\b")
+# matches = pattern.finditer(main_string)
+# show_result = 0
+# for show in matches:
+#     show_result += 1
+#
+# print(show_result)
