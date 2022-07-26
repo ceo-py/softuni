e0e258e7e3d0fbdb4f1da45d816e847314c9c2df
ceo-py <sfmf@abv.bg>
2022-07-26 07:48:56 +0200

    ref

--- Python-Fundamentals/Regular Expressions - Lab/1_match_full_name.py
+++ Python-Fundamentals/Regular Expressions - Lab/1_match_full_name.py
@@ -3,13 +3,33 @@ import re
 name_string = input()
 
 find_name = re.compile(r"(\b[A-Z][a-z]+ [A-Z][a-z]+\b)")
-
-# for name in name_string:
-#     match_find = re.finditer(find_name, name)
-#     for show in match_find:
-#         if len(show[0]) == len(name):
-#             print(show[0], end=" ")
-
-
 match_find = re.findall(find_name, name_string)
 print(*match_find, sep= " ")
+
+
+# import re
+#
+# main_string = input()
+#
+# patterns = re.compile(r"\b(?P<word>[A-Z][a-z]{1,}) (?P<word2>[A-Z][a-z]{1,})\b")
+# list_result = list()
+# result = re.finditer(patterns, main_string)
+#
+# for show in result:
+#     list_result.append(f"{show['word']} {show['word2']}")
+#
+# print(*list_result)
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
