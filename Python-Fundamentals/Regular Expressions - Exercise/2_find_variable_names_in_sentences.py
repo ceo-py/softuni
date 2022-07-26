0a21f7536d78855d3c13dea3bd6571b08e8fc067
ceo-py <sfmf@abv.bg>
2022-07-26 08:49:11 +0200

    ref

--- Python-Fundamentals/Regular Expressions - Exercise/2_find_variable_names_in_sentences.py
+++ Python-Fundamentals/Regular Expressions - Exercise/2_find_variable_names_in_sentences.py
@@ -1,10 +1,28 @@
 import re
 
 main_string = input()
-pattern = re.compile(r"\b_([a-zA-Z0-9]+)\b")
-matches = pattern.finditer(main_string)
-show_result = []
-for show in matches:
-    show_result.append(show[1])
 
-print(",".join(show_result))
+patterns = re.compile(r"\b_(?P<word>[A-Za-z0-9]+\b)")
+result = re.finditer(patterns, main_string)
+result_show = []
+for show in result:
+    result_show.append(show["word"])
+
+print(','.join(result_show))
+
+
+
+
+#
+#
+#
+# import re
+#
+# main_string = input()
+# pattern = re.compile(r"\b_([a-zA-Z0-9]+)\b")
+# matches = pattern.finditer(main_string)
+# show_result = []
+# for show in matches:
+#     show_result.append(show[1])
+#
+# print(",".join(show_result))
