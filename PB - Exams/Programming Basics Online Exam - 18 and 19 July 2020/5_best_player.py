001a46395b4025051dcb501c48c2618e1a283565
ceo-py <sfmf@abv.bg>
2022-07-27 16:54:33 +0200

    ref

--- PB - Exams/Programming Basics Online Exam - 18 and 19 July 2020/5_best_player.py
+++ PB - Exams/Programming Basics Online Exam - 18 and 19 July 2020/5_best_player.py
@@ -1,25 +1,50 @@
-player_data = {}
-while True:
-    player = input()
+max_goals = 0
+max_goals_player = ""
 
-    if player == "END":
-        break
 
+player = input()
+while player != "END":
     goals = int(input())
-    player_data[player] = goals
-
+    if goals > max_goals:
+        max_goals = goals
+        max_goals_player = player
     if goals >= 10:
         break
+    player = input()
 
-player_data = sorted(player_data.items(), key=lambda x: x[1], reverse=True)
+print(f"{max_goals_player} is the best player!")
+if 3 <= max_goals:
+    print(f"He has scored {max_goals} goals and made a hat-trick !!!")
+else:
+    print(f"He has scored {max_goals} goals.")
 
-for name, goal in player_data:
-    print(f"{name} is the best player!")
 
-    if goal >= 3:
-        print(f"He has scored {goal} goals and made a hat-trick !!!")
 
-    else:
 
-        print(f"He has scored {goal} goals.")
-    break
+
+#
+# player_data = {}
+# while True:
+#     player = input()
+#
+#     if player == "END":
+#         break
+#
+#     goals = int(input())
+#     player_data[player] = goals
+#
+#     if goals >= 10:
+#         break
+#
+# player_data = sorted(player_data.items(), key=lambda x: x[1], reverse=True)
+#
+# for name, goal in player_data:
+#     print(f"{name} is the best player!")
+#
+#     if goal >= 3:
+#         print(f"He has scored {goal} goals and made a hat-trick !!!")
+#
+#     else:
+#
+#         print(f"He has scored {goal} goals.")
+#     break
