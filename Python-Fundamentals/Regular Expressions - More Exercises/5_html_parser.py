a7905b6a7cd9484dfe50c231e41fcdf2a80ac76f
ceo-py <sfmf@abv.bg>
2022-07-28 21:46:37 +0200

    ref

--- Python-Fundamentals/Regular Expressions - More Exercises/5_html_parser.py
+++ Python-Fundamentals/Regular Expressions - More Exercises/5_html_parser.py
@@ -1,18 +1,30 @@
 import re
 
-text = input()
-
-pattern_title = r"(?:<title>)(?P<title>.+)(?:</title>)"
-pattern_body = r"(?:<body>)(?P<body>.+)(?:</body>)"
-title = re.search(pattern_title, text).group("title")
-body = re.search(pattern_body, text).group("body")
-title = re.sub(r"[ ]+", " ", re.sub(r"\\n|\\t", "", re.sub(r"<[^>]*>", "", title)))
-body = re.sub(r"[ ]+", " ", re.sub(r"\\n|\\t", "", re.sub(r"<[^>]*>", "", body)))
-print(f"Title: {title}")
-print(f"Content: {body}")
+html_content = input()
+pattern = re.compile(r"(?:<title>)(?P<title_html>.+)(?:</title>).*(?:<body>)(?P<body_html>.+)(?:</body>)")
+m_space, tabs_n, between = r"[ ]+", r"\\n|\\t", r"<[^>]*>"
+result = re.finditer(pattern, html_content)
+for show in result:
+    print(f'Title: {re.sub(m_space, " ", re.sub(tabs_n, "", re.sub(between, "", show["title_html"])))}')
+    print(f'Content: {re.sub(m_space, " ", re.sub(tabs_n, "", re.sub(between, "", show["body_html"])))}')
 
 
-
+#
+# import re
+#
+# text = input()
+#
+# pattern_title = r"(?:<title>)(?P<title>.+)(?:</title>)"
+# pattern_body = r"(?:<body>)(?P<body>.+)(?:</body>)"
+# title = re.search(pattern_title, text).group("title")
+# body = re.search(pattern_body, text).group("body")
+# title = re.sub(r"[ ]+", " ", re.sub(r"\\n|\\t", "", re.sub(r"<[^>]*>", "", title)))
+# body = re.sub(r"[ ]+", " ", re.sub(r"\\n|\\t", "", re.sub(r"<[^>]*>", "", body)))
+# print(f"Title: {title}")
+# print(f"Content: {body}")
+#
+#
+#
 
 
 
