1d8f008e28f82bebccb7dad84dc8d7f149962dc2
ceo-py <sfmf@abv.bg>
2022-07-28 14:34:31 +0200

    ref

--- Python-Fundamentals/Regular Expressions - More Exercises/3_star_enigma.py
+++ Python-Fundamentals/Regular Expressions - More Exercises/3_star_enigma.py
@@ -1,32 +1,66 @@
 import re
 
 number_of_messages = int(input())
-attacked_planets = []
-destroyed_planets = []
-pattern = r'[starSTAR]'
-planets_pattern = r'.*@(?P<planet>[A-Z][a-z]+)[^\@\-\!\:\>]*:(?P<population>\d+)[^\@\-\!\:\>]*\!(?P<attack_type>A|D)\![^\@\-\!\:\>]*\->(?P<soldiers>\d+).*'
-for message in range(number_of_messages):
-    encrypted_message = input()
-    decrypted_message = ''
-    matches = len(re.findall(pattern, encrypted_message))
 
-    for character in encrypted_message:
-        decrypted_character = chr((ord(character)) - matches)
-        decrypted_message += decrypted_character
+planets_info = {"Attacked": {}, "Destroyed": {}}
+pattern = re.compile(r"@(?P<planet_name>[A-Za-z]+)([^\@\-\!\:\>]*)"
+                     r":(?P<population>[0-9]+)([^\@\-\!\:\>]*)(\!)"
+                     r"(?P<attack_type>[AD])\5([^\@\-\!\:\>]*)->"
+                     r"(?P<soldier>[0-9]+)")
 
-    planet_matches = re.finditer(planets_pattern, decrypted_message)
-    for value in planet_matches:
-        planet, attack_type = value.group('planet'), value.group('attack_type')
+for msg in range(number_of_messages):
+    current_msg = input()
+    code_decrypt = sum(current_msg.lower().count(char) for char in "star")
+    decode_msg = "".join(chr(ord(char) - code_decrypt) for char in current_msg)
+    result = re.finditer(pattern, decode_msg)
+    for show in result:
+        if show["attack_type"] == "A":
+            planets_info["Attacked"][show["planet_name"]] = show["population"]
+        elif show["attack_type"] == "D":
+            planets_info["Destroyed"][show["planet_name"]] = show["population"]
 
-        if attack_type == 'A':
-            attacked_planets.append(planet)
-        else:
-            destroyed_planets.append(planet)
+for type_attack in planets_info:
+    planet_count = len(planets_info[type_attack].keys())
+    print(f"{type_attack} planets: {planet_count}")
+    if planet_count:
+        for planet in sorted(planets_info[type_attack].keys()):
+            print(f"-> {planet}")
 
-print(f'Attacked planets: {len(attacked_planets)}')
-for planet in sorted(attacked_planets):
-    print(f'-> {planet}')
 
-print(f'Destroyed planets: {len(destroyed_planets)}')
-for planet in sorted(destroyed_planets):
-    print(f'-> {planet}')
+
+
+
+
+#
+# import re
+#
+# number_of_messages = int(input())
+# attacked_planets = []
+# destroyed_planets = []
+# pattern = r'[starSTAR]'
+# planets_pattern = r'.*@(?P<planet>[A-Z][a-z]+)[^\@\-\!\:\>]*:(?P<population>\d+)[^\@\-\!\:\>]*\!(?P<attack_type>A|D)\![^\@\-\!\:\>]*\->(?P<soldiers>\d+).*'
+# for message in range(number_of_messages):
+#     encrypted_message = input()
+#     decrypted_message = ''
+#     matches = len(re.findall(pattern, encrypted_message))
+#
+#     for character in encrypted_message:
+#         decrypted_character = chr((ord(character)) - matches)
+#         decrypted_message += decrypted_character
+#
+#     planet_matches = re.finditer(planets_pattern, decrypted_message)
+#     for value in planet_matches:
+#         planet, attack_type = value.group('planet'), value.group('attack_type')
+#
+#         if attack_type == 'A':
+#             attacked_planets.append(planet)
+#         else:
+#             destroyed_planets.append(planet)
+#
+# print(f'Attacked planets: {len(attacked_planets)}')
+# for planet in sorted(attacked_planets):
+#     print(f'-> {planet}')
+#
+# print(f'Destroyed planets: {len(destroyed_planets)}')
+# for planet in sorted(destroyed_planets):
+#     print(f'-> {planet}')
