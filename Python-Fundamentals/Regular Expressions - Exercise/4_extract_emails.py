0b1e0079075b57eeef43fef236fbe200fa896ce2
ceo-py <sfmf@abv.bg>
2022-07-28 11:19:50 +0200

    ref

--- Python-Fundamentals/Regular Expressions - Exercise/4_extract_emails.py
+++ Python-Fundamentals/Regular Expressions - Exercise/4_extract_emails.py
@@ -1,7 +1,26 @@
 import re
-input_string = input()
-pattern = re.compile(r"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))")
-matches = pattern.finditer(input_string)
 
-for show in matches:
+main_string = input()
+
+pattern = re.compile(
+    r"(^|(?<=\s))([A-Za-z0-9])+([\.\-\_][A-Za-z0-9]+)*@([A-Za-z-]+)\.([A-Za-z-]+)([\.A-Za-z-])*(\b|(?=\s))")
+result = re.finditer(pattern, main_string)
+for show in result:
     print(show[0])
+
+
+
+
+
+
+
+
+#
+#
+# import re
+# input_string = input()
+# pattern = re.compile(r"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))")
+# matches = pattern.finditer(input_string)
+#
+# for show in matches:
+#     print(show[0])
