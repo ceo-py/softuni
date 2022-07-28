0e8b898f74df44edc26e0e8f27491a3bdacbd071
ceo-py <sfmf@abv.bg>
2022-07-28 12:50:47 +0200

    ref

--- Python-Fundamentals/Regular Expressions - More Exercises/1_race.py
+++ Python-Fundamentals/Regular Expressions - More Exercises/1_race.py
@@ -19,8 +19,33 @@ print(f"3rd place: {name_of_participants[2][0]}")
 
 
 
-
-
+# names_info = {x: 0 for x in input().split(", ")}
+#
+# info = input()
+# while info != "end of race":
+#     name, distance = "", []
+#
+#     for char in info:
+#         if char.isdigit():
+#             distance.append(int(char))
+#         elif char.isalpha():
+#             name += char
+#
+#     if name in names_info:
+#         names_info[name] += sum(distance)
+#     info = input()
+#
+#
+# names_info = sorted(names_info.items(), key=lambda x: -x[1])
+#
+# print(f"1st place: {names_info[0][0]}")
+# print(f"2nd place: {names_info[1][0]}")
+# print(f"3rd place: {names_info[2][0]}")
+#
+#
+#
+#
+#
 
 
 
