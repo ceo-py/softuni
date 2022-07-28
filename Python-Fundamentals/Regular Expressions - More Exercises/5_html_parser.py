2f1da5174b0992dba0a968ac9e5bf01240fd40cd
ceo-py <sfmf@abv.bg>
2022-07-28 21:36:35 +0200

    ref

--- Python-Fundamentals/Regular Expressions - More Exercises/5_html_parser.py
+++ Python-Fundamentals/Regular Expressions - More Exercises/5_html_parser.py
@@ -1,24 +1,46 @@
 import re
 
-pattern_title = r"(?:<title>)(?P<title>.+)(?:</title>)"
-pattern_body = r"(?:<body>)(?P<body>.+)(?:</body>)"
-pattern_remove_tags = r"<[^>]*>"
-pattern_remove_pseudo_space = r"\\n|\\t"            # "new lines" and "tabs"
-pattern_remove_spaces = r"[ ]+"
-
 text = input()
 
+pattern_title = r"(?:<title>)(?P<title>.+)(?:</title>)"
+pattern_body = r"(?:<body>)(?P<body>.+)(?:</body>)"
 title = re.search(pattern_title, text).group("title")
 body = re.search(pattern_body, text).group("body")
-
-title = re.sub(pattern_remove_tags, "", title, re.IGNORECASE | re.UNICODE)
-body = re.sub(pattern_remove_tags, "", body, re.IGNORECASE | re.UNICODE)
-
-title = re.sub(pattern_remove_pseudo_space, "", title).strip()
-body = re.sub(pattern_remove_pseudo_space, "", body).strip()
-
-title = re.sub(pattern_remove_spaces, " ", title).strip()
-body = re.sub(pattern_remove_spaces, " ", body).strip()
-
+title = re.sub(r"[ ]+", " ", re.sub(r"\\n|\\t", "", re.sub(r"<[^>]*>", "", title)))
+body = re.sub(r"[ ]+", " ", re.sub(r"\\n|\\t", "", re.sub(r"<[^>]*>", "", body)))
 print(f"Title: {title}")
 print(f"Content: {body}")
+
+
+
+
+
+
+
+#
+#
+#
+# import re
+#
+# pattern_title = r"(?:<title>)(?P<title>.+)(?:</title>)"
+# pattern_body = r"(?:<body>)(?P<body>.+)(?:</body>)"
+# pattern_remove_tags = r"<[^>]*>"
+# pattern_remove_pseudo_space = r"\\n|\\t"            # "new lines" and "tabs"
+# pattern_remove_spaces = r"[ ]+"
+#
+# text = input()
+#
+# title = re.search(pattern_title, text).group("title")
+# body = re.search(pattern_body, text).group("body")
+#
+# title = re.sub(pattern_remove_tags, "", title, re.IGNORECASE | re.UNICODE)
+# body = re.sub(pattern_remove_tags, "", body, re.IGNORECASE | re.UNICODE)
+#
+# title = re.sub(pattern_remove_pseudo_space, "", title).strip()
+# body = re.sub(pattern_remove_pseudo_space, "", body).strip()
+#
+# title = re.sub(pattern_remove_spaces, " ", title).strip()
+# body = re.sub(pattern_remove_spaces, " ", body).strip()
+#
+# print(f"Title: {title}")
+# print(f"Content: {body}")
