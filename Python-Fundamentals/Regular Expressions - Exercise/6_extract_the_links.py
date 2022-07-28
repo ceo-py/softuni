e71e42e58cfb3f02a16445be35bd446d248e130f
ceo-py <sfmf@abv.bg>
2022-07-28 11:56:27 +0200

    ref

--- Python-Fundamentals/Regular Expressions - Exercise/6_extract_the_links.py
+++ Python-Fundamentals/Regular Expressions - Exercise/6_extract_the_links.py
@@ -1,10 +1,32 @@
 import re
-pattern = re.compile(r"\bwww\.[a-zA-Z0-9-]+\.[a-z]+(\.[a-z]+)*\b")
+
+pattern = re.compile(
+    r"www.([A-Za-z0-9\-]+)(\.[a-z]+)+")
+
 while True:
     main_string = input()
     if main_string:
-        matches = pattern.finditer(main_string)
-        for show in matches:
+        result = re.finditer(pattern, main_string)
+        for show in result:
             print(show[0])
     else:
         break
+
+
+
+
+
+
+#
+#
+#
+# import re
+# pattern = re.compile(r"\bwww\.[a-zA-Z0-9-]+\.[a-z]+(\.[a-z]+)*\b")
+# while True:
+#     main_string = input()
+#     if main_string:
+#         matches = pattern.finditer(main_string)
+#         for show in matches:
+#             print(show[0])
+#     else:
+#         break
