ad65e3dc764b5989bad664d2a294db1034fbd949
ceo-py <sfmf@abv.bg>
2022-07-28 14:48:29 +0200

    ref

--- Python-Fundamentals/Regular Expressions - More Exercises/3_star_enigma.py
+++ Python-Fundamentals/Regular Expressions - More Exercises/3_star_enigma.py
@@ -2,7 +2,7 @@ import re
 
 number_of_messages = int(input())
 
-planets_info = {"Attacked": {}, "Destroyed": {}}
+planets_info = {"Attacked": {}, "Destroyed": {}, "A": "Attacked", "D": "Destroyed"}
 pattern = re.compile(r"@(?P<planet_name>[A-Za-z]+)([^\@\-\!\:\>]*)"
                      r":(?P<population>[0-9]+)([^\@\-\!\:\>]*)(\!)"
                      r"(?P<attack_type>[AD])\5([^\@\-\!\:\>]*)->"
@@ -14,12 +14,9 @@ for msg in range(number_of_messages):
     decode_msg = "".join(chr(ord(char) - code_decrypt) for char in current_msg)
     result = re.finditer(pattern, decode_msg)
     for show in result:
-        if show["attack_type"] == "A":
-            planets_info["Attacked"][show["planet_name"]] = show["population"]
-        elif show["attack_type"] == "D":
-            planets_info["Destroyed"][show["planet_name"]] = show["population"]
+        planets_info[planets_info[show["attack_type"]]][show["planet_name"]] = show["population"]
 
-for type_attack in planets_info:
+for type_attack in list(planets_info.keys())[:2]:
     planet_count = len(planets_info[type_attack].keys())
     print(f"{type_attack} planets: {planet_count}")
     if planet_count:
