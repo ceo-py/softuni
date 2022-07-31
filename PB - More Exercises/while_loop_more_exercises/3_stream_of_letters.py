13e829a481789ff2e9c7f77a84bde101a121bc1f
ceo-py <sfmf@abv.bg>
2022-07-31 22:36:45 +0200

    ref

--- PB - More Exercises/while_loop_more_exercises/3_stream_of_letters.py
+++ PB - More Exercises/while_loop_more_exercises/3_stream_of_letters.py
@@ -1,27 +1,48 @@
-skip_letters = "con"
-
 letter = input()
-complete_word = ""
-skip_letters_count = ""
-guess_word = ""
+
+complete_word, skip_letters_count, guess_word = "",  "",  ""
 while letter != "End":
-    if letter in skip_letters:
+    if letter in "con":
         skip_letters_count += letter
-    else:
-        if letter.islower() or letter.isupper():
-            guess_word += letter
+    elif letter.islower() or letter.isupper():
+        guess_word += letter
     if "c" in skip_letters_count and "o" in skip_letters_count and "n" in skip_letters_count:
-        skip_letters_count = ""
         complete_word += f"{guess_word} "
-        guess_word = ""
+        guess_word, skip_letters_count = "", ""
     if skip_letters_count.count(letter) > 1:
         guess_word += letter
-
     letter = input()
 
 print(complete_word)
 
 
+
+
+#
+# skip_letters = "con"
+#
+# letter = input()
+# complete_word = ""
+# skip_letters_count = ""
+# guess_word = ""
+# while letter != "End":
+#     if letter in skip_letters:
+#         skip_letters_count += letter
+#     else:
+#         if letter.islower() or letter.isupper():
+#             guess_word += letter
+#     if "c" in skip_letters_count and "o" in skip_letters_count and "n" in skip_letters_count:
+#         skip_letters_count = ""
+#         complete_word += f"{guess_word} "
+#         guess_word = ""
+#     if skip_letters_count.count(letter) > 1:
+#         guess_word += letter
+#
+#     letter = input()
+#
+# print(complete_word)
+#
+#
 
 
 
