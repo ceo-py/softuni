2614e06c6b6a219116e36d9cdf400758a334acf7
ceo-py <sfmf@abv.bg>
2022-08-05 13:51:37 +0200

    ref

--- Fundamentals-Exams/03. Programming Fundamentals Final Exam Retake/3_need_for_speed_iii.py
+++ Fundamentals-Exams/03. Programming Fundamentals Final Exam Retake/3_need_for_speed_iii.py
@@ -1,68 +1,206 @@
 number_of_cars = int(input())
 
-
 cars_info = {}
 
 for car in range(number_of_cars):
-    car_name, car_mileage, car_fuel = [int(x) if x.isdigit() else x for x in input().split("|")]
-    cars_info[car_name] = cars_info.get(car_name, {})
-    cars_info[car_name]["Mileage"] = car_mileage
-    cars_info[car_name]["Fuel"] = car_fuel
+    car_name, *info = (int(x) if x.isdigit() else x for x in input().split("|"))
+    cars_info[car_name] = info
 
 
-def drive_car(car_name, car_distance, car_fuel):
-    if cars_info[car_name]["Fuel"] < car_fuel:
-        print("Not enough fuel to make that ride")
-    else:
-        cars_info[car_name]["Fuel"] -= car_fuel
-        cars_info[car_name]["Mileage"] += car_distance
-        print(f"{car_name} driven for {car_distance} kilometers. {car_fuel} liters of fuel consumed.")
-        if cars_info[car_name]["Mileage"] >= 100_000:
+def drive(info):
+    car_name, distance, fuel = info
+    if cars_info[car_name][1] >= fuel:
+        cars_info[car_name][1] -= fuel
+        cars_info[car_name][0] += distance
+        print(f"{car_name} driven for {distance} kilometers. {fuel} liters of fuel consumed.")
+        if cars_info[car_name][0] >= 100_000:
             del cars_info[car_name]
             print(f"Time to sell the {car_name}!")
-
-
-def refuel_car(car_name, car_fuel):
-    if cars_info[car_name]["Fuel"] + car_fuel > 75:
-        car_fuel = 75 - cars_info[car_name]["Fuel"]
-        cars_info[car_name]["Fuel"] = 75
     else:
-        cars_info[car_name]["Fuel"] += car_fuel
-    print(f"{car_name} refueled with {car_fuel} liters")
+        print("Not enough fuel to make that ride")
 
 
-def revert_car(car_name, car_kilometers):
-    if cars_info[car_name]["Mileage"] - car_kilometers < 10_000:
-        cars_info[car_name]["Mileage"] = 10_000
+def refuel(info):
+    car_name, fuel = info
+    if cars_info[car_name][1] + fuel > 75:
+        fuel = 75 - cars_info[car_name][1]
+    cars_info[car_name][1] += fuel
+    print(f"{car_name} refueled with {fuel} liters")
+
+
+def revert(info):
+    car_name, kilometers = info
+    cars_info[car_name][0] -= kilometers
+    if cars_info[car_name][0] < 10_000:
+        cars_info[car_name][0] = 10_000
     else:
-        cars_info[car_name]["Mileage"] -= car_kilometers
-        print(f"{car_name} mileage decreased by {car_kilometers} kilometers")
+        print(f"{car_name} mileage decreased by {kilometers} kilometers")
 
 
 def show_result():
-    for car_name in cars_info:
-        print(f"{car_name} -> Mileage: {cars_info[car_name]['Mileage']} kms,"
-              f" Fuel in the tank: {cars_info[car_name]['Fuel']} lt.")
+    for car in cars_info:
+        print(f"{car} -> Mileage: {cars_info[car][0]} kms, Fuel in the tank: {cars_info[car][1]} lt.")
 
+
+command_func = {
+    "Drive": drive,
+    "Refuel": refuel,
+    "Revert": revert
+}
 
 command = input()
 while command != "Stop":
-    command_type, *others = [int(x) if x.isdigit() else x for x in command.split(" : ")]
-    if "Drive" == command_type:
-        car_name, car_mileage, car_fuel = others
-        drive_car(car_name, car_mileage, car_fuel)
-    else:
-        car_name, fuel_or_kilometers = others
-        if "Refuel" == command_type:
-            refuel_car(car_name, fuel_or_kilometers)
-        elif "Revert" == command_type:
-            revert_car(car_name, fuel_or_kilometers)
+    command_type, *info = (int(x) if x.isdigit() else x for x in command.split(" : "))
+    command_func[command_type](info)
     command = input()
 
 show_result()
 
 
 
+#
+# car_numbers = int(input())
+#
+# car_info = {}
+#
+#
+# class Car:
+#
+#     def __init__(self, name):
+#         self.name = name
+#         self.mileage = 0
+#         self.fuel = 0
+#
+#     def add_mileage(self, mileage):
+#         self.mileage += mileage
+#
+#     def add_fuel(self, fuel):
+#         self.fuel += fuel
+#
+#     def drive(self, distance, fuel):
+#         if self.fuel > fuel:
+#             self.fuel -= fuel
+#             self.mileage += distance
+#             print(f"{self.name} driven for {distance} kilometers. {fuel} liters of fuel consumed.")
+#             if self.mileage >= 100_000:
+#                 del car_info[self.name]
+#                 print(f"Time to sell the {self.name}!")
+#         else:
+#             print("Not enough fuel to make that ride")
+#
+#     def refuel(self, fuel):
+#         if self.fuel + fuel > 75:
+#             fuel = 75 - self.fuel
+#         self.fuel += fuel
+#         print(f"{self.name} refueled with {fuel} liters")
+#
+#     def revert(self, kilometers):
+#         self.mileage -= kilometers
+#         if self.mileage < 10_000:
+#             self.mileage = 10_000
+#             return
+#         print(f"{self.name} mileage decreased by {kilometers} kilometers")
+#
+#     def __repr__(self):
+#         return f"{self.name} -> Mileage: {self.mileage} kms, Fuel in the tank: {self.fuel} lt."
+#
+#
+# for car in range(car_numbers):
+#     car_name, mileage, fuel = [int(x) if x.isdigit() else x for x in input().split("|")]
+#     car_info[car_name] = car_info.get(car_name, Car(car_name))
+#     car_info[car_name].add_mileage(mileage)
+#     car_info[car_name].add_fuel(fuel)
+#
+#
+# command = input()
+#
+# while command != "Stop":
+#     command_type, car_name, *info = [int(x) if x.isdigit() else x for x in command.split(" : ")]
+#     if command_type == "Drive":
+#         distance, fuel = info
+#         car_info[car_name].drive(distance, fuel)
+#     elif command_type == "Refuel":
+#         fuel = info[0]
+#         car_info[car_name].refuel(fuel)
+#     elif command_type == "Revert":
+#         kilometers = info[0]
+#         car_info[car_name].revert(kilometers)
+#     command = input()
+#
+# for car in car_info.values():
+#     print(car)
+#
+#
+#
+
+
+
+#
+# number_of_cars = int(input())
+#
+#
+# cars_info = {}
+#
+# for car in range(number_of_cars):
+#     car_name, car_mileage, car_fuel = [int(x) if x.isdigit() else x for x in input().split("|")]
+#     cars_info[car_name] = cars_info.get(car_name, {})
+#     cars_info[car_name]["Mileage"] = car_mileage
+#     cars_info[car_name]["Fuel"] = car_fuel
+#
+#
+# def drive_car(car_name, car_distance, car_fuel):
+#     if cars_info[car_name]["Fuel"] < car_fuel:
+#         print("Not enough fuel to make that ride")
+#     else:
+#         cars_info[car_name]["Fuel"] -= car_fuel
+#         cars_info[car_name]["Mileage"] += car_distance
+#         print(f"{car_name} driven for {car_distance} kilometers. {car_fuel} liters of fuel consumed.")
+#         if cars_info[car_name]["Mileage"] >= 100_000:
+#             del cars_info[car_name]
+#             print(f"Time to sell the {car_name}!")
+#
+#
+# def refuel_car(car_name, car_fuel):
+#     if cars_info[car_name]["Fuel"] + car_fuel > 75:
+#         car_fuel = 75 - cars_info[car_name]["Fuel"]
+#         cars_info[car_name]["Fuel"] = 75
+#     else:
+#         cars_info[car_name]["Fuel"] += car_fuel
+#     print(f"{car_name} refueled with {car_fuel} liters")
+#
+#
+# def revert_car(car_name, car_kilometers):
+#     if cars_info[car_name]["Mileage"] - car_kilometers < 10_000:
+#         cars_info[car_name]["Mileage"] = 10_000
+#     else:
+#         cars_info[car_name]["Mileage"] -= car_kilometers
+#         print(f"{car_name} mileage decreased by {car_kilometers} kilometers")
+#
+#
+# def show_result():
+#     for car_name in cars_info:
+#         print(f"{car_name} -> Mileage: {cars_info[car_name]['Mileage']} kms,"
+#               f" Fuel in the tank: {cars_info[car_name]['Fuel']} lt.")
+#
+#
+# command = input()
+# while command != "Stop":
+#     command_type, *others = [int(x) if x.isdigit() else x for x in command.split(" : ")]
+#     if "Drive" == command_type:
+#         car_name, car_mileage, car_fuel = others
+#         drive_car(car_name, car_mileage, car_fuel)
+#     else:
+#         car_name, fuel_or_kilometers = others
+#         if "Refuel" == command_type:
+#             refuel_car(car_name, fuel_or_kilometers)
+#         elif "Revert" == command_type:
+#             revert_car(car_name, fuel_or_kilometers)
+#     command = input()
+#
+# show_result()
+#
+#
+#
 
 
 
