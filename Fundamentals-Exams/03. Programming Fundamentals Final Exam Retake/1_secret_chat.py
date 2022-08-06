987d0bc6d9ac4a54b032bb9aef68e00f0468962c
ceo-py <sfmf@abv.bg>
2022-08-06 11:09:07 +0200

    ref

--- Fundamentals-Exams/03. Programming Fundamentals Final Exam Retake/1_secret_chat.py
+++ Fundamentals-Exams/03. Programming Fundamentals Final Exam Retake/1_secret_chat.py
@@ -1,45 +1,79 @@
-main_msg = input()
+raw_string_msg = [input()]
+
+
+def insert_space(info):
+    index_ = info[0]
+    raw_string_msg[0] = f"{raw_string_msg[0][:index_]} {raw_string_msg[0][index_:]}"
+
+
+def reverse_string(info):
+    substring = info[0]
+    if substring in raw_string_msg[0]:
+        raw_string_msg[0] = raw_string_msg[0].replace(substring, "", 1) + substring[::-1]
+    else:
+        return "error"
+
+
+def change_all(info):
+    substring, replacement = info
+    raw_string_msg[0] = raw_string_msg[0].replace(substring, replacement)
+
+
+command_func = {
+    "InsertSpace": insert_space,
+    "Reverse": reverse_string,
+    "ChangeAll": change_all
+}
+
 command = input()
 
-
-def change_msg(substring, replacement, main_msg):
-    return main_msg.replace(substring, replacement)
-
-
-def inset_space(index, main_msg):
-    return main_msg[:index] + " " + main_msg[index:]
-
-
-def reverse(substring, main_msg):
-    word_find = main_msg.find(substring)
-    return main_msg[:word_find] + main_msg[word_find + (len(substring)):] + substring[::-1]
-
-
 while command != "Reveal":
-    command_type, *info = command.split(":|:")
-    found_error = False
-    if command_type == "ChangeAll":
-        substring, replacement = info
-        main_msg = change_msg(substring, replacement, main_msg)
-
-    elif command_type == "Reverse":
-        substring = info[0]
-        if substring not in main_msg:
-            print("error")
-            found_error = True
-        else:
-            main_msg = reverse(substring, main_msg)
-
-    elif command_type == "InsertSpace":
-        index_for_insert = int(info[0])
-        main_msg = inset_space(index_for_insert, main_msg)
-    if not found_error:
-        print(main_msg)
-
+    command_type, *info = (int(x) if x.isdigit() else x for x in command.split(":|:"))
+    show = command_func[command_type](info)
+    if "error" == show:
+        print("error")
+    else:
+        print(raw_string_msg[0])
     command = input()
 
-print(f"You have a new text message: {main_msg}")
+print(f"You have a new text message: {raw_string_msg[0]}")
 
+
+
+
+
+
+
+
+#
+# main_msg = input()
+# command = input()
+#
+#
+# while command != "Reveal":
+#     command_type, *info = command.split(":|:")
+#     found_error = False
+#     if command_type == "ChangeAll":
+#         main_msg = main_msg.replace(info[0], info[1])
+#
+#     elif command_type == "Reverse":
+#         substring = info[0]
+#         if substring not in main_msg:
+#             print("error")
+#             found_error = True
+#         else:
+#             main_msg = main_msg.replace(substring, "", 1) + substring[::-1]
+#
+#     elif command_type == "InsertSpace":
+#         index_for_insert = int(info[0])
+#         main_msg = main_msg[:index_for_insert] + " " + main_msg[index_for_insert:]
+#
+#     if not found_error:
+#         print(main_msg)
+#
+#     command = input()
+#
+# print(f"You have a new text message: {main_msg}")
 
 
 
