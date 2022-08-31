c9879303c3c304487d5adc32c03e8debaf1286cb
ceo-py <sfmf@abv.bg>
2022-08-31 17:53:07 +0300

    ref

--- Python Advanced/Python Advanced - Exercises/Multidimensional Lists - Exercise 2/3_knight_game.py
+++ Python Advanced/Python Advanced - Exercises/Multidimensional Lists - Exercise 2/3_knight_game.py
@@ -27,6 +27,24 @@ def find_all_knights():
     return positions
 
 
+# while True:
+#     pos_knights = find_all_knights()
+#     result = {}
+#     for i in range(0, len(pos_knights), 2):
+#         row, col = pos_knights[i], pos_knights[i + 1]
+#         for m_row, m_col in movement.values():
+#             knight_row, knight_col = row + m_row, col + m_col
+#             if check_valid_index(knight_row, knight_col):
+#                 if matrix[knight_row][knight_col] == "K":
+#                     result[f"{row} {col}"] = result.get(f"{row} {col}", 0) + 1
+#     if sum(result.values()) == 0:
+#         break
+#     total_knights[0] += 1
+#     row, col = max(result, key=result.get).split()
+#     matrix[int(row)][int(col)] = "0"
+
+
+
 def check_knights(pos_knights):
     result = {}
     for i in range(0, len(pos_knights), 2):
@@ -38,13 +56,11 @@ def check_knights(pos_knights):
                     result[f"{row} {col}"] = result.get(f"{row} {col}", 0) + 1
     if sum(result.values()) == 0:
         return
-    else:
-        total_knights[0] += 1
-        row, col = max(result, key=result.get).split()
-        matrix[int(row)][int(col)] = "0"
-        check_knights(find_all_knights())
+    total_knights[0] += 1
+    row, col = max(result, key=result.get).split()
+    matrix[int(row)][int(col)] = "0"
+    check_knights(find_all_knights())
 
 
-check_knights(find_all_knights())
 print(total_knights[0])
 
