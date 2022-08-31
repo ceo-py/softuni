e33284f7bc071135ca3d855b014570aa11b8b72c
ceo-py <sfmf@abv.bg>
2022-08-31 10:23:12 +0300

    ref

---
+++ Python Advanced/Python Advanced - Exercises/Multidimensional Lists - Lab/1_sum_matrix_elements.py
@@ -0,0 +1,4 @@
+rows, cols = [int(x) for x in input().split(", ")]
+matrix = [[int(x) for x in input().split(", ")] for _ in range(rows)]
+print(sum([sum(idx) for idx in zip(*matrix)]))
+print(matrix)
