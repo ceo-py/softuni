e4b781e378c7dfaf5e47ad01d29788a0f327fe3d
ceo-py <sfmf@abv.bg>
2022-09-02 16:53:00 +0300

    ref

--- Python Advanced/Homeworк/Exercise File Handling/4_directory_traversal.py
+++ Python Advanced/Homeworк/Exercise File Handling/4_directory_traversal.py
@@ -1,27 +1,26 @@
 import os
 
-
 result = {}
 
 
-def get_directory_files():
-    return os.listdir()
+def get_directory_files(directory):
+    return os.listdir(directory)
 
 
-def save_report(data):
-    with open("output.txt", "a", encoding="utf-8") as file:
+def save_report(data, directory):
+    with open(f"{directory}\output.txt", "a", encoding="utf-8") as file:
         file.write(data)
 
 
-def prepare_for_data_for_save():
-    files = get_directory_files()
+def prepare_for_data_for_save(directory):
+    files = get_directory_files(directory)
     for file in files:
         extension = file[file.find("."):]
         result[extension] = result.get(extension, []) + [file]
-    sorting_data_for_save()
+    sorting_data_for_save(directory)
 
 
-def sorting_data_for_save(): # умишлено е така за да не се налага за всеки ред да се отваря файла на ново !
+def sorting_data_for_save(directory):  # умишлено е така за да не се налага за всеки ред да се отваря файла на ново !
     data = ""
     for pos, extension in enumerate(sorted(result.keys())):
         if pos > 0:
@@ -30,7 +29,7 @@ def sorting_data_for_save(): # умишлено е така за да не се 
         for file in sorted(result[extension]):
             data += "\n"
             data += f"- - - {file}"
-    save_report(data)
+    save_report(data, directory)
 
 
-prepare_for_data_for_save()
+prepare_for_data_for_save(input())
