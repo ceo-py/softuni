8b6eb2ca566fe097387d6709e7006044a0fa45f6
ceo-py <sfmf@abv.bg>
2022-09-02 14:46:02 +0300

    ref

---
+++ Python Advanced/Homewor/Exercise File Handling/2_line_numbers.py
@@ -0,0 +1,40 @@
+import string
+# from pathlib import Path
+
+
+# ВАЖНО !!!!
+# откоментирай кода ако искаш файла да се създаде автоматично в който ще се запазва аутпута!!
+
+
+def open_file():
+    with open("text.txt", "r", encoding="utf-8") as file:
+        return file.read().split("\n")
+
+
+def write_file(info):
+    # filename = Path(f"output.txt")
+    # filename.parent.mkdir(parents=True, exist_ok=True)
+    # filename.touch(exist_ok=True)
+    with open("output.txt", "w", encoding="utf-8") as file:
+        file.write("\n".join(info))
+
+
+def count_letters(text):
+    return sum([1 for x in text if x.isalpha()])
+
+
+def count_punctuation(text):
+    return sum([text.count(symbol) for symbol in string.punctuation])
+
+
+def show_result():
+    data = open_file()
+    data_to_save_in_file = []
+    for pos, row in enumerate(range(len(data)), 1):
+        letters_ = count_letters(data[row])
+        punctuation_ = count_punctuation(data[row])
+        data_to_save_in_file.append(f"Line {pos}: {data[row]} ({letters_})({punctuation_})")
+    write_file(data_to_save_in_file)
+
+
+show_result()
