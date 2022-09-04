0a80c695229c2fbd135b31fb730af9fbcc7997d2
ceo-py <sfmf@abv.bg>
2022-09-04 10:46:21 +0300

    ref

---
+++ Python Advanced/Python Advanced - Exercises/Functions Advanced - Lab/4_rectangle.py
@@ -0,0 +1,35 @@
+# def rectangle(*args):
+#     # if any([True for x in args if not isinstance(x, int)]):
+#
+#     if not all([True if isinstance(x, int) else False for x in args]):
+#         return "Enter valid values!"
+#
+#     def area():
+#         return args[0] * args[1]
+#
+#     def perimeter():
+#         return (args[0] + args[1]) * 2
+#
+#     return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
+#
+
+
+def rectangle(length, width):
+    # if any([True for x in args if not isinstance(x, int)]):
+
+    if not all([True if isinstance(x, int) else False for x in (length, width)]):
+        return "Enter valid values!"
+
+    def area():
+        return length * width
+
+    def perimeter():
+        return (length + width) * 2
+
+    return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
+
+
+
+
+print(rectangle(2, 10))
+print(rectangle('2', 10))
