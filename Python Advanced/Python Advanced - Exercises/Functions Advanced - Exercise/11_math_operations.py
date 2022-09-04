c30dbb3f785c98bb1a3ae64fc47dfd32f406fda9
ceo-py <sfmf@abv.bg>
2022-09-05 00:28:53 +0300

    ref

--- Python Advanced/Python Advanced - Exercises/Functions Advanced - Exercise/11_math_operations.py
+++ Python Advanced/Python Advanced - Exercises/Functions Advanced - Exercise/11_math_operations.py
@@ -13,12 +13,10 @@ def math_operations(*args, **kwargs):
         pos += 1
         if pos > 4:
             pos = 1
-    for key, value in sorted(kwargs.items(), key=lambda x:(-x[1], x[0])):
+    for key, value in sorted(kwargs.items(), key=lambda x: (-x[1], x[0])):
         result += f"{key}: {value:.1f}\n"
 
     return result
-
-
 
 
 # print(math_operations(2.1, 12.56, 0.0, -3.899, 6.0, -20.65, a=1, s=7, d=33, m=15))
