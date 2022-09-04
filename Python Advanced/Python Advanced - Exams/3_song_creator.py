fe21c30bc9432e3cbadf381f12d6be8bf411b82f
ceo-py <sfmf@abv.bg>
2022-09-05 00:52:23 +0300

    ref

---
+++ Python Advanced/Python Advanced - Exams/3_song_creator.py
@@ -0,0 +1,25 @@
+def add_songs(*args):
+    result, print_result = {}, ""
+    for song_name, lyrics in args:
+        result[song_name] = result.get(song_name, []) + lyrics
+    for p_song_name, p_lyrics in result.items():
+        print_result += f"- {p_song_name}\n" + '\n'.join(p_lyrics)
+        if p_lyrics:
+            print_result += "\n"
+    return print_result
+
+
+print(add_songs(
+    ("Love of my life",
+     ["Love of my life, you've hurt me",
+      "You've broken my heart, and now you leave me",
+      "Love of my life, can't you see?",
+      "Bring it back, bring it back"]),
+    ("Beat It", []),
+    ("Love of my life",
+     ["Don't take it away from me",
+      "Because you don't know",
+      "What it means to me"]),
+    ("Dream On",
+     ["Every time that I look in the mirror"]),
+))
