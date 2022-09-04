3d950c59d89306dc2f7bef4cfe161a79c9efe508
ceo-py <sfmf@abv.bg>
2022-09-04 12:10:49 +0300

    ref

--- Python Advanced/Python Advanced - Exercises/Functions Advanced - Exercise/1_negative_vs_positive.py
+++ Python Advanced/Python Advanced - Exercises/Functions Advanced - Exercise/1_negative_vs_positive.py
@@ -1,7 +1,7 @@
 def negative_vs_positive(data):
-    min_number = data.index(min([x for x in data if x > 0]))
-    positive_numbers = sum(data[min_number:])
-    negative_numbers = sum(data[:min_number])
+    min_number = data.index(min([x for x in data if x > 0])) # O(n) + O(n) , ind 0(1)
+    positive_numbers = sum(data[min_number:]) # O(k+n)
+    negative_numbers = sum(data[:min_number]) # O(k+n)
 
     if positive_numbers > abs(negative_numbers):
         text = "The positives are stronger than the negatives"
@@ -10,7 +10,7 @@ def negative_vs_positive(data):
     return f"{negative_numbers}\n{positive_numbers}\n{text}"
 
 
-input_numbers = sorted(int(x) for x in input().split())
+input_numbers = sorted(int(x) for x in input().split()) # O(n log n)
 print(negative_vs_positive(input_numbers))
 
 
