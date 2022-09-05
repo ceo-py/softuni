c7e1b15cf3a2f3a2a0de1900f33629f853f3e562
ceo-py <sfmf@abv.bg>
2022-09-05 15:42:17 +0300

    ref

---
+++ Python Advanced/Python Advanced - Exams/Python Advanced Exam - 25 June 2022/3_shopping_cart.py
@@ -0,0 +1,86 @@
+def shopping_cart(*args):
+    output, result = "", {"Dessert": [], "Pizza": [], "Soup": []}
+    for info in args:
+        if "Stop" in info:
+            break
+        meal, product, = info
+        for meal_type, number_product in (("Soup", 3), ("Pizza", 4), ("Dessert", 2)):
+            if meal == meal_type and len(result[meal]) != number_product and product not in result[meal]:
+                result[meal].append(product)
+                break
+    for p_meal, p_product in sorted(result.items(), key=lambda x: (-len(x[1]), x[0])):
+        output += f"{p_meal}:\n"
+        for item in sorted(p_product):
+            output += f" - {item}\n"
+    if len(output) == 22:
+        output = "No products in the cart!"
+    return output
+
+
+
+
+#
+# def shopping_cart(*args):
+#     output, result = "", {"Dessert": [], "Pizza": [], "Soup": []}
+#     for info in args:
+#         if "Stop" in info:
+#             break
+#         meal, product,  = info
+#         if meal == "Soup" and len(result[meal]) != 3 and product not in result[meal]:
+#             result[meal].append(product)
+#         elif meal == "Pizza" and len(result[meal]) != 4 and product not in result[meal]:
+#             result[meal].append(product)
+#         elif meal == "Dessert" and len(result[meal]) != 2 and product not in result[meal]:
+#             result[meal].append(product)
+#     for p_meal, p_product in sorted(result.items(), key= lambda x: (-len(x[1]), x[0])):
+#         output += f"{p_meal}:\n"
+#         for item in sorted(p_product):
+#             output += f" - {item}\n"
+#     if len(output) == 22:
+#         output = "No products in the cart!"
+#     return output
+#
+#
+
+
+
+
+'''
+Pizza:
+ - cheese
+ - flour
+ - ham
+ - mushrooms
+Dessert:
+ - milk
+Soup:
+ - carrots
+'''
+
+'''
+    n the end, sort the meals by the number of bought products in descending order. 
+    If there are meals with an equal number of products, sort them (the meals) by their name 
+    in ascending order (alphabetically). For each meal sort its products in ascending order (alphabetically).'''
+
+# print(shopping_cart(
+#     ('Pizza', 'ham'),
+#     ('Soup', 'carrots'),
+#     ('Pizza', 'cheese'),
+#     ('Pizza', 'flour'),
+#     ('Pizza', 'mushrooms'),
+#     ('Pizza', 'tomatoes'),
+#     'Stop',
+# ))
+
+# print(shopping_cart(
+#     ('Pizza', 'ham'),
+#     ('Dessert', 'milk'),
+#     ('Pizza', 'ham'),
+#     'Stop',
+# ))
+
+print(shopping_cart(
+    'Stop',
+    ('Pizza', 'ham'),
+    ('Pizza', 'mushrooms'),
+))
