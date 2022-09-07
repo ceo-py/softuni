712e0fd0557e9c3e958a1ee51c4794b0d527c114
ceo-py <sfmf@abv.bg>
2022-09-07 16:55:41 +0300

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Exam - 24 October 2020/3_list_pureness.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Exam - 24 October 2020/3_list_pureness.py
@@ -4,21 +4,49 @@ from collections import deque
 def best_list_pureness(*args):
     list_rotate, number_of_rotations = deque(args[0]), args[1]
     list_len = len(list_rotate)
-
-    def sum_pureness(list_data):
-        total = 0
-        for i in range(list_len):
-            total += list_data[i] * i
-        return total
-
     result = {
-        "0": sum_pureness(list_rotate)
+        "0": sum([list_rotate[i] * i for i in range(list_len)])
     }
     for rotation in range(1, number_of_rotations + 1):
         list_rotate.rotate(1)
-        result[rotation] = result.get(rotation, sum_pureness(list_rotate))
+        result[rotation] = result.get(rotation, sum([list_rotate[i] * i for i in range(list_len)]))
     best_rotation = max(result, key=result.get)
     return f"Best pureness {result[best_rotation]} after {best_rotation} rotations"
+
+
+
+
+#
+#
+#
+# from collections import deque
+#
+#
+# def best_list_pureness(*args):
+#     list_rotate, number_of_rotations = deque(args[0]), args[1]
+#     list_len = len(list_rotate)
+#
+#     def sum_pureness(list_data):
+#         total = 0
+#         for i in range(list_len):
+#             total += list_data[i] * i
+#         return total
+#
+#     result = {
+#         "0": sum_pureness(list_rotate)
+#     }
+#     for rotation in range(1, number_of_rotations + 1):
+#         list_rotate.rotate(1)
+#         result[rotation] = result.get(rotation, sum_pureness(list_rotate))
+#     best_rotation = max(result, key=result.get)
+#     return f"Best pureness {result[best_rotation]} after {best_rotation} rotations"
+#
+#
+#
+#
+
+
+
 
 
 # test = ([4, 3, 2, 6], 4)
