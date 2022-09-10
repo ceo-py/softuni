0887e1f5d51ed5170688a301842bceeee441549a
ceo-py <sfmf@abv.bg>
2022-09-10 11:44:04 +0300

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 18 August 2022/2_crud.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 18 August 2022/2_crud.py
@@ -8,32 +8,25 @@ def add_step(row, col, direction):
     return row + directions[direction][0], col + directions[direction][1]
 
 
+def number_or_letter():
+    return matrix[row_position][col_position].isalpha() or matrix[row_position][col_position].isdigit()
+
+
 def create(args):
-    step_row, step_col = add_step(row_position, col_position, args[0])
-    if matrix[step_row][step_col] == ".":
-        matrix[step_row][step_col] = args[1]
-    return step_row, step_col
+    if matrix[row_position][col_position] == ".":
+        matrix[row_position][col_position] = args[1]
 
 
 def update(args):
-    step_row, step_col = add_step(row_position, col_position, args[0])
-    if matrix[step_row][step_col].isalpha() or matrix[step_row][step_col][-1].isdigit():
-        matrix[step_row][step_col] = args[1]
-    return step_row, step_col
+    matrix[row_position][col_position] = args[1]
 
 
-def delete(args):
-    step_row, step_col = add_step(row_position, col_position, args[0])
-    if matrix[step_row][step_col].isalpha() or matrix[step_row][step_col][-1].isdigit():
-        matrix[step_row][step_col] = "."
-    return step_row, step_col
+def delete(_):
+    matrix[row_position][col_position] = "."
 
 
-def read(args):
-    step_row, step_col = add_step(row_position, col_position, args[0])
-    if matrix[step_row][step_col].isalpha() or matrix[step_row][step_col][-1].isdigit():
-        print(matrix[step_row][step_col])
-    return step_row, step_col
+def read(_):
+    print(matrix[row_position][col_position])
 
 
 commands = {
@@ -46,7 +39,82 @@ command = input()
 
 while command != "Stop":
     command_type, *info = command.split(", ")
-    row_position, col_position = commands[command_type](info)
+    row_position, col_position = add_step(row_position, col_position, info[0])
+    if command_type in list(commands.keys())[1:]:
+        if number_or_letter():
+            commands[command_type](info)
+    else:
+        commands[command_type](info)
     command = input()
 
 [print(*matrix[row]) for row in range(MATRIX_SIZE)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+# MATRIX_SIZE = 6
+# matrix = [input().split() for _ in range(MATRIX_SIZE)]
+# row_position, col_position = [int(x) for x in input().replace("(", "").replace(")", "").split(",")]
+# directions = {"up": [-1, 0], "down": [1, 0], "left": [0, -1], "right": [0, 1]}
+#
+#
+# def add_step(row, col, direction):
+#     return row + directions[direction][0], col + directions[direction][1]
+#
+#
+# def create(args):
+#     step_row, step_col = add_step(row_position, col_position, args[0])
+#     if matrix[step_row][step_col] == ".":
+#         matrix[step_row][step_col] = args[1]
+#     return step_row, step_col
+#
+#
+# def update(args):
+#     step_row, step_col = add_step(row_position, col_position, args[0])
+#     if matrix[step_row][step_col].isalpha() or matrix[step_row][step_col][-1].isdigit():
+#         matrix[step_row][step_col] = args[1]
+#     return step_row, step_col
+#
+#
+# def delete(args):
+#     step_row, step_col = add_step(row_position, col_position, args[0])
+#     if matrix[step_row][step_col].isalpha() or matrix[step_row][step_col][-1].isdigit():
+#         matrix[step_row][step_col] = "."
+#     return step_row, step_col
+#
+#
+# def read(args):
+#     step_row, step_col = add_step(row_position, col_position, args[0])
+#     if matrix[step_row][step_col].isalpha() or matrix[step_row][step_col][-1].isdigit():
+#         print(matrix[step_row][step_col])
+#     return step_row, step_col
+#
+#
+# commands = {
+#     "Create": create,
+#     "Update": update,
+#     "Read": read,
+#     "Delete": delete
+# }
+# command = input()
+#
+# while command != "Stop":
+#     command_type, *info = command.split(", ")
+#     row_position, col_position = commands[command_type](info)
+#     command = input()
+#
+# [print(*matrix[row]) for row in range(MATRIX_SIZE)]
