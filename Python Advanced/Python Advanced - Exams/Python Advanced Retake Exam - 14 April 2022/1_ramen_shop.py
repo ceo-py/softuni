ad8ff678016aa0a273e26488f49a0d46b5456e85
ceo-py <sfmf@abv.bg>
2022-09-12 12:29:06 +0300

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 14 April 2022/1_ramen_shop.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 14 April 2022/1_ramen_shop.py
@@ -4,26 +4,20 @@ bowls = [int(x) for x in input().split(", ")]
 customers = deque(int(x) for x in input().split(", "))
 
 
-def remove_both():
-    del bowls[-1]
-    del customers[0]
-
-
 while bowls and customers:
     bowl = bowls.pop()
     customer = customers.popleft()
     if bowl == customer:
         continue
+
     if bowl > customer:
         bowl -= customer
         bowls.append(bowl)
-        if customers and bowl == customers[0]:
-            remove_both()
+
     elif customer > bowl:
         customer -= bowl
         customers.appendleft(customer)
-        if bowls and customer == bowls[-1]:
-            remove_both()
+
 
 if not customers:
     print("Great job! You served all the customers.")
