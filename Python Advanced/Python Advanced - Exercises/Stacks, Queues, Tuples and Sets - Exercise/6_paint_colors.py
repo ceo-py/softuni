c3baaac3b89d4e25c5d281829a81ffc60222dff5
ceo-py <sfmf@abv.bg>
2022-09-19 19:57:50 +0300

    ref

--- Python Advanced/Python Advanced - Exercises/Stacks, Queues, Tuples and Sets - Exercise/6_paint_colors.py
+++ Python Advanced/Python Advanced - Exercises/Stacks, Queues, Tuples and Sets - Exercise/6_paint_colors.py
@@ -3,12 +3,11 @@ from collections import deque
 substring = deque(input().split())
 
 colors = {
-    "main colors": ["red", "yellow", "blue"],
-    "secondary colors": ["orange", "purple", "green"],
+    "all colors": ("red", "yellow", "blue", "orange", "purple", "green"),
     "required colors": {
-        "orange": ["red", "yellow"],
-        "purple": ["red", "blue"],
-        "green": ["yellow", "blue"]
+        "orange": ("red", "yellow"),
+        "purple": ("red", "blue"),
+        "green": ("yellow", "blue")
     }
 }
 
@@ -20,14 +19,13 @@ while substring:
         last_part = substring.pop()
     first_part = substring.popleft()
     for color in (first_part + last_part, last_part + first_part):
-        if any(color in x for x in list(colors.values())[:2]):
+        if color in colors["all colors"]:
             result.append(color)
             break
     else:
         for item in (first_part[:-1], last_part[:-1]):
             if item:
                 substring.insert(len(substring) // 2, item)
-
 
 for color, req_colors in colors["required colors"].items():
     if any(x not in result and color in result for x in req_colors):
