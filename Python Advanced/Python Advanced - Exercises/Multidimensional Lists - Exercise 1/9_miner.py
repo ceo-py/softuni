62b95bd256b3acac5db7aca091862b6132e489e0
ceo-py <sfmf@abv.bg>
2022-09-29 13:33:12 +0300

    ref

--- Python Advanced/Python Advanced - Exercises/Multidimensional Lists - Exercise 1/9_miner.py
+++ Python Advanced/Python Advanced - Exercises/Multidimensional Lists - Exercise 1/9_miner.py
@@ -3,13 +3,12 @@ from collections import deque
 MATRIX_SIZE = int(input())
 commands = deque(input().split())
 
-
 coal = 0
 collected_coal = 0
 start_row, start_col = 0, 0
 matrix = []
 movement = {
-    "up": (-1, 0),  "down": (1, 0),
+    "up": (-1, 0), "down": (1, 0),
     "left": (0, -1), "right": (0, 1)
 }
 
@@ -19,42 +18,29 @@ for row in range(MATRIX_SIZE):
     if "s" in matrix[row]:
         start_row, start_col = row, matrix[row].index("s")
 
-
 while commands:
     direction = commands.popleft()
     moving_row, moving_col = start_row + movement[direction][0], start_col + movement[direction][1]
-    if 0 <= moving_row < MATRIX_SIZE and 0 <= moving_col < MATRIX_SIZE:
+    if not 0 <= moving_row < MATRIX_SIZE or not 0 <= moving_col < MATRIX_SIZE:
+        continue
 
-        symbol = matrix[moving_row][moving_col]
+    symbol = matrix[moving_row][moving_col]
 
-        if symbol == "e":
-            print(f"Game over! {moving_row, moving_col}")
-            break
+    if symbol == "e":
+        print(f"Game over! {moving_row, moving_col}")
+        break
 
-        if symbol == "c":
-            matrix[moving_row][moving_col] = "*"
-            collected_coal += 1
+    if symbol == "c":
+        matrix[moving_row][moving_col] = "*"
+        collected_coal += 1
 
-        if collected_coal == coal:
-            print(f"You collected all coal! {moving_row, moving_col}")
-            break
+    if collected_coal == coal:
+        print(f"You collected all coal! {moving_row, moving_col}")
+        break
 
-        start_row, start_col = moving_row, moving_col
+    start_row, start_col = moving_row, moving_col
 else:
     print(f"{coal - collected_coal} pieces of coal left. {moving_row, moving_col}")
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -112,10 +98,6 @@ else:
 #             break
 # else:
 #     print(f"{total_coal} pieces of coal left. ({row}, {col})")
-
-
-
-
 
 
 # field_numbers = int(input())
