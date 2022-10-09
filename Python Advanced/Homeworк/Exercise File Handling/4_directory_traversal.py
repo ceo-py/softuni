89e34cd863f6d31e4c51d703896eb141a3f4ac7d
ceo-py <sfmf@abv.bg>
2022-10-09 15:11:44 +0300

    ref

--- Python Advanced/Homeworк/Exercise File Handling/4_directory_traversal.py
+++ Python Advanced/Homeworк/Exercise File Handling/4_directory_traversal.py
@@ -22,14 +22,11 @@ def prepare_for_data_for_save(directory):
 
 def sorting_data_for_save(directory):  # умишлено е така за да не се налага за всеки ред да се отваря файла на ново !
     data = ""
-    for pos, extension in enumerate(sorted(result.keys())):
-        if pos > 0:
-            data += "\n"
-        data += f"{extension}"
+    for extension in sorted(result.keys()):
+        data += f"{extension}\n"
         for file in sorted(result[extension]):
-            data += "\n"
-            data += f"- - - {file}"
-    save_report(data, directory)
+            data += f"- - - {file}\n"
+    save_report(data.rstrip(), directory)
 
 
 prepare_for_data_for_save(input())
