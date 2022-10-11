f69857ced7c966d8014317c5467843b80384b3c5
ceo-py <sfmf@abv.bg>
2022-10-11 16:11:04 +0300

    ref

--- Fundamentals-Exams/01. Programming Fundamentals Mid Exam Retake/2_the_lift.py
+++ Fundamentals-Exams/01. Programming Fundamentals Mid Exam Retake/2_the_lift.py
@@ -1,40 +1,62 @@
-passengers = int(input())
-floors = [int(n) for n in input().split()]
-floors_lift = list()
-enough_spots = True
-total = passengers
-equal_spots = True
+people = int(input())
+current_wagons = [int(x) for x in input().split()]
+for i in range(len(current_wagons)):
+    max_people = min(4 - current_wagons[i], people)
+    current_wagons[i] += max_people
+    people -= max_people
 
-
-def check_floors():
-    global passengers
-    global total
-    for floor in floors:
-        for i in range(4):
-            if passengers > 0:
-                if floor + 1 <= 4:
-                    floor += 1
-                    passengers += - 1
-        floors_lift.append(floor)
-
-
-check_floors()
-max_people = (len(floors_lift) * 4) - sum(floors)
-if max_people == total:
-    equal_spots = False
-
-elif passengers != 0:
-    enough_spots = False
-
-if enough_spots and equal_spots:
+if people > 0:
+    print(f"There isn't enough space! {people} people in a queue!")
+elif any([wagon < 4 for wagon in current_wagons]):
     print("The lift has empty spots!")
 
-if not enough_spots and equal_spots:
-    print(f"There isn't enough space! {passengers} people in a queue!")
-
-print(*floors_lift, sep=" ")
+print(*current_wagons)
 
 
+
+
+
+
+
+
+#
+# passengers = int(input())
+# floors = [int(n) for n in input().split()]
+# floors_lift = list()
+# enough_spots = True
+# total = passengers
+# equal_spots = True
+#
+#
+# def check_floors():
+#     global passengers
+#     global total
+#     for floor in floors:
+#         for i in range(4):
+#             if passengers > 0:
+#                 if floor + 1 <= 4:
+#                     floor += 1
+#                     passengers += - 1
+#         floors_lift.append(floor)
+#
+#
+# check_floors()
+# max_people = (len(floors_lift) * 4) - sum(floors)
+# if max_people == total:
+#     equal_spots = False
+#
+# elif passengers != 0:
+#     enough_spots = False
+#
+# if enough_spots and equal_spots:
+#     print("The lift has empty spots!")
+#
+# if not enough_spots and equal_spots:
+#     print(f"There isn't enough space! {passengers} people in a queue!")
+#
+# print(*floors_lift, sep=" ")
+#
+#
 
 
 #
