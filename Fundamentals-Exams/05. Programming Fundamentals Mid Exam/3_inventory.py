12b0e622bb7f5fd3ab71d007cc32ebfc58b5952d
ceo-py <sfmf@abv.bg>
2022-10-11 16:25:23 +0300

    ref

--- Fundamentals-Exams/05. Programming Fundamentals Mid Exam/3_inventory.py
+++ Fundamentals-Exams/05. Programming Fundamentals Mid Exam/3_inventory.py
@@ -1,86 +1,107 @@
-dungeons_rooms = input().split("|")
-
-hero_info = {
-    "bitcoins": 0,
-    "health": 100,
-}
+items = input().split(", ")
 
 
-def potions(points_command):
-    if hero_info["health"] + points_command > 100:
-        print(f"You healed for {100 - hero_info['health']} hp.")
-        hero_info["health"] = 100
-    else:
-        hero_info["health"] += points_command
-        print(f"You healed for {points_command} hp.")
-    print(f"Current health: {hero_info['health']} hp.")
+data_info = input()
+while data_info != "Craft!":
+    command, item = data_info.split(" - ")
 
+    if command == "Collect":
+        if item not in items:
+            items.append(item)
+    elif command == "Drop":
+        if item in items:
+            items.remove(item)
 
-def chest(points_command):
-    hero_info["bitcoins"] += points_command
-    print(f"You found {points_command} bitcoins.")
+    elif command == "Combine Items":
+        old_item, new_item = item.split(":")
+        if old_item in items:
+            old_item_index = items.index(old_item)
+            items.insert(old_item_index + 1, new_item)
 
+    elif command == "Renew":
+        if item in items:
+            item = items.pop(items.index(item))
+            items.append(item)
 
-def monster_slay(type_command):
-    if all([hero_info['health'] > 0, "chest" != type_command != "potion"]):
-        print(f"You slayed {type_command}.")
+    data_info = input()
 
+print(*items, sep=", ")
 
-for room, command in enumerate(dungeons_rooms, 1):
-    type_command, points_command = [int(x) if x.isdigit() else x for x in command.split()]
-    if type_command == "potion":
-        potions(points_command)
-    elif type_command == "chest":
-        chest(points_command)
-    else:
-        hero_info['health'] -= points_command
-    monster_slay(type_command)
-    if hero_info['health'] <= 0:
-        print(f"You died! Killed by {type_command}.")
-        print(f"Best room: {room}")
-        break
-else:
-    print("You've made it!")
-    print(f"Bitcoins: {hero_info['bitcoins']}")
-    print(f"Health: {hero_info['health']}")
 
 #
 #
-# dungeons_rooms = input().split("|")
+# def collect_func(item):
+#     if item not in inventory:
+#         inventory.append(item)
 #
-# hero_info = {
-#     "bitcoins": 0,
-#     "health": 100,
-# }
-# bitcoins = "bitcoins"
-# health = "health"
-# finish_dungeon = True
-# for room, command in enumerate(dungeons_rooms):
-#     command = command.split()
+#
+# def drop_func(item):
+#     if item in inventory:
+#         inventory.remove(item)
+#
+#
+# def combine_items_func(old_item, new_item):
+#     if old_item in inventory:
+#         inventory.insert(inventory.index(old_item) + 1, new_item)
+#
+#
+# def renew_func(item):
+#     if item in inventory:
+#         inventory.remove(item)
+#         inventory.append(item)
+#
+#
+# inventory = input().split(', ')
+# command = input()
+#
+# while command != "Craft!":
+#     command = command.split(' - ')
+#     command_name = command[0]
+#     command_item = command[1]
+#
+#     if command_name == 'Collect':
+#         collect_func(command_item)
+#     elif command_name == 'Drop':
+#         drop_func(command_item)
+#     elif command_name == 'Combine Items':
+#         command_item = command_item.split(':')
+#         first_item = command_item[0]
+#         second_item = command_item[-1]
+#         combine_items_func(first_item, second_item)
+#     elif command_name == 'Renew':
+#         renew_func(command_item)
+#
+#     command = input()
+#
+# print(', '.join(inventory))
+#
+#
+#
+
+
+
+# items_collect = input().split(", ")
+#
+# command = input()
+#
+# while command != "Craft!":
+#     command = command.split(" - ")
 #     type_command = command[0]
-#     points_command = int(command[-1])
-#     if type_command == "potion":
-#         if hero_info[health] + points_command > 100:
-#             print(f"You healed for {100 - hero_info[health]} hp.")
-#             hero_info[health] = 100
-#         else:
-#             hero_info[health] += points_command
-#             print(f"You healed for {points_command} hp.")
-#         print(f"Current health: {hero_info[health]} hp.")
-#     elif type_command == "chest":
-#         hero_info[bitcoins] += points_command
-#         print(f"You found {points_command} bitcoins.")
-#     else:
-#         hero_info[health] -= points_command
-#     if hero_info[health] > 0 and type_command != "potion" and type_command != "chest":
-#         print(f"You slayed {type_command}.")
-#     if hero_info[health] <= 0:
-#         print(f"You died! Killed by {type_command}.")
-#         print(f"Best room: {room + 1}")
-#         finish_dungeon = False
-#         break
+#     item = command[1]
+#     if type_command == "Collect":
+#         if item not in items_collect:
+#             items_collect.append(item)
+#     elif type_command == "Drop":
+#         if item in items_collect:
+#             items_collect.remove(item)
+#     elif type_command == "Renew":
+#         if item in items_collect:
+#             items_collect.remove(item)
+#             items_collect.append(item)
+#     elif type_command == "Combine Items":
+#         item = command[1].split(":")
+#         if item[0] in items_collect:
+#             items_collect.insert(items_collect.index(item[0]) + 1, item[1])
+#     command = input()
 #
-# if finish_dungeon:
-#     print("You've made it!")
-#     print(f"Bitcoins: {hero_info[bitcoins]}")
-#     print(f"Health: {hero_info[health]}")
+# print(*items_collect, sep=", ")
