ea0cb30ed3b7cbb3c07c1f62245cac907dc5f6fc
ceo-py <sfmf@abv.bg>
2022-10-11 16:22:16 +0300

    ref

--- Fundamentals-Exams/04. Programming Fundamentals Mid Exam/3_heart_delivery.py
+++ Fundamentals-Exams/04. Programming Fundamentals Mid Exam/3_heart_delivery.py
@@ -1,32 +1,65 @@
-neighborhood = [int(n) for n in input().split("@")]
+neighborhood = [int(x) for x in input().split("@")]
+jump_data = input()
+neighborhood_len = len(neighborhood)
+length = 0
 
-jump_command = input()
-jump_position = 0
+while jump_data != "Love!":
+    length += int(jump_data.split()[-1])
+    if length >= neighborhood_len:
+        length = 0
 
-
-def jump_neighborhood(length_d):
-    global jump_position
-    jump_position += length_d
-    if jump_position >= len(neighborhood):
-        jump_position = 0
-    if neighborhood[jump_position] == 0:
-        print(f"Place {jump_position} already had Valentine's day.")
+    if neighborhood[length] > 2:
+        neighborhood[length] -= 2
     else:
-        neighborhood[jump_position] -= 2
-        if neighborhood[jump_position] == 0:
-            print(f"Place {jump_position} has Valentine's day.")
+        if neighborhood[length] != 0:
+            neighborhood[length] -= 2
+            text = "has"
+        else:
+            text = "already had"
+        print(f"Place {length} {text} Valentine's day.")
+    jump_data = input()
 
+print(f"Cupid's last position was {length}.")
 
-while jump_command != "Love!":
-    jump_command = jump_command.split()
-    jump_neighborhood(int(jump_command[1]))
+failed_houses = sum(1 for x in neighborhood if x != 0)
 
-    jump_command = input()
-
-print(f"Cupid's last position was {jump_position}.")
-
-if sum(neighborhood) == 0:
-    print("Mission was successful.")
+if failed_houses:
+    print(f"Cupid has failed {failed_houses} places.")
 else:
-    fail_count = neighborhood.count(0)
-    print(f"Cupid has failed {len(neighborhood) - fail_count} places.")
+    print("Mission was successful.")
+
+
+
+#
+# neighborhood = [int(n) for n in input().split("@")]
+#
+# jump_command = input()
+# jump_position = 0
+#
+#
+# def jump_neighborhood(length_d):
+#     global jump_position
+#     jump_position += length_d
+#     if jump_position >= len(neighborhood):
+#         jump_position = 0
+#     if neighborhood[jump_position] == 0:
+#         print(f"Place {jump_position} already had Valentine's day.")
+#     else:
+#         neighborhood[jump_position] -= 2
+#         if neighborhood[jump_position] == 0:
+#             print(f"Place {jump_position} has Valentine's day.")
+#
+#
+# while jump_command != "Love!":
+#     jump_command = jump_command.split()
+#     jump_neighborhood(int(jump_command[1]))
+#
+#     jump_command = input()
+#
+# print(f"Cupid's last position was {jump_position}.")
+#
+# if sum(neighborhood) == 0:
+#     print("Mission was successful.")
+# else:
+#     fail_count = neighborhood.count(0)
+#     print(f"Cupid has failed {len(neighborhood) - fail_count} places.")
