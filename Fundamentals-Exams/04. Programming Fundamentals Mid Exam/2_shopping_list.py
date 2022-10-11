125da15864d77aca63c88ea42abc5788fa646b39
ceo-py <sfmf@abv.bg>
2022-10-11 16:21:29 +0300

    ref

--- Fundamentals-Exams/04. Programming Fundamentals Mid Exam/2_shopping_list.py
+++ Fundamentals-Exams/04. Programming Fundamentals Mid Exam/2_shopping_list.py
@@ -1,41 +1,75 @@
-groceries = input().split("!")
+initial_list = input().split("!")
+info_data = input()
 
 
-def urgent_item(item):
-    if item not in groceries:
-        groceries.insert(0, item)
+while info_data != "Go Shopping!":
 
+    if "Correct" in info_data:
+        _, old_item, new_item = info_data.split()
+        if old_item in initial_list:
+            initial_list[initial_list.index(old_item)] = new_item
+        info_data = input()
+        continue
 
-def unnecessary_item(item):
-    if item in groceries:
-        groceries.remove(item)
+    command, item = info_data.split()
 
+    if command == "Urgent":
+        if item not in initial_list:
+            initial_list.insert(0, item)
 
-def correct_item(old_item, new_item):
-    if old_item in groceries:
-        groceries[groceries.index(old_item)] = new_item
+    elif command == "Unnecessary":
+        if item in initial_list:
+            initial_list.remove(item)
 
+    elif command == "Rearrange":
+        if item in initial_list:
+            initial_list.remove(item)
+            initial_list.append(item)
 
-def rearrange_item(item):
-    if item in groceries:
-        groceries.remove(item)
-        groceries.append(item)
+    info_data = input()
 
-
-command = input()
-while command != "Go Shopping!":
-    command = command.split()
-    type_command = command[0]
-    item_command = command[1]
-    if type_command == "Urgent":
-        urgent_item(item_command)
-    elif type_command == "Unnecessary":
-        unnecessary_item(item_command)
-    elif type_command == "Correct":
-        correct_item(item_command, command[-1])
-    elif type_command == "Rearrange":
-        rearrange_item(item_command)
-
-    command = input()
-
-print(*groceries, sep=", ")
+print(*initial_list, sep=", ")
+#
+#
+#
+# groceries = input().split("!")
+#
+#
+# def urgent_item(item):
+#     if item not in groceries:
+#         groceries.insert(0, item)
+#
+#
+# def unnecessary_item(item):
+#     if item in groceries:
+#         groceries.remove(item)
+#
+#
+# def correct_item(old_item, new_item):
+#     if old_item in groceries:
+#         groceries[groceries.index(old_item)] = new_item
+#
+#
+# def rearrange_item(item):
+#     if item in groceries:
+#         groceries.remove(item)
+#         groceries.append(item)
+#
+#
+# command = input()
+# while command != "Go Shopping!":
+#     command = command.split()
+#     type_command = command[0]
+#     item_command = command[1]
+#     if type_command == "Urgent":
+#         urgent_item(item_command)
+#     elif type_command == "Unnecessary":
+#         unnecessary_item(item_command)
+#     elif type_command == "Correct":
+#         correct_item(item_command, command[-1])
+#     elif type_command == "Rearrange":
+#         rearrange_item(item_command)
+#
+#     command = input()
+#
+# print(*groceries, sep=", ")
