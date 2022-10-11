9a247822b3aeb7eb1189fc96b7b35189ad8b6a73
ceo-py <sfmf@abv.bg>
2022-10-11 16:08:02 +0300

    ref

--- Fundamentals-Exams/01. Programming Fundamentals Mid Exam Retake/1_computer_store.py
+++ Fundamentals-Exams/01. Programming Fundamentals Mid Exam Retake/1_computer_store.py
@@ -1,34 +1,77 @@
-command = 0
-total = 0
-buying_parts = True
+total_sum = 0
+data = input()
 
 
-def buying_pc(off):
-    taxes = total * 0.20
-    global buying_parts
-    if off:
-        final_price = (total + taxes) - (total + taxes) * 0.10
-    else:
-        final_price = total + taxes
-    buying_parts = False
-    if total == 0:
-        print("Invalid order!")
-    else:
-        print("Congratulations you've just bought a new computer!")
-        print(f"Price without taxes: {total:.2f}$")
-        print(f"Taxes: {taxes:.2f}$")
-        print("-----------")
-        print(f"Total price: {final_price:.2f}$")
+special = 1
 
 
-while buying_parts:
-    command = float(command)
-    if command >= 0:
-        total += command
+while data != "special" and data != "regular":
+    price = float(data)
+
+    if price > 0:
+        total_sum += price
     else:
         print("Invalid price!")
-    command = input()
-    if command == "special":
-        buying_pc(True)
-    elif command == "regular":
-        buying_pc(False)
+
+    data = input()
+
+taxes = total_sum * 0.2
+
+if data == "special":
+    special = 0.9
+
+if total_sum == 0:
+    print("Invalid order!")
+
+else:
+    print("Congratulations you've just bought a new computer!")
+    print(f"Price without taxes: {total_sum:.2f}$")
+    print(f"Taxes: {taxes:.2f}$")
+    print("-----------")
+    print(f"Total price: {(total_sum + taxes) * special:.2f}$")
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# command = 0
+# total = 0
+# buying_parts = True
+#
+#
+# def buying_pc(off):
+#     taxes = total * 0.20
+#     global buying_parts
+#     if off:
+#         final_price = (total + taxes) - (total + taxes) * 0.10
+#     else:
+#         final_price = total + taxes
+#     buying_parts = False
+#     if total == 0:
+#         print("Invalid order!")
+#     else:
+#         print("Congratulations you've just bought a new computer!")
+#         print(f"Price without taxes: {total:.2f}$")
+#         print(f"Taxes: {taxes:.2f}$")
+#         print("-----------")
+#         print(f"Total price: {final_price:.2f}$")
+#
+#
+# while buying_parts:
+#     command = float(command)
+#     if command >= 0:
+#         total += command
+#     else:
+#         print("Invalid price!")
+#     command = input()
+#     if command == "special":
+#         buying_pc(True)
+#     elif command == "regular":
+#         buying_pc(False)
