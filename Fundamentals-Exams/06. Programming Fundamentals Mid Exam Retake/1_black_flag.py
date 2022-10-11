0f721f59855d49be27571dd545365e56888f75d5
ceo-py <sfmf@abv.bg>
2022-10-11 16:26:25 +0300

    ref

--- Fundamentals-Exams/06. Programming Fundamentals Mid Exam Retake/1_black_flag.py
+++ Fundamentals-Exams/06. Programming Fundamentals Mid Exam Retake/1_black_flag.py
@@ -1,16 +1,42 @@
-days, daily_plunder, expected_plunder = int(input()), int(input()), float(input())
+days = int(input())
+daily_plunder = int(input())
+expected_plunder = float(input())
 
-total_plunder = 0
+current_plunder = 0
+
 
 for day in range(1, days + 1):
-    total_plunder += daily_plunder
+    fifth_day = 1
+    third_day = 0
     if day % 3 == 0:
-        total_plunder += (daily_plunder * 0.50)
+        third_day = daily_plunder * 0.50
     if day % 5 == 0:
-        total_plunder -= (total_plunder * 0.30)
+        fifth_day = 0.70
 
-if total_plunder >= expected_plunder:
-    print(f"Ahoy! {total_plunder:.2f} plunder gained.")
+    current_plunder = (daily_plunder + current_plunder + third_day) * fifth_day
+
+if current_plunder >= expected_plunder:
+    print(f"Ahoy! {current_plunder:.2f} plunder gained.")
 else:
-    percent_left = (total_plunder / expected_plunder) * 100
-    print(f"Collected only {percent_left:.2f}% of the plunder.")
+    print(f"Collected only {(current_plunder / expected_plunder) * 100:.2f}% of the plunder.")
+
+
+
+#
+#
+# days, daily_plunder, expected_plunder = int(input()), int(input()), float(input())
+#
+# total_plunder = 0
+#
+# for day in range(1, days + 1):
+#     total_plunder += daily_plunder
+#     if day % 3 == 0:
+#         total_plunder += (daily_plunder * 0.50)
+#     if day % 5 == 0:
+#         total_plunder -= (total_plunder * 0.30)
+#
+# if total_plunder >= expected_plunder:
+#     print(f"Ahoy! {total_plunder:.2f} plunder gained.")
+# else:
+#     percent_left = (total_plunder / expected_plunder) * 100
+#     print(f"Collected only {percent_left:.2f}% of the plunder.")
