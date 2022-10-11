9fbb601ec5d6885840be1e0c48a4db15176f1312
ceo-py <sfmf@abv.bg>
2022-10-11 16:13:09 +0300

    ref

--- Fundamentals-Exams/02. Programming Fundamentals Mid Exam/3_numbers.py
+++ Fundamentals-Exams/02. Programming Fundamentals Mid Exam/3_numbers.py
@@ -1,12 +1,25 @@
-main_list = [int(x) for x in input().split()]
-average_number = sum(main_list) / len(main_list)
-main_list.sort(reverse=True)
-print_numbers = False
+numbers = [int(x) for x in input().split()]
 
-for index, number in enumerate(main_list):
-    if index <= 4 and number > average_number:
-        print(number, end=" ")
-        print_numbers = True
-    if not print_numbers:
-        print("No")
-        break
+result = sorted([x for x in numbers if x > sum(numbers)/len(numbers)])
+
+if result:
+    print(*result[-5:][-1::-1])
+else:
+    print("No")
+#
+#
+#
+#
+#
+# main_list = [int(x) for x in input().split()]
+# average_number = sum(main_list) / len(main_list)
+# main_list.sort(reverse=True)
+# print_numbers = False
+#
+# for index, number in enumerate(main_list):
+#     if index <= 4 and number > average_number:
+#         print(number, end=" ")
+#         print_numbers = True
+#     if not print_numbers:
+#         print("No")
+#         break
