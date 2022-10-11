f7da948dcaa916c232d7179360b6e9002c37d761
ceo-py <sfmf@abv.bg>
2022-10-11 16:23:56 +0300

    ref

--- Fundamentals-Exams/05. Programming Fundamentals Mid Exam/2_muonline.py
+++ Fundamentals-Exams/05. Programming Fundamentals Mid Exam/2_muonline.py
@@ -1,44 +1,90 @@
-dungeons_rooms = input().split("|")
-
-hero_info = {
-    "bitcoins": 0,
-    "health": 100,
-}
+rooms_data = [data.split() for data in input().split("|")]
 
 
-def potions(points_command):
-    if hero_info["health"] + points_command > 100:
-        print(f"You healed for {100 - hero_info['health']} hp.")
-        hero_info["health"] = 100
+initial_health = 100
+bitcoins = 0
+room = 0
+
+for room_data in rooms_data:
+    item, value = [int(x) if x.isdigit() else x for x in room_data]
+    room += 1
+
+    if item == "potion":
+        if initial_health + value > 100:
+            value = 100 - initial_health
+        initial_health += value
+        print(f"You healed for {value} hp.")
+        print(f"Current health: {initial_health} hp.")
+
+    elif item == "chest":
+        bitcoins += value
+        print(f"You found {value} bitcoins.")
+
     else:
-        hero_info["health"] += points_command
-        print(f"You healed for {points_command} hp.")
-    print(f"Current health: {hero_info['health']} hp.")
-
-
-def chest(points_command):
-    hero_info["bitcoins"] += points_command
-    print(f"You found {points_command} bitcoins.")
-
-
-for room, command in enumerate(dungeons_rooms, 1):
-    type_command, points_command = [int(x) if x.isdigit() else x for x in command.split()]
-    if type_command == "potion":
-        potions(points_command)
-    elif type_command == "chest":
-        chest(points_command)
-    else:
-        hero_info['health'] -= points_command
-    if all([hero_info['health'] > 0, type_command != "potion", type_command != "chest"]):
-        print(f"You slayed {type_command}.")
-    if hero_info['health'] <= 0:
-        print(f"You died! Killed by {type_command}.")
-        print(f"Best room: {room}")
-        break
+        if initial_health - value <= 0:
+            print(f"You died! Killed by {item}.")
+            print(f"Best room: {room}")
+            break
+        else:
+            initial_health -= value
+            print(f"You slayed {item}.")
 else:
     print("You've made it!")
-    print(f"Bitcoins: {hero_info['bitcoins']}")
-    print(f"Health: {hero_info['health']}")
+    print(f"Bitcoins: {bitcoins}")
+    print(f"Health: {initial_health}")
+
+
+
+
+
+
+
+#
+#
+# dungeons_rooms = input().split("|")
+#
+# hero_info = {
+#     "bitcoins": 0,
+#     "health": 100,
+# }
+#
+#
+# def potions(points_command):
+#     if hero_info["health"] + points_command > 100:
+#         print(f"You healed for {100 - hero_info['health']} hp.")
+#         hero_info["health"] = 100
+#     else:
+#         hero_info["health"] += points_command
+#         print(f"You healed for {points_command} hp.")
+#     print(f"Current health: {hero_info['health']} hp.")
+#
+#
+# def chest(points_command):
+#     hero_info["bitcoins"] += points_command
+#     print(f"You found {points_command} bitcoins.")
+#
+#
+# for room, command in enumerate(dungeons_rooms, 1):
+#     type_command, points_command = [int(x) if x.isdigit() else x for x in command.split()]
+#     if type_command == "potion":
+#         potions(points_command)
+#     elif type_command == "chest":
+#         chest(points_command)
+#     else:
+#         hero_info['health'] -= points_command
+#     if all([hero_info['health'] > 0, type_command != "potion", type_command != "chest"]):
+#         print(f"You slayed {type_command}.")
+#     if hero_info['health'] <= 0:
+#         print(f"You died! Killed by {type_command}.")
+#         print(f"Best room: {room}")
+#         break
+# else:
+#     print("You've made it!")
+#     print(f"Bitcoins: {hero_info['bitcoins']}")
+#     print(f"Health: {hero_info['health']}")
+#
+
+
 
 #
 #
