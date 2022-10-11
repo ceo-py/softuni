3567fb59b55ac214fa8a3d5ad8e2c2312f9938e7
ceo-py <sfmf@abv.bg>
2022-10-11 16:27:05 +0300

    ref

--- Fundamentals-Exams/06. Programming Fundamentals Mid Exam Retake/2_treasure_hunt.py
+++ Fundamentals-Exams/06. Programming Fundamentals Mid Exam Retake/2_treasure_hunt.py
@@ -1,86 +1,119 @@
-main_treasure_check = input().split("|")
+initial_loot = input().split("|")
 
-command = input()
+data_info = input()
 
+while data_info != "Yohoho!":
+    command, *data = [x for x in data_info.split()]
 
-def loot_(command):
-    for index, item in enumerate(command):
-        if index != 0 and item not in main_treasure_check:
-            main_treasure_check.insert(0, item)
+    if command == "Loot":
+        for item in data:
+            if item not in initial_loot:
+                initial_loot.insert(0, item)
 
+    elif command == "Drop":
+        index = int(data[0])
+        if 0 <= index < len(initial_loot):
+            initial_loot.append(initial_loot.pop(index))
 
-def drop_(command):
-    index_item = int(command[-1])
-    if abs(index_item) < len(main_treasure_check):
-        main_treasure_check.append(main_treasure_check.pop(index_item))
-
-
-def steal_(command):
-    global main_treasure_check
-    count_items = int(command[-1])
-    stolen_items = main_treasure_check[-count_items:]
-    main_treasure_check = main_treasure_check[:-count_items]
-    print(*stolen_items, sep=", ")
-
-
-def show_result():
-    if main_treasure_check:
-        print(f"Average treasure gain: {(sum(len(item) for item in main_treasure_check) / len(main_treasure_check)):.2f} pirate credits.")
-    else:
-        print(f"Failed treasure hunt.")
+    elif command == "Steal":
+        count = int(data[0])
+        stolen_items = initial_loot[-count:]
+        initial_loot = initial_loot[:-count]
+        print(*stolen_items, sep=", ")
+    data_info = input()
 
 
-while command != "Yohoho!":
-    command = command.split()
-    if "Loot" in command:
-        loot_(command)
-    elif "Drop" in command:
-        drop_(command)
-    elif "Steal" in command:
-        steal_(command)
-    command = input()
-
-show_result()
-
-
-
-
-
-
-
-
-
-
-
-
+if initial_loot:
+    average_treasure_gain = sum(len(x) for x in initial_loot) / len(initial_loot)
+    print(f"Average treasure gain: {average_treasure_gain:.2f} pirate credits.")
+else:
+    print("Failed treasure hunt.")
+#
+#
 #
 # main_treasure_check = input().split("|")
 #
 # command = input()
 #
+#
+# def loot_(command):
+#     for index, item in enumerate(command):
+#         if index != 0 and item not in main_treasure_check:
+#             main_treasure_check.insert(0, item)
+#
+#
+# def drop_(command):
+#     index_item = int(command[-1])
+#     if abs(index_item) < len(main_treasure_check):
+#         main_treasure_check.append(main_treasure_check.pop(index_item))
+#
+#
+# def steal_(command):
+#     global main_treasure_check
+#     count_items = int(command[-1])
+#     stolen_items = main_treasure_check[-count_items:]
+#     main_treasure_check = main_treasure_check[:-count_items]
+#     print(*stolen_items, sep=", ")
+#
+#
+# def show_result():
+#     if main_treasure_check:
+#         print(f"Average treasure gain: {(sum(len(item) for item in main_treasure_check) / len(main_treasure_check)):.2f} pirate credits.")
+#     else:
+#         print(f"Failed treasure hunt.")
+#
+#
 # while command != "Yohoho!":
 #     command = command.split()
-#     type_command = command[0]
-#     if type_command == "Loot":
-#         for index, item in enumerate(command):
-#             if index != 0 and item not in main_treasure_check:
-#                 main_treasure_check.insert(0, item)
-#     elif type_command == "Drop":
-#         index_item = int(command[-1])
-#         if abs(index_item) < len(main_treasure_check):
-#             main_treasure_check.append(main_treasure_check.pop(index_item))
-#     elif type_command == "Steal":
-#         count_items = int(command[-1])
-#         stolen_items = main_treasure_check[-count_items:]
-#         main_treasure_check = main_treasure_check[:-count_items]
-#         print(*stolen_items, sep=", ")
-#
+#     if "Loot" in command:
+#         loot_(command)
+#     elif "Drop" in command:
+#         drop_(command)
+#     elif "Steal" in command:
+#         steal_(command)
 #     command = input()
 #
-# if main_treasure_check:
-#     average_treasure = 0
-#     for item in main_treasure_check:
-#         average_treasure += len(item)
-#     print(f"Average treasure gain: {(average_treasure / len(main_treasure_check)):.2f} pirate credits.")
-# else:
-#     print(f"Failed treasure hunt.")
+# show_result()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# #
+# # main_treasure_check = input().split("|")
+# #
+# # command = input()
+# #
+# # while command != "Yohoho!":
+# #     command = command.split()
+# #     type_command = command[0]
+# #     if type_command == "Loot":
+# #         for index, item in enumerate(command):
+# #             if index != 0 and item not in main_treasure_check:
+# #                 main_treasure_check.insert(0, item)
+# #     elif type_command == "Drop":
+# #         index_item = int(command[-1])
+# #         if abs(index_item) < len(main_treasure_check):
+# #             main_treasure_check.append(main_treasure_check.pop(index_item))
+# #     elif type_command == "Steal":
+# #         count_items = int(command[-1])
+# #         stolen_items = main_treasure_check[-count_items:]
+# #         main_treasure_check = main_treasure_check[:-count_items]
+# #         print(*stolen_items, sep=", ")
+# #
+# #     command = input()
+# #
+# # if main_treasure_check:
+# #     average_treasure = 0
+# #     for item in main_treasure_check:
+# #         average_treasure += len(item)
+# #     print(f"Average treasure gain: {(average_treasure / len(main_treasure_check)):.2f} pirate credits.")
+# # else:
+# #     print(f"Failed treasure hunt.")
