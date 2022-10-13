711a67c67cc9be1edb447d272393a2df96276d47
ceo-py <sfmf@abv.bg>
2022-10-13 11:31:39 +0300

    ref

--- Python-Fundamentals/Functions - Exercise/9_password_validator.py
+++ Python-Fundamentals/Functions - Exercise/9_password_validator.py
@@ -1,8 +1,23 @@
-#
-#
-#
-#
-#
+errors = {
+    "leng": "Password must be between 6 and 10 characters",
+    "consist": "Password must consist only of letters and digits",
+    "least": "Password must have at least 2 digits"
+}
+
+
+def password_validation(password):
+    if not 6 <= len(password) <= 10:
+        print(errors.pop("leng"))
+    if not password.isalnum():
+        print(errors.pop("consist"))
+    if sum(1 for x in password if x.isdigit()) < 2:
+        print(errors.pop("least"))
+
+
+current_password = input()
+password_validation(current_password)
+if len(errors) == 3:
+    print("Password is valid")
 #
 #
 # errors = {
