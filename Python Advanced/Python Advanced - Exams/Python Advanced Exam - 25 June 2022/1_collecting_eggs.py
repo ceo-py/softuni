e86f2e23794271b1eca21e949486ac36bd2bfb04
ceo-py <sfmf@abv.bg>
2022-10-20 10:48:23 +0300

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Exam - 25 June 2022/1_collecting_eggs.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Exam - 25 June 2022/1_collecting_eggs.py
@@ -1,20 +1,25 @@
 from collections import deque
 
 eggs_size = deque(int(x) for x in input().split(", "))
-paper_size = [int(x) for x in input().split(", ")]
-BOXES_SIZE = 50
+piece_of_paper = deque(int(x) for x in input().split(", "))
+
+box_size = 50
 filled_boxes = 0
 
-while eggs_size and paper_size:
+while eggs_size and piece_of_paper:
     egg = eggs_size.popleft()
+
     if egg <= 0:
         continue
+
     if egg == 13:
-        paper_size[0], paper_size[-1] = paper_size[-1], paper_size[0]
+        piece_of_paper[0], piece_of_paper[-1] = piece_of_paper[-1], piece_of_paper[0]
         continue
-    paper = paper_size.pop()
-    wrapping_size = egg + paper
-    if wrapping_size <= BOXES_SIZE:
+
+    paper = piece_of_paper.pop()
+    total_sum = egg + paper
+
+    if total_sum <= box_size:
         filled_boxes += 1
 
 if filled_boxes:
@@ -25,5 +30,46 @@ else:
 if eggs_size:
     print(f"Eggs left: {', '.join(str(x) for x in eggs_size)}")
 
-if paper_size:
-    print(f"Pieces of paper left: {', '.join(str(x) for x in paper_size)}")
+if piece_of_paper:
+    print(f"Pieces of paper left: ", end="")
+    print(*piece_of_paper, sep=", ")
+
+
+
+
+
+
+
+#
+#
+#
+#
+# from collections import deque
+#
+# eggs_size = deque(int(x) for x in input().split(", "))
+# paper_size = [int(x) for x in input().split(", ")]
+# BOXES_SIZE = 50
+# filled_boxes = 0
+#
+# while eggs_size and paper_size:
+#     egg = eggs_size.popleft()
+#     if egg <= 0:
+#         continue
+#     if egg == 13:
+#         paper_size[0], paper_size[-1] = paper_size[-1], paper_size[0]
+#         continue
+#     paper = paper_size.pop()
+#     wrapping_size = egg + paper
+#     if wrapping_size <= BOXES_SIZE:
+#         filled_boxes += 1
+#
+# if filled_boxes:
+#     print(f"Great! You filled {filled_boxes} boxes.")
+# else:
+#     print("Sorry! You couldn't fill any boxes!")
+#
+# if eggs_size:
+#     print(f"Eggs left: {', '.join(str(x) for x in eggs_size)}")
+#
+# if paper_size:
+#     print(f"Pieces of paper left: {', '.join(str(x) for x in paper_size)}")
