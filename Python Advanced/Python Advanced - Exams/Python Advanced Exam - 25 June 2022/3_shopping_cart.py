d9b0adebb9cc6cf1556001a72f50f3e3ea767489
ceo-py <sfmf@abv.bg>
2022-10-20 10:49:26 +0300

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Exam - 25 June 2022/3_shopping_cart.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Exam - 25 June 2022/3_shopping_cart.py
@@ -1,22 +1,54 @@
 def shopping_cart(*args):
-    output, result = "", {"Dessert": [], "Pizza": [], "Soup": []}
+    result, output = {"Dessert": [], "Pizza": [], "Soup": []}, []
     for info in args:
-        if "Stop" in info:
+        if info == "Stop":
             break
-        meal, product, = info
-        for meal_type, number_product in (("Soup", 3), ("Pizza", 4), ("Dessert", 2)):
-            if meal == meal_type and len(result[meal]) != number_product and product not in result[meal]:
+        meal, product = info
+
+        if product not in result[meal]:
+            if meal == "Soup" and len(result[meal]) != 3:
                 result[meal].append(product)
-                break
-    for p_meal, p_product in sorted(result.items(), key=lambda x: (-len(x[1]), x[0])):
-        output += f"{p_meal}:\n"
-        for item in sorted(p_product):
-            output += f" - {item}\n"
-    if len(output) == 22:
-        output = "No products in the cart!"
-    return output
+            elif meal == "Pizza" and len(result[meal]) != 4:
+                result[meal].append(product)
+            elif meal == "Dessert" and len(result[meal]) != 2:
+                result[meal].append(product)
+
+    for s_meal, s_product in sorted(result.items(), key= lambda x: (-len(x[1]), x[0])):
+        output.append(f"{s_meal}:")
+        for a_product in sorted(s_product):
+            output.append(f" - {a_product}")
+
+    if any(len(x) != 0 for x in result.values()):
+        return '\n'.join(output)
+
+    return "No products in the cart!"
 
 
+
+
+
+
+
+
+# def shopping_cart(*args):
+#     output, result = "", {"Dessert": [], "Pizza": [], "Soup": []}
+#     for info in args:
+#         if "Stop" in info:
+#             break
+#         meal, product, = info
+#         for meal_type, number_product in (("Soup", 3), ("Pizza", 4), ("Dessert", 2)):
+#             if meal == meal_type and len(result[meal]) != number_product and product not in result[meal]:
+#                 result[meal].append(product)
+#                 break
+#     for p_meal, p_product in sorted(result.items(), key=lambda x: (-len(x[1]), x[0])):
+#         output += f"{p_meal}:\n"
+#         for item in sorted(p_product):
+#             output += f" - {item}\n"
+#     if len(output) == 22:
+#         output = "No products in the cart!"
+#     return output
+#
+#
 
 
 #
