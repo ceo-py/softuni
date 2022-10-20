94be6b28227ad91086767a44e6b45c8d338f4098
ceo-py <sfmf@abv.bg>
2022-10-20 10:51:17 +0300

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 14 April 2022/3_words_sorting.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 14 April 2022/3_words_sorting.py
@@ -1,14 +1,32 @@
 def words_sorting(*args):
-    output, result = "", {}
-    for word in args:
-        result[word] = result.get(word, 0) + sum([ord(x) for x in word])
-    if sum(result.values()) % 2 != 0:
-        for p_word, p_value in sorted(result.items(), key=lambda x: -x[1]):
-            output += f"{p_word} - {p_value}\n"
+    result = {x: sum(ord(l) for l in x) for x in args}
+    total_sum = sum(result.values())
+
+    if total_sum % 2 == 0:
+        output = [f"{key} - {value}" for key, value in sorted(result.items(), key= lambda x: x[0])]
     else:
-        for p_word in sorted(result):
-            output += f"{p_word} - {result[p_word]}\n"
-    return output
+        output = [f"{key} - {value}" for key, value in sorted(result.items(), key= lambda x: -x[1])]
+
+    return '\n'.join(output)
+
+
+
+
+
+
+
+
+# def words_sorting(*args):
+#     output, result = "", {}
+#     for word in args:
+#         result[word] = result.get(word, 0) + sum([ord(x) for x in word])
+#     if sum(result.values()) % 2 != 0:
+#         for p_word, p_value in sorted(result.items(), key=lambda x: -x[1]):
+#             output += f"{p_word} - {p_value}\n"
+#     else:
+#         for p_word in sorted(result):
+#             output += f"{p_word} - {result[p_word]}\n"
+#     return output
 
 # print(
 #     words_sorting(
