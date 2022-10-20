eef066c47ab3f692864a617363458d8421c8e84c
ceo-py <sfmf@abv.bg>
2022-10-20 10:50:17 +0300

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 14 April 2022/1_ramen_shop.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 14 April 2022/1_ramen_shop.py
@@ -1,28 +1,61 @@
 from collections import deque
 
-bowls = [int(x) for x in input().split(", ")]
-customers = deque(int(x) for x in input().split(", "))
+bowls_ramen = [int(x) for x in input().split(", ")]
+customers = deque([int(x) for x in input().split(", ")])
 
 
-while bowls and customers:
-    bowl = bowls.pop()
+while bowls_ramen and customers:
+
+    ramen = bowls_ramen.pop()
     customer = customers.popleft()
-    if bowl == customer:
-        continue
 
-    if bowl > customer:
-        bowl -= customer
-        bowls.append(bowl)
+    if ramen == customer:
+        pass
 
-    elif customer > bowl:
-        customer -= bowl
-        customers.appendleft(customer)
+    elif ramen > customer:
+        bowls_ramen.append(ramen - customer)
 
+    elif customer > ramen:
+        customers.appendleft(customer - ramen)
 
 if not customers:
     print("Great job! You served all the customers.")
-    if bowls:
-        print(f"Bowls of ramen left: {', '.join(str(x) for x in bowls)}")
+    if bowls_ramen:
+        print(f"Bowls of ramen left: {', '.join(str(x) for x in bowls_ramen)}")
 else:
     print("Out of ramen! You didn't manage to serve all customers.")
     print(f"Customers left: {', '.join(str(x) for x in customers)}")
+
+
+
+
+#
+#
+# from collections import deque
+#
+# bowls = [int(x) for x in input().split(", ")]
+# customers = deque(int(x) for x in input().split(", "))
+#
+#
+# while bowls and customers:
+#     bowl = bowls.pop()
+#     customer = customers.popleft()
+#     if bowl == customer:
+#         continue
+#
+#     if bowl > customer:
+#         bowl -= customer
+#         bowls.append(bowl)
+#
+#     elif customer > bowl:
+#         customer -= bowl
+#         customers.appendleft(customer)
+#
+#
+# if not customers:
+#     print("Great job! You served all the customers.")
+#     if bowls:
+#         print(f"Bowls of ramen left: {', '.join(str(x) for x in bowls)}")
+# else:
+#     print("Out of ramen! You didn't manage to serve all customers.")
+#     print(f"Customers left: {', '.join(str(x) for x in customers)}")
