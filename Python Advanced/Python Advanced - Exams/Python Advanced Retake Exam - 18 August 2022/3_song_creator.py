11dfdadf7f8e14fc25f18b5004e877951eb6a6e3
ceo-py <sfmf@abv.bg>
2022-10-20 10:47:36 +0300

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 18 August 2022/3_song_creator.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 18 August 2022/3_song_creator.py
@@ -1,13 +1,33 @@
 def add_songs(*args):
-    result, print_result = {}, ""
-    for song_name, lyrics in args:
-        result[song_name] = result.get(song_name, []) + lyrics
-    for p_song_name, p_lyrics in result.items():
-        print_result += f"- {p_song_name}\n" + '\n'.join(p_lyrics)
-        if p_lyrics:
-            print_result += "\n"
-    return print_result
+    result, output = {}, []
+    for song, lyrics in args:
+        result[song] = result.get(song, []) + lyrics
 
+    for key, value in result.items():
+        output.append(f"- {key}")
+        for text in value:
+            output.append(text)
+
+    return '\n'.join(output)
+
+
+
+
+
+
+
+
+#
+# def add_songs(*args):
+#     result, print_result = {}, ""
+#     for song_name, lyrics in args:
+#         result[song_name] = result.get(song_name, []) + lyrics
+#     for p_song_name, p_lyrics in result.items():
+#         print_result += f"- {p_song_name}\n" + '\n'.join(p_lyrics)
+#         if p_lyrics:
+#             print_result += "\n"
+#     return print_result
+#
 
 print(add_songs(
     ("Love of my life",
