908bf36b89d1b08971e5fe6f29bd944553cc999e
ceo-py <sfmf@abv.bg>
2022-10-20 10:47:06 +0300

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 18 August 2022/2_crud.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 18 August 2022/2_crud.py
@@ -1,58 +1,98 @@
-MATRIX_SIZE = 6
-matrix = [input().split() for _ in range(MATRIX_SIZE)]
-row_position, col_position = [int(x) for x in input()[1:-1].split(",")]
-directions = {"up": [-1, 0], "down": [1, 0], "left": [0, -1], "right": [0, 1]}
+from collections import deque
 
+seats = input().split(", ")
+first_number = deque(int(x) for x in input().split(", "))
+second_number = deque(int(x) for x in input().split(", "))
 
-def add_step(row, col, direction):
-    return row + directions[direction][0], col + directions[direction][1]
+rotations, matched_seats = 0, []
 
+while len(matched_seats) != 3 and rotations != 10:
+    first_num = first_number.popleft()
+    second_num = second_number.pop()
+    rotations += 1
 
-def number_or_letter():
-    return matrix[row_position][col_position].isalpha() or matrix[row_position][col_position].isdigit()
+    letter = chr(first_num + second_num)
+    found = False
+    for seat in (f"{first_num}{letter}", f"{second_num}{letter}"):
+        if seat in seats:
+            matched_seats.append(seat)
+            seats.remove(seat)
+            found = True
 
+    if found:
+        continue
 
-def create(value):
-    if matrix[row_position][col_position] == ".":
-        matrix[row_position][col_position] = value
+    first_number.append(first_num)
+    second_number.appendleft(second_num)
 
-
-def update(value):
-    matrix[row_position][col_position] = value
-
-
-def delete(_):
-    matrix[row_position][col_position] = "."
-
-
-def read(_):
-    print(matrix[row_position][col_position])
-
-
-commands = {
-    "Create": create,
-    "Update": update,
-    "Read": read,
-    "Delete": delete
-}
-command, commands_no_create = input(), tuple(commands.keys())[1:]
-
-while command != "Stop":
-    command_type, *info = command.split(", ")
-    row_position, col_position = add_step(row_position, col_position, info[0])
-    if command_type in commands_no_create:
-        if number_or_letter():
-            commands[command_type](info[-1])
-    else:
-        commands[command_type](info[-1])
-    command = input()
-
-[print(*matrix[row]) for row in range(MATRIX_SIZE)]
+print(f"Seat matches: {', '.join(matched_seats)}")
+print(f"Rotations count: {rotations}")
 
 
 
 
 
+
+
+
+#
+#
+#
+# MATRIX_SIZE = 6
+# matrix = [input().split() for _ in range(MATRIX_SIZE)]
+# row_position, col_position = [int(x) for x in input()[1:-1].split(",")]
+# directions = {"up": [-1, 0], "down": [1, 0], "left": [0, -1], "right": [0, 1]}
+#
+#
+# def add_step(row, col, direction):
+#     return row + directions[direction][0], col + directions[direction][1]
+#
+#
+# def number_or_letter():
+#     return matrix[row_position][col_position].isalpha() or matrix[row_position][col_position].isdigit()
+#
+#
+# def create(value):
+#     if matrix[row_position][col_position] == ".":
+#         matrix[row_position][col_position] = value
+#
+#
+# def update(value):
+#     matrix[row_position][col_position] = value
+#
+#
+# def delete(_):
+#     matrix[row_position][col_position] = "."
+#
+#
+# def read(_):
+#     print(matrix[row_position][col_position])
+#
+#
+# commands = {
+#     "Create": create,
+#     "Update": update,
+#     "Read": read,
+#     "Delete": delete
+# }
+# command, commands_no_create = input(), tuple(commands.keys())[1:]
+#
+# while command != "Stop":
+#     command_type, *info = command.split(", ")
+#     row_position, col_position = add_step(row_position, col_position, info[0])
+#     if command_type in commands_no_create:
+#         if number_or_letter():
+#             commands[command_type](info[-1])
+#     else:
+#         commands[command_type](info[-1])
+#     command = input()
+#
+# [print(*matrix[row]) for row in range(MATRIX_SIZE)]
+#
+#
+#
+#
+#
 
 
 
