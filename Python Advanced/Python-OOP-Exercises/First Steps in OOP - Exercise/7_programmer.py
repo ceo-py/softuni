19425b2ec8b88787838335d5fedf9785ed56626e
ceo-py <sfmf@abv.bg>
2022-10-26 12:40:37 +0300

    ref

--- Python Advanced/Python-OOP-Exercises/First Steps in OOP - Exercise/7_programmer.py
+++ Python Advanced/Python-OOP-Exercises/First Steps in OOP - Exercise/7_programmer.py
@@ -4,7 +4,7 @@ class Programmer:
         self.language = language
         self.skills = int(skills)
 
-    def watch_course(self,course_name, language, skills_earned):
+    def watch_course(self, course_name, language, skills_earned):
         if language == self.language:
             self.skills += skills_earned
             return f"{self.name} watched {course_name}"
@@ -15,8 +15,7 @@ class Programmer:
             old_language = self.language
             self.language = new_language
             return f"{self.name} switched from {old_language} to {new_language}"
-        elif all([skills_needed <= self.skills, new_language == self.language]):
+        if all([skills_needed <= self.skills, new_language == self.language]):
             return f"{self.name} already knows {new_language}"
-        elif skills_needed > self.skills:
-            return f"{self.name} needs {abs(self.skills-skills_needed)} more skills"
-
+        if skills_needed > self.skills:
+            return f"{self.name} needs {abs(self.skills - skills_needed)} more skills"
