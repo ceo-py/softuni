eaffde7802de2ba5a8f0cce75324ae2a0c9cdf5d
ceo-py <sfmf@abv.bg>
2022-10-27 14:48:07 +0300

    ref

--- Python Advanced/Python-OOP-Exercises/Inheritance - Lab/6_stack_of_strings.py
+++ Python Advanced/Python-OOP-Exercises/Inheritance - Lab/6_stack_of_strings.py
@@ -1,40 +1,25 @@
+
 class Stack:
-    def __init__(self):
-        self.data = []
+    data = []
 
     def push(self, element):
-        self.data.append(element)
+        if isinstance(element, str):
+            Stack.data.append(element)
 
     def pop(self):
-        return self.data.pop(-1)
+        return Stack.data.pop()
 
     def top(self):
-        return self.data[-1]
+        return Stack.data[-1]
 
     def is_empty(self):
-        if self.data:
+        if Stack.data:
             return False
         return True
 
     def __str__(self):
-        return f'[{", ".join(self.data)}]'
+        return f"[{', '.join(Stack.data[::-1])}]"
 
 
-import unittest
 
 
-class StackTests(unittest.TestCase):
-    def test_zero(self):
-        stack = Stack()
-        stack.push("apple")
-        stack.push("carrot")
-        self.assertEqual(str(stack), '[carrot, apple]')
-        self.assertEqual(stack.pop(), 'carrot')
-        self.assertEqual(stack.top(), 'apple')
-        stack.push("cucumber")
-        self.assertEqual(str(stack), '[cucumber, apple]')
-        self.assertEqual(stack.is_empty(), False)
-
-
-if __name__ == '__main__':
-    unittest.main()
