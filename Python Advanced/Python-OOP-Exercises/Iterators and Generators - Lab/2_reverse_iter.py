88bb047413becfec70a451699e5a10f6401829e9
ceo-py <sfmf@abv.bg>
2022-11-02 10:48:11 +0200

    ref

--- Python Advanced/Python-OOP-Exercises/Iterators and Generators - Lab/2_reverse_iter.py
+++ Python Advanced/Python-OOP-Exercises/Iterators and Generators - Lab/2_reverse_iter.py
@@ -1,13 +1,31 @@
 class reverse_iter:
     def __init__(self, list_of_numbers: list):
         self.list_of_numbers = list_of_numbers
+        self.index = len(list_of_numbers) - 1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        while True:
-            try:
-                return self.list_of_numbers.pop()
-            except IndexError:
-                raise StopIteration()
+        if self.index >= 0:
+            output = self.list_of_numbers[self.index]
+            self.index -= 1
+            return output
+        else:
+            raise StopIteration()
+
+
+
+
+# class reverse_iter:
+#     def __init__(self, list_of_numbers: list):
+#         self.list_of_numbers = list_of_numbers[::-1]
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         try:
+#             return self.list_of_numbers.pop()
+#         except IndexError:
+#             raise StopIteration()
