62f5952887f18d27ed8fb65f3e2cebb2d1ade9ab
ceo-py <sfmf@abv.bg>
2022-11-06 19:51:14 +0200

    ref

--- Python-Fundamentals/Dictionaries - More Exercises/2_judge.py
+++ Python-Fundamentals/Dictionaries - More Exercises/2_judge.py
@@ -1,36 +1,71 @@
-command_judge = input()
+courses, individual = {}, {}
+data = input()
+while data != "no more time":
 
-judge_info = {"user": {}, "contest": {}}
-user_d = "user"
-contest_d = "contest"
+    name, course, score = [x if x.isalpha() else int(x) for x in data.split(" -> ")]
+    courses[course] = courses.get(course, {})
+    courses[course][name] = courses[course].get(name, 0)
+    if courses[course][name] < score:
+        courses[course][name] = score
+    data = input()
 
-while command_judge != "no more time":
-    user_name, contest, points = [int(x) if x.isdigit() else x for x in command_judge.split(" -> ")]
-    judge_info[contest_d][contest] = judge_info[contest_d].get(contest, {})
-    judge_info[contest_d][contest][user_name] = judge_info[contest_d][contest].get(user_name, 0)
-    judge_info[user_d][user_name] = judge_info[user_d].get(user_name, 0)
-    if user_name in judge_info[user_d] and judge_info[user_d][user_name] == points:
-        judge_info[user_d][user_name] += points
-    if judge_info[contest_d][contest][user_name] < points:
-        judge_info[user_d][user_name] += points - judge_info[contest_d][contest][user_name]
-        judge_info[contest_d][contest][user_name] = points
+for course in courses:
+    print(f"{course}: {len(courses[course])} participants")
+    for pos, (user, score) in enumerate(sorted(courses[course].items(), key= lambda x: (-x[1], x[0])), 1):
+        print(f"{pos}. {user} <::> {score}")
+        individual[user] = individual.get(user, 0) + score
 
-    command_judge = input()
-
-
-def show_result():
-    for contest in judge_info[contest_d]:
-        print(f"{contest}: {len(judge_info[contest_d][contest])} participants")
-        for pos, (name, points) in enumerate(
-                sorted(judge_info[contest_d][contest].items(), key=lambda item: (-item[1], item[0])), 1):
-            print(f"{pos}. {name} <::> {points}")
-    print("Individual standings:")
-    for pos, (name, points) in enumerate(sorted(judge_info[user_d].items(), key=lambda item: (-item[1], item[0])), 1):
-        print(f"{pos}. {name} -> {points}")
+print("Individual standings:")
+for pos, (user, score) in enumerate(sorted(individual.items(), key=lambda x: (-x[1], x[0])), 1):
+    print(f"{pos}. {user} -> {score}")
 
 
-show_result()
 
+
+
+
+
+
+
+
+
+
+
+#
+#
+# command_judge = input()
+#
+# judge_info = {"user": {}, "contest": {}}
+# user_d = "user"
+# contest_d = "contest"
+#
+# while command_judge != "no more time":
+#     user_name, contest, points = [int(x) if x.isdigit() else x for x in command_judge.split(" -> ")]
+#     judge_info[contest_d][contest] = judge_info[contest_d].get(contest, {})
+#     judge_info[contest_d][contest][user_name] = judge_info[contest_d][contest].get(user_name, 0)
+#     judge_info[user_d][user_name] = judge_info[user_d].get(user_name, 0)
+#     if user_name in judge_info[user_d] and judge_info[user_d][user_name] == points:
+#         judge_info[user_d][user_name] += points
+#     if judge_info[contest_d][contest][user_name] < points:
+#         judge_info[user_d][user_name] += points - judge_info[contest_d][contest][user_name]
+#         judge_info[contest_d][contest][user_name] = points
+#
+#     command_judge = input()
+#
+#
+# def show_result():
+#     for contest in judge_info[contest_d]:
+#         print(f"{contest}: {len(judge_info[contest_d][contest])} participants")
+#         for pos, (name, points) in enumerate(
+#                 sorted(judge_info[contest_d][contest].items(), key=lambda item: (-item[1], item[0])), 1):
+#             print(f"{pos}. {name} <::> {points}")
+#     print("Individual standings:")
+#     for pos, (name, points) in enumerate(sorted(judge_info[user_d].items(), key=lambda item: (-item[1], item[0])), 1):
+#         print(f"{pos}. {name} -> {points}")
+#
+#
+# show_result()
+#
 
 
 
