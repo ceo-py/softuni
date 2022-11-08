ac43b76da7f7f7e90fdb968023598159fa673041
ceo-py <sfmf@abv.bg>
2022-11-08 23:02:11 +0200

    ref

--- Python Basics/While Loop - Exercise/5_coins.py
+++ Python Basics/While Loop - Exercise/5_coins.py
@@ -2,35 +2,52 @@ change = float(input())
 coins = 0
 change = change * 100
 
-currCoins = int(change / 200)
-change -= currCoins * 200
-coins += currCoins
-
-currCoins = int(change / 100)
-change -= currCoins * 100
-coins += currCoins
-
-currCoins = int(change / 50)
-change -= currCoins * 50
-coins += currCoins
-
-currCoins = int(change / 20)
-change -= currCoins * 20
-coins += currCoins
-
-currCoins = int(change / 10)
-change -= currCoins * 10
-coins += currCoins
-
-currCoins = int(change / 5)
-change -= currCoins * 5
-coins += currCoins
-
-currCoins = int(change / 2)
-change -= currCoins * 2
-coins += currCoins
+for x in (200, 100, 50, 20, 10, 5, 2):
+    currCoins = int(change / x)
+    change -= currCoins * x
+    coins += currCoins
 
 currCoins = int(change)
 coins += currCoins
 
 print(coins)
+
+
+
+#
+# change = float(input())
+# coins = 0
+# change = change * 100
+#
+# currCoins = int(change / 200)
+# change -= currCoins * 200
+# coins += currCoins
+#
+# currCoins = int(change / 100)
+# change -= currCoins * 100
+# coins += currCoins
+#
+# currCoins = int(change / 50)
+# change -= currCoins * 50
+# coins += currCoins
+#
+# currCoins = int(change / 20)
+# change -= currCoins * 20
+# coins += currCoins
+#
+# currCoins = int(change / 10)
+# change -= currCoins * 10
+# coins += currCoins
+#
+# currCoins = int(change / 5)
+# change -= currCoins * 5
+# coins += currCoins
+#
+# currCoins = int(change / 2)
+# change -= currCoins * 2
+# coins += currCoins
+#
+# currCoins = int(change)
+# coins += currCoins
+#
+# print(coins)
