fa5df97884f0589c14e38e64f08b4151d3642b55
ceo-py <sfmf@abv.bg>
2022-11-11 15:22:14 +0200

    ref

--- Python Advanced/Python-OOP-Exams/Python OOP Retake Exam - 18 April 2022/project_1_movie_app/validation/validation.py
+++ Python Advanced/Python-OOP-Exams/Python OOP Retake Exam - 18 April 2022/project_1_movie_app/validation/validation.py
@@ -44,7 +44,7 @@ class Validation:
 
     @staticmethod
     def user_owned_movie(client_class, movie_class):
-        if movie_class.owner != client_class:
+        if movie_class.owner.username != client_class.username:
             raise Exception(f"{client_class.username} is not the owner of the movie {movie_class.title}!")
 
     @staticmethod
