c997c5f6a21a655dfb6d21745b0997415ef9c509
ceo-py <sfmf@abv.bg>
2022-11-16 14:37:09 +0200

    ref

--- Python Advanced/Python-OOP-Exercises/Polymorphism and Abstraction - Exercise/2_groups.py
+++ Python Advanced/Python-OOP-Exercises/Polymorphism and Abstraction - Exercise/2_groups.py
@@ -28,3 +28,36 @@ class Group:
         return f"Group {self.name} with members {', '.join(str(x) for x in self.people)}"
 
 
+aaa = [1,2,3,4]
+
+for x in aaa:
+
+
+
+
+p0 = Person('Aliko', 'Dangote')
+p1 = Person('Bill', 'Gates')
+
+p3 = p0 + p1
+print(p3.name)
+print(p3.surname)
+p2 = Person('Warren', 'Buffet')
+p3 = Person('Elon', 'Musk')
+p4 = p2 + p3
+
+
+
+first_group = Group('__VIP__', [p0, p1, p2])
+second_group = Group('Special', [p3, p4])
+third_group = first_group + second_group
+
+# print(len(first_group))
+# print(second_group)
+# print(third_group[0])
+
+# for person in third_group:
+#     print(person)
+
+
+for person in first_group:
+    print(person)
--- Python-Fundamentals/Text Processing - Exercise/10_winning_ticket.py
+++ Python-Fundamentals/Text Processing - Exercise/10_winning_ticket.py
@@ -1,40 +1,79 @@
-tickets_sting = input().split(", ")
+import re
 
+
+tickets_sting = input().split(", ")
 symbols = ['@', '#', '$', '^']
 
 
-def check_next(symbol, first, second):
-    back = 6
-    for i in range(7, 11):
-        if (i * symbol) in first and (i * symbol) in second:
-            back += 1
-    return back
-
-
 for ticket_characters in tickets_sting:
-    price_find = False
-    jackpot_find = False
     ticket_characters = ticket_characters.replace(" ", "")
     if len(ticket_characters) == 20:
         how_long = int(len(ticket_characters) / 2)
-        left_side = ticket_characters[:how_long]
-        right_side = ticket_characters[how_long:]
-        for index, symbol in enumerate(symbols):
-            if 10 * symbol in left_side and 10 * symbol in right_side:
-                print(f'ticket "{ticket_characters}" - 10{symbols[index]} Jackpot!')
-                jackpot_find = True
+        left_side, right_side = ticket_characters[:how_long], ticket_characters[how_long:]
+        for symbol in symbols:
+            opz = f"\{symbol}+"
+            try:
+                l_occurs = max([len(i) for i in re.findall(opz, left_side)])
+                r_occurs = max([len(i) for i in re.findall(opz, right_side)])
+            except ValueError:
+                continue
+            if l_occurs == r_occurs == 10:
+                print(f'ticket "{ticket_characters}" - 10{symbol} Jackpot!')
                 break
-            elif 6 * symbol in left_side and 6 * symbol in right_side:
-                symbol_type = symbols[index]
-                total = check_next(symbol_type, left_side, right_side)
-                price_find = True
+            winners_price = min(l_occurs, r_occurs)
+            if winners_price >= 6:
+                print(f'ticket "{ticket_characters}" - {winners_price}{symbol}')
                 break
-        if price_find:
-            print(f'ticket "{ticket_characters}" - {total}{symbol_type}')
-        elif not jackpot_find and not price_find:
+        else:
             print(f'ticket "{ticket_characters}" - no match')
     else:
         print("invalid ticket")
+
+
+
+
+
+
+
+
+
+# tickets_sting = input().split(", ")
+#
+# symbols = ['@', '#', '$', '^']
+#
+#
+# def check_next(symbol, first, second):
+#     back = 6
+#     for i in range(7, 11):
+#         if (i * symbol) in first and (i * symbol) in second:
+#             back += 1
+#     return back
+#
+#
+# for ticket_characters in tickets_sting:
+#     price_find = False
+#     jackpot_find = False
+#     ticket_characters = ticket_characters.replace(" ", "")
+#     if len(ticket_characters) == 20:
+#         how_long = int(len(ticket_characters) / 2)
+#         left_side = ticket_characters[:how_long]
+#         right_side = ticket_characters[how_long:]
+#         for index, symbol in enumerate(symbols):
+#             if 10 * symbol in left_side and 10 * symbol in right_side:
+#                 print(f'ticket "{ticket_characters}" - 10{symbols[index]} Jackpot!')
+#                 jackpot_find = True
+#                 break
+#             elif 6 * symbol in left_side and 6 * symbol in right_side:
+#                 symbol_type = symbols[index]
+#                 total = check_next(symbol_type, left_side, right_side)
+#                 price_find = True
+#                 break
+#         if price_find:
+#             print(f'ticket "{ticket_characters}" - {total}{symbol_type}')
+#         elif not jackpot_find and not price_find:
+#             print(f'ticket "{ticket_characters}" - no match')
+#     else:
+#         print("invalid ticket")
 
 
 
