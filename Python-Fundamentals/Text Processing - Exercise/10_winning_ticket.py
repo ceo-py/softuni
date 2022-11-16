e4d8f8fbc029c5304f97e688208f01593718bfb3
ceo-py <sfmf@abv.bg>
2022-11-16 15:19:05 +0200

    ref

--- Python-Fundamentals/Text Processing - Exercise/10_winning_ticket.py
+++ Python-Fundamentals/Text Processing - Exercise/10_winning_ticket.py
@@ -1,8 +1,20 @@
-import re
-
-
 tickets_sting = input().split(", ")
 symbols = ['@', '#', '$', '^']
+
+
+def get_max_in_a_row(string, symbol):
+    next_, current = 0, 0
+    for x in range(len(string)):
+        if string[x] == symbol:
+            next_ += 1
+            if x == len(string) - 1:
+                if current < next_:
+                    current = next_
+        else:
+            if current < next_:
+                current = next_
+            next_ = 0
+    return current
 
 
 for ticket_characters in tickets_sting:
@@ -11,16 +23,11 @@ for ticket_characters in tickets_sting:
         how_long = int(len(ticket_characters) / 2)
         left_side, right_side = ticket_characters[:how_long], ticket_characters[how_long:]
         for symbol in symbols:
-            opz = f"\{symbol}+"
-            try:
-                l_occurs = max([len(i) for i in re.findall(opz, left_side)])
-                r_occurs = max([len(i) for i in re.findall(opz, right_side)])
-            except ValueError:
-                continue
-            if l_occurs == r_occurs == 10:
+            sum_of_l, sum_of_r = get_max_in_a_row(left_side, symbol), get_max_in_a_row(right_side, symbol)
+            if sum_of_l == sum_of_r == 10:
                 print(f'ticket "{ticket_characters}" - 10{symbol} Jackpot!')
                 break
-            winners_price = min(l_occurs, r_occurs)
+            winners_price = min(sum_of_l, sum_of_r)
             if winners_price >= 6:
                 print(f'ticket "{ticket_characters}" - {winners_price}{symbol}')
                 break
@@ -30,6 +37,43 @@ for ticket_characters in tickets_sting:
         print("invalid ticket")
 
 
+
+
+
+#
+#
+# import re
+#
+#
+# tickets_sting = input().split(", ")
+# symbols = ['@', '#', '$', '^']
+#
+#
+# for ticket_characters in tickets_sting:
+#     ticket_characters = ticket_characters.replace(" ", "")
+#     if len(ticket_characters) == 20:
+#         how_long = int(len(ticket_characters) / 2)
+#         left_side, right_side = ticket_characters[:how_long], ticket_characters[how_long:]
+#         for symbol in symbols:
+#             opz = f"\{symbol}+"
+#             try:
+#                 l_occurs = max(len(i) for i in re.findall(opz, left_side))
+#                 r_occurs = max(len(i) for i in re.findall(opz, right_side))
+#             except ValueError:
+#                 continue
+#             if l_occurs == r_occurs == 10:
+#                 print(f'ticket "{ticket_characters}" - {l_occurs}{symbol} Jackpot!')
+#                 break
+#             winners_price = min(l_occurs, r_occurs)
+#             if winners_price >= 6:
+#                 print(f'ticket "{ticket_characters}" - {winners_price}{symbol}')
+#                 break
+#         else:
+#             print(f'ticket "{ticket_characters}" - no match')
+#     else:
+#         print("invalid ticket")
+#
+#
 
 
 
