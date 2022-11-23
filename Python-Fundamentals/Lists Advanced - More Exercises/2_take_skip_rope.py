9347cba34348b085cc1b1b04a41e1e32f79bb192
ceo-py <sfmf@abv.bg>
2022-11-23 18:51:10 +0200

    ref

--- Python-Fundamentals/Lists Advanced - More Exercises/2_take_skip_rope.py
+++ Python-Fundamentals/Lists Advanced - More Exercises/2_take_skip_rope.py
@@ -4,6 +4,10 @@ numbers, letters, output = [], [], []
 [numbers.append(int(x)) if x.isdigit() else letters.append(x) for x in main_string]
 while numbers:
     take, skip = numbers.pop(0), numbers.pop(0)
+
+    # for x in range(0, len(numbers), 2): - no while loop / this is faster method
+    #     take, skip = numbers[x], numbers[x + 1]
+
     output += letters[:take]
     letters = letters[take + skip:]
 
