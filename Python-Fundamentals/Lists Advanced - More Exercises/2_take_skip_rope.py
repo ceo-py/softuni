d6d297196428bf4ff70c0b2f4d1dd4a633331f4b
ceo-py <sfmf@abv.bg>
2022-11-23 18:47:19 +0200

    ref

--- Python-Fundamentals/Lists Advanced - More Exercises/2_take_skip_rope.py
+++ Python-Fundamentals/Lists Advanced - More Exercises/2_take_skip_rope.py
@@ -1,29 +1,47 @@
 main_string = input()
 
-numbers = list()
-letter = ""
-take_list = list()
-skip_list = list()
+numbers, letters, output = [], [], []
+[numbers.append(int(x)) if x.isdigit() else letters.append(x) for x in main_string]
+while numbers:
+    take, skip = numbers.pop(0), numbers.pop(0)
+    output += letters[:take]
+    letters = letters[take + skip:]
 
-result = ""
+print(*output, sep="")
 
-for character in main_string:
-    if character.isdigit():
-        numbers.append(int(character))
-    else:
-        letter += character
 
-for ind, num in enumerate(numbers):
-    if ind % 2 == 0:
-        take_list.append(num)
-    else:
-        skip_list.append(num)
 
-for i, n in zip(take_list, skip_list):
-    if i == 0:
-        letter = letter[n:]
-    elif i != 0:
-        result = result + letter[:i]
-        letter = letter[n + i:]
 
-print(result)
+
+
+
+#
+# main_string = input()
+#
+# numbers = list()
+# letter = ""
+# take_list = list()
+# skip_list = list()
+#
+# result = ""
+#
+# for character in main_string:
+#     if character.isdigit():
+#         numbers.append(int(character))
+#     else:
+#         letter += character
+#
+# for ind, num in enumerate(numbers):
+#     if ind % 2 == 0:
+#         take_list.append(num)
+#     else:
+#         skip_list.append(num)
+#
+# for i, n in zip(take_list, skip_list):
+#     if i == 0:
+#         letter = letter[n:]
+#     elif i != 0:
+#         result = result + letter[:i]
+#         letter = letter[n + i:]
+#
+# print(result)
