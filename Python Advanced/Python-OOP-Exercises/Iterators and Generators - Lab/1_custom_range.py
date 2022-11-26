4f528b6d96ee6ff20b7341874cf502d76b852d95
ceo-py <sfmf@abv.bg>
2022-11-27 01:01:15 +0200

    ref

--- Python Advanced/Python-OOP-Exercises/Iterators and Generators - Lab/1_custom_range.py
+++ Python Advanced/Python-OOP-Exercises/Iterators and Generators - Lab/1_custom_range.py
@@ -1,15 +1,27 @@
 class custom_range:
-    def __init__(self, start, end):
-        self.end = end
+    def __init__(self, start, end, step=None):
         self.start = start
+        self.end = end
+        self.step = step
+        self.counter = 0
+        self.incrementator = 1
+
+        if step and step < 0:
+            self.start, self.end = self.end, self.start
+            self.incrementator = -1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.start <= self.end:
-            output = self.start
-            self.start += 1
-            return output
+        if self.incrementator < 0:
+            if self.start >= self.end:
+                temp = self.start
+                self.start += self.incrementator
+                return temp
+        else:
+            if self.start <= self.end:
+                temp = self.start
+                self.start += self.incrementator
+                return temp
         raise StopIteration()
-
