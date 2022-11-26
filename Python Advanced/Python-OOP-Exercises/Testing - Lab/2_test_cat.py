521215826ab74b6a5c85a512dfe26c4a2bd4f2ed
ceo-py <sfmf@abv.bg>
2022-11-26 12:31:14 +0200

    ref

---
+++ Python Advanced/Python-OOP-Exercises/Testing - Lab/2_test_cat.py
@@ -0,0 +1,45 @@
+from unittest import TestCase, main
+# from project.cat import Cat
+
+
+class TestGunitSquad(TestCase):
+
+    def setUp(self) -> None:
+        self.cat = Cat("Test")
+
+    def test_successful_initial(self):
+        self.assertEqual("Test", self.cat.name)
+        self.assertEqual(False, self.cat.fed)
+        self.assertEqual(False, self.cat.sleepy)
+        self.assertEqual(0, self.cat.size)
+
+    def test_unsuccessful_eat_raise_exception(self):
+        self.cat.fed = True
+
+        with self.assertRaises(Exception) as ex:
+            self.cat.eat()
+
+        self.assertEqual('Already fed.', str(ex.exception))
+
+    def test_successful_eat(self):
+        self.cat.eat()
+
+        self.assertEqual(True, self.cat.fed)
+        self.assertEqual(True, self.cat.sleepy)
+        self.assertEqual(1, self.cat.size)
+
+    def test_unsuccessful_sleep_raise_exception(self):
+        with self.assertRaises(Exception) as ex:
+            self.cat.sleep()
+
+        self.assertEqual('Cannot sleep while hungry', str(ex.exception))
+
+    def test_successful_sleep(self):
+        self.cat.fed = True
+        self.cat.sleepy = True
+        self.cat.sleep()
+        self.assertEqual(False, self.cat.sleepy)
+
+
+if __name__ == "__main__":
+    main()
