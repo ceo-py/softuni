95909416429b4b327810da0b11a8c14ff12f5051
ceo-py <sfmf@abv.bg>
2022-11-27 01:02:37 +0200

    ref

--- Python Advanced/Python-OOP-Exercises/First Steps in OOP - Lab/2_scope_mess.py
+++ Python Advanced/Python-OOP-Exercises/First Steps in OOP - Lab/2_scope_mess.py
@@ -1,6 +1,24 @@
-class Book:
-    def __init__(self, name, author, pages):
-        self.name = name
-        self.author = author
-        self.pages = int(pages)
+x = "global"
 
+
+def outer():
+    x = "local"
+
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+
+    def change_global():
+        global x
+        x = "global: changed!"
+
+    print("outer:", x)
+    inner()
+    print("outer:", x)
+    change_global()
+
+
+print(x)
+outer()
+print(x)
