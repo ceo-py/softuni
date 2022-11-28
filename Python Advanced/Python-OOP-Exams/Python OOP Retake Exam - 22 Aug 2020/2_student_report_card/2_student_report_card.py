30e83cd9030a06e11fbb7162d1824787745be543
ceo-py <sfmf@abv.bg>
2022-11-28 18:22:14 +0200

    ref

--- Python Advanced/Python-OOP-Exams/Python OOP Retake Exam - 22 Aug 2020/2_student_report_card/2_student_report_card.py
+++ Python Advanced/Python-OOP-Exams/Python OOP Retake Exam - 22 Aug 2020/2_student_report_card/2_student_report_card.py
@@ -12,57 +12,66 @@ class TestGunitSquad(TestCase):
         self.assertEqual(12, self.student_report_card.school_year)
         self.assertEqual({}, self.student_report_card.grades_by_subject)
 
-    def test_unsuccessful_student_name_setter_raise_value_error(self):
+    def test_unsuccessful_student_name_setter_raise_value_error_empty_string(self):
         with self.assertRaises(ValueError) as ve:
             self.student_report_card.student_name = ""
 
         self.assertEqual("Student Name cannot be an empty string!", str(ve.exception))
 
+    def test_successful_year(self):
+        self.student_report_card.school_year = 1
 
-    def test_unsuccessful_school_year_setter_raise_value_error(self):
+        self.assertEqual(1, self.student_report_card.school_year)
+
+    def test_unsuccessful_school_year_setter_raise_value_error_less_than_1_and_more_than_12(self):
         with self.assertRaises(ValueError) as ve:
             self.student_report_card.school_year = 13
 
         self.assertEqual("School Year must be between 1 and 12!", str(ve.exception))
 
-    def test_successful_add_grade_to_dict(self):
-        self.student_report_card.add_grade("Python", 6)
-        self.student_report_card.add_grade("R", 6)
+    def test_successful_add_grade(self):
+        self.student_report_card.add_grade("Python", 6.00)
 
-        self.assertEqual({"Python": [6], "R": [6]}, self.student_report_card.grades_by_subject)
-        self.student_report_card.add_grade("Python", 5.50)
+        self.assertEqual({"Python": [6.00]}, self.student_report_card.grades_by_subject)
+        self.student_report_card.add_grade("Python", 6.00)
+        self.assertEqual({"Python": [6.00, 6.00]}, self.student_report_card.grades_by_subject)
 
-        self.assertEqual({"Python": [6, 5.50], "R": [6]}, self.student_report_card.grades_by_subject)
-
-    def test_successful_average_grade_by_subject_return_string(self):
-        self.student_report_card.add_grade("Python", 6)
-        self.student_report_card.add_grade("R", 6)
-        self.student_report_card.add_grade("Python", 5.50)
+    def test_successful_average_grade_by_subject(self):
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Fund", 6.00)
         result = self.student_report_card.average_grade_by_subject()
 
-        self.assertEqual("Python: 5.75\n"
-                         "R: 6.00", result)
+        self.assertEqual([6.0, 6.0], self.student_report_card.grades_by_subject["P - Basic"])
+        self.assertEqual([6.0], self.student_report_card.grades_by_subject["P - Fund"])
+        self.assertEqual(1, len(self.student_report_card.grades_by_subject["P - Fund"]))
+        self.assertEqual("P - Basic: 6.00\n"
+                         "P - Fund: 6.00", result)
 
-    def test_successful_average_grade_for_all_subjects_return_string(self):
-        self.student_report_card.add_grade("Python", 6)
-        self.student_report_card.add_grade("R", 6)
-        self.student_report_card.add_grade("Python", 5.50)
+    def test_successful_average_grade_for_all_subjects(self):
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Fund", 6.00)
 
         result = self.student_report_card.average_grade_for_all_subjects()
 
-        self.assertEqual("Average Grade: 5.83", result)
+        self.assertEqual("Average Grade: 6.00", result)
+        self.assertEqual([6.0], self.student_report_card.grades_by_subject["P - Fund"])
+        self.assertEqual([6.0, 6.0], self.student_report_card.grades_by_subject["P - Basic"])
 
     def test__repr__(self):
-        self.student_report_card.add_grade("Python", 6)
-        self.student_report_card.add_grade("R", 6)
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Basic", 6.00)
+        self.student_report_card.add_grade("P - Fund", 6.00)
+
+        result = self.student_report_card.__repr__()
 
         self.assertEqual("Name: Joro\n"
                          "Year: 12\n"
                          "----------\n"
-                         "Python: 6.00\n"
-                         "R: 6.00\n"
+                         f"{self.student_report_card.average_grade_by_subject()}\n"
                          "----------\n"
-                         "Average Grade: 6.00", self.student_report_card.__repr__())
+                         f"{self.student_report_card.average_grade_for_all_subjects()}", result)
 
 
 if __name__ == "__main__":
