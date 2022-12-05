e8a9d67688c2bfba6e4f93083e247b33ed7447b4
ceo-py <sfmf@abv.bg>
2022-12-05 21:15:48 +0200

    ref

--- Softuniada/Softuniada 2022/4_spiral_matrix.py
+++ Softuniada/Softuniada 2022/4_spiral_matrix.py
@@ -3,14 +3,48 @@ matrix = [input().split() for _ in range(rows)]
 
 while True:
     try:
-        [print(matrix[0].pop(0), end = " ") for _ in range(len(matrix[0]))]
+        [print(matrix[0].pop(0), end=" ") for _ in range(len(matrix[0]))]
         del matrix[0]
-        [print(matrix[r].pop(), end = " ") for r in range(len(matrix))]
+        [print(matrix[r].pop(), end=" ") for r in range(len(matrix))]
         [print(matrix[len(matrix) - 1].pop(), end=" ") for _ in range(len(matrix[0]))]
         del matrix[-1]
         [print(matrix[r].pop(0), end=" ") for r in range(len(matrix) - 1, -1, -1)]
     except IndexError:
         break
+
+
+
+
+# def print_matrix_spiral(matrix, rows, cols):
+#     # helper function to print a matrix in spiral way
+#     def print_spiral(matrix, r1, r2, c1, c2):
+#         if r1 > r2 or c1 > c2:
+#             return
+#
+#         for c in range(c1, c2 + 1):
+#             print(matrix[r1][c], end=" ")
+#
+#         for r in range(r1 + 1, r2 + 1):
+#             print(matrix[r][c2], end=" ")
+#
+#         if r1 < r2 and c1 < c2:
+#             for c in range(c2 - 1, c1, -1):
+#                 print(matrix[r2][c], end=" ")
+#             for r in range(r2, r1, -1):
+#                 print(matrix[r][c1], end=" ")
+#
+#         print_spiral(matrix, r1 + 1, r2 - 1, c1 + 1, c2 - 1)
+#
+#     # main function to print the matrix in spiral way
+#     if rows < 1 or cols < 1:
+#         return
+#     print_spiral(matrix, 0, rows - 1, 0, cols - 1)
+#
+#
+# rows, cols = [int(input()) for _ in range(2)]
+# matrix = [input().split() for _ in range(rows)]
+# print_matrix_spiral(matrix, rows, cols)
+
 
 
 
