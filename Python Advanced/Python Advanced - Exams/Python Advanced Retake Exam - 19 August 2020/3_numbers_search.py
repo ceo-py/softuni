349d283c31da460548348bd27a870c4d4108ba0c
ceo-py <sfmf@abv.bg>
2022-12-12 18:31:51 +0200

    ref

--- Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 19 August 2020/3_numbers_search.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Retake Exam - 19 August 2020/3_numbers_search.py
@@ -4,6 +4,21 @@ def numbers_searching(*args):
 
 
 
+
+#
+#
+# def numbers_searching(*args):
+#     number_not_in, duplicate_numbers = 0, []
+#     min_number, max_number = min(args), max(args)
+#     for n in range(min_number, max_number + 1):
+#         if n not in args:
+#             number_not_in = n
+#         if args.count(n) > 1 and n not in duplicate_numbers:
+#             duplicate_numbers.append(n)
+#     duplicate_numbers.sort()
+#     return [number_not_in, duplicate_numbers]
+#
+
 #
 #
 # def numbers_searching(*args):
