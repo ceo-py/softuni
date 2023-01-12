7bd47d7ed462a1286b7ec7cb34cdfbe2e3764974
ceo-py <sfmf@abv.bg>
2023-01-12 11:08:48 +0200

    ref

--- Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/9_easter_bread.py
+++ Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/9_easter_bread.py
@@ -14,3 +14,28 @@ for number_breads in range(1, int(budget) + 1):
         print(
             f"You made {number_breads - 1} loaves of Easter bread! Now you have {colored_eggs} eggs and {budget:.2f}BGN left.")
         break
+
+
+
+
+# budget = float(input())
+# flour_price = float(input())
+# eggs_price = flour_price * 0.75
+# milk_price = (flour_price * 1.25) / 4
+# loaf_price = flour_price + eggs_price + milk_price
+#
+# colored_eggs = 0
+# bread_loaves = 0
+#
+# while True:
+#
+#     if budget <= loaf_price:
+#         print(f"You made {bread_loaves} loaves of Easter bread! Now you have {colored_eggs} eggs and {budget:.2f}BGN left.")
+#         break
+#
+#     budget -= loaf_price
+#     bread_loaves += 1
+#     colored_eggs += 3
+#
+#     if bread_loaves % 3 == 0:
+#         colored_eggs -= bread_loaves - 2
