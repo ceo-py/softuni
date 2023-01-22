33767966aed544adadd6801f50056c426f1c3ade
ceo-py <sfmf@abv.bg>
2023-01-22 20:30:14 +0200

    ref

--- Python Basics/Conditional Statements Advanced - Lab/11_fruit_shop.py
+++ Python Basics/Conditional Statements Advanced - Lab/11_fruit_shop.py
@@ -11,11 +11,10 @@ pineapple_price = 5.50
 grapes_price = 3.85
 price = 0
 
+week_days = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"
+correct_fruits = "banana, apple, grapefruit, kiwi, grapes, orange, pineapple"
 
-if day_of_week != "Monday" and day_of_week != "Tuesday" and day_of_week != "Wednesday" and \
-        day_of_week != "Thursday" and day_of_week != "Friday" and day_of_week != "Saturday" \
-        and day_of_week != "Sunday" or fruit != 'banana' and fruit != 'apple' and fruit != 'grapefruit' \
-        and fruit != 'kiwi' and fruit != 'grapes' and fruit != 'orange' and fruit != 'pineapple':
+if day_of_week not in week_days or fruit not in correct_fruits:
     print('error')
 else:
     if day_of_week == 'Sunday' or day_of_week == 'Saturday':
@@ -47,38 +46,36 @@ else:
 
     elif fruit == 'pineapple':
         price = pineapple_price
+
     total_price = quantity * price
     print(f'{total_price:.2f}')
 
 
-#
-#
-#
-#
-#
-#
-# type_food = input()
-# days_of_the_week = input()
+
+
+
+# fruit = input()
+# day_of_week = input()
 # quantity = float(input())
 #
-# prices = {
+# fruits = {
 #     "banana": [2.50, 2.70],
 #     "apple": [1.20, 1.25],
-#     "orange": [0.85, 0.90],
 #     "grapefruit": [1.45, 1.60],
 #     "kiwi": [2.70, 3.00],
-#     "pineapple": [5.50, 5.60],
 #     "grapes": [3.85, 4.20],
-#     "weekend": 1,
-#     "week": 0}
+#     "orange": [0.85, 0.90],
+#     "pineapple": [5.50, 5.60],
+#     'Sunday': 1,
+#     'Saturday': 1,
+# }
 #
+# week_days = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"
+# correct_fruits = "banana, apple, grapefruit, kiwi, grapes, orange, pineapple"
 #
-# day_check = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-# if days_of_the_week in day_check and type_food in prices:
-#     if days_of_the_week in day_check[:5]:
-#         total = prices[type_food][prices["week"]] * quantity
-#     else:
-#         total = prices[type_food][prices["weekend"]] * quantity
-#     print(f"{total:.2f}")
+# if day_of_week not in week_days or fruit not in fruits:
+#     print('error')
 # else:
-#     print("error")
+#     price = fruits[fruit][fruits.get(day_of_week, 0)]
+#     total_price = quantity * price
+#     print(f'{total_price:.2f}')
