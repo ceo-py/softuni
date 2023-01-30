188305d066104e180498b151990059e79a92ea24
ceo-py <sfmf@abv.bg>
2023-01-30 21:57:22 +0200

    ref

--- Python Basics/For Loop - Lab/8_number_sequence.py
+++ Python Basics/For Loop - Lab/8_number_sequence.py
@@ -1,9 +1,26 @@
-input_number = int(input())
+num = int(input())
 
-numbers = list()
-for n in range(0, input_number):
-    numbers_to_sum = int(input())
-    numbers.append(numbers_to_sum)
+max_number = float('-inf')
+min_number = float('inf')
+for i in range(num):
+    number = int(input())
+    if number >= max_number:
+        max_number = number
+    if number <= min_number:
+        min_number = number
+print(f"Max number: {max_number}")
+print(f"Min number: {min_number}")
 
-numbers.sort()
-print(f"Max number: {numbers[-1]}\nMin number: {numbers[0]}")
+
+
+#
+#
+# input_number = int(input())
+#
+# numbers = list()
+# for n in range(0, input_number):
+#     numbers_to_sum = int(input())
+#     numbers.append(numbers_to_sum)
+#
+# numbers.sort()
+# print(f"Max number: {numbers[-1]}\nMin number: {numbers[0]}")
