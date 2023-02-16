bfc10c7e26341fb2133c1f2208bbb55561902bd6
ceo-py <sfmf@abv.bg>
2023-02-16 13:20:54 +0200

    ref

--- PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/1_basketball_equipment.py
+++ PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/1_basketball_equipment.py
@@ -1,10 +1,10 @@
-year_tax_basketball = int(input())
+yearly_tax = int(input())
 
-sneakers_price = year_tax_basketball - (year_tax_basketball * 0.40)
-gear = sneakers_price - (sneakers_price * 0.20)
-ball = gear / 4
-accesories = ball / 5
+basketball_sneakers = yearly_tax * 0.60
+basketball_equip = basketball_sneakers * 0.80
+basketball_ball = basketball_equip / 4
+basketball_accessories = basketball_ball / 5
 
-total = year_tax_basketball + sneakers_price + gear + ball + accesories
+total_sum_needed = yearly_tax + basketball_accessories + basketball_equip + basketball_ball + basketball_sneakers
 
-print(f"{total:.2f}")
+print(f"{total_sum_needed:.2f}")
