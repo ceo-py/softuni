b4d7f3bf17a0488173738c18a3bad9a689af1ad3
ceo-py <sfmf@abv.bg>
2023-02-16 14:07:19 +0200

    ref

--- PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/3_gymnastics.py
+++ PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/3_gymnastics.py
@@ -1,51 +1,49 @@
 country = input()
-type_device = input()
+apparatus = input()
 
-total_score = 0
+difficulty = 0
+turn = 0
 
-if country == "Bulgaria" and type_device == "ribbon":
-    total_score = 9.600 + 9.400
-    points_left = 20 - total_score
-    score = (points_left / 20) * 100
+if apparatus == "ribbon":
+    if country == "Russia":
+        difficulty = 9.100
+        turn = 9.400
+    elif country == "Bulgaria":
+        difficulty = 9.600
+        turn = 9.400
+    elif country == "Italy":
+        difficulty = 9.200
+        turn = 9.500
 
-elif country == "Bulgaria" and type_device == "hoop":
-    total_score = 9.550 + 9.750
-    points_left = 20 - total_score
-    score = (points_left / 20) * 100
+elif apparatus == "hoop":
+    if country == "Russia":
+        difficulty = 9.300
+        turn = 9.800
+    elif country == "Bulgaria":
+        difficulty = 9.550
+        turn = 9.750
+    elif country == "Italy":
+        difficulty = 9.450
+        turn = 9.350
 
-elif country == "Bulgaria" and type_device == "rope":
-    total_score = 9.500 + 9.400
-    points_left = 20 - total_score
-    score = (points_left / 20) * 100
+elif apparatus == "rope":
+    if country == "Russia":
+        difficulty = 9.600
+        turn = 9.000
+    elif country == "Bulgaria":
+        difficulty = 9.500
+        turn = 9.400
+    elif country == "Italy":
+        difficulty = 9.700
+        turn = 9.150
 
-elif country == "Russia" and type_device == "ribbon":
-    total_score = 9.100 + 9.400
-    points_left = 20 - total_score
-    score = (points_left / 20) * 100
+total_score = difficulty + turn
+difference_to_max_score = 20 - total_score
 
-elif country == "Russia" and type_device == "hoop":
-    total_score = 9.300 + 9.800
-    points_left = 20 - total_score
-    score = (points_left / 20) * 100
+print(f"The team of {country} get {total_score:.3f} on {apparatus}.")
+print(f"{(difference_to_max_score / 20) * 100:.2f}%")
 
-elif country == "Russia" and type_device == "rope":
-    total_score = 9.600 + 9.000
-    points_left = 20 - total_score
-    score = (points_left / 20) * 100
 
-elif country == "Italy" and type_device == "ribbon":
-    total_score = 9.200 + 9.500
-    points_left = 20 - total_score
-    score = (points_left / 20) * 100
 
-elif country == "Italy" and type_device == "hoop":
-    total_score = 9.450 + 9.350
-    points_left = 20 - total_score
-    score = (points_left / 20) * 100
 
-elif country == "Italy" and type_device == "rope":
-    total_score = 9.700 + 9.150
-    points_left = 20 - total_score
-    score = (points_left / 20) * 100
 
-print(f"The team of {country} get {total_score:.3f} on {type_device}.\n{score:.2f}%")
