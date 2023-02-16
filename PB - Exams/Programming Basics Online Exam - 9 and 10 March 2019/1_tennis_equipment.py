0290fbc7c4c1df53f53eb2c66967bf3111b7b517
ceo-py <sfmf@abv.bg>
2023-02-16 13:14:40 +0200

    ref

--- PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/1_tennis_equipment.py
+++ PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/1_tennis_equipment.py
@@ -7,7 +7,7 @@ number_tennis_sneakers = int(input())
 rackets_total_price = price_tennis_racket * total_tennis_racket
 one_sneakers_price = price_tennis_racket / 6
 all_sneakers_price = one_sneakers_price * number_tennis_sneakers
-total_price_equipment = rackets_total_price + all_sneakers_price + (rackets_total_price + all_sneakers_price) * 0.2
+total_price_equipment = (rackets_total_price + all_sneakers_price) * 1.2
 
 djokovic_price = total_price_equipment / 8
 sponsors_price = total_price_equipment * (7 / 8)
