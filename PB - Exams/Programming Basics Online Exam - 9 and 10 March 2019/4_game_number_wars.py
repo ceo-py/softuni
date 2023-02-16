db07df84fa82b0bdc91dbdb3ca5ff9c5f5060169
ceo-py <sfmf@abv.bg>
2023-02-16 14:48:12 +0200

    ref

--- PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/4_game_number_wars.py
+++ PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/4_game_number_wars.py
@@ -3,34 +3,83 @@ player_two = input()
 
 player_one_score = 0
 player_two_score = 0
+player_one_card = input()
 
-while True:
+while player_one_card != "End of game":
+    player_one_card = int(player_one_card)
+    player_two_card = int(input())
 
-    card_player_one = input()
+    difference_score = abs(player_one_card - player_two_card)
 
-    if card_player_one == "End of game":
-        print(f"{player_one} has {player_one_score} points\n{player_two} has {player_two_score} points")
-        break
+    if player_one_card > player_two_card:
+        player_one_score += difference_score
+
+    elif player_one_card < player_two_card:
+        player_two_score += difference_score
 
     else:
-        card_player_one = int(card_player_one)
+        player_one_card = int(input())
+        player_two_card = int(input())
 
-    card_player_two = int(input())
+        if player_one_card > player_two_card:
+            winner = player_one
+            winner_points = player_one_score + difference_score
 
-    if card_player_one > card_player_two:
-        player_one_score += card_player_one - card_player_two
+        elif player_one_card < player_two_card:
+            winner = player_two
+            winner_points = player_two_score + difference_score
 
-    elif card_player_two > card_player_one:
-        player_two_score += card_player_two - card_player_one
+        print("Number wars!")
+        print(f"{winner} is winner with {winner_points} points")
+        break
+    player_one_card = input()
 
-    elif card_player_two == card_player_one:
-        card_player_one = int(input())
-        card_player_two = int(input())
+else:
+    print(f"{player_one} has {player_one_score} points")
+    print(f"{player_two} has {player_two_score} points")
 
-        if card_player_one > card_player_two:
-            print(f"Number wars!\n{player_one} is winner with {player_one_score} points")
-            break
 
-        else:
-            print(f"Number wars!\n{player_two} is winner with {player_two_score} points")
-            break
+
+
+
+
+
+
+
+
+# player_one = input()
+# player_two = input()
+#
+# player_one_score = 0
+# player_two_score = 0
+#
+# while True:
+#
+#     card_player_one = input()
+#
+#     if card_player_one == "End of game":
+#         print(f"{player_one} has {player_one_score} points\n{player_two} has {player_two_score} points")
+#         break
+#
+#     else:
+#         card_player_one = int(card_player_one)
+#
+#     card_player_two = int(input())
+#
+#     if card_player_one > card_player_two:
+#         player_one_score += card_player_one - card_player_two
+#
+#     elif card_player_two > card_player_one:
+#         player_two_score += card_player_two - card_player_one
+#
+#     elif card_player_two == card_player_one:
+#         card_player_one = int(input())
+#         card_player_two = int(input())
+#
+#         if card_player_one > card_player_two:
+#             print(f"Number wars!\n{player_one} is winner with {player_one_score} points")
+#             break
+#
+#         else:
+#             print(f"Number wars!\n{player_two} is winner with {player_two_score} points")
+#             break
