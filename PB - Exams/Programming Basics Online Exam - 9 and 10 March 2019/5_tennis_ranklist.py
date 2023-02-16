253738cfc98971def2a4ede09391b51a119752c8
ceo-py <sfmf@abv.bg>
2023-02-16 15:14:13 +0200

    ref

--- PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/5_tennis_ranklist.py
+++ PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/5_tennis_ranklist.py
@@ -1,24 +1,59 @@
-import math
-
-tournaments_count = int(input())
+number_tournaments = int(input())
 starting_points = int(input())
 
-tournament_points = {
-    "W": 2000,
-    "F": 1200,
-    "SF": 720
-}
-tournament_win_count = 0
-total_points = starting_points
+tournaments_points = 0
+wins = 0
 
-for torunament in range(0, tournaments_count):
-    tournament_finish = input()
-    total_points += + tournament_points[tournament_finish]
-    if tournament_finish == "W":
-        tournament_win_count += 1
+for tournament in range(number_tournaments):
+    tournament_output = input()
 
-average_points = (total_points - starting_points) / tournaments_count
-tournament_win_percent = (tournament_win_count / tournaments_count) * 100
-print(f"Final points: {total_points}")
-print(f"Average points: {math.floor(average_points)}")
-print(f"{tournament_win_percent:.2f}%")
+    if tournament_output == "W":
+        tournaments_points += 2000
+        wins += 1
+
+    elif tournament_output == "F":
+        tournaments_points += 1200
+
+    elif tournament_output == "SF":
+        tournaments_points += 720
+
+print(f"Final points: {starting_points + tournaments_points}")
+print(f"Average points: {int(tournaments_points / number_tournaments)}")
+print(f"{(wins / number_tournaments) * 100:.2f}%")
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# import math
+#
+# tournaments_count = int(input())
+# starting_points = int(input())
+#
+# tournament_points = {
+#     "W": 2000,
+#     "F": 1200,
+#     "SF": 720
+# }
+# tournament_win_count = 0
+# total_points = starting_points
+#
+# for torunament in range(0, tournaments_count):
+#     tournament_finish = input()
+#     total_points += tournament_points[tournament_finish]
+#     if tournament_finish == "W":
+#         tournament_win_count += 1
+#
+# average_points = (total_points - starting_points) / tournaments_count
+# tournament_win_percent = (tournament_win_count / tournaments_count) * 100
+# print(f"Final points: {total_points}")
+# print(f"Average points: {math.floor(average_points)}")
+# print(f"{tournament_win_percent:.2f}%")
