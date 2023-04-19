9bc66afd058394e4e850e2e7fe64755f3e8e2ec5
ceo-py <sfmf@abv.bg>
2023-04-19 12:40:58 +0300

    added another solution

--- PB - Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/2_safari.py
+++ PB - Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/2_safari.py
@@ -6,10 +6,10 @@ fuel_price = fuel_needed * 2.10
 total_price = fuel_price + 100
 
 if day_of_the_week == "Saturday":
-    total_price += - (total_price * 0.10)
+    total_price *= 0.90
 
 elif day_of_the_week == "Sunday":
-    total_price += - (total_price * 0.20)
+    total_price *= 0.80
 
 money_left = abs(budget - total_price)
 
@@ -18,3 +18,29 @@ if budget >= total_price:
 
 else:
     print(f"Not enough money! Money needed: {money_left:.2f} lv.")
+
+
+
+
+
+
+# budget = float(input())
+# fuel_needed = float(input())
+# day_of_the_week = input()
+#
+# fuel_price = fuel_needed * 2.10
+# total_price = fuel_price + 100
+#
+# if day_of_the_week == "Saturday":
+#     total_price += - (total_price * 0.10)
+#
+# elif day_of_the_week == "Sunday":
+#     total_price += - (total_price * 0.20)
+#
+# money_left = abs(budget - total_price)
+#
+# if budget >= total_price:
+#     print(f"Safari time! Money left: {money_left:.2f} lv. ")
+#
+# else:
+#     print(f"Not enough money! Money needed: {money_left:.2f} lv.")
