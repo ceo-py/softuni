952ced00b882bf6dc927d6197ac7de58887f0180
ceo-py <sfmf@abv.bg>
2023-04-19 13:30:02 +0300

    added another solution

--- PB - Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/6_vet_parking.py
+++ PB - Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/6_vet_parking.py
@@ -61,8 +61,8 @@ print(f"Total: {total_tax:.2f} leva")
 # hours_per_day = int(input())
 #
 #
-# [print(f"Day: {day} - {sum([(2.5 if day % 2 == 0 and hour % 2 != 0 else 1.25 if day % 2 != 0 and hour % 2 == 0 else 1) for hour in range(1, hours_per_day + 1)]):.2f} leva") for day in range(1, number_days + 1)]
+# [print(f"Day: {day} - {sum((2.5 if day % 2 == 0 and hour % 2 != 0 else 1.25 if day % 2 != 0 and hour % 2 == 0 else 1) for hour in range(1, hours_per_day + 1)):.2f} leva") for day in range(1, number_days + 1)]
 #
-# print(f"Total: {sum([(2.5 if day % 2 == 0 and hour % 2 != 0 else 1.25 if day % 2 != 0 and hour % 2 == 0 else 1) for day in range(1, number_days + 1) for hour in range(1, hours_per_day + 1)]):.2f} leva")
-
-
+# print(f"Total: {sum((2.5 if day % 2 == 0 and hour % 2 != 0 else 1.25 if day % 2 != 0 and hour % 2 == 0 else 1) for day in range(1, number_days + 1) for hour in range(1, hours_per_day + 1)):.2f} leva")
+#
+#
