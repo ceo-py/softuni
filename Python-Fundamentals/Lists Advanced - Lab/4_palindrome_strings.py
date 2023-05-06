725a23a7947e350bf9bafa4e02fba0f9759fcdfd
ceo-py <sfmf@abv.bg>
2023-05-06 13:11:07 +0300

    added another solution

--- Python-Fundamentals/Lists Advanced - Lab/4_palindrome_strings.py
+++ Python-Fundamentals/Lists Advanced - Lab/4_palindrome_strings.py
@@ -1,12 +1,29 @@
 main_list = input().split(" ")
-
 searched_palindrome = input()
 
-find_words = list()
-
-for word in main_list:
-    if word == word[::-1]:
-        find_words.append(word)
+find_words = [word for word in main_list if word == word[::-1]]
 
 print(f"{find_words}")
 print(f"Found palindrome {find_words.count(searched_palindrome)} times")
+
+
+
+
+
+
+
+
+#
+# main_list = input().split(" ")
+#
+# searched_palindrome = input()
+#
+# find_words = list()
+#
+# for word in main_list:
+#     if word == word[::-1]:
+#         find_words.append(word)
+#
+# print(f"{find_words}")
+# print(f"Found palindrome {find_words.count(searched_palindrome)} times")
+#
