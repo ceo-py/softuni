33a41285dbc564afc3d38171546aa40a4f81bf95
ceo-py <sfmf@abv.bg>
2023-05-06 13:09:06 +0300

    added another solution

--- Python-Fundamentals/Lists Advanced - Lab/2_trains.py
+++ Python-Fundamentals/Lists Advanced - Lab/2_trains.py
@@ -4,39 +4,27 @@ command = input()
 
 
 def add_people(people):
-    train[-1] = train[-1] + people
-
+    train[-1] += people
 
 def insert_people(wagon, number_people):
-    train[wagon] = train[wagon] + number_people
-
+    train[wagon] += number_people
 
 def leave_people(wagon, number_people):
-    train[wagon] = train[wagon] - number_people
+    train[wagon] -= number_people
 
+
+commands = {
+    "add": add_people,
+    "insert": insert_people,
+    "leave": leave_people
+}
 
 while command != "End":
     command, *info = [int(x) if x.isdigit() else x for x in command.split()]
-
-    if "add" in command:
-        people = info[0]
-        add_people(people)
-    else:
-        index, people = info
-        if "insert" in command:
-            insert_people(index, people)
-
-        elif "leave" in command:
-            leave_people(index, people)
-
+    commands[command](*info)
     command = input()
 
 print(train)
-
-
-
-
-
 
 
 
