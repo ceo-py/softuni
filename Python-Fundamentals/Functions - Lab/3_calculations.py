f349d5e90ea900b9d05d29112a67df73edf1375c
ceo-py <sfmf@abv.bg>
2023-05-07 12:31:30 +0300

    added another solution

--- Python-Fundamentals/Functions - Lab/3_calculations.py
+++ Python-Fundamentals/Functions - Lab/3_calculations.py
@@ -2,23 +2,39 @@ operation = input()
 number_one = int(input())
 number_two = int(input())
 
-
 def show_result(type_operation, n_one, n_two):
-    if type_operation == "multiply":
-        result = n_one * n_two
-        return result
-
-    elif type_operation == "divide":
-        result = n_one / n_two
-        return int(result)
-
-    elif type_operation == "add":
-        result = n_one + n_two
-        return result
-
-    elif type_operation == "subtract":
-        result = n_one - n_two
-        return result
+    operator = {"multiply": '*',
+                "divide": '/',
+                "add": '+',
+                "subtract": '-'}
+    return int(eval(f'{n_one} {operator[type_operation]} {n_two}'))
 
 
 print(show_result(operation, number_one, number_two))
+
+
+
+
+
+# operation = input()
+# number_one = int(input())
+# number_two = int(input())
+#
+#
+# def show_result(type_operation, n_one, n_two):
+#     if type_operation == "multiply":
+#         result = n_one * n_two
+#
+#     elif type_operation == "divide":
+#         result = n_one / n_two
+#
+#     elif type_operation == "add":
+#         result = n_one + n_two
+#
+#     elif type_operation == "subtract":
+#         result = n_one - n_two
+#
+#     return int(result)
+#
+#
+# print(show_result(operation, number_one, number_two))
