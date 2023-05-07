f6bb6d483b489a1c8913a180aa986781b5c4803e
ceo-py <sfmf@abv.bg>
2023-05-07 12:33:29 +0300

    added another solution

--- Python-Fundamentals/Functions - Lab/6_calculate_rectangle_area.py
+++ Python-Fundamentals/Functions - Lab/6_calculate_rectangle_area.py
@@ -1,10 +1,25 @@
-width_rectangle = int(input())
-height_rectangle = int(input())
+width_rectangle = input()
+height_rectangle = input()
 
 
 def calc_area_rectangle(a, b):
-    result = a * b
-    return result
+    return eval(f'{a} * {b}')
 
 
 print(calc_area_rectangle(width_rectangle, height_rectangle))
+
+
+
+
+#
+#
+# width_rectangle = int(input())
+# height_rectangle = int(input())
+#
+#
+# def calc_area_rectangle(a, b):
+#     result = a * b
+#     return result
+#
+#
+# print(calc_area_rectangle(width_rectangle, height_rectangle))
