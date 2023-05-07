09914c65b898d0553645ad2a58c7b92c61146df0
ceo-py <sfmf@abv.bg>
2023-05-07 12:22:06 +0300

    added another solution

--- Python-Fundamentals/Functions - Exercise/9_password_validator.py
+++ Python-Fundamentals/Functions - Exercise/9_password_validator.py
@@ -1,17 +1,16 @@
-errors = []
-
-
 def password_validation(password):
+    errors = []
     if not 6 <= len(password) <= 10:
         errors.append("Password must be between 6 and 10 characters")
     if not password.isalnum():
         errors.append("Password must consist only of letters and digits")
     if sum(1 for x in password if x.isdigit()) < 2:
         errors.append("Password must have at least 2 digits")
+    return errors
 
 
 current_password = input()
-password_validation(current_password)
+errors = password_validation(current_password)
 if errors:
     [print(show) for show in errors]
 else:
