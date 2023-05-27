46b039480a6d10e842ec9f53b295e6a4b3df3348
ceo-py <sfmf@abv.bg>
2023-05-27 12:46:34 +0300

    added another solution

--- Python Basics/While Loop - Exercise/1_old_books.py
+++ Python Basics/While Loop - Exercise/1_old_books.py
@@ -12,3 +12,24 @@ while True:
         print(f"You checked {book_check_count} books and found it.")
         break
     book_check_count += 1
+
+
+
+# book = input()
+#
+# count = 0
+# input_line = input()
+# flag = False
+#
+# while input_line != 'No More Books':
+#     if input_line == book:
+#         flag = True
+#         break
+#     count += 1
+#     input_line = input()
+#
+# if flag == True:
+#     print(f'You checked {count} books and found it.')
+# else:
+#     print('The book you search is not here!')
+#     print(f'You checked {count} books.')
