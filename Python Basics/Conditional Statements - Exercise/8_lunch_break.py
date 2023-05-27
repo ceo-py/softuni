c40711c63e73ffe0d0b922732e4cddb895764455
ceo-py <sfmf@abv.bg>
2023-05-27 12:19:15 +0300

    added another solution

--- Python Basics/Conditional Statements - Exercise/8_lunch_break.py
+++ Python Basics/Conditional Statements - Exercise/8_lunch_break.py
@@ -16,3 +16,25 @@ else:
     time_left = serial_len - time_left
     print(f"You don't have enough time to watch {name_serial}, "
           f"you need {math.ceil(time_left)} more minutes.")
+
+
+
+
+# from math import ceil
+#
+# movie = str(input())
+# movie_time = int(input())
+# break_time = int(input())
+#
+# lunch_time = break_time / 8
+# relax_time = break_time / 4
+#
+# available_time = break_time - lunch_time - relax_time
+# diff = abs(movie_time - available_time)
+#
+# if movie_time > available_time:
+#     print(f"You don't have enough time to watch {movie}, you need {ceil(diff)} more minutes.")
+# else:
+#     print(f"You have enough time to watch {movie} and left with {ceil(diff)} minutes free time.")
+#
+#
