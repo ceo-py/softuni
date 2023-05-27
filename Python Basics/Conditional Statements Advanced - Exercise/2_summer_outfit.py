9c71341e9e2bf7894192462a9419a387c786c552
ceo-py <sfmf@abv.bg>
2023-05-27 12:21:09 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Exercise/2_summer_outfit.py
+++ Python Basics/Conditional Statements Advanced - Exercise/2_summer_outfit.py
@@ -1,25 +1,69 @@
-tempreture_celsius = int(input())
-weather_time = input()
+degrees = int(input())
+time_of_the_day = input()
 
-outfits_depending_on_time = {
-    "Outfit 10 - 18": ["Sweatshirt", "Shirt", "Shirt"],
-    "Shoes 10 - 18": ["Sneakers", "Moccasins", "Moccasins"],
-    "Outfit 18 - 24": ["Shirt", "T-Shirt", "Shirt"],
-    "Shoes 18 - 24": ["Moccasins", "Sandals", "Moccasins"],
-    "Outfit => 25": ["T-Shirt", "Swim Suit", "Shirt"],
-    "Shoes => 25": ["Sandals", "Barefoot", "Moccasins"],
-    "Morning": 0,"Afternoon": 1,"Evening":2}
+outfit = None
+shoes = None
 
-if 10 <= tempreture_celsius <= 18:
-    outfit = outfits_depending_on_time["Outfit 10 - 18"][outfits_depending_on_time[weather_time]]
-    shoes = outfits_depending_on_time["Shoes 10 - 18"][outfits_depending_on_time[weather_time]]
-elif tempreture_celsius <= 24:
-    outfit = outfits_depending_on_time["Outfit 18 - 24"][outfits_depending_on_time[weather_time]]
-    shoes = outfits_depending_on_time["Shoes 18 - 24"][outfits_depending_on_time[weather_time]]
-elif tempreture_celsius >= 24:
-    outfit = outfits_depending_on_time["Outfit => 25"][outfits_depending_on_time[weather_time]]
-    shoes = outfits_depending_on_time["Shoes => 25"][outfits_depending_on_time[weather_time]]
-print(f"It's {tempreture_celsius} degrees, get your {outfit} and {shoes}.")
+if 10 <= degrees <= 18:
+    if time_of_the_day == "Morning":
+        outfit = "Sweatshirt"
+        shoes = "Sneakers"
+    elif time_of_the_day == "Afternoon":
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif time_of_the_day == "Evening":
+        outfit = "Shirt"
+        shoes = "Moccasins"
+
+elif 18 < degrees <= 24:
+    if time_of_the_day == "Morning":
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif time_of_the_day == "Afternoon":
+        outfit = "T-Shirt"
+        shoes = "Sandals"
+    elif time_of_the_day == "Evening":
+        outfit = "T-Shirt"
+        shoes = "Moccasins"
+
+elif degrees >= 25:
+    if time_of_the_day == "Morning":
+        outfit = "T-Shirt"
+        shoes = "Sandals"
+    elif time_of_the_day == "Afternoon":
+        outfit = "Swim Suit"
+        shoes = "Barefoot"
+    elif time_of_the_day == "Evening":
+        outfit = "Shirt"
+        shoes = "Moccasins"
+
+print(f"It's {degrees} degrees, get your {outfit} and {shoes}.")
+
+
+
+
+# tempreture_celsius = int(input())
+# weather_time = input()
+#
+# outfits_depending_on_time = {
+#     "Outfit 10 - 18": ["Sweatshirt", "Shirt", "Shirt"],
+#     "Shoes 10 - 18": ["Sneakers", "Moccasins", "Moccasins"],
+#     "Outfit 18 - 24": ["Shirt", "T-Shirt", "Shirt"],
+#     "Shoes 18 - 24": ["Moccasins", "Sandals", "Moccasins"],
+#     "Outfit => 25": ["T-Shirt", "Swim Suit", "Shirt"],
+#     "Shoes => 25": ["Sandals", "Barefoot", "Moccasins"],
+#     "Morning": 0,"Afternoon": 1,"Evening":2}
+#
+# if 10 <= tempreture_celsius <= 18:
+#     outfit = outfits_depending_on_time["Outfit 10 - 18"][outfits_depending_on_time[weather_time]]
+#     shoes = outfits_depending_on_time["Shoes 10 - 18"][outfits_depending_on_time[weather_time]]
+# elif tempreture_celsius <= 24:
+#     outfit = outfits_depending_on_time["Outfit 18 - 24"][outfits_depending_on_time[weather_time]]
+#     shoes = outfits_depending_on_time["Shoes 18 - 24"][outfits_depending_on_time[weather_time]]
+# elif tempreture_celsius >= 24:
+#     outfit = outfits_depending_on_time["Outfit => 25"][outfits_depending_on_time[weather_time]]
+#     shoes = outfits_depending_on_time["Shoes => 25"][outfits_depending_on_time[weather_time]]
+# print(f"It's {tempreture_celsius} degrees, get your {outfit} and {shoes}.")
 
 
 # tempreture_celsius = int(input())
