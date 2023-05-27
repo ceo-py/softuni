ac10ff7a12ab2b30520430ad5b785e9e7dd2a2a2
ceo-py <sfmf@abv.bg>
2023-05-27 12:42:22 +0300

    added another solution

--- Python Basics/For Loop - Lab/10_odd_even_sum.py
+++ Python Basics/For Loop - Lab/10_odd_even_sum.py
@@ -1,22 +1,81 @@
-number_enter = int(input())
+number_of_lines = int(input())
+even_sum = 0
+odd_sum = 0
 
-even_number = 0
-odd_number = 0
-count = 2
+for i in range(1, number_of_lines + 1):
+    current_number = int(input())
 
-for position in range(number_enter):
-    number = int(input())
-    if (position % 2) != 0:
-        odd_number += number
+    if i % 2 == 0:
+        even_sum += current_number
     else:
-        even_number += number
-    count += 1
+        odd_sum += current_number
 
-if even_number == odd_number:
-    print(f"Yes\nSum = {abs(even_number)}")
+if even_sum == odd_sum:
+
+    print('Yes')
+    print(f'Sum = {even_sum}')
+
 else:
-    total = odd_number - even_number
-    print(f"No\nDiff = {abs(total)}")
+    diff = abs(even_sum - odd_sum)
+    print('No')
+    print(f'Diff = {diff}')
+
+
+
+
+
+
+
+
+
+
+# number_enter = int(input())
+#
+# even_number = 0
+# odd_number = 0
+# count = 2
+#
+# for position in range(number_enter):
+#     number = int(input())
+#     if (position % 2) != 0:
+#         odd_number += number
+#     else:
+#         even_number += number
+#     count += 1
+#
+# if even_number == odd_number:
+#     print(f"Yes\nSum = {abs(even_number)}")
+# else:
+#     total = odd_number - even_number
+#     print(f"No\nDiff = {abs(total)}")
+
+
+
+
+
+
+
+#
+# iteration_count = int(input())
+# sum_odd_nums = 0
+# sum_even_nums = 0
+# for i in range(1, iteration_count + 1):
+#     number = int(input())
+#     if i % 2 == 0:
+#         sum_even_nums += number
+#     else:
+#         sum_odd_nums += number
+# if sum_even_nums == sum_odd_nums:
+#     print('Yes')
+#     print(f'Sum = {sum_even_nums}')
+# else:
+#     difference = abs(sum_even_nums - sum_odd_nums)
+#     print('No')
+#     print(f'Diff = {difference}')
+#
+#
+#
+
 
 
 
