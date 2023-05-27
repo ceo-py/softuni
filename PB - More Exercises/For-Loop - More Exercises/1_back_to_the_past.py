4062ecfadd9541c9df4dbaf464d1893ad9054dbc
ceo-py <sfmf@abv.bg>
2023-05-27 13:18:32 +0300

    added another solution

--- PB - More Exercises/For-Loop - More Exercises/1_back_to_the_past.py
+++ PB - More Exercises/For-Loop - More Exercises/1_back_to_the_past.py
@@ -1,21 +1,44 @@
-heritage_money = float(input())
-year_till_he_will_live = int(input())
-
-total_heritage_left = heritage_money
-starting_year = 18
-expense_per_year = 12_000
-
-for year in range(1800, year_till_he_will_live + 1):
+money = float(input())
+year = int(input())
+total_summ = money
+age = 18
+for year in range(1800, year + 1):
 
     if year % 2 == 0:
-        total_heritage_left = total_heritage_left - expense_per_year
-        starting_year += 1
+        total_summ -= 12000
 
     else:
-        total_heritage_left = total_heritage_left - (expense_per_year + (starting_year * 50))
-        starting_year += 1
+        total_summ -= (12000 + (age * 50))
+    age += 1
 
-if total_heritage_left >= 0:
-    print(f"Yes! He will live a carefree life and will have {total_heritage_left:.2f} dollars left.")
+
+if total_summ >= 0:
+    print(f'Yes! He will live a carefree life and will have {total_summ:.2f} dollars left.')
 else:
-    print(f"He will need {abs(total_heritage_left):.2f} dollars to survive.")
+    print(f'He will need {abs(total_summ):.2f} dollars to survive.')
+
+
+
+
+
+# heritage_money = float(input())
+# year_till_he_will_live = int(input())
+#
+# total_heritage_left = heritage_money
+# starting_year = 18
+# expense_per_year = 12_000
+#
+# for year in range(1800, year_till_he_will_live + 1):
+#
+#     if year % 2 == 0:
+#         total_heritage_left = total_heritage_left - expense_per_year
+#         starting_year += 1
+#
+#     else:
+#         total_heritage_left = total_heritage_left - (expense_per_year + (starting_year * 50))
+#         starting_year += 1
+#
+# if total_heritage_left >= 0:
+#     print(f"Yes! He will live a carefree life and will have {total_heritage_left:.2f} dollars left.")
+# else:
+#     print(f"He will need {abs(total_heritage_left):.2f} dollars to survive.")
