7a945fd1c22ad2015a8cb95f693e822f4402e9cf
ceo-py <sfmf@abv.bg>
2023-05-27 12:34:26 +0300

    added another solution

--- Python Basics/First Steps in Coding - Exercise/5_supplies_for_school.py
+++ Python Basics/First Steps in Coding - Exercise/5_supplies_for_school.py
@@ -1,5 +1,21 @@
-a = int(input()) * 5.80
-b = int(input()) * 7.20
-c = int(input()) * 1.20
-d = int(input()) / 100
-print( a + b + c-(( a + b + c) * d ))
+pens_packs = int(input())
+marker_packs = int(input())
+detergent_liters = int(input())
+discount = int(input()) / 100
+
+pens_price = 5.80
+marker_price = 7.20
+detergent_price = 1.20
+
+price_before_discount = (pens_packs * pens_price) + (marker_packs * marker_price) + (detergent_liters * detergent_price)
+total_price = price_before_discount - price_before_discount * discount
+print(total_price)
+
+
+
+#
+# a = int(input()) * 5.80
+# b = int(input()) * 7.20
+# c = int(input()) * 1.20
+# d = int(input()) / 100
+# print( a + b + c-(( a + b + c) * d ))
