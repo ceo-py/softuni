c8774634dfa6dff2015fa47f0e61ebc6f82bba0d
ceo-py <sfmf@abv.bg>
2023-05-27 12:28:10 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Lab/4_personal_titles.py
+++ Python Basics/Conditional Statements Advanced - Lab/4_personal_titles.py
@@ -1,20 +1,39 @@
-person_age = float(input())
-person_sex = input()
+age = float(input())
+gender = input()
 
-title_check = {
-    "m under 16": "Master",
-    "m over 16": "Mr.",
-    "f under 16": "Miss",
-    "f over 16": "Ms."
-}
+if gender == 'm':
+    if age >= 16:
+        print('Mr.')
+if gender == 'm':
+    if age < 16:
+        print('Master')
+if gender == 'f':
+    if age >= 16:
+        print('Ms.')
+if gender == 'f':
+    if age < 16:
+        print('Miss')
 
-if person_sex == "m":
-    if person_age < 16:
-        print(title_check["m under 16"])
-    else:
-        print(title_check["m over 16"])
-elif person_sex == "f":
-    if person_age < 16:
-        print(title_check["f under 16"])
-    else:
-        print(title_check["f over 16"])
+
+
+
+# person_age = float(input())
+# person_sex = input()
+#
+# title_check = {
+#     "m under 16": "Master",
+#     "m over 16": "Mr.",
+#     "f under 16": "Miss",
+#     "f over 16": "Ms."
+# }
+#
+# if person_sex == "m":
+#     if person_age < 16:
+#         print(title_check["m under 16"])
+#     else:
+#         print(title_check["m over 16"])
+# elif person_sex == "f":
+#     if person_age < 16:
+#         print(title_check["f under 16"])
+#     else:
+#         print(title_check["f over 16"])
