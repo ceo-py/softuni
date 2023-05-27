2a2e3f2cd957a5b572362fba0f78fff33a42932b
ceo-py <sfmf@abv.bg>
2023-05-27 12:33:50 +0300

    added another solution

--- Python Basics/First Steps in Coding - Exercise/3_deposit_calculator.py
+++ Python Basics/First Steps in Coding - Exercise/3_deposit_calculator.py
@@ -1,7 +1,19 @@
-a = float(input())
-b = int(input())
-c = float(input())/100
-interest = a * c
-m_interest = interest / 12
-total = a + (m_interest * b)
-print(total)
+deposit = float(input())
+deposit_term_in_months = int(input())
+yaerly_percent = float(input()) / 100
+
+sum_deposit = deposit + deposit_term_in_months * ((deposit * yaerly_percent) / 12)
+
+print(sum_deposit)
+
+
+
+
+#
+# a = float(input())
+# b = int(input())
+# c = float(input())/100
+# interest = a * c
+# m_interest = interest / 12
+# total = a + (m_interest * b)
+# print(total)
