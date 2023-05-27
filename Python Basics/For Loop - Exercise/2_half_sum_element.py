da9e8fa47f2a19918590a54ab346220943963c97
ceo-py <sfmf@abv.bg>
2023-05-27 12:36:58 +0300

    added another solution

--- Python Basics/For Loop - Exercise/2_half_sum_element.py
+++ Python Basics/For Loop - Exercise/2_half_sum_element.py
@@ -12,3 +12,22 @@ if numbers_input[-1] == sum_of_all_numbers:
     print(f"Yes\nSum = {numbers_input[-1]}")
 else:
     print(f"No\nDiff = {abs(difrenc_numbers)}")
+
+
+#
+# n = int(input())
+# list_numb = []
+#
+# for i in range(n):
+#     numb = int(input())
+#     list_numb.append(numb)
+#
+# if max(list_numb) == sum(list_numb) - max(list_numb):
+#     print("Yes")
+#     print(f"Sum = {max(list_numb)}")
+# elif max(list_numb) != sum(list_numb) - max(list_numb):
+#     difference = abs(max(list_numb) - (sum(list_numb) - max(list_numb)))
+#     print("No")
+#     print(f"Diff = {difference}")
+#
+#
