92b50e57ec442a835093f96b56e8bb9449b3db8d
ceo-py <sfmf@abv.bg>
2023-05-27 13:16:26 +0300

    added another solution

--- PB - More Exercises/first_steps_in_coding_more_exercises/5_training_lab.py
+++ PB - More Exercises/first_steps_in_coding_more_exercises/5_training_lab.py
@@ -12,3 +12,29 @@ other_space = 3
 
 total_space = width * length - other_space
 print(total_space)
+
+
+
+
+
+
+
+#
+# w = float(input())
+# h = float(input())
+#
+# chair_h = 70
+# chair_w = 120
+#
+# total_w = w * 100
+# total_h = h * 100
+#
+# free_row_h = total_h - 100
+# chair_row_h = free_row_h // chair_h
+#
+#
+# chair_row_w = total_w // chair_w
+#
+# all_chairs = int((chair_row_h * chair_row_w) - 3)
+#
+# print(all_chairs)
