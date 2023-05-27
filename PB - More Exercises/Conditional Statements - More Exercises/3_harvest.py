a203bfeec3050d6f713f8e17a4efa0bdad885fe9
ceo-py <sfmf@abv.bg>
2023-05-27 12:54:33 +0300

    added another solution

--- PB - More Exercises/Conditional Statements - More Exercises/3_harvest.py
+++ PB - More Exercises/Conditional Statements - More Exercises/3_harvest.py
@@ -18,3 +18,51 @@ if wine >= liters_needed_wine:
 else:
     wine = abs(wine - liters_needed_wine)
     print(f"It will be a tough winter! More {math.floor(wine)} liters wine needed.")
+
+
+
+
+
+
+
+# from math import floor
+# from math import ceil
+#
+# vineyard_square_size = int(input())
+# grapes_per_square = float(input())
+# vine_lt_for_sell = int(input())
+# workers = int(input())
+#
+# all_grape_harvest = vineyard_square_size * grapes_per_square
+# total_vine = (all_grape_harvest * 0.40) / 2.5
+#
+# if total_vine >= vine_lt_for_sell:
+#     total_vine = total_vine
+#     vine_left = total_vine - vine_lt_for_sell
+#     vine_per_worker = vine_left / workers
+#     print(f"Good harvest this year! Total wine: {floor(total_vine)} liters."
+#           f"\n{ceil(vine_left)} liters left -> {ceil(vine_per_worker)} liters per person.")
+# else:
+#     vine_need = abs(vine_lt_for_sell - total_vine)
+#     print(f"It will be a tough winter! More {floor(vine_need)} liters wine needed.")
+#
+#
+
+
+
+# import math
+# x_area_sqm = int(input())
+# y_greap_sqm = float(input())
+# z_necessery_vine = int(input())
+# workers = int(input())
+# kolichestvo = (x_area_sqm*0.4*y_greap_sqm/2.5)
+# if kolichestvo < z_necessery_vine:
+#     q = (z_necessery_vine - kolichestvo)
+#     print(f'It will be a tough winter! More {math.floor(q)} liters wine needed.')
+# else:
+#     print(f'Good harvest this year! Total wine: {math.floor(kolichestvo)} liters.')
+#     v = (kolichestvo - z_necessery_vine)/workers
+#     vino_poveche = (kolichestvo-z_necessery_vine)
+#     print(f'{math.ceil(vino_poveche)} liters left -> {math.ceil(v)} liters per person.')
+#
+#
