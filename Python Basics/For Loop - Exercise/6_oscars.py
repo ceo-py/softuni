b0c06904f1e38d9a28f0bcadce06f8f9462d98f5
ceo-py <sfmf@abv.bg>
2023-05-27 12:37:39 +0300

    added another solution

--- Python Basics/For Loop - Exercise/6_oscars.py
+++ Python Basics/For Loop - Exercise/6_oscars.py
@@ -17,3 +17,27 @@ for judge in range(0, judges_number):
 if nominated == False:
     judges_score = 1250.5 - judges_score
     print(f"Sorry, {actor_name} you need {judges_score:.1f} more!")
+
+
+
+
+# name = input()
+# score = float(input())
+# comiss = int(input())
+# if 2.0 <= score <= 450.5 and 1 <= comiss <= 20:
+#     while comiss > 0:
+#         if score <= 1250.5:
+#             com_name = input()
+#             com_name_score = float(input())
+#             if 1.0 <= com_name_score <= 50.0:
+#                 points = len(com_name)*com_name_score/2
+#                 score = score + points
+#             else:
+#                 pass
+#         comiss -= 1
+#     if score > 1250.5:
+#         print(f'Congratulations, {name} got a nominee for leading role with {score:.1f}!')
+#     else:
+#         print(f'Sorry, {name} you need {(1250.5 - score):.1f} more!')
+#
+#
