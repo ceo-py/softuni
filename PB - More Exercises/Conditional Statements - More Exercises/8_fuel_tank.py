6d8ea8ba5d9d454ed6d1c042da8b716af9f5e6b7
ceo-py <sfmf@abv.bg>
2023-05-27 12:57:44 +0300

    added another solution

--- PB - More Exercises/Conditional Statements - More Exercises/8_fuel_tank.py
+++ PB - More Exercises/Conditional Statements - More Exercises/8_fuel_tank.py
@@ -1,16 +1,78 @@
-fuel_type = input()
-fuel_capacity = int(input())
-fuel_type = fuel_type.lower()
+fuel_type = input().lower()
+current_fuel_quantity = float(input())
 
-fuel_info = {
-    "diesel": "diesel",
-    "gasoline": "gasoline",
-    "gas": "gas"
-}
-
-if fuel_type in fuel_info and fuel_capacity < 25:
-    print(f"Fill your tank with {fuel_type}!")
-elif fuel_type in fuel_info and fuel_capacity >= 25:
+if fuel_type in "diesel gasoline gas" and current_fuel_quantity >= 25:
     print(f"You have enough {fuel_type}.")
+elif fuel_type in "diesel gasoline gas" and current_fuel_quantity < 25:
+    print(f"Fill your tank with {fuel_type}!")
 else:
     print("Invalid fuel!")
+
+
+
+#
+# fuel_type = input()
+# current_fuel_quantity = float(input())
+# diesel_fuel = "diesel"
+# gasoline_fuel = "gasoline"
+# gas_fuel = "gas"
+# if fuel_type == "Diesel":
+#     if current_fuel_quantity >= 25:
+#         print(f"You have enough {diesel_fuel}.")
+#     else:
+#         print(f"Fill your tank with {diesel_fuel}!")
+# elif fuel_type == "Gasoline":
+#     if current_fuel_quantity >= 25:
+#         print(f"You have enough {gasoline_fuel}.")
+#     else:
+#         print(f"Fill your tank with {gasoline_fuel}!")
+# elif fuel_type == "Gas":
+#     if current_fuel_quantity >= 25:
+#         print(f"You have enough {gas_fuel}.")
+#     else:
+#         print(f"Fill your tank with {gas_fuel}!")
+# else:
+#     print("Invalid fuel!")
+
+
+
+
+# fuel_type = input()
+# fuel_capacity = int(input())
+# fuel_type = fuel_type.lower()
+#
+# if fuel_type == "diesel" and fuel_capacity < 25:
+#     print(f"Fill your tank with {fuel_type}!")
+# elif fuel_type == "diesel" and fuel_capacity >= 25:
+#     print(f"You have enough {fuel_type}.")
+# elif fuel_type == "gasoline" and fuel_capacity < 25:
+#     print(f"Fill your tank with {fuel_type}!")
+# elif fuel_type == "gasoline" and fuel_capacity >= 25:
+#     print(f"You have enough {fuel_type}.")
+# elif fuel_type == "gas" and fuel_capacity < 25:
+#     print(f"Fill your tank with {fuel_type}!")
+# elif fuel_type == "gas" and fuel_capacity >= 25:
+#     print(f"You have enough {fuel_type}.")
+# else:
+#     print("Invalid fuel!")
+
+
+
+
+
+# fuel_type = input()
+# fuel_capacity = int(input())
+# fuel_type = fuel_type.lower()
+#
+# fuel_info = {
+#     "diesel": "diesel",
+#     "gasoline": "gasoline",
+#     "gas": "gas"
+# }
+#
+# if fuel_type in fuel_info and fuel_capacity < 25:
+#     print(f"Fill your tank with {fuel_type}!")
+# elif fuel_type in fuel_info and fuel_capacity >= 25:
+#     print(f"You have enough {fuel_type}.")
+# else:
+#     print("Invalid fuel!")
