74f4219c8a73604f381a9a73aab7ab4e2679971d
ceo-py <sfmf@abv.bg>
2023-05-29 12:14:30 +0300

    added another solution

--- Python-Fundamentals/Text Processing - More Exercises/3_treasure_finder.py
+++ Python-Fundamentals/Text Processing - More Exercises/3_treasure_finder.py
@@ -37,3 +37,27 @@ while secret_msg != "find":
 #     for show in matches:
 #         print(f"Found {show[1]} at {show[2]}")
 #     secret_msg = input()
+
+
+
+# import re
+# secret_key = [int(x) for x in input().split()]
+# secret_msg = input()
+#
+# decode_msg = ""
+# how_long = len(secret_key)
+# pattern = re.compile(r"&(\w+)&.+<(.+)>")
+# while secret_msg != "find":
+#     decode_msg = ""
+#     start_ = 0
+#     for x in secret_msg:
+#         decode_msg += chr(ord(x) - secret_key[start_])
+#         start_ += 1
+#         if start_ >= how_long:
+#             start_ = 0
+#     matches = pattern.finditer(decode_msg)
+#     for show in matches:
+#         print(f"Found {show[1]} at {show[2]}")
+#     secret_msg = input()
+
+
