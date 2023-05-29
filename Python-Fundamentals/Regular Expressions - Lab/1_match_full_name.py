269ab760137a88cfc274a5bad354531ee935c635
ceo-py <sfmf@abv.bg>
2023-05-29 12:16:21 +0300

    added another solution

--- Python-Fundamentals/Regular Expressions - Lab/1_match_full_name.py
+++ Python-Fundamentals/Regular Expressions - Lab/1_match_full_name.py
@@ -7,6 +7,30 @@ match_find = re.findall(find_name, name_string)
 print(*match_find, sep= " ")
 
 
+
+
+
+
+
+# import re
+#
+# main_string = input()
+#
+# patterns = re.compile(r"\b(?P<word>[A-Z][a-z]{1,}) (?P<word2>[A-Z][a-z]{1,})\b")
+# list_result = list()
+# result = re.finditer(patterns, main_string)
+#
+# for show in result:
+#     list_result.append(f"{show['word']} {show['word2']}")
+#
+# print(*list_result)
+#
+#
+
+
+
+
+
 # import re
 #
 # main_string = input()
