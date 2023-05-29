e8f09cecc62f473e0e66fad136629fb55e305f91
ceo-py <sfmf@abv.bg>
2023-05-29 12:00:11 +0300

    added another solution

--- Python-Fundamentals/Lists Advanced - Lab/5_sorting_names.py
+++ Python-Fundamentals/Lists Advanced - Lab/5_sorting_names.py
@@ -4,3 +4,13 @@ print(sorted(input().split(", "), key= lambda x: (-len(x), x)))
 # name_list = input().split(", ")
 # sorted_list = sorted(name_list, key= lambda x: (-len(x), x))
 # print(sorted_list)
+
+
+
+# def Sorting(lst):
+#     lst2 = sorted(lst, key= lambda x: (-len(x), x))
+#     return lst2
+#
+#
+# lst = input().split(", ")
+# print(Sorting(lst))
