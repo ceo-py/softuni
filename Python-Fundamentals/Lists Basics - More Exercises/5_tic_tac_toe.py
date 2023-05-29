3f00f041a962b81f749d3b4927c18ff054231cbd
ceo-py <sfmf@abv.bg>
2023-05-29 11:45:27 +0300

    added another solution

--- Python-Fundamentals/Lists Basics - More Exercises/5_tic_tac_toe.py
+++ Python-Fundamentals/Lists Basics - More Exercises/5_tic_tac_toe.py
@@ -2,32 +2,60 @@ first_row = input().split()
 second_row = input().split()
 third_row = input().split()
 
-winner = {
-    "1": "First",
-    "2": "Second"
-}
-
-
-def tic_tac(player):
-    lines = [player] * 3
-    if any([lines == first_row, second_row == lines, third_row == lines]):
-        return player
-    for row in range(0, 3):
-        if all([first_row[row] == player, second_row[row] == player, third_row[row] == player]):
-            return player
-    if all([first_row[0] == player, second_row[1] == player, third_row[2] == player]):
-        return player
-    elif all([first_row[2] == player, second_row[1] == player, third_row[0] == player]):
-        return player
-
+name_player = "First"
+found = False
 
 for player in range(1, 3):
-    player_win = tic_tac(str(player))
-    if any([player_win == "1", player_win == "2"]):
-        print(f"{winner[player_win]} player won")
+    player = str(player)
+    line = [player, player, player]
+    if line == first_row or second_row == line or third_row == line:  # преверка на редовете
+        found = True
+    for index in range(0, 3):
+        if first_row[index] == player and second_row[index] == player and third_row[index] == player:  # проверка на колоните
+            found = True
+    if first_row[0] == player and second_row[1] == player and third_row[2] == player:  # ляв диагонал
+        found = True
+    elif first_row[2] == player and second_row[1] == player and third_row[0] == player:  # десен диагонал
+        found = True
+    if found:
+        print(f"{name_player} player won")
         break
+    name_player = "Second"
 else:
     print("Draw!")
+
+
+
+# first_row = input().split()
+# second_row = input().split()
+# third_row = input().split()
+#
+# winner = {
+#     "1": "First",
+#     "2": "Second"
+# }
+#
+#
+# def tic_tac(player):
+#     lines = [player] * 3
+#     if any([lines == first_row, second_row == lines, third_row == lines]):
+#         return player
+#     for row in range(0, 3):
+#         if all([first_row[row] == player, second_row[row] == player, third_row[row] == player]):
+#             return player
+#     if all([first_row[0] == player, second_row[1] == player, third_row[2] == player]):
+#         return player
+#     elif all([first_row[2] == player, second_row[1] == player, third_row[0] == player]):
+#         return player
+#
+#
+# for player in range(1, 3):
+#     player_win = tic_tac(str(player))
+#     if any([player_win == "1", player_win == "2"]):
+#         print(f"{winner[player_win]} player won")
+#         break
+# else:
+#     print("Draw!")
 
 
 
@@ -75,8 +103,52 @@ else:
 
 
 
-
-
+#
+# field = []
+#
+# first_player_win = False
+# second_player_win = False
+# draw = False
+# #  drawing the game field in separate lists
+# for field_row in range(3):
+#     current_row = input().split()
+#     field.append(current_row)
+# #  checking each row to see if there is a winner in the row
+# for row in range(3):
+#     if field[row][0] == field[row][1] == field[row][2] != "0":
+#         if field[row][0] == field[row][1] == field[row][2] == "1":
+#             first_player_win = True
+#         elif field[row][0] == field[row][1] == field[row][2] == "2":
+#             second_player_win = True
+# #  checking each column to see if there is a winner in the column
+# for col in range(3):
+#     if field[0][col] == field[1][col] == field[2][col] != "0":
+#         if field[0][col] == field[1][col] == field[2][col] == "1":
+#             first_player_win = True
+#         elif field[0][col] == field[1][col] == field[2][col] == "2":
+#             second_player_win = True
+# #  checking left diagonal if there is a winner
+# if field[0][0] == field[1][1] == field[2][2] != "0":
+#     if field[0][0] == field[1][1] == field[2][2] == "1":
+#         first_player_win = True
+#     elif field[0][0] == field[0][1] == field[2][2] == "2":
+#         second_player_win = True
+# #  checking right diagonal if there is a winner
+# elif field[0][2] == field[1][1] == field[2][0] != "0":
+#     if field[0][2] == field[1][1] == field[2][0] == "1":
+#         first_player_win = True
+#     elif field[0][2] == field[1][1] == field[2][0] == "2":
+#         second_player_win = True
+# # if there is no winner found the game is a draw
+# if not first_player_win and not second_player_win:
+#     draw = True
+#
+# if first_player_win:
+#     print("First player won")
+# elif second_player_win:
+#     print("Second player won")
+# elif draw:
+#     print("Draw!")
 
 
 
