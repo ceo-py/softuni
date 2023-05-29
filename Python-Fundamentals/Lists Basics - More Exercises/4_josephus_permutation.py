0814f44a51a47ff89136922350077c7d3dc38c73
ceo-py <sfmf@abv.bg>
2023-05-29 11:43:15 +0300

    added another solution

--- Python-Fundamentals/Lists Basics - More Exercises/4_josephus_permutation.py
+++ Python-Fundamentals/Lists Basics - More Exercises/4_josephus_permutation.py
@@ -39,3 +39,31 @@ print(f"[{','.join(final_numbers)}]")
 #
 #
 #
+
+
+# main_list = input().split()
+# skip_number_k = int(input())
+# number_to_check = list()
+# final_numbers = list()
+#
+# for number in main_list:
+#     number_to_check.append(int(number))
+#
+# pos = skip_number_k - 1
+# index = 0
+# len_list = (len(number_to_check))
+#
+# while len_list > 0:
+#     index = (pos + index) % len_list
+#     final_numbers.append(int(number_to_check.pop(index)))
+#     len_list -= 1
+#
+# i = 1
+# print("[",end="")
+# len_list = (len(main_list))
+# for n in final_numbers:
+#     if i != len_list:
+#         print(f"{n},",end="")
+#     else:
+#         print(f"{n}]")
+#     i += 1
