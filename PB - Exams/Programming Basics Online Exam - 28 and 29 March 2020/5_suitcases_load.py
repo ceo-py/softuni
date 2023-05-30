d78f1cc79192e945361d4df446238c44b7f9f7f5
ceo-py <sfmf@abv.bg>
2023-05-30 13:03:25 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 28 and 29 March 2020/5_suitcases_load.py
+++ PB - Exams/Programming Basics Online Exam - 28 and 29 March 2020/5_suitcases_load.py
@@ -1,23 +1,55 @@
-capacity_lagges = float(input())
-number_lagges = 0
-check_space = 0
+lugged_capacity = float(input())
 
+lugged = input()
+suitcases = 0
 
-while True:
-    lagge = input()
+while lugged != "End":
+    lugged = float(lugged)
+    suitcases += 1
+    added_lugged_space = 0
+    if suitcases % 3 == 0:
+        added_lugged_space = lugged * 0.10
 
-    if "End" in lagge:
-        print(f"Congratulations! All suitcases are loaded!\nStatistic: {number_lagges} suitcases loaded.")
+    lugged_capacity -= lugged + added_lugged_space
+
+    if lugged_capacity <= 0:
+        if lugged_capacity != 0:
+            suitcases -= 1
+        print("No more space!")
+
         break
+    lugged = input()
 
-    number_lagges += 1
-    lagges = float(lagge)
-    check_space += + lagges
+else:
+    print(f"Congratulations! All suitcases are loaded!")
+print(f"Statistic: {suitcases} suitcases loaded.")
 
-    if number_lagges % 3 == 0:
-        check_space += lagges * 0.10
 
-    if capacity_lagges < check_space:
-        number_lagges += - 1
-        print(f"No more space!\nStatistic: {number_lagges} suitcases loaded.")
-        break
+
+
+
+
+
+# capacity_lagges = float(input())
+# number_lagges = 0
+# check_space = 0
+#
+#
+# while True:
+#     lagge = input()
+#
+#     if "End" in lagge:
+#         print(f"Congratulations! All suitcases are loaded!\nStatistic: {number_lagges} suitcases loaded.")
+#         break
+#
+#     number_lagges += 1
+#     lagges = float(lagge)
+#     check_space += + lagges
+#
+#     if number_lagges % 3 == 0:
+#         check_space += lagges * 0.10
+#
+#     if capacity_lagges < check_space:
+#         number_lagges += - 1
+#         print(f"No more space!\nStatistic: {number_lagges} suitcases loaded.")
+#         break
