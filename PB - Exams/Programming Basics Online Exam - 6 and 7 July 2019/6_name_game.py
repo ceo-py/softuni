8e7b88b3652671b9f6741e6a06a68e11c87ec529
ceo-py <sfmf@abv.bg>
2023-05-30 13:00:32 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/6_name_game.py
+++ PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/6_name_game.py
@@ -46,3 +46,72 @@ print(f'The winner is {winner} with {max_points} points!')
 #             second_name = name_of_the_player
 #
 #         total_points_for_name = 0
+
+
+
+# total_points = 0
+# first_name = ""
+# second_name = ""
+# total_points_for_name = 0
+#
+# while True:
+#     name_of_the_player = input()
+#     first_name = name_of_the_player
+#     if name_of_the_player == "Stop":
+#         print(f"The winner is {second_name} with {total_points} points!")
+#         break
+#
+#     else:
+#         for letter in name_of_the_player:
+#             guesses_number = int(input())
+#
+#             if guesses_number == ord(letter):
+#                 total_points_for_name += 10
+#
+#             else:
+#                 total_points_for_name += 2
+#
+#         if total_points_for_name >= total_points:
+#             total_points = total_points_for_name
+#             second_name = name_of_the_player
+#
+#         total_points_for_name = 0
+
+
+
+# stop = False
+# b = []
+# i = 0
+# summ = 0
+# winner_name = ''
+# winner_score = 0
+# while stop is False:
+#     name = input()
+#     b = list(name)
+#     le = len(name)
+#     if name == 'Stop':
+#         stop = True
+#         break
+#     while le > 0:
+#         n = input()
+#         if n == 'Stop':
+#             stop = True
+#             break
+#
+#         if int(n) == ord(b[i]):
+#                 summ += 10
+#         elif int(n) != ord(b[i]):
+#                 summ += 2
+#         i += 1
+#         le -= 1
+#         if le == 0:
+#             i = 0
+#             break
+#     if summ >= winner_score:
+#         winner_name = name
+#         winner_score = summ
+#     summ = 0
+#     if stop:
+#         break
+# print(f'The winner is {winner_name} with {winner_score} points!')
+#
