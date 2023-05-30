7de5954dc779ce39e94dc21c76e3e3f60b7f0c40
ceo-py <sfmf@abv.bg>
2023-05-30 13:06:00 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 18 and 19 July 2020/3_aluminum_joinery.py
+++ PB - Exams/Programming Basics Online Exam - 18 and 19 July 2020/3_aluminum_joinery.py
@@ -1,76 +1,135 @@
-number_joinery = int(input())
-joinery_type = input()
-deliver = input()
+window_count = int(input())
+window_type = str(input())
+delivery = str(input())
+window_price = float()
+total_cost_before_discount = float()
+transport_cost = float()
+stop_under_order = False
 
-joinery_size_ninety = 110
-joinery_size_onehundred = 140
-joinery_size_onehundred_thurty = 190
-joinery_size_twohundred = 250
-delivery_fee = 60
-
-if number_joinery < 10:
+if window_count < 10:
     print("Invalid order")
-    stop_under_order = False
 
+if window_type == "90X130" and window_count <= 30:
+    window_price = 110
+elif window_type == "90X130" and window_count <= 60:
+    window_price = 110 * 0.95
+elif window_type == "90X130" and window_count > 60:
+    window_price = 110 * 0.92
+elif window_type == "100X150" and window_count <= 40:
+    window_price = 140
+elif window_type == "100X150" and window_count <= 80:
+    window_price = 140 * 0.94
+elif window_type == "100X150" and window_count > 80:
+    window_price = 140 * 0.90
+elif window_type == "130X180" and window_count <= 20:
+    window_price = 190
+elif window_type == "130X180" and window_count <= 50:
+    window_price = 190 * 0.93
+elif window_type == "130X180" and window_count > 50:
+    window_price = 190 * 0.88
+elif window_type == "200X300" and window_count <= 25:
+    window_price = 250
+elif window_type == "200X300" and window_count <= 50:
+    window_price = 250 * 0.91
+elif window_type == "200X300" and window_count > 50:
+    window_price = 250 * 0.86
+total_cost_before_discount = window_count * window_price + transport_cost
+if delivery == "With delivery":
+    transport_cost = 60
+elif delivery == "Without delivery":
+    transport_cost = 0
+if window_count >= 99:
+    total_cost_before_discount = (total_cost_before_discount + transport_cost) * 0.96
 else:
+    total_cost_before_discount = total_cost_before_discount + transport_cost
+result = "{:.2f}".format(total_cost_before_discount)
+if window_count >= 10:
+    print(f"{result} BGN")
 
-    if "90X130" == joinery_type:
-        if number_joinery <= 30:
-            joinery_price_off = number_joinery * joinery_size_ninety
 
-        if number_joinery > 30:
-            joinery_price_off = number_joinery * joinery_size_ninety
-            joinery_price_off = joinery_price_off - joinery_price_off * 0.05
 
-        if number_joinery > 60:
-            joinery_price_off = number_joinery * joinery_size_ninety
-            joinery_price_off = joinery_price_off - joinery_price_off * 0.08
 
-    elif "100X150" == joinery_type:
-        if number_joinery <= 40:
-            joinery_price_off = number_joinery * joinery_size_onehundred
 
-        if number_joinery > 40:
-            joinery_price_off = number_joinery * joinery_size_onehundred
-            joinery_price_off = joinery_price_off - joinery_price_off * 0.06
 
-        if number_joinery > 80:
-            joinery_price_off = number_joinery * joinery_size_onehundred
-            joinery_price_off = joinery_price_off - joinery_price_off * 0.10
 
-    elif "130X180" == joinery_type:
-        if number_joinery <= 20:
-            joinery_price_off = number_joinery * joinery_size_onehundred_thurty
 
-        if number_joinery > 20:
-            joinery_price_off = number_joinery * joinery_size_onehundred_thurty
-            joinery_price_off = joinery_price_off - joinery_price_off * 0.07
 
-        if number_joinery > 50:
-            joinery_price_off = number_joinery * joinery_size_onehundred_thurty
-            joinery_price_off = joinery_price_off - joinery_price_off * 0.12
 
-    elif "200X300" == joinery_type:
-        if number_joinery <= 25:
-            joinery_price_off = number_joinery * joinery_size_twohundred
-
-        if number_joinery > 25:
-            joinery_price_off = number_joinery * joinery_size_twohundred
-            joinery_price_off = joinery_price_off - joinery_price_off * 0.09
-
-        if number_joinery > 50:
-            joinery_price_off = number_joinery * joinery_size_twohundred
-            joinery_price_off = joinery_price_off - joinery_price_off * 0.14
-
-    if "With delivery" == deliver:
-        joinery_price_off = joinery_price_off + delivery_fee
-
-    if "Without delivery" == deliver:
-        joinery_price_off = joinery_price_off
-
-    if number_joinery > 99:
-        joinery_price_off = joinery_price_off - joinery_price_off * 0.04
-
-    joinery_price_off = "{:.2f}".format(joinery_price_off)
-
-    print(f"{joinery_price_off} BGN")
+#
+#
+# number_joinery = int(input())
+# joinery_type = input()
+# deliver = input()
+#
+# joinery_size_ninety = 110
+# joinery_size_onehundred = 140
+# joinery_size_onehundred_thurty = 190
+# joinery_size_twohundred = 250
+# delivery_fee = 60
+#
+# if number_joinery < 10:
+#     print("Invalid order")
+#     stop_under_order = False
+#
+# else:
+#
+#     if "90X130" == joinery_type:
+#         if number_joinery <= 30:
+#             joinery_price_off = number_joinery * joinery_size_ninety
+#
+#         if number_joinery > 30:
+#             joinery_price_off = number_joinery * joinery_size_ninety
+#             joinery_price_off = joinery_price_off - joinery_price_off * 0.05
+#
+#         if number_joinery > 60:
+#             joinery_price_off = number_joinery * joinery_size_ninety
+#             joinery_price_off = joinery_price_off - joinery_price_off * 0.08
+#
+#     elif "100X150" == joinery_type:
+#         if number_joinery <= 40:
+#             joinery_price_off = number_joinery * joinery_size_onehundred
+#
+#         if number_joinery > 40:
+#             joinery_price_off = number_joinery * joinery_size_onehundred
+#             joinery_price_off = joinery_price_off - joinery_price_off * 0.06
+#
+#         if number_joinery > 80:
+#             joinery_price_off = number_joinery * joinery_size_onehundred
+#             joinery_price_off = joinery_price_off - joinery_price_off * 0.10
+#
+#     elif "130X180" == joinery_type:
+#         if number_joinery <= 20:
+#             joinery_price_off = number_joinery * joinery_size_onehundred_thurty
+#
+#         if number_joinery > 20:
+#             joinery_price_off = number_joinery * joinery_size_onehundred_thurty
+#             joinery_price_off = joinery_price_off - joinery_price_off * 0.07
+#
+#         if number_joinery > 50:
+#             joinery_price_off = number_joinery * joinery_size_onehundred_thurty
+#             joinery_price_off = joinery_price_off - joinery_price_off * 0.12
+#
+#     elif "200X300" == joinery_type:
+#         if number_joinery <= 25:
+#             joinery_price_off = number_joinery * joinery_size_twohundred
+#
+#         if number_joinery > 25:
+#             joinery_price_off = number_joinery * joinery_size_twohundred
+#             joinery_price_off = joinery_price_off - joinery_price_off * 0.09
+#
+#         if number_joinery > 50:
+#             joinery_price_off = number_joinery * joinery_size_twohundred
+#             joinery_price_off = joinery_price_off - joinery_price_off * 0.14
+#
+#     if "With delivery" == deliver:
+#         joinery_price_off = joinery_price_off + delivery_fee
+#
+#     if "Without delivery" == deliver:
+#         joinery_price_off = joinery_price_off
+#
+#     if number_joinery > 99:
+#         joinery_price_off = joinery_price_off - joinery_price_off * 0.04
+#
+#     joinery_price_off = "{:.2f}".format(joinery_price_off)
+#
+#     print(f"{joinery_price_off} BGN")
