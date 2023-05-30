5e4f153fdd3ad52798bef47918f037b908d85568
ceo-py <sfmf@abv.bg>
2023-05-30 12:47:41 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/6_high_jump.py
+++ PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/6_high_jump.py
@@ -80,3 +80,32 @@ else:
 #
 # print(f'Tihomir failed at {first_high}cm after {jump} jumps.')
 #
+
+
+
+# required_height = int(input())
+# initial_height = required_height - 30
+# failed_jumps = 0
+# failed_height = 0
+# fail_flag = False
+# jumps_number = 0
+# while failed_jumps < 3:
+#     jump_height = int(input())
+#     if jump_height > initial_height:
+#         initial_height += 5
+#         failed_jumps = 0
+#         jumps_number += 1
+#     elif jump_height <= initial_height:
+#         failed_jumps += 1
+#         jumps_number += 1
+#     if failed_jumps == 3:
+#         fail_flag = True
+#     if initial_height > required_height:
+#         break
+#
+# if fail_flag:
+#     print(f"Tihomir failed at {initial_height}cm after {jumps_number} jumps.")
+# if not fail_flag:
+#     print(f"Tihomir succeeded, he jumped over {required_height}cm after {jumps_number} jumps.")
+#
+
