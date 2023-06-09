13fce9b6dc312e0f88422a9e25e7f5a2d27f1b38
ceo-py <sfmf@abv.bg>
2023-06-09 14:20:27 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 18 and 19 July 2020/3_aluminum_joinery.py
+++ PB - Exams/Programming Basics Online Exam - 18 and 19 July 2020/3_aluminum_joinery.py
@@ -1,57 +1,103 @@
-window_count = int(input())
-window_type = str(input())
-delivery = str(input())
-window_price = float()
-total_cost_before_discount = float()
-transport_cost = float()
-stop_under_order = False
+window_frames_number = int(input())
+window_frames_type = input()
+delivery_type = input()
 
-if window_count < 10:
-    print("Invalid order")
+single_price = 0
+discount = 1
+output = 'Invalid order'
 
-if window_type == "90X130" and window_count <= 30:
-    window_price = 110
-elif window_type == "90X130" and window_count <= 60:
-    window_price = 110 * 0.95
-elif window_type == "90X130" and window_count > 60:
-    window_price = 110 * 0.92
-elif window_type == "100X150" and window_count <= 40:
-    window_price = 140
-elif window_type == "100X150" and window_count <= 80:
-    window_price = 140 * 0.94
-elif window_type == "100X150" and window_count > 80:
-    window_price = 140 * 0.90
-elif window_type == "130X180" and window_count <= 20:
-    window_price = 190
-elif window_type == "130X180" and window_count <= 50:
-    window_price = 190 * 0.93
-elif window_type == "130X180" and window_count > 50:
-    window_price = 190 * 0.88
-elif window_type == "200X300" and window_count <= 25:
-    window_price = 250
-elif window_type == "200X300" and window_count <= 50:
-    window_price = 250 * 0.91
-elif window_type == "200X300" and window_count > 50:
-    window_price = 250 * 0.86
-total_cost_before_discount = window_count * window_price + transport_cost
-if delivery == "With delivery":
-    transport_cost = 60
-elif delivery == "Without delivery":
-    transport_cost = 0
-if window_count >= 99:
-    total_cost_before_discount = (total_cost_before_discount + transport_cost) * 0.96
-else:
-    total_cost_before_discount = total_cost_before_discount + transport_cost
-result = "{:.2f}".format(total_cost_before_discount)
-if window_count >= 10:
-    print(f"{result} BGN")
+if window_frames_type == '90X130':
+    single_price = 110
+    if window_frames_number > 60:
+        discount = 0.92
+    elif window_frames_number > 30:
+        discount = 0.95
+
+elif window_frames_type == '100X150':
+    single_price = 140
+    if window_frames_number > 80:
+        discount = 0.90
+    elif window_frames_number > 40:
+        discount = 0.94
+
+elif window_frames_type == '130X180':
+    single_price = 190
+    if window_frames_number > 50:
+        discount = 0.88
+    elif window_frames_number > 20:
+        discount = 0.93
+
+elif window_frames_type == '200X300':
+    single_price = 250
+    if window_frames_number > 50:
+        discount = 0.86
+    elif window_frames_number > 25:
+        discount = 0.91
 
 
+if window_frames_number > 10:
+    output = single_price * window_frames_number * discount
 
+    if delivery_type == 'With delivery':
+        output += 60
 
+    if window_frames_number > 99:
+        output *= 0.96
+
+    output = f'{output:.2f} BGN'
+
+print(output)
 
 
 
+
+# window_count = int(input())
+# window_type = str(input())
+# delivery = str(input())
+# window_price = float()
+# total_cost_before_discount = float()
+# transport_cost = float()
+# stop_under_order = False
+#
+# if window_count < 10:
+#     print("Invalid order")
+#
+# if window_type == "90X130" and window_count <= 30:
+#     window_price = 110
+# elif window_type == "90X130" and window_count <= 60:
+#     window_price = 110 * 0.95
+# elif window_type == "90X130" and window_count > 60:
+#     window_price = 110 * 0.92
+# elif window_type == "100X150" and window_count <= 40:
+#     window_price = 140
+# elif window_type == "100X150" and window_count <= 80:
+#     window_price = 140 * 0.94
+# elif window_type == "100X150" and window_count > 80:
+#     window_price = 140 * 0.90
+# elif window_type == "130X180" and window_count <= 20:
+#     window_price = 190
+# elif window_type == "130X180" and window_count <= 50:
+#     window_price = 190 * 0.93
+# elif window_type == "130X180" and window_count > 50:
+#     window_price = 190 * 0.88
+# elif window_type == "200X300" and window_count <= 25:
+#     window_price = 250
+# elif window_type == "200X300" and window_count <= 50:
+#     window_price = 250 * 0.91
+# elif window_type == "200X300" and window_count > 50:
+#     window_price = 250 * 0.86
+# total_cost_before_discount = window_count * window_price + transport_cost
+# if delivery == "With delivery":
+#     transport_cost = 60
+# elif delivery == "Without delivery":
+#     transport_cost = 0
+# if window_count >= 99:
+#     total_cost_before_discount = (total_cost_before_discount + transport_cost) * 0.96
+# else:
+#     total_cost_before_discount = total_cost_before_discount + transport_cost
+# result = "{:.2f}".format(total_cost_before_discount)
+# if window_count >= 10:
+#     print(f"{result} BGN")
 
 
 
