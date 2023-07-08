f91510068bda7c117d46d56637bf7e36e936bee8
ceo-py <sfmf@abv.bg>
2023-07-08 15:13:30 +0300

    added another solution

--- Python-Fundamentals/Dictionaries - More Exercises/5_dragon_army.py
+++ Python-Fundamentals/Dictionaries - More Exercises/5_dragon_army.py
@@ -48,6 +48,38 @@ show_result()
 
 
 
+# number_of_dragons = int(input())
+# dragons_by_colours = {}
+#
+# for _ in range(number_of_dragons):
+#     colour, name, damage, health, armor = input().split()
+#     if colour not in dragons_by_colours:
+#         dragons_by_colours[colour] = {}
+#     dragons_by_colours[colour][name] = [45 if damage == "null" else int(damage),
+#                                         250 if health == "null" else int(health),
+#                                         10 if armor == "null" else int(armor)]
+#
+# average_per_colour = {}
+# for colour, name in dragons_by_colours.items():
+#     avg_for_colour = []
+#     for dragon, info in name.items():
+#         if len(name) > 1:
+#             avg_for_colour.append(info)
+#         else:
+#             avg_for_colour.append(info)
+#         avg_result = [sum(i) / len(avg_for_colour) for i in zip(*avg_for_colour)]
+#     average_per_colour[colour] = avg_result
+#
+# sorted_dictionary = {k: {x: y for x, y in sorted(v.items())} for k, v in dragons_by_colours.items()}
+#
+# for colour, average in average_per_colour.items():
+#     print(f"{colour}::({average[0]:.2f}/{average[1]:.2f}/{average[2]:.2f})")
+#     for name, info in sorted_dictionary[colour].items():
+#         print(f"-{name} -> damage: {info[0]}, health: {info[1]}, armor: {info[2]}")
+#
+
+
+
 
 # number_of_dragons = int(input())
 # dragons_by_colours = {}
