800e85b3bac8c8eb84d3f0c2f44a7a8b359a0deb
ceo-py <sfmf@abv.bg>
2023-08-24 16:40:06 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 6 and 7 April 2019/4_cinema_voucher.py
+++ PB - Exams/Programming Basics Online Exam - 6 and 7 April 2019/4_cinema_voucher.py
@@ -1,41 +1,74 @@
-vouchers = int(input())
+valcher = int(input())
 
-total_money_left = vouchers
-movie_tickets_count = 0
-items_count = 0
-items = ""
+item = input()
+movie_tickets = 0
+other_stuff = 0
 
-while True:
-    movie_ticket_price = 0
-    items_price = 0
+while item != 'End':
 
-    items = input()
-    if items == "End" or total_money_left < 0:
-        print(f"{movie_tickets_count}\n{items_count}")
-        break
+    if len(item) > 8:
+        valcher -= ord(item[1]) + ord(item[0])
 
-    items_length = len(items)
+        if valcher < 0:
+            break
 
-    if items_length > 8:
-        movie_ticket_price = ord(items[0]) + ord(items[1])
-        total_money_left += - movie_ticket_price
+        movie_tickets += 1
 
-        if total_money_left >= 0:
-            movie_tickets_count += 1
+    else:
+        valcher -= ord(item[0])
 
-        else:
-            if items == "End" or total_money_left < 0:
-                print(f"{movie_tickets_count}\n{items_count}")
-                break
+        if valcher < 0:
+            break
 
-    elif items_length <= 8:
-        items_price = ord(items[0])
-        total_money_left += - items_price
+        other_stuff += 1
 
-        if total_money_left >= 0:
-            items_count += 1
+    item = input()
 
-        else:
-            if items == "End" or total_money_left < 0:
-                print(f"{movie_tickets_count}\n{items_count}")
-                break
+print(movie_tickets)
+print(other_stuff)
+
+
+
+
+
+# vouchers = int(input())
+#
+# total_money_left = vouchers
+# movie_tickets_count = 0
+# items_count = 0
+# items = ""
+#
+# while True:
+#     movie_ticket_price = 0
+#     items_price = 0
+#
+#     items = input()
+#     if items == "End" or total_money_left < 0:
+#         print(f"{movie_tickets_count}\n{items_count}")
+#         break
+#
+#     items_length = len(items)
+#
+#     if items_length > 8:
+#         movie_ticket_price = ord(items[0]) + ord(items[1])
+#         total_money_left += - movie_ticket_price
+#
+#         if total_money_left >= 0:
+#             movie_tickets_count += 1
+#
+#         else:
+#             if items == "End" or total_money_left < 0:
+#                 print(f"{movie_tickets_count}\n{items_count}")
+#                 break
+#
+#     elif items_length <= 8:
+#         items_price = ord(items[0])
+#         total_money_left += - items_price
+#
+#         if total_money_left >= 0:
+#             items_count += 1
+#
+#         else:
+#             if items == "End" or total_money_left < 0:
+#                 print(f"{movie_tickets_count}\n{items_count}")
+#                 break
