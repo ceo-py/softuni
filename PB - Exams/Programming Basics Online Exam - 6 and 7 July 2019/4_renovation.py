b585f2d0ec7787b2eb5d8239c5ae85908f570d85
ceo-py <sfmf@abv.bg>
2023-08-24 14:05:56 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/4_renovation.py
+++ PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/4_renovation.py
@@ -1,31 +1,64 @@
 import math
 
-height_walls = int(input())
-width_walls = int(input())
-surface_not_to_paint = int(input())
+high_wall = int(input())
+width_wall = int(input())
+no_paint_wall_percent = int(input())
 
-walls_total_surface = height_walls * width_walls * 4
-wall_surface_to_paint = math.ceil(walls_total_surface - ((walls_total_surface * surface_not_to_paint) / 100))
-is_wall_not_painted = True
-while is_wall_not_painted:
+paint_lt = input()
+total_room_size = math.ceil((high_wall * width_wall * 4) * (1 - no_paint_wall_percent / 100))
 
-    painted_surface = input()
 
-    if painted_surface == "Tired!":
-        print(f"{wall_surface_to_paint} quadratic m left.")
-        is_wall_not_painted = False
+while paint_lt != 'Tired!':
+    paint_lt = int(paint_lt)
 
-    else:
-        painted_surface = int(painted_surface)
-        wall_surface_to_paint -= painted_surface
+    total_room_size -= paint_lt
 
-    if wall_surface_to_paint < 0:
-        print(f"All walls are painted and you have {abs(wall_surface_to_paint)} l paint left!")
-        is_wall_not_painted = False
+    if total_room_size < 0:
+        print(f'All walls are painted and you have {abs(total_room_size)} l paint left!')
+        break
 
-    elif wall_surface_to_paint == 0:
-        print(f"All walls are painted! Great job, Pesho!")
-        is_wall_not_painted = False
+    if total_room_size == 0:
+        print('All walls are painted! Great job, Pesho!')
+        break
+
+    paint_lt = input()
+
+else:
+    print(f'{total_room_size} quadratic m left.')
+
+
+
+
+
+
+# import math
+#
+# height_walls = int(input())
+# width_walls = int(input())
+# surface_not_to_paint = int(input())
+#
+# walls_total_surface = height_walls * width_walls * 4
+# wall_surface_to_paint = math.ceil(walls_total_surface - ((walls_total_surface * surface_not_to_paint) / 100))
+# is_wall_not_painted = True
+# while is_wall_not_painted:
+#
+#     painted_surface = input()
+#
+#     if painted_surface == "Tired!":
+#         print(f"{wall_surface_to_paint} quadratic m left.")
+#         is_wall_not_painted = False
+#
+#     else:
+#         painted_surface = int(painted_surface)
+#         wall_surface_to_paint -= painted_surface
+#
+#     if wall_surface_to_paint < 0:
+#         print(f"All walls are painted and you have {abs(wall_surface_to_paint)} l paint left!")
+#         is_wall_not_painted = False
+#
+#     elif wall_surface_to_paint == 0:
+#         print(f"All walls are painted! Great job, Pesho!")
+#         is_wall_not_painted = False
 
 
 
