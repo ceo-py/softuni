10165c2c994d48f772821e2d86f5e7e2ba56b90f
ceo-py <sfmf@abv.bg>
2023-08-24 16:38:58 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 6 and 7 April 2019/2_godzilla_vs_kong.py
+++ PB - Exams/Programming Basics Online Exam - 6 and 7 April 2019/2_godzilla_vs_kong.py
@@ -1,22 +1,46 @@
 movie_budget = float(input())
-extras = int(input())
-extras_clothing_price = float(input())
+person_number = int(input())
+person_cloth_price = float(input())
 
-decor = movie_budget * 0.10
+decore = movie_budget * 0.10
 
-if extras > 150:
-    extras_clothing_total = (extras_clothing_price * extras) - ((extras_clothing_price * extras) * 0.10)
+if person_number > 150:
+    person_cloth_price *= 0.90
+
+total_cost = (person_cloth_price * person_number) + decore
+difference = f'{abs(total_cost - movie_budget):.2f}'
+
+if total_cost <= movie_budget:
+    print('Action!')
+    print(f'Wingard starts filming with {difference} leva left.')
 
 else:
-    extras_clothing_total = extras_clothing_price * extras
+    print('Not enough money!')
+    print(f'Wingard needs {difference} leva more.')
 
-total_movie_cost = movie_budget - (decor + extras_clothing_total)
 
-if movie_budget > total_movie_cost and total_movie_cost >= 0:
-    print(f"Action!\nWingard starts filming with {total_movie_cost:.2f} leva left.")
 
-else:
-    print(f"Not enough money!\nWingard needs {abs(total_movie_cost):.2f} leva more.")
+
+
+# movie_budget = float(input())
+# extras = int(input())
+# extras_clothing_price = float(input())
+#
+# decor = movie_budget * 0.10
+#
+# if extras > 150:
+#     extras_clothing_total = (extras_clothing_price * extras) - ((extras_clothing_price * extras) * 0.10)
+#
+# else:
+#     extras_clothing_total = extras_clothing_price * extras
+#
+# total_movie_cost = movie_budget - (decor + extras_clothing_total)
+#
+# if movie_budget > total_movie_cost and total_movie_cost >= 0:
+#     print(f"Action!\nWingard starts filming with {total_movie_cost:.2f} leva left.")
+#
+# else:
+#     print(f"Not enough money!\nWingard needs {abs(total_movie_cost):.2f} leva more.")
 
 
 
