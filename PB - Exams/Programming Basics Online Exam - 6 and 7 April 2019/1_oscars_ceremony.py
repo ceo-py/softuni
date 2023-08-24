b8c3a01c98f0e63d4bd33845aafd2ffb381ad173
ceo-py <sfmf@abv.bg>
2023-08-24 16:38:27 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 6 and 7 April 2019/1_oscars_ceremony.py
+++ PB - Exams/Programming Basics Online Exam - 6 and 7 April 2019/1_oscars_ceremony.py
@@ -1,9 +1,23 @@
 hall_rent = int(input())
 
-figurines = hall_rent * 0.70
-catering = figurines * 0.85
-sound = catering / 2
+statue = hall_rent * 0.70
+food = statue * 0.85
+sound = food / 2
 
-total_price = figurines + catering + sound + hall_rent
+total_sum = hall_rent + statue + food + sound
+print(f'{total_sum:.2f}')
 
-print(f"{total_price:.2f}")
+
+
+
+
+
+# hall_rent = int(input())
+#
+# figurines = hall_rent * 0.70
+# catering = figurines * 0.85
+# sound = catering / 2
+#
+# total_price = figurines + catering + sound + hall_rent
+#
+# print(f"{total_price:.2f}")
