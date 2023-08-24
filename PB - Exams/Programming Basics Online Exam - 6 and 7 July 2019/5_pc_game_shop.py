f5a104399100493248cf9602fa38fc18d4aac9de
ceo-py <sfmf@abv.bg>
2023-08-24 14:07:42 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/5_pc_game_shop.py
+++ PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/5_pc_game_shop.py
@@ -1,32 +1,66 @@
-number_sell_games = int(input())
+number_sold_games = int(input())
 
-hearthstone_sold_games = 0
-fornite_sold_games = 0
-overwatch_sold_games = 0
-other_sold_games = 0
+hearthstone = 0
+fornite = 0
+overwatch = 0
+others = 0
 
-for _ in range(0, number_sell_games):
+for game in range(number_sold_games):
 
     game_name = input()
 
-    if game_name == "Hearthstone":
-        hearthstone_sold_games += 1
+    if game_name == 'Hearthstone':
+        hearthstone += 1
 
-    elif game_name == "Fornite":
-        fornite_sold_games += 1
+    elif game_name == 'Fornite':
+        fornite += 1
 
-    elif game_name == "Overwatch":
-        overwatch_sold_games += 1
+    elif game_name == 'Overwatch':
+        overwatch += 1
 
     else:
-        other_sold_games += 1
+        others += 1
 
-hearthstone_percent_sold_games = (hearthstone_sold_games / number_sell_games) * 100
-fornite_sold_games = (fornite_sold_games / number_sell_games) * 100
-overwatch_sold_games = (overwatch_sold_games / number_sell_games) * 100
-other_sold_games = (other_sold_games / number_sell_games) * 100
+total_game = hearthstone + fornite + others + overwatch
 
-print(f"Hearthstone - {hearthstone_percent_sold_games:.2f}%")
-print(f"Fornite - {fornite_sold_games:.2f}%")
-print(f"Overwatch - {overwatch_sold_games:.2f}%")
-print(f"Others - {other_sold_games:.2f}%")
+print(f'Hearthstone - {hearthstone / total_game * 100:.2f}%')
+print(f'Fornite - {fornite / total_game * 100:.2f}%')
+print(f'Overwatch - {overwatch / total_game * 100:.2f}%')
+print(f'Others - {others / total_game * 100:.2f}%')
+
+
+
+
+
+# number_sell_games = int(input())
+#
+# hearthstone_sold_games = 0
+# fornite_sold_games = 0
+# overwatch_sold_games = 0
+# other_sold_games = 0
+#
+# for _ in range(0, number_sell_games):
+#
+#     game_name = input()
+#
+#     if game_name == "Hearthstone":
+#         hearthstone_sold_games += 1
+#
+#     elif game_name == "Fornite":
+#         fornite_sold_games += 1
+#
+#     elif game_name == "Overwatch":
+#         overwatch_sold_games += 1
+#
+#     else:
+#         other_sold_games += 1
+#
+# hearthstone_percent_sold_games = (hearthstone_sold_games / number_sell_games) * 100
+# fornite_sold_games = (fornite_sold_games / number_sell_games) * 100
+# overwatch_sold_games = (overwatch_sold_games / number_sell_games) * 100
+# other_sold_games = (other_sold_games / number_sell_games) * 100
+#
+# print(f"Hearthstone - {hearthstone_percent_sold_games:.2f}%")
+# print(f"Fornite - {fornite_sold_games:.2f}%")
+# print(f"Overwatch - {overwatch_sold_games:.2f}%")
+# print(f"Others - {other_sold_games:.2f}%")
