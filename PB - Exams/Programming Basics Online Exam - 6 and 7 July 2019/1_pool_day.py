de15bfbb09e01b9e09c64b1282d8c576d6b75fa0
ceo-py <sfmf@abv.bg>
2023-08-24 14:01:57 +0300

    added another solution

--- PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/1_pool_day.py
+++ PB - Exams/Programming Basics Online Exam - 6 and 7 July 2019/1_pool_day.py
@@ -1,14 +1,35 @@
-import math
+from math import ceil
 
-number_people = int(input())
-entry_tax = float(input())
-price_deck_chair = float(input())
-price_umbrella = float(input())
+people_number = int(input())
+tax = float(input())
+sleeping_bed = float(input())
+unbrela = float(input())
 
-total_entry_tax = number_people * entry_tax
-deck_chair_money = math.ceil(number_people * 0.75) * price_deck_chair
-umbrella_money = math.ceil(number_people / 2) * price_umbrella
 
-total = total_entry_tax + deck_chair_money + umbrella_money
+total_taxes = people_number * tax
+unbrelas_total_price = ceil(people_number / 2) * unbrela
+sleeping_bed_total_price = ceil(people_number * 0.75) * sleeping_bed
 
-print(f"{total:.2f} lv.")
+total = total_taxes + unbrelas_total_price + sleeping_bed_total_price
+
+print(f'{total:.2f} lv.')
+
+
+
+
+
+
+# import math
+#
+# number_people = int(input())
+# entry_tax = float(input())
+# price_deck_chair = float(input())
+# price_umbrella = float(input())
+#
+# total_entry_tax = number_people * entry_tax
+# deck_chair_money = math.ceil(number_people * 0.75) * price_deck_chair
+# umbrella_money = math.ceil(number_people / 2) * price_umbrella
+#
+# total = total_entry_tax + deck_chair_money + umbrella_money
+#
+# print(f"{total:.2f} lv.")
