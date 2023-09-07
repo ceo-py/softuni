afa530952f305c31cc6883a740b61ab1a1fd3dca
ceo-py <sfmf@abv.bg>
2023-09-07 15:48:04 +0300

    added another solution

--- Python Basics/While Loop - Exercise/2_exam_preparation.py
+++ Python Basics/While Loop - Exercise/2_exam_preparation.py
@@ -1,25 +1,61 @@
 allow_bad_grades = int(input())
 
-taks_names = list()
-scores = 0
-penalties = 0
-total_taks = 0
-task_score = 0
+total_score = 0
+task = input()
+total_bad_grades = 0
+problems_counter = 0
+last_problem = ''
 
-while True:
-    taks_names.append(input())
-    if "Enough" in taks_names:
-        average_score = (scores / total_taks)
-        print(f"Average score: {average_score:.2f}\nNumber of problems: {total_taks}\nLast problem: {taks_names[-2]}")
-        break
-    task_score = int(input())
-    if task_score <= 4:
-        penalties += 1
-        total_taks += 1
-        scores += task_score
-    else:
-        scores += task_score
-        total_taks += 1
-    if penalties == allow_bad_grades:
-        print(f"You need a break, {penalties} poor grades.")
-        break
+while task != 'Enough':
+
+    score = int(input())
+    total_score += score
+    problems_counter += 1
+
+    if score <= 4:
+        total_bad_grades += 1
+
+        if allow_bad_grades == total_bad_grades:
+            print(f'You need a break, {total_bad_grades} poor grades.')
+            break
+    last_problem = task
+    task = input()
+
+else:
+    print(f'Average score: {total_score / problems_counter:.2f}')
+    print(f'Number of problems: {problems_counter}')
+    print(f'Last problem: {last_problem}')
+
+
+
+
+
+
+
+
+
+# allow_bad_grades = int(input())
+#
+# taks_names = list()
+# scores = 0
+# penalties = 0
+# total_taks = 0
+# task_score = 0
+#
+# while True:
+#     taks_names.append(input())
+#     if "Enough" in taks_names:
+#         average_score = (scores / total_taks)
+#         print(f"Average score: {average_score:.2f}\nNumber of problems: {total_taks}\nLast problem: {taks_names[-2]}")
+#         break
+#     task_score = int(input())
+#     if task_score <= 4:
+#         penalties += 1
+#         total_taks += 1
+#         scores += task_score
+#     else:
+#         scores += task_score
+#         total_taks += 1
+#     if penalties == allow_bad_grades:
+#         print(f"You need a break, {penalties} poor grades.")
+#         break
