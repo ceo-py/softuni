ae8c2fde2ae0c0b96a631508347d571b1f46ecc6
ceo-py <sfmf@abv.bg>
2023-09-07 12:20:41 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Lab/8_cinema_ticket.py
+++ Python Basics/Conditional Statements Advanced - Lab/8_cinema_ticket.py
@@ -1,13 +1,26 @@
 day_of_the_week = input()
 
-week = {
-    "Monday": 12,
-    "Tuesday": 12,
-    "Wednesday": 14,
-    "Thursday": 14,
-    "Friday": 12,
-    "Saturday": 16,
-    "Sunday": 16
-}
+output = 16
 
-print(week[day_of_the_week])
+if day_of_the_week in 'Monday, Tuesday, Friday':
+    output = 12
+
+elif day_of_the_week in 'Wednesday, Thursday':
+    output = 14
+
+print(output)
+
+
+# day_of_the_week = input()
+#
+# week = {
+#     "Monday": 12,
+#     "Tuesday": 12,
+#     "Wednesday": 14,
+#     "Thursday": 14,
+#     "Friday": 12,
+#     "Saturday": 16,
+#     "Sunday": 16
+# }
+#
+# print(week[day_of_the_week])
