35c191c6129339219d947e7d3db5e1ded6fa2b6c
ceo-py <sfmf@abv.bg>
2023-09-07 16:46:20 +0300

    added another solution

--- Python Basics/While Loop - Lab/3_sum_numbers.py
+++ Python Basics/While Loop - Lab/3_sum_numbers.py
@@ -1,9 +1,22 @@
 number = int(input())
 total = 0
-while True:
-    a = int(input())
-    total += a
-    if total >= number:
-        break
+while total < number:
+    added_number = int(input())
+    total += added_number
 
 print(total)
+
+
+
+
+
+
+# number = int(input())
+# total = 0
+# while True:
+#     a = int(input())
+#     total += a
+#     if total >= number:
+#         break
+#
+# print(total)
