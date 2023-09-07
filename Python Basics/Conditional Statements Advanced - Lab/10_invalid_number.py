a145d203f45448c51bf7edda374dddc704c476a3
ceo-py <sfmf@abv.bg>
2023-09-07 12:26:49 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Lab/10_invalid_number.py
+++ Python Basics/Conditional Statements Advanced - Lab/10_invalid_number.py
@@ -1,6 +1,15 @@
 number_check = int(input())
 
-if number_check == 0:
-    print("")
-elif number_check not in range(100, 201):
-    print("invalid")
+if number_check != 0 and not 100 <= number_check <= 200:
+    print('invalid')
+
+
+
+
+
+# number_check = int(input())
+#
+# if number_check == 0:
+#     print("")
+# elif number_check not in range(100, 201):
+#     print("invalid")
