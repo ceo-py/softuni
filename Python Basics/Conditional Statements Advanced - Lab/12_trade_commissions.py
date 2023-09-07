6900f55970051bdbe63e7033ef33e58386b1fd7e
ceo-py <sfmf@abv.bg>
2023-09-07 12:36:45 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Lab/12_trade_commissions.py
+++ Python Basics/Conditional Statements Advanced - Lab/12_trade_commissions.py
@@ -1,47 +1,106 @@
 city = input()
 selling_volume = float(input())
-comission = 0
-flag = True
 
-if city == 'Sofia':
-    if 0 <= selling_volume <= 500:
-        comission = selling_volume * 0.05
-    elif 500 < selling_volume <= 1000:
-        comission = selling_volume * 0.07
-    elif 1000 < selling_volume <= 10000:
-        comission = selling_volume * 0.08
-    elif 10000 < selling_volume:
-        comission = selling_volume * 0.12
-    else:
-        flag = False
+
+commission = 0
+output = 'error'
+
+if selling_volume < 0:
+    pass
+
+elif city == 'Sofia':
+
+    if selling_volume <= 500:
+        commission = 0.05
+
+    elif selling_volume <= 1000:
+        commission = 0.07
+
+    elif selling_volume <= 10_000:
+        commission = 0.08
+
+    elif selling_volume > 10_000:
+        commission = 0.12
+
 elif city == 'Varna':
-    if 0 <= selling_volume <= 500:
-        comission = selling_volume * 0.045
-    elif 500 < selling_volume <= 1000:
-        comission = selling_volume * 0.075
-    elif 1000 < selling_volume <= 10000:
-        comission = selling_volume * 0.10
-    elif 10000 < selling_volume:
-        comission = selling_volume * 0.13
-    else:
-        flag = False
+
+    if selling_volume <= 500:
+        commission = 0.045
+
+    elif selling_volume <= 1000:
+        commission = 0.075
+
+    elif selling_volume <= 10_000:
+        commission = 0.10
+
+    elif selling_volume > 10_000:
+        commission = 0.13
+
 elif city == 'Plovdiv':
-    if 0 <= selling_volume <= 500:
-        comission = selling_volume * 0.055
-    elif 500 < selling_volume <= 1000:
-        comission = selling_volume * 0.08
-    elif 1000 < selling_volume <= 10000:
-        comission = selling_volume * 0.12
-    elif 10000 < selling_volume:
-        comission = selling_volume * 0.145
-    else:
-        flag = False
-else:
-    flag = False
-if flag:
-    print(f'{comission:.2f}')
-else:
-    print('error')
+
+    if selling_volume <= 500:
+        commission = 0.055
+
+    elif selling_volume <= 1000:
+        commission = 0.08
+
+    elif selling_volume <= 10_000:
+        commission = 0.12
+
+    elif selling_volume > 10_000:
+        commission = 0.145
+
+if commission:
+    output = f'{selling_volume * commission:.2f}'
+print(output)
+
+
+
+
+# city = input()
+# selling_volume = float(input())
+# comission = 0
+# flag = True
+#
+# if city == 'Sofia':
+#     if 0 <= selling_volume <= 500:
+#         comission = selling_volume * 0.05
+#     elif 500 < selling_volume <= 1000:
+#         comission = selling_volume * 0.07
+#     elif 1000 < selling_volume <= 10000:
+#         comission = selling_volume * 0.08
+#     elif 10000 < selling_volume:
+#         comission = selling_volume * 0.12
+#     else:
+#         flag = False
+# elif city == 'Varna':
+#     if 0 <= selling_volume <= 500:
+#         comission = selling_volume * 0.045
+#     elif 500 < selling_volume <= 1000:
+#         comission = selling_volume * 0.075
+#     elif 1000 < selling_volume <= 10000:
+#         comission = selling_volume * 0.10
+#     elif 10000 < selling_volume:
+#         comission = selling_volume * 0.13
+#     else:
+#         flag = False
+# elif city == 'Plovdiv':
+#     if 0 <= selling_volume <= 500:
+#         comission = selling_volume * 0.055
+#     elif 500 < selling_volume <= 1000:
+#         comission = selling_volume * 0.08
+#     elif 1000 < selling_volume <= 10000:
+#         comission = selling_volume * 0.12
+#     elif 10000 < selling_volume:
+#         comission = selling_volume * 0.145
+#     else:
+#         flag = False
+# else:
+#     flag = False
+# if flag:
+#     print(f'{comission:.2f}')
+# else:
+#     print('error')
 
 
 
