13bf860a1c6c2b603553f9aa1b3360637a50ebf0
ceo-py <sfmf@abv.bg>
2023-09-07 16:34:50 +0300

    added another solution

--- Python Basics/While Loop - Exercise/6_cake.py
+++ Python Basics/While Loop - Exercise/6_cake.py
@@ -1,19 +1,48 @@
 cake_width = int(input())
 cake_length = int(input())
+cake_slices = input()
 
 cake_size = cake_width * cake_length
-cake_slices_taken = 0
 
-while True:
+while cake_slices != 'STOP':
+
+    cake_slices = int(cake_slices)
+    cake_size -= cake_slices
+
+    if cake_size <= 0:
+        print(f"No more cake left! You need {abs(cake_size)} pieces more.")
+        break
+
     cake_slices = input()
 
-    if cake_slices == "STOP":
-        cake_size += - cake_slices_taken
-        print(f"{cake_size} pieces are left.")
-        break
+else:
+    print(f"{cake_size} pieces are left.")
 
-    cake_slices_taken += + int(cake_slices)
-    if cake_size < cake_slices_taken:
-        cake_slices_taken += - cake_size
-        print(f"No more cake left! You need {cake_slices_taken} pieces more.")
-        break
+
+
+
+
+
+
+
+
+
+# cake_width = int(input())
+# cake_length = int(input())
+#
+# cake_size = cake_width * cake_length
+# cake_slices_taken = 0
+#
+# while True:
+#     cake_slices = input()
+#
+#     if cake_slices == "STOP":
+#         cake_size -= cake_slices_taken
+#         print(f"{cake_size} pieces are left.")
+#         break
+#
+#     cake_slices_taken += int(cake_slices)
+#     if cake_size < cake_slices_taken:
+#         cake_slices_taken -= cake_size
+#         print(f"No more cake left! You need {cake_slices_taken} pieces more.")
+#         break
