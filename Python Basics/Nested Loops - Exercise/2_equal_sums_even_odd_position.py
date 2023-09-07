5ad1b8e3153f4d0cece47026c748fe04e0a134b1
ceo-py <sfmf@abv.bg>
2023-09-07 13:28:38 +0300

    added another solution

--- Python Basics/Nested Loops - Exercise/2_equal_sums_even_odd_position.py
+++ Python Basics/Nested Loops - Exercise/2_equal_sums_even_odd_position.py
@@ -1,18 +1,43 @@
 number_one = int(input())
 number_two = int(input())
 
-for number in range(number_one, number_two + 1):
-    number_to_str = str(number)
-    even_sum = 0
-    odd_sum = 0
 
-    for index, digit in enumerate(number_to_str):
-        if index % 2 == 0:
-            even_sum += int(digit)
+for number in range(number_one, number_two + 1):
+
+    odd_sum = 0
+    even_sum = 0
+    number = str(number)
+
+    for i in range(len(number)):
+        num = int(number[i])
+
+        if i % 2 == 0:
+            even_sum += num
+
         else:
-            odd_sum += int(digit)
-    if even_sum == odd_sum:
-        print(number, end=" ")
+            odd_sum += num
+
+    if odd_sum == even_sum:
+        print(number, end=' ')
+
+
+
+
+# number_one = int(input())
+# number_two = int(input())
+#
+# for number in range(number_one, number_two + 1):
+#     number_to_str = str(number)
+#     even_sum = 0
+#     odd_sum = 0
+#
+#     for index, digit in enumerate(number_to_str):
+#         if index % 2 == 0:
+#             even_sum += int(digit)
+#         else:
+#             odd_sum += int(digit)
+#     if even_sum == odd_sum:
+#         print(number, end=" ")
 
 
 
