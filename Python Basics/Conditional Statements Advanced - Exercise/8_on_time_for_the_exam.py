1fa38af6e6d1d1cf98086bd17e76fdd9841215b7
ceo-py <sfmf@abv.bg>
2023-09-07 11:53:56 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Exercise/8_on_time_for_the_exam.py
+++ Python Basics/Conditional Statements Advanced - Exercise/8_on_time_for_the_exam.py
@@ -8,25 +8,84 @@ time_for_arrival = hour_of_arrival * 60 + minutes_of_arrival
 diff = abs(time_for_exam - time_for_arrival)
 hour = diff // 60
 minutes = diff % 60
+
+
 if time_for_exam == time_for_arrival:
     print('On Time')
 
-elif time_for_arrival > time_for_exam:
-    print('Late')
-    if diff > 59:
-        print(f'{hour}:{minutes:02d} hours after the start')
-    else:
-        print(f'{minutes} minutes after the start')
-elif time_for_arrival < time_for_exam:
-    if diff <= 30:
-        print('On time')
-        print(f'{diff} minutes before the start')
-    else:
-        print('Early')
-        if diff > 59:
-            print(f'{hour}:{minutes:02d} hours before the start')
+if time_for_arrival != time_for_exam:
+    diff = time_for_arrival - time_for_exam
+    on_time = 'after'
+    status = 'Late'
+
+    if diff < 0:
+        on_time = 'before'
+
+        if diff >= -30:
+            status = 'On time'
         else:
-            print(f'{minutes} minutes before the start')
+            status = 'Early'
+
+    print(status)
+
+    if abs(diff) // 60 == 0:
+        print(f'{minutes} minutes {on_time} the start')
+
+    else:
+        print(f'{hour}:{minutes:02d} hours {on_time} the start')
+
+
+
+
+
+
+
+
+
+
+
+
+# hour_of_exam = int(input())
+# minutes_of_exam = int(input())
+# hour_of_arrival = int(input())
+# minutes_of_arrival = int(input())
+#
+# time_for_exam = hour_of_exam * 60 + minutes_of_exam
+# time_for_arrival = hour_of_arrival * 60 + minutes_of_arrival
+# diff = abs(time_for_exam - time_for_arrival)
+# hour = diff // 60
+# minutes = diff % 60
+# if time_for_exam == time_for_arrival:
+#     print('On Time')
+#
+# elif time_for_arrival > time_for_exam:
+#     print('Late')
+#     if diff > 59:
+#         print(f'{hour}:{minutes:02d} hours after the start')
+#     else:
+#         print(f'{minutes} minutes after the start')
+# elif time_for_arrival < time_for_exam:
+#     if diff <= 30:
+#         print('On time')
+#         print(f'{diff} minutes before the start')
+#     else:
+#         print('Early')
+#         if diff > 59:
+#             print(f'{hour}:{minutes:02d} hours before the start')
+#         else:
+#             print(f'{minutes} minutes before the start')
+
+
+
+
+
+
+
+
+
+
+
+
 
 #
 #
