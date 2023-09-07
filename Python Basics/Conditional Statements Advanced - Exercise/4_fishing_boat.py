8924c98c5dcdac4e99d35bc992b734cd7418bd38
ceo-py <sfmf@abv.bg>
2023-09-07 11:06:02 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Exercise/4_fishing_boat.py
+++ Python Basics/Conditional Statements Advanced - Exercise/4_fishing_boat.py
@@ -2,29 +2,79 @@ budget = int(input())
 season = input()
 fishers = int(input())
 
-total_price = 0
 
-if season == "Spring":
-    total_price = 3000
-elif season == "Winter":
-    total_price = 2600
-else:
-    total_price = 4200
+rent = 0
+discount = 1
+additional_discount = 1
+
+if season == 'Spring':
+    rent = 3000
+
+elif season in 'Summer, Autumn':
+    rent = 4200
+
+elif season == 'Winter':
+    rent = 2600
 
 if fishers <= 6:
-    total_price *= 0.90
-elif 7 <= fishers <= 11:
-    total_price *= 0.85
-else:
-    total_price *= 0.75
+    discount = 0.90
 
-if fishers % 2 == 0 and season != "Autumn":
-    total_price *= 0.95
+elif fishers <= 11:
+    discount = 0.85
 
-if total_price <= budget:
-    print(f"Yes! You have {budget - total_price:.2f} leva left.")
+elif fishers >= 12:
+    discount = 0.75
+
+total_rent = rent * discount
+
+
+if fishers % 2 == 0 and season != 'Autumn':
+    additional_discount = 0.95
+
+
+total_rent = budget - (total_rent * additional_discount)
+
+
+if total_rent >= 0:
+    print(f'Yes! You have {total_rent:.2f} leva left.')
+
 else:
-    print(f"Not enough money! You need {total_price - budget:.2f} leva.")
+    print(f'Not enough money! You need {abs(total_rent):.2f} leva.')
+
+
+
+
+
+
+
+
+# budget = int(input())
+# season = input()
+# fishers = int(input())
+#
+# total_price = 0
+#
+# if season == "Spring":
+#     total_price = 3000
+# elif season == "Winter":
+#     total_price = 2600
+# else:
+#     total_price = 4200
+#
+# if fishers <= 6:
+#     total_price *= 0.90
+# elif 7 <= fishers <= 11:
+#     total_price *= 0.85
+# else:
+#     total_price *= 0.75
+#
+# if fishers % 2 == 0 and season != "Autumn":
+#     total_price *= 0.95
+#
+# if total_price <= budget:
+#     print(f"Yes! You have {budget - total_price:.2f} leva left.")
+# else:
+#     print(f"Not enough money! You need {total_price - budget:.2f} leva.")
 
 
 
