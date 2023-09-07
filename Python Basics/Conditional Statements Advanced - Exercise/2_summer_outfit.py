e29db6ceb0854a6fefe66bdea6fa817c12ad72c5
ceo-py <sfmf@abv.bg>
2023-09-07 10:46:25 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Exercise/2_summer_outfit.py
+++ Python Basics/Conditional Statements Advanced - Exercise/2_summer_outfit.py
@@ -1,43 +1,97 @@
 degrees = int(input())
 time_of_the_day = input()
 
-outfit = None
-shoes = None
+outfit = ''
+shoes = ''
 
 if 10 <= degrees <= 18:
-    if time_of_the_day == "Morning":
-        outfit = "Sweatshirt"
-        shoes = "Sneakers"
-    elif time_of_the_day == "Afternoon":
-        outfit = "Shirt"
-        shoes = "Moccasins"
-    elif time_of_the_day == "Evening":
-        outfit = "Shirt"
-        shoes = "Moccasins"
 
-elif 18 < degrees <= 24:
-    if time_of_the_day == "Morning":
-        outfit = "Shirt"
-        shoes = "Moccasins"
-    elif time_of_the_day == "Afternoon":
-        outfit = "T-Shirt"
-        shoes = "Sandals"
-    elif time_of_the_day == "Evening":
-        outfit = "T-Shirt"
-        shoes = "Moccasins"
+    if time_of_the_day == 'Morning':
+        outfit = 'Sweatshirt'
+        shoes = 'Sneakers'
+
+    elif time_of_the_day == 'Afternoon':
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+
+    elif time_of_the_day == 'Evening':
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+
+elif degrees <= 24:
+
+    if time_of_the_day == 'Morning':
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+
+    elif time_of_the_day == 'Afternoon':
+        outfit = 'T-Shirt'
+        shoes = 'Sandals'
+
+    elif time_of_the_day == 'Evening':
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
 
 elif degrees >= 25:
-    if time_of_the_day == "Morning":
-        outfit = "T-Shirt"
-        shoes = "Sandals"
-    elif time_of_the_day == "Afternoon":
-        outfit = "Swim Suit"
-        shoes = "Barefoot"
-    elif time_of_the_day == "Evening":
-        outfit = "Shirt"
-        shoes = "Moccasins"
+
+    if time_of_the_day == 'Morning':
+        outfit = 'T-Shirt'
+        shoes = 'Sandals'
+
+    elif time_of_the_day == 'Afternoon':
+        outfit = 'Swim Suit'
+        shoes = 'Barefoot'
+
+    elif time_of_the_day == 'Evening':
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
 
 print(f"It's {degrees} degrees, get your {outfit} and {shoes}.")
+
+
+
+
+
+# degrees = int(input())
+# time_of_the_day = input()
+#
+# outfit = None
+# shoes = None
+#
+# if 10 <= degrees <= 18:
+#     if time_of_the_day == "Morning":
+#         outfit = "Sweatshirt"
+#         shoes = "Sneakers"
+#     elif time_of_the_day == "Afternoon":
+#         outfit = "Shirt"
+#         shoes = "Moccasins"
+#     elif time_of_the_day == "Evening":
+#         outfit = "Shirt"
+#         shoes = "Moccasins"
+#
+# elif 18 < degrees <= 24:
+#     if time_of_the_day == "Morning":
+#         outfit = "Shirt"
+#         shoes = "Moccasins"
+#     elif time_of_the_day == "Afternoon":
+#         outfit = "T-Shirt"
+#         shoes = "Sandals"
+#     elif time_of_the_day == "Evening":
+#         outfit = "T-Shirt"
+#         shoes = "Moccasins"
+#
+# elif degrees >= 25:
+#     if time_of_the_day == "Morning":
+#         outfit = "T-Shirt"
+#         shoes = "Sandals"
+#     elif time_of_the_day == "Afternoon":
+#         outfit = "Swim Suit"
+#         shoes = "Barefoot"
+#     elif time_of_the_day == "Evening":
+#         outfit = "Shirt"
+#         shoes = "Moccasins"
+#
+# print(f"It's {degrees} degrees, get your {outfit} and {shoes}.")
 
 
 
