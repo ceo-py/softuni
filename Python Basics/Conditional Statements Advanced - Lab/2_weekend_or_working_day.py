14a730aead8f76bce26e4cf539a21c28d2a7a432
ceo-py <sfmf@abv.bg>
2023-09-07 12:06:12 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Lab/2_weekend_or_working_day.py
+++ Python Basics/Conditional Statements Advanced - Lab/2_weekend_or_working_day.py
@@ -1,23 +1,33 @@
-check_what_day_is = input()
+day = input()
+
+output = 'Error'
+
+if day in 'Monday, Tuesday, Wednesday, Thursday, Friday':
+    output = 'Working day'
+
+elif day in 'Saturday, Sunday':
+    output = 'Weekend'
+
+
+print(output)
 
 
 
+# check_what_day_is = input()
+#
+# week = {
+#     "Monday": "Working day",
+#     "Tuesday": "Working day",
+#     "Wednesday": "Working day",
+#     "Thursday": "Working day",
+#     "Friday": "Working day",
+#     "Saturday": "Weekend",
+#     "Sunday": "Weekend",
+#
+# }
+#
+# print(week.get(check_what_day_is, 'Error'))
 
-week = {
-    "Monday": "Working day",
-    "Tuesday": "Working day",
-    "Wednesday": "Working day",
-    "Thursday": "Working day",
-    "Friday": "Working day",
-    "Saturday": "Weekend",
-    "Sunday": "Weekend",
-
-}
-
-if check_what_day_is in week:
-    print(week[check_what_day_is])
-else:
-    print("Error")
 
 
 
