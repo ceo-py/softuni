0d3502990c608dec12a1fe6663dd4706e49f9368
ceo-py <sfmf@abv.bg>
2023-09-07 13:09:44 +0300

    added another solution

--- Python Basics/For Loop - Lab/7_sum_numbers.py
+++ Python Basics/For Loop - Lab/7_sum_numbers.py
@@ -1,10 +1,22 @@
 enter_number = int(input())
-numbers = list()
-total = 0
-for n in range(0, enter_number):
-    numbers_to_sum = int(input())
-    numbers.append(numbers_to_sum)
 
-for _ in numbers:
-    total += _
-print(total)
+total_sum = 0
+
+for num in range(enter_number):
+    total_sum += int(input())
+
+print(total_sum)
+
+
+
+
+# enter_number = int(input())
+# numbers = list()
+# total = 0
+# for n in range(0, enter_number):
+#     numbers_to_sum = int(input())
+#     numbers.append(numbers_to_sum)
+#
+# for _ in numbers:
+#     total += _
+# print(total)
