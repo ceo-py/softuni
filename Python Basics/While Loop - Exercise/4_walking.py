d0f58536497a413887ea4e497f01bf294663ab25
ceo-py <sfmf@abv.bg>
2023-09-07 16:20:32 +0300

    added another solution

--- Python Basics/While Loop - Exercise/4_walking.py
+++ Python Basics/While Loop - Exercise/4_walking.py
@@ -1,21 +1,44 @@
-total_steps = 0
-goal = 10000
-while True:
-    steps_walk = input()
-    if steps_walk == "Going home":
-        steps_walk_in_a_day = int(input())
-        total_steps += steps_walk_in_a_day
-        if total_steps >= goal:
-            total_steps += - goal
-            print(f"Goal reached! Good job!\n{total_steps} steps over the goal!")
-            break
-        else:
-            total_steps = goal - total_steps
-            print(f"{total_steps} more steps to reach goal.")
-            break
-    total_steps += int(steps_walk)
+goal_steps = 10_000
 
-    if total_steps >= goal:
-        total_steps += - goal
-        print(f"Goal reached! Good job!\n{total_steps} steps over the goal!")
+
+while goal_steps > 0:
+    steps = input()
+
+    if steps == 'Going home':
+        goal_steps -= int(input())
         break
+
+    goal_steps -= int(steps)
+
+
+if goal_steps <= 0:
+        print('Goal reached! Good job!')
+        print(f'{abs(goal_steps)} steps over the goal!')
+else:
+    print(f'{abs(goal_steps)} more steps to reach goal.')
+
+
+
+
+
+# total_steps = 0
+# goal = 10000
+# while True:
+#     steps_walk = input()
+#     if steps_walk == "Going home":
+#         steps_walk_in_a_day = int(input())
+#         total_steps += steps_walk_in_a_day
+#         if total_steps >= goal:
+#             total_steps += - goal
+#             print(f"Goal reached! Good job!\n{total_steps} steps over the goal!")
+#             break
+#         else:
+#             total_steps = goal - total_steps
+#             print(f"{total_steps} more steps to reach goal.")
+#             break
+#     total_steps += int(steps_walk)
+#
+#     if total_steps >= goal:
+#         total_steps += - goal
+#         print(f"Goal reached! Good job!\n{total_steps} steps over the goal!")
+#         break
