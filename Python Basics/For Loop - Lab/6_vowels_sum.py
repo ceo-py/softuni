7ec65e6f3003f6e6ffc79d3c2cd7b8be2b8b228f
ceo-py <sfmf@abv.bg>
2023-09-07 13:07:16 +0300

    added another solution

--- Python Basics/For Loop - Lab/6_vowels_sum.py
+++ Python Basics/For Loop - Lab/6_vowels_sum.py
@@ -1,15 +1,40 @@
 text_input = input()
 
-letters = {
-    "a": 1,
-    "e": 2,
-    "i": 3,
-    "o": 4,
-    "u": 5
-}
 summary = 0
-letter_check = ("a", "e", "i", "o" ,"u")
+
 for letter in text_input:
-    if letter in letter_check:
-        summary += letters[letter]
+
+    if letter == 'a':
+        summary += 1
+
+    elif letter == 'e':
+        summary += 2
+
+    elif letter == 'i':
+        summary += 3
+
+    elif letter == 'o':
+        summary += 4
+
+    elif letter == 'u':
+        summary += 5
+
 print(summary)
+
+
+
+
+
+# text_input = input()
+#
+# letters = {
+#     "a": 1,
+#     "e": 2,
+#     "i": 3,
+#     "o": 4,
+#     "u": 5
+# }
+# summary = 0
+# for letter in text_input:
+#     summary += letters.get(letter, 0)
+# print(summary)
