da6ec3ef608c4c74611db63ce948ec490f2ad300
ceo-py <sfmf@abv.bg>
2023-09-07 13:01:10 +0300

    added another solution

--- Python Basics/For Loop - Exercise/5_salary.py
+++ Python Basics/For Loop - Exercise/5_salary.py
@@ -1,30 +1,62 @@
 open_tab_browser = int(input())
 salary = int(input())
 
-salary_info = {"penalty": {
-    "Facebook": 150,
-    "Instagram": 100,
-    "Reddit": 50, },
-    "tabs counter": {
-        "Facebook": 0,
-        "Instagram": 0,
-        "Reddit": 0}
-}
-salary_left = salary
-tabs_check = ["Facebook", "Instagram", "Reddit"]
-no_money_left = False
 
-for tab in range(0, open_tab_browser):
-    tab_name = input()
-    if tab_name in tabs_check:
-        salary_left = salary_left - salary_info["penalty"][tab_name]
-    if salary_left <= 0:
-        no_money_left = True
-        print("You have lost your salary.")
+for tab_number in range(open_tab_browser):
+
+    tab = input()
+
+    if tab == 'Facebook':
+        salary -= 150
+
+    elif tab == 'Instagram':
+        salary -= 100
+
+    elif tab == 'Reddit':
+        salary -= 50
+
+    if salary <= 0:
+        print('You have lost your salary.')
         break
 
-if no_money_left == False:
-    if salary_left == salary:
-        print(salary_left)
-    else:
-        print(salary_left)
+else:
+    print(salary)
+
+
+
+
+
+
+
+
+
+# open_tab_browser = int(input())
+# salary = int(input())
+#
+# salary_info = {"penalty": {
+#     "Facebook": 150,
+#     "Instagram": 100,
+#     "Reddit": 50, },
+#     "tabs counter": {
+#         "Facebook": 0,
+#         "Instagram": 0,
+#         "Reddit": 0}
+# }
+# salary_left = salary
+# tabs_check = ["Facebook", "Instagram", "Reddit"]
+# no_money_left = False
+#
+# for tab in range(0, open_tab_browser):
+#     tab_name = input()
+#     if tab_name in tabs_check:
+#         salary_left = salary_left - salary_info["penalty"][tab_name]
+#     if salary_left <= 0:
+#         no_money_left = True
+#         print("You have lost your salary.")
+#         break
+#
+# if no_money_left == False:
+#     if salary_left == salary:
+#         print(salary_left)
+#     else:
+#         print(salary_left)
