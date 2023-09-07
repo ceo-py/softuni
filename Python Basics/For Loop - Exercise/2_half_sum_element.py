14306f4f5a344cd573d9bc2b2a43f6bec41e323c
ceo-py <sfmf@abv.bg>
2023-09-07 12:47:15 +0300

    added another solution

--- Python Basics/For Loop - Exercise/2_half_sum_element.py
+++ Python Basics/For Loop - Exercise/2_half_sum_element.py
@@ -1,17 +1,48 @@
 numbers_to_enter = int(input())
 
-numbers_input = list()
+total_sum = 0
+max_number = 0
 
-for i in range(numbers_to_enter):
-    numbers_input.append(int(input()))
+for number in range(numbers_to_enter):
 
-numbers_input.sort()
-sum_of_all_numbers = sum(numbers_input) - numbers_input[-1]
-difrenc_numbers = numbers_input[-1] - (sum(numbers_input) - numbers_input[-1])
-if numbers_input[-1] == sum_of_all_numbers:
-    print(f"Yes\nSum = {numbers_input[-1]}")
+    input_number = int(input())
+
+    if input_number > max_number:
+        max_number = input_number
+
+    total_sum += input_number
+
+difference = total_sum - max_number
+
+if difference == max_number:
+    print('Yes')
+    print(f'Sum = {max_number}')
+
 else:
-    print(f"No\nDiff = {abs(difrenc_numbers)}")
+    print('No')
+    print(f'Diff = {abs(difference - max_number)}')
+
+
+
+
+# numbers_to_enter = int(input())
+#
+# numbers_input = list()
+#
+# for i in range(numbers_to_enter):
+#     numbers_input.append(int(input()))
+#
+# numbers_input.sort()
+# sum_of_all_numbers = sum(numbers_input) - numbers_input[-1]
+# difrenc_numbers = numbers_input[-1] - (sum(numbers_input) - numbers_input[-1])
+# if numbers_input[-1] == sum_of_all_numbers:
+#     print(f"Yes\nSum = {numbers_input[-1]}")
+# else:
+#     print(f"No\nDiff = {abs(difrenc_numbers)}")
+
+
+
+
 
 
 #
