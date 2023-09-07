52758971e76d4d43195835bb67e162c0842b35df
ceo-py <sfmf@abv.bg>
2023-09-07 12:02:23 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Lab/1_day_of_week.py
+++ Python Basics/Conditional Statements Advanced - Lab/1_day_of_week.py
@@ -1,16 +1,43 @@
-days_of_the_week = int(input())
+days_of_the_week = input()
 
-week = {
-    1: "Monday",
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday",
-    7: "Sunday"
-}
+output = 'Error'
 
-if days_of_the_week in range(1, 8):
-    print(week[days_of_the_week])
-else:
-    print("Error")
+if days_of_the_week == '1':
+    output = 'Monday'
+
+elif days_of_the_week == '2':
+    output = 'Tuesday'
+
+elif days_of_the_week == '3':
+    output = 'Wednesday'
+
+elif days_of_the_week == '4':
+    output = 'Thursday'
+
+elif days_of_the_week == '5':
+    output = 'Friday'
+
+elif days_of_the_week == '6':
+    output = 'Saturday'
+
+elif days_of_the_week == '7':
+    output = 'Sunday'
+
+print(output)
+
+
+
+# days_of_the_week = input()
+#
+# week = {
+#     "1": "Monday",
+#     "2": "Tuesday",
+#     "3": "Wednesday",
+#     "4": "Thursday",
+#     "5": "Friday",
+#     "6": "Saturday",
+#     "7": "Sunday"
+# }
+#
+# print(week.get(days_of_the_week, 'Error'))
+
