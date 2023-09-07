becd45ff002bddda8ac28d02c3d0235a273bec72
ceo-py <sfmf@abv.bg>
2023-09-07 10:38:56 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Exercise/1_cinema.py
+++ Python Basics/Conditional Statements Advanced - Exercise/1_cinema.py
@@ -2,11 +2,32 @@ type_screen_play = input()
 number_rows = int(input())
 number_columns = int(input())
 
-screen_type = {
-    "Premiere": 12.00,
-    "Normal": 7.50,
-    "Discount": 5.00
-}
-capacity = number_rows * number_columns
-total = screen_type[type_screen_play] * capacity
-print(f"{total:.2f} leva")
+ticket_price = 0
+
+if type_screen_play == 'Premiere':
+    ticket_price = 12
+
+elif type_screen_play == 'Normal':
+    ticket_price = 7.50
+
+elif type_screen_play == 'Discount':
+    ticket_price = 5
+
+total_sum = ticket_price * (number_rows * number_columns)
+print(f'{total_sum:.2f} leva')
+
+
+
+
+# type_screen_play = input()
+# number_rows = int(input())
+# number_columns = int(input())
+#
+# screen_type = {
+#     "Premiere": 12.00,
+#     "Normal": 7.50,
+#     "Discount": 5.00
+# }
+# capacity = number_rows * number_columns
+# total = screen_type[type_screen_play] * capacity
+# print(f"{total:.2f} leva")
