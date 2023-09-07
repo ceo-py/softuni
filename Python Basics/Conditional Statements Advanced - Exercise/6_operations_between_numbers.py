06e41cf1b0c17c6e8e520906a95243999426dbb0
ceo-py <sfmf@abv.bg>
2023-09-07 11:30:22 +0300

    added another solution

--- Python Basics/Conditional Statements Advanced - Exercise/6_operations_between_numbers.py
+++ Python Basics/Conditional Statements Advanced - Exercise/6_operations_between_numbers.py
@@ -2,25 +2,53 @@ number_one = int(input())
 number_two = int(input())
 operator = input()
 
-if number_one == 0 == number_two:
-    print(f"Cannot divide {number_one} by zero")
-elif operator == "/":
-    total = number_one / number_two
-    print(f"{number_one} / {number_two} = {total:.2f}")
-elif operator == "%":
-    total = number_one % number_two
-    print(f"{number_one} % {number_two} = {total}")
+
+if number_one == 0 or number_two == 0:
+    print(f'Cannot divide {number_one} by zero')
+
+elif operator == '%':
+    result = number_one % number_two
+    print(f'{number_one} % {number_two} = {result}')
+
+elif operator == '/':
+    result = number_one / number_two
+    print(f'{number_one} {operator} {number_two} = {result:.2f}')
+
 else:
-    if operator == "+":
-        total = number_one + number_two
-    elif operator == "-":
-        total = number_one - number_two
-    elif operator == "*":
-        total = number_one * number_two
-    if total % 2 == 0:
-        print(f"{number_one} {operator} {number_two} = {total} - even")
+    result = eval(f'{number_one} {operator} {number_two}')
+
+    if result % 2 == 0:
+        type_number = 'even'
     else:
-        print(f"{number_one} {operator} {number_two} = {total} - odd")
+        type_number = 'odd'
+
+    print(f'{number_one} {operator} {number_two} = {result} - {type_number}')
+
+
+
+# number_one = int(input())
+# number_two = int(input())
+# operator = input()
+#
+# if number_one == 0 == number_two:
+#     print(f"Cannot divide {number_one} by zero")
+# elif operator == "/":
+#     total = number_one / number_two
+#     print(f"{number_one} / {number_two} = {total:.2f}")
+# elif operator == "%":
+#     total = number_one % number_two
+#     print(f"{number_one} % {number_two} = {total}")
+# else:
+#     if operator == "+":
+#         total = number_one + number_two
+#     elif operator == "-":
+#         total = number_one - number_two
+#     elif operator == "*":
+#         total = number_one * number_two
+#     if total % 2 == 0:
+#         print(f"{number_one} {operator} {number_two} = {total} - even")
+#     else:
+#         print(f"{number_one} {operator} {number_two} = {total} - odd")
 
 
 
