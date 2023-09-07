ffc2e7069dd392412ba69060eaaeb21790ceacbd
ceo-py <sfmf@abv.bg>
2023-09-07 16:51:37 +0300

    added another solution

--- Python Basics/While Loop - Lab/5_account_balance.py
+++ Python Basics/While Loop - Lab/5_account_balance.py
@@ -1,12 +1,31 @@
 total = 0
-while True:
-    money_deposit = input()
-    if money_deposit[0] == "-":
+money_deposit = input()
+
+while money_deposit != 'NoMoreMoney':
+    money_deposit = float(money_deposit)
+    if money_deposit < 0:
         print("Invalid operation!")
         break
-    elif money_deposit == "NoMoreMoney":
-        break
-    else:
-        total += float(money_deposit)
-        print("Increase:","{:.2f}".format(float(money_deposit)))
+
+    total += money_deposit
+    print(f"Increase: {money_deposit:.2f}")
+    money_deposit = input()
+
 print(f"Total: {total:.2f}")
+
+
+
+
+
+# total = 0
+# while True:
+#     money_deposit = input()
+#     if money_deposit[0] == "-":
+#         print("Invalid operation!")
+#         break
+#     elif money_deposit == "NoMoreMoney":
+#         break
+#     else:
+#         total += float(money_deposit)
+#         print("Increase:","{:.2f}".format(float(money_deposit)))
+# print(f"Total: {total:.2f}")
