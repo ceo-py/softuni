c660219aac837af8ad31030c1cebe2f2ae344ec3
ceo-py <sfmf@abv.bg>
2023-09-08 13:49:37 +0300

    added another solution

--- Python-Fundamentals/Dictionaries - Exercise/1_count_chars_in_a_string.py
+++ Python-Fundamentals/Dictionaries - Exercise/1_count_chars_in_a_string.py
@@ -3,8 +3,7 @@ characters = input().replace(" ", "")
 character_count = {}
 
 for character in characters:
-    character_count[character] = character_count.get(character, 0)
-    character_count[character] += 1
+    character_count[character] = character_count.get(character, 0) + 1
 for key, value in character_count.items():
     print(f"{key} -> {value}")
 
