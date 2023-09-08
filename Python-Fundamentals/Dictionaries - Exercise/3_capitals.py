c78c2246155212f64bce362cfb8327a94ec55a48
ceo-py <sfmf@abv.bg>
2023-09-08 13:53:15 +0300

    added another solution

--- Python-Fundamentals/Dictionaries - Exercise/3_capitals.py
+++ Python-Fundamentals/Dictionaries - Exercise/3_capitals.py
@@ -1,8 +1,17 @@
 country_name = input().split(", ")
 capital_name = input().split(", ")
 
-country_information = {key: value for key, value in zip(country_name, capital_name)}
-# for country, capital in zip(country_name, capital_name):
-#     print(f"{country} -> {capital}")
+country_information = {country_name[i]: capital_name[i] for i in range(len(country_name))}
+
 for country, capital in country_information.items():
     print(f"{country} -> {capital}")
+
+
+
+# country_name = input().split(", ")
+# capital_name = input().split(", ")
+#
+# country_information = {key: value for key, value in zip(country_name, capital_name)}
+#
+# for country, capital in country_information.items():
+#     print(f"{country} -> {capital}")
