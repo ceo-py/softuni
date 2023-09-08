f9e85ee38a2bbbc37d8047a299031eaca0d9cfab
ceo-py <sfmf@abv.bg>
2023-09-08 12:02:55 +0300

    added another solution

--- PB - More Exercises/Conditional Statements Advanced - More Exercises/9_numbers_from_1_to_10.py
+++ PB - More Exercises/Conditional Statements Advanced - More Exercises/9_numbers_from_1_to_10.py
@@ -1,6 +1,30 @@
-i = 0
-while True:
-    i += 1
-    print(i)
-    if i == 10:
-        break
+for number in range(1, 11):
+    print(number)
+
+
+
+# number = 1
+# while number <= 10:
+#     print(number)
+#     number += 1
+
+
+
+# i = 0
+# while True:
+#     i += 1
+#     print(i)
+#     if i == 10:
+#         break
+
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# print(7)
+# print(8)
+# print(9)
+# print(10)
