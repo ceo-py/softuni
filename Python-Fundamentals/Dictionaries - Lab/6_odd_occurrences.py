a5425d46a6626d575daf434535912d67af4653c1
ceo-py <sfmf@abv.bg>
2023-09-08 15:31:13 +0300

    added another solution

--- Python-Fundamentals/Dictionaries - Lab/6_odd_occurrences.py
+++ Python-Fundamentals/Dictionaries - Lab/6_odd_occurrences.py
@@ -1,23 +1,39 @@
-elements = input().lower().split()
+words = input().lower().split()
 
-# split_elements = elements.split()
-# check_element = list()
-# for element in split_elements:
-#     count = split_elements.count(element)
-#     if count % 2 != 0 and element not in check_element:
-#         print(element, end= " ")
-#         check_element.append(element)
+output = {}
+for word in words:
+    output[word] = output.get(word, 0) + 1
 
-elements_dic = dict()
+for word, quantity in output.items():
+    if quantity % 2 != 0:
+        print(word, end=' ')
 
-for character in elements:
-    if character not in elements_dic:
-        elements_dic[character] = 0
-    elements_dic[character] += 1
 
-for key, value in elements_dic.items():
-    if value % 2 != 0:
-        print(key, end= " ")
+
+
+
+
+
+# elements = input().lower().split()
+#
+# # split_elements = elements.split()
+# # check_element = list()
+# # for element in split_elements:
+# #     count = split_elements.count(element)
+# #     if count % 2 != 0 and element not in check_element:
+# #         print(element, end= " ")
+# #         check_element.append(element)
+#
+# elements_dic = dict()
+#
+# for character in elements:
+#     if character not in elements_dic:
+#         elements_dic[character] = 0
+#     elements_dic[character] += 1
+#
+# for key, value in elements_dic.items():
+#     if value % 2 != 0:
+#         print(key, end= " ")
 
 
 
