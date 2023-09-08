68beea7fa70e871c5fa8558397643ac68b690b4b
ceo-py <sfmf@abv.bg>
2023-09-08 11:13:51 +0300

    added another solution

--- PB - More Exercises/Conditional Statements - More Exercises/6_pets.py
+++ PB - More Exercises/Conditional Statements - More Exercises/6_pets.py
@@ -10,12 +10,9 @@ dog_food_needed = number_days * food_for_dog_per_kg
 cat_food_needed = number_days * food_for_cat_per_kg
 turtle_food_needed = (number_days * food_for_turtle_per_grams) / 1000
 
-total_food = dog_food_needed + cat_food_needed + turtle_food_needed
+total_food = left_foot_inkg - (dog_food_needed + cat_food_needed + turtle_food_needed)
 
-food_left = left_foot_inkg - total_food
-
-if total_food <= left_foot_inkg:
-    print(f"{math.floor(food_left)} kilos of food left.")
+if total_food >= 0:
+    print(f"{math.floor(total_food)} kilos of food left.")
 else:
-    total_food = total_food - left_foot_inkg
-    print(f"{math.ceil(total_food)} more kilos of food are needed.")
+    print(f"{math.ceil(abs(total_food))} more kilos of food are needed.")
