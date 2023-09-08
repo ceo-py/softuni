0b778d39c32d611eb8e9cac4db2891d8dc9cfe10
ceo-py <sfmf@abv.bg>
2023-09-08 14:51:39 +0300

    added another solution

--- Python-Fundamentals/Dictionaries - Lab/1_bakery.py
+++ Python-Fundamentals/Dictionaries - Lab/1_bakery.py
@@ -1,19 +1,28 @@
-food_type = input().split()
-
-food_info_dic = dict()
-i = 0
-for string in food_type:
-    if i % 2 == 0:
-        food_info_dic[string] = 0
-        name = string
-    if i % 2 != 0:
-        food_info_dic[name] = int(string)
-        name = ""
-    i += 1
+initial_list = input().split()
+food_type = {initial_list[i]: int(initial_list[i + 1]) for i in range(0, len(initial_list), 2)}
+print(food_type)
 
 
-# for i in range(0, len(food_type), 2):
-#     food_info_dic[food_type[i]] = int(food_type[(i + 1)])
 
 
-print(food_info_dic)
+
+
+# food_type = input().split()
+#
+# food_info_dic = dict()
+# i = 0
+# for string in food_type:
+#     if i % 2 == 0:
+#         food_info_dic[string] = 0
+#         name = string
+#     if i % 2 != 0:
+#         food_info_dic[name] = int(string)
+#         name = ""
+#     i += 1
+#
+#
+# # for i in range(0, len(food_type), 2):
+# #     food_info_dic[food_type[i]] = int(food_type[(i + 1)])
+#
+#
+# print(food_info_dic)
