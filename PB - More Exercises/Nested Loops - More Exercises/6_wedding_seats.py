382ff565e76be9a14a0df5e9aaf608de501a709a
ceo-py <sfmf@abv.bg>
2023-09-08 12:49:16 +0300

    added another solution

--- PB - More Exercises/Nested Loops - More Exercises/6_wedding_seats.py
+++ PB - More Exercises/Nested Loops - More Exercises/6_wedding_seats.py
@@ -1,46 +1,81 @@
-import string
-
 sector_simbols = input()
 number_rows_sector_one = int(input())
 number_space_odd_row = int(input())
 
-alphabet_list_lower = string.ascii_lowercase
-alphabet_list_up = string.ascii_uppercase
-sector_one = alphabet_list_up.index(sector_simbols)
-checking_letters = alphabet_list_up[:sector_one + 1]
-even_rows = number_space_odd_row + 1
-counter_operation = 0
-add_row = 0
+starting_sector = ord('A')
+ending_sector = ord(sector_simbols)
+starting_place = ord('a')
+additional_row = 0
+additional_ron_odd = 2
+total_seats = 0
 
-for letter in checking_letters:
-    even_rows += 1
-    add_row += 1
 
-    for row in range(1, number_rows_sector_one + add_row):
-        added_rows = 0
-        even_rows_count = 0
+for sector in range(starting_sector, ending_sector + 1):
+    additional_row += 1
 
-        for possition in alphabet_list_lower:
+    for row in range(1, number_rows_sector_one + additional_row):
+        place_start = ord('a')
 
-            if added_rows > even_rows:
-                break
+        if row % 2 != 0:
+            for place in range(place_start, place_start + number_space_odd_row):
+                print(f'{chr(sector)}{row}{chr(place)}')
+                total_seats += 1
 
-            if letter == "A":
+        else:
+            for place in range(place_start, place_start + number_space_odd_row + additional_ron_odd):
+                print(f'{chr(sector)}{row}{chr(place)}')
+                total_seats += 1
 
-                if row % 2 == 0 and even_rows_count < even_rows:
-                    print(f"{letter}{row}{possition}")
-                    even_rows_count += 1
-                    counter_operation += 1
+print(total_seats)
 
-            else:
-                if row % 2 == 0 and even_rows_count < even_rows - add_row + 1:
-                    print(f"{letter}{row}{possition}")
-                    even_rows_count += 1
-                    counter_operation += 1
 
-            if row % 2 != 0 and number_space_odd_row > added_rows:
-                print(f"{letter}{row}{possition}")
-                counter_operation += 1
-            added_rows += 1
 
-print(counter_operation)
+
+
+
+# import string
+#
+# sector_simbols = input()
+# number_rows_sector_one = int(input())
+# number_space_odd_row = int(input())
+#
+# alphabet_list_lower = string.ascii_lowercase
+# alphabet_list_up = string.ascii_uppercase
+# sector_one = alphabet_list_up.index(sector_simbols)
+# checking_letters = alphabet_list_up[:sector_one + 1]
+# even_rows = number_space_odd_row + 1
+# counter_operation = 0
+# add_row = 0
+#
+# for letter in checking_letters:
+#     even_rows += 1
+#     add_row += 1
+#
+#     for row in range(1, number_rows_sector_one + add_row):
+#         added_rows = 0
+#         even_rows_count = 0
+#
+#         for possition in alphabet_list_lower:
+#
+#             if added_rows > even_rows:
+#                 break
+#
+#             if letter == "A":
+#
+#                 if row % 2 == 0 and even_rows_count < even_rows:
+#                     print(f"{letter}{row}{possition}")
+#                     even_rows_count += 1
+#                     counter_operation += 1
+#
+#             else:
+#                 if row % 2 == 0 and even_rows_count < even_rows - add_row + 1:
+#                     print(f"{letter}{row}{possition}")
+#                     even_rows_count += 1
+#                     counter_operation += 1
+#
+#             if row % 2 != 0 and number_space_odd_row > added_rows:
+#                 print(f"{letter}{row}{possition}")
+#                 counter_operation += 1
+#             added_rows += 1
+#
+# print(counter_operation)
