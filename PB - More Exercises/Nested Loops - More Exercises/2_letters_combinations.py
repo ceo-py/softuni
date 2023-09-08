d1e84165b734b7de9fef1ec272e103d37eef9ed1
ceo-py <sfmf@abv.bg>
2023-09-08 12:20:04 +0300

    added another solution

--- PB - More Exercises/Nested Loops - More Exercises/2_letters_combinations.py
+++ PB - More Exercises/Nested Loops - More Exercises/2_letters_combinations.py
@@ -1,22 +1,48 @@
-import string
+start_letter = input()
+end_letter = input()
+skip_letter = input()
 
-all_letters = string.ascii_lowercase
 
-letter_a = input()
-letter_b = input()
-letter_c = input()
+letter_ascii_number = ord(skip_letter)
+start_loop_range = ord(start_letter)
+end_loop_range = ord(end_letter) + 1
+found_combinations = 0
 
-starting_point_loop = all_letters.index(letter_a)
-ending_point_loop = all_letters.index(letter_b)
-combinations_of_letters = all_letters[starting_point_loop:ending_point_loop + 1]
-combinations_of_letters = combinations_of_letters.replace(letter_c, "")
-combinations = 0
-for first_letter in combinations_of_letters:
-    for second_letter in combinations_of_letters:
-        for third_letter in combinations_of_letters:
-            combinations += 1
-            print(f"{first_letter}{second_letter}{third_letter}", end=" ")
-print(combinations)
+for first_letter in range(start_loop_range, end_loop_range):
+    for middle_letter in range(start_loop_range, end_loop_range):
+        for end_letter in range(start_loop_range, end_loop_range):
+
+            if first_letter != letter_ascii_number and middle_letter != letter_ascii_number and end_letter != letter_ascii_number:
+                found_combinations += 1
+                print(f'{chr(first_letter)}{chr(middle_letter)}{chr(end_letter)}', end=' ')
+
+print(found_combinations)
+
+
+
+
+# import string
+#
+# all_letters = string.ascii_lowercase
+#
+# letter_a = input()
+# letter_b = input()
+# letter_c = input()
+#
+# starting_point_loop = all_letters.index(letter_a)
+# ending_point_loop = all_letters.index(letter_b)
+# combinations_of_letters = all_letters[starting_point_loop:ending_point_loop + 1]
+# combinations_of_letters = combinations_of_letters.replace(letter_c, "")
+# combinations = 0
+# for first_letter in combinations_of_letters:
+#     for second_letter in combinations_of_letters:
+#         for third_letter in combinations_of_letters:
+#             combinations += 1
+#             print(f"{first_letter}{second_letter}{third_letter}", end=" ")
+# print(combinations)
+
+
+
 
 
 # import itertools
@@ -42,6 +68,9 @@ print(combinations)
 #         combinations_count += 1
 #
 # print(combinations_count)
+
+
+
 
 
 
