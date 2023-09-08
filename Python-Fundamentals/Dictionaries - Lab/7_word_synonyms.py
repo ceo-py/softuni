cb5eeb8a71ae6da7cd96934961ec972f3f008029
ceo-py <sfmf@abv.bg>
2023-09-08 15:34:50 +0300

    added another solution

--- Python-Fundamentals/Dictionaries - Lab/7_word_synonyms.py
+++ Python-Fundamentals/Dictionaries - Lab/7_word_synonyms.py
@@ -1,20 +1,39 @@
 number_words = int(input())
 
+words_synonyms = {}
 
-synonyms = {}
 for _ in range(number_words):
-    word_one = input()
-    word_two = input()
-    if word_one not in synonyms:
-        synonyms[word_one] = list()
-        synonyms[word_one].append(word_two)
-    else:
-        synonyms[word_one].append(word_two)
+    word = input()
+    synonym = input()
+    words_synonyms[word] = words_synonyms.get(word, []) + [synonym]
 
-for key, value in synonyms.items():
-    print(f"{key} - ", end="")
-    print(*value, sep=", ")
+for word in words_synonyms:
+    print(f'{word} - {", ".join(words_synonyms[word])}')
 
 
-# for word in synonyms:
-#     print(f"{word} - {', '. join(synonyms[word])}")
+
+
+
+
+
+
+# number_words = int(input())
+#
+#
+# synonyms = {}
+# for _ in range(number_words):
+#     word_one = input()
+#     word_two = input()
+#     if word_one not in synonyms:
+#         synonyms[word_one] = list()
+#         synonyms[word_one].append(word_two)
+#     else:
+#         synonyms[word_one].append(word_two)
+#
+# for key, value in synonyms.items():
+#     print(f"{key} - ", end="")
+#     print(*value, sep=", ")
+#
+#
+# # for word in synonyms:
+# #     print(f"{word} - {', '. join(synonyms[word])}")
