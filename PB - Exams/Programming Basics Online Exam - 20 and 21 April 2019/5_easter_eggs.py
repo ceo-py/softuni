e7e721694e43b7109927494ef99f571e8d2b9806
ceo-py <sfmf@abv.bg>
2023-10-08 20:45:41 +0300

    Another Solution

--- PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/5_easter_eggs.py
+++ PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/5_easter_eggs.py
@@ -1,42 +1,96 @@
-number_colored_eggs = int(input())
+egg_colored_numbers = int(input())
 
-red_eggs_number = 0
-orange_eggs_number = 0
-blue_eggs_number = 0
-green_eggs_number = 0
-max_eggs_number_from_color = 0
-for _ in range(number_colored_eggs):
-    egg_color = input()
-    if egg_color == "red":
-        red_eggs_number += 1
-    elif egg_color == "orange":
-        orange_eggs_number += 1
-    elif egg_color == "blue":
-        blue_eggs_number += 1
-    elif egg_color == "green":
-        green_eggs_number += 1
+red = 0
+orange = 0
+blue = 0
+green = 0
 
-if blue_eggs_number < red_eggs_number > orange_eggs_number and red_eggs_number > green_eggs_number:
-    color = "red"
-    max_eggs_number_from_color = red_eggs_number
+for egg in range(egg_colored_numbers):
 
-elif blue_eggs_number < orange_eggs_number > red_eggs_number and orange_eggs_number > green_eggs_number:
-    color = "orange"
-    max_eggs_number_from_color = orange_eggs_number
+    color = input()
 
-elif orange_eggs_number < blue_eggs_number > red_eggs_number and blue_eggs_number > green_eggs_number:
-    color = "blue"
-    max_eggs_number_from_color = blue_eggs_number
+    if color == 'red':
+        red += 1
 
-else:
-    color = "green"
-    max_eggs_number_from_color = green_eggs_number
+    elif color == 'orange':
+        orange += 1
 
-print(f"Red eggs: {red_eggs_number}")
-print(f"Orange eggs: {orange_eggs_number}")
-print(f"Blue eggs: {blue_eggs_number}")
-print(f"Green eggs: {green_eggs_number}")
-print(f"Max eggs: {max_eggs_number_from_color} -> {color}")
+    elif color == 'blue':
+        blue += 1
+
+    elif color == 'green':
+        green += 1
+
+max_color = 'red'
+max_number = red
+
+if orange > max_number:
+    max_number = orange
+    max_color = 'orange'
+
+if blue > max_number:
+    max_number = blue
+    max_color = 'blue'
+
+if green > max_number:
+    max_number = green
+    max_color = 'green'
+
+print(f'Red eggs: {red}')
+print(f'Orange eggs: {orange}')
+print(f'Blue eggs: {blue}')
+print(f'Green eggs: {green}')
+print(f'Max eggs: {max_number} -> {max_color}')
+
+
+
+
+
+
+
+
+
+
+
+# number_colored_eggs = int(input())
+#
+# red_eggs_number = 0
+# orange_eggs_number = 0
+# blue_eggs_number = 0
+# green_eggs_number = 0
+# max_eggs_number_from_color = 0
+# for _ in range(number_colored_eggs):
+#     egg_color = input()
+#     if egg_color == "red":
+#         red_eggs_number += 1
+#     elif egg_color == "orange":
+#         orange_eggs_number += 1
+#     elif egg_color == "blue":
+#         blue_eggs_number += 1
+#     elif egg_color == "green":
+#         green_eggs_number += 1
+#
+# if blue_eggs_number < red_eggs_number > orange_eggs_number and red_eggs_number > green_eggs_number:
+#     color = "red"
+#     max_eggs_number_from_color = red_eggs_number
+#
+# elif blue_eggs_number < orange_eggs_number > red_eggs_number and orange_eggs_number > green_eggs_number:
+#     color = "orange"
+#     max_eggs_number_from_color = orange_eggs_number
+#
+# elif orange_eggs_number < blue_eggs_number > red_eggs_number and blue_eggs_number > green_eggs_number:
+#     color = "blue"
+#     max_eggs_number_from_color = blue_eggs_number
+#
+# else:
+#     color = "green"
+#     max_eggs_number_from_color = green_eggs_number
+#
+# print(f"Red eggs: {red_eggs_number}")
+# print(f"Orange eggs: {orange_eggs_number}")
+# print(f"Blue eggs: {blue_eggs_number}")
+# print(f"Green eggs: {green_eggs_number}")
+# print(f"Max eggs: {max_eggs_number_from_color} -> {color}")
 
 
 
