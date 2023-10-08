4cbecb8099c3677bc71506fd5180e5621a38e872
ceo-py <sfmf@abv.bg>
2023-10-08 19:58:31 +0300

    Another Solution

--- PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/3_easter_trip.py
+++ PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/3_easter_trip.py
@@ -1,23 +1,70 @@
-holiday_destination = input()
-dates_for_reservation = input()
-number_sleep_over = int(input())
+destination = input()
+dates = input()
+night_numbers = int(input())
 
-holiday_info = {
-    "21-23": {
-        "France": 30,
-        "Italy": 28,
-        "Germany": 32},
-    "24-27": {
-        "France": 35,
-        "Italy": 32,
-        "Germany": 37},
-    "28-31": {
-        "France": 40,
-        "Italy": 39,
-        "Germany": 43},
+price = 0
 
-}
+if destination == 'France':
 
-total = holiday_info[dates_for_reservation][holiday_destination] * number_sleep_over
+    if dates == '21-23':
+        price = 30
 
-print(f"Easter trip to {holiday_destination} : {total:.2f} leva.")
+    elif dates == '24-27':
+        price = 35
+
+    elif dates == '28-31':
+        price = 40
+
+elif destination == 'Italy':
+
+    if dates == '21-23':
+        price = 28
+
+    elif dates == '24-27':
+        price = 32
+
+    elif dates == '28-31':
+        price = 39
+
+elif destination == 'Germany':
+
+    if dates == '21-23':
+        price = 32
+
+    elif dates == '24-27':
+        price = 37
+
+    elif dates == '28-31':
+        price = 43
+
+total_price = price * night_numbers
+
+print(f'Easter trip to {destination} : {total_price:.2f} leva.')
+
+
+
+
+
+# holiday_destination = input()
+# dates_for_reservation = input()
+# number_sleep_over = int(input())
+#
+# holiday_info = {
+#     "21-23": {
+#         "France": 30,
+#         "Italy": 28,
+#         "Germany": 32},
+#     "24-27": {
+#         "France": 35,
+#         "Italy": 32,
+#         "Germany": 37},
+#     "28-31": {
+#         "France": 40,
+#         "Italy": 39,
+#         "Germany": 43},
+#
+# }
+#
+# total = holiday_info[dates_for_reservation][holiday_destination] * number_sleep_over
+#
+# print(f"Easter trip to {holiday_destination} : {total:.2f} leva.")
