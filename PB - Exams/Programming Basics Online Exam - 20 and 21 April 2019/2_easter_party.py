5baca5f0b95728951ae107a67064daeec641c518
ceo-py <sfmf@abv.bg>
2023-10-08 19:35:40 +0300

    Another Solution

--- PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/2_easter_party.py
+++ PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/2_easter_party.py
@@ -1,27 +1,60 @@
-guest_numbers = int(input())
-price_reservation = float(input())
+number_guests = int(input())
+reservation_one_person = float(input())
 budget = float(input())
 
-off_reservation = 0
+cake_price = budget * 0.10
+discount = 0
 
-if guest_numbers < 10:
-    off_reservation = price_reservation
+if number_guests < 10:
+    discount = 1
 
-elif guest_numbers <= 15:
-    off_reservation = price_reservation - (price_reservation * 0.15)
+elif 10 <= number_guests <= 15:
+    discount = 0.85
 
-elif guest_numbers <= 20:
-    off_reservation = price_reservation - (price_reservation * 0.20)
+elif number_guests <= 20:
+    discount = 0.80
 
-elif guest_numbers > 20:
-    off_reservation = price_reservation - (price_reservation * 0.25)
+elif number_guests > 20:
+    discount = 0.75
 
-cake = budget - (budget * 0.90)
-total = (off_reservation * guest_numbers) + cake
-money_left = abs(budget - total)
+expenses = ((number_guests * reservation_one_person) * discount) + cake_price
+total_sum = budget - expenses
 
-if total <= budget:
-    print(f"It is party time! {money_left:.2f} leva left.")
-
+if total_sum >= 0:
+    print(f'It is party time! {total_sum:.2f} leva left.')
 else:
-    print(f"No party! {money_left:.2f} leva needed.")
+    print(f'No party! {abs(total_sum):.2f} leva needed.')
+
+
+
+
+
+
+
+# guest_numbers = int(input())
+# price_reservation = float(input())
+# budget = float(input())
+#
+# off_reservation = 0
+#
+# if guest_numbers < 10:
+#     off_reservation = price_reservation
+#
+# elif guest_numbers <= 15:
+#     off_reservation = price_reservation - (price_reservation * 0.15)
+#
+# elif guest_numbers <= 20:
+#     off_reservation = price_reservation - (price_reservation * 0.20)
+#
+# elif guest_numbers > 20:
+#     off_reservation = price_reservation - (price_reservation * 0.25)
+#
+# cake = budget - (budget * 0.90)
+# total = (off_reservation * guest_numbers) + cake
+# money_left = abs(budget - total)
+#
+# if total <= budget:
+#     print(f"It is party time! {money_left:.2f} leva left.")
+#
+# else:
+#     print(f"No party! {money_left:.2f} leva needed.")
