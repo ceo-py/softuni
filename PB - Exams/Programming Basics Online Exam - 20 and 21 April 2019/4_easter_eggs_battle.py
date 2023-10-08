dbc6a2260c6d0b2999398acab2d8dc4c86528215
ceo-py <sfmf@abv.bg>
2023-10-08 20:15:57 +0300

    Another Solution

--- PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/4_easter_eggs_battle.py
+++ PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/4_easter_eggs_battle.py
@@ -1,25 +1,54 @@
 player_one_eggs = int(input())
 player_two_eggs = int(input())
+command = input()
 
-player_one_eggs_left = player_one_eggs
-player_two_eggs_left = player_two_eggs
+while command != 'End':
+    if command == 'one':
+        player_two_eggs -= 1
+        if player_two_eggs == 0:
+            print(f'Player two is out of eggs. Player one has {player_one_eggs} eggs left.')
+            break
 
-while True:
-    winner = input()
-    if winner == "End of battle":
-        print(f"Player one has {player_one_eggs_left} eggs left.\nPlayer two has {player_two_eggs_left} eggs left.")
-        break
+    elif command == 'two':
+        player_one_eggs -= 1
+        if player_one_eggs == 0:
+            print(f'Player one is out of eggs. Player two has {player_two_eggs} eggs left.')
+            break
 
-    if winner == "one":
-        player_two_eggs_left -= 1
+    command = input()
 
-    else:
-        player_one_eggs_left -= 1
+else:
+    print(f'Player one has {player_one_eggs} eggs left.')
+    print(f'Player two has {player_two_eggs} eggs left.')
 
-    if player_one_eggs_left == 0:
-        print(f"Player one is out of eggs. Player two has {player_two_eggs_left} eggs left.")
-        break
 
-    elif player_two_eggs_left == 0:
-        print(f"Player two is out of eggs. Player one has {player_one_eggs_left} eggs left.")
-        break
+
+
+
+
+
+# player_one_eggs = int(input())
+# player_two_eggs = int(input())
+#
+# player_one_eggs_left = player_one_eggs
+# player_two_eggs_left = player_two_eggs
+#
+# while True:
+#     winner = input()
+#     if winner == "End of battle":
+#         print(f"Player one has {player_one_eggs_left} eggs left.\nPlayer two has {player_two_eggs_left} eggs left.")
+#         break
+#
+#     if winner == "one":
+#         player_two_eggs_left -= 1
+#
+#     else:
+#         player_one_eggs_left -= 1
+#
+#     if player_one_eggs_left == 0:
+#         print(f"Player one is out of eggs. Player two has {player_two_eggs_left} eggs left.")
+#         break
+#
+#     elif player_two_eggs_left == 0:
+#         print(f"Player two is out of eggs. Player one has {player_one_eggs_left} eggs left.")
+#         break
