c17b0ceeaf85f016e08cce682a35a40aa75e5aa8
ceo-py <sfmf@abv.bg>
2023-10-08 20:25:16 +0300

    Another Solution

--- PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/4_easter_shop.py
+++ PB - Exams/Programming Basics Online Exam - 20 and 21 April 2019/4_easter_shop.py
@@ -1,25 +1,57 @@
-eggs_in_store = int(input())
+egg_starting_number = int(input())
 
-total_eggs = eggs_in_store
-total_sold_eggs = 0
+command = input()
+sold_eggs = 0
 
-while True:
-    action_type = input()
+while command != 'Close':
+    egg_number = int(input())
 
-    if action_type == "Close":
-        print(f"Store is closed!\n{total_sold_eggs} eggs sold.")
-        break
+    if command == 'Buy':
+        egg_starting_number -= egg_number
+        sold_eggs += egg_number
 
-    else:
-        number_eggs = int(input())
+        if egg_starting_number < 0:
+            print(f'Not enough eggs in store!')
+            print(f'You can buy only {egg_starting_number + egg_number}.')
+            break
 
-    if action_type == "Buy":
-        total_eggs = total_eggs - number_eggs
-        total_sold_eggs += number_eggs
+    elif command == 'Fill':
+        egg_starting_number += egg_number
 
-    elif action_type == "Fill":
-        total_eggs = total_eggs + number_eggs
+    command = input()
 
-    if total_eggs < 0:
-        print(f"Not enough eggs in store!\nYou can buy only {total_eggs + number_eggs}.")
-        break
+else:
+    print(f'Store is closed!')
+    print(f'{sold_eggs} eggs sold.')
+
+
+
+
+
+
+
+# eggs_in_store = int(input())
+#
+# total_eggs = eggs_in_store
+# total_sold_eggs = 0
+#
+# while True:
+#     action_type = input()
+#
+#     if action_type == "Close":
+#         print(f"Store is closed!\n{total_sold_eggs} eggs sold.")
+#         break
+#
+#     else:
+#         number_eggs = int(input())
+#
+#     if action_type == "Buy":
+#         total_eggs = total_eggs - number_eggs
+#         total_sold_eggs += number_eggs
+#
+#     elif action_type == "Fill":
+#         total_eggs = total_eggs + number_eggs
+#
+#     if total_eggs < 0:
+#         print(f"Not enough eggs in store!\nYou can buy only {total_eggs + number_eggs}.")
+#         break
