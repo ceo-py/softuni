920ef603402880ee377c7f51edea577207b06918
ceo-py <sfmf@abv.bg>
2023-10-12 15:02:51 +0300

    Another Solution

--- Fundamentals-Exams/05. Programming Fundamentals Final Exam/1_activation_keys.py
+++ Fundamentals-Exams/05. Programming Fundamentals Final Exam/1_activation_keys.py
@@ -1,44 +1,91 @@
-main_string = input()
+raw_activation_key = input()
+
+command_data = input()
+
+while command_data != 'Generate':
+
+    command, *data = [int(x) if x.isdigit() else x for x in command_data.split('>>>')]
+
+    if command == 'Contains':
+        substring = str(data[0])
+
+        if substring in raw_activation_key:
+            print(f'{raw_activation_key} contains {substring}')
+        else:
+            print('Substring not found!')
+
+    if command == 'Flip':
+        case, start_index, end_index = data
+        if case == 'Upper':
+            change = raw_activation_key[start_index:end_index].upper()
+        else:
+            change = raw_activation_key[start_index:end_index].lower()
+
+        raw_activation_key = raw_activation_key[:start_index] + \
+            change + \
+            raw_activation_key[end_index:]
+
+    elif command == 'Slice':
+        start_index, end_index = data
+        # raw_activation_key = raw_activation_key.replace(raw_activation_key[start_index:end_index], '')
+        raw_activation_key = raw_activation_key[:start_index] + raw_activation_key[end_index:]
+
+    if command != 'Contains':
+        print(raw_activation_key)
+
+    command_data = input()
+
+print(f'Your activation key is: {raw_activation_key}')
 
 
-def string_contains(substring, main_string):
-    if substring in main_string:
-        return f"{main_string} contains {substring}"
-    return "Substring not found!"
 
 
-def flip_string(type_letters, start_index, end_index, main_string):
-    if "Upper" in type_letters:
-        result = main_string[:start_index] + main_string[start_index:end_index].upper() \
-                 + main_string[end_index:]
-    elif "Lower" in type_letters:
-        result = main_string[:start_index] + main_string[start_index:end_index].lower() \
-                 + main_string[end_index:]
-    return result
 
 
-def slice_string(start_index, end_index, main_string):
-    return main_string[:start_index] + main_string[end_index:]
 
 
-command = input()
 
-while command != "Generate":
-    command_name, *others = command.split(">>>")
-    if "Contains" == command_name:
-        print(string_contains(others[0], main_string))
-    elif "Flip" == command_name:
-        type_letters, start_index, end_index = others
-        main_string = flip_string(type_letters, int(start_index), int(end_index), main_string)
-    elif "Slice" == command_name:
-        start_index, end_index = others
-        main_string = slice_string(int(start_index), int(end_index), main_string)
-    if "Contains" != command_name:
-        print(main_string)
-
-    command = input()
-
-print(f"Your activation key is: {main_string}")
+# main_string = input()
+#
+#
+# def string_contains(substring, main_string):
+#     if substring in main_string:
+#         return f"{main_string} contains {substring}"
+#     return "Substring not found!"
+#
+#
+# def flip_string(type_letters, start_index, end_index, main_string):
+#     if "Upper" in type_letters:
+#         result = main_string[:start_index] + main_string[start_index:end_index].upper() \
+#                  + main_string[end_index:]
+#     elif "Lower" in type_letters:
+#         result = main_string[:start_index] + main_string[start_index:end_index].lower() \
+#                  + main_string[end_index:]
+#     return result
+#
+#
+# def slice_string(start_index, end_index, main_string):
+#     return main_string[:start_index] + main_string[end_index:]
+#
+#
+# command = input()
+#
+# while command != "Generate":
+#     command_name, *others = command.split(">>>")
+#     if "Contains" == command_name:
+#         print(string_contains(others[0], main_string))
+#     elif "Flip" == command_name:
+#         type_letters, start_index, end_index = others
+#         main_string = flip_string(type_letters, int(start_index), int(end_index), main_string)
+#     elif "Slice" == command_name:
+#         start_index, end_index = others
+#         main_string = slice_string(int(start_index), int(end_index), main_string)
+#     if "Contains" != command_name:
+#         print(main_string)
+#
+#     command = input()
+#
+# print(f"Your activation key is: {main_string}")
 
 
 
