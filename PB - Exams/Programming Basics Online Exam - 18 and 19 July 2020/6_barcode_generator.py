781ea633066fbb66638157e39c43a669028ec21b
ceo-py <sfmf@abv.bg>
2023-10-14 02:20:32 +0300

    add another solution

--- PB - Exams/Programming Basics Online Exam - 18 and 19 July 2020/6_barcode_generator.py
+++ PB - Exams/Programming Basics Online Exam - 18 and 19 July 2020/6_barcode_generator.py
@@ -1,18 +1,6 @@
 start = input()
 end = input()
 
-
-# first_start = int(start / 1000)
-# second_start = int((start / 100) % 10)
-# third_start = int((start / 10) % 10)
-# fourth_start = int(start % 10)
-#
-# first_end = int(end / 1000)
-# second_end = int((end / 100) % 10)
-# third_end = int((end / 10) % 10)
-# fourth_end = int(end % 10)
-
-
 first_start = int(start[0])
 second_start = int(start[1])
 third_start = int(start[2])
@@ -34,3 +22,34 @@ for num_1 in range(first_start, first_end + 1):
 
                 if num_1 % 2 != 0 and num_2 % 2 != 0 and num_3 % 2 != 0 and num_4 % 2 != 0:
                     print(f'{num_1}{num_2}{num_3}{num_4}', end=' ')
+
+
+
+
+
+# start = input()
+# end = input()
+#
+#
+# first_start = int(start / 1000)
+# second_start = int((start / 100) % 10)
+# third_start = int((start / 10) % 10)
+# fourth_start = int(start % 10)
+#
+# first_end = int(end / 1000)
+# second_end = int((end / 100) % 10)
+# third_end = int((end / 10) % 10)
+# fourth_end = int(end % 10)
+#
+#
+#
+#
+# for num_1 in range(first_start if first_start % 2 != 0 else first_start + 1, first_end + 1, 2):
+#
+#     for num_2 in range(second_start if second_start % 2 != 0 else second_start + 1, second_end + 1, 2):
+#
+#         for num_3 in range(third_start if third_start % 2 != 0 else third_start + 1, third_end + 1, 2):
+#
+#             for num_4 in range(fourth_start if fourth_start % 2 != 0 else fourth_start + 1, fourth_end + 1, 2):
+#
+#                     print(f'{num_1}{num_2}{num_3}{num_4}', end=' ')
