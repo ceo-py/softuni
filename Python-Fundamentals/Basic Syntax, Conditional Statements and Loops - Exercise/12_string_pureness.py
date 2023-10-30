00bce852fd330b13d994464537cfad310200f741
ceo-py <sfmf@abv.bg>
2023-10-30 14:58:53 +0200

    added another solution

--- Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/12_string_pureness.py
+++ Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/12_string_pureness.py
@@ -1,12 +1,24 @@
-how_many_strings = int(input())
+n = int(input())
+symbols = ".,_"
 
-skip_parameters = [",", ".", "_"]
-
-for _ in range(how_many_strings):
-    string_ = input()
-    for element in skip_parameters:
-        if element in string_:
-            print(f"{string_} is not pure!")
-            break
+for words in range(n):
+    string_to_input = input()
+    if any(x in string_to_input for x in symbols):
+        print(f"{string_to_input} is not pure!")
     else:
-        print(f"{string_} is pure.")
+        print(f"{string_to_input} is pure.")
+
+
+
+# how_many_strings = int(input())
+#
+# skip_parameters = [",", ".", "_"]
+#
+# for _ in range(how_many_strings):
+#     string_ = input()
+#     for element in skip_parameters:
+#         if element in string_:
+#             print(f"{string_} is not pure!")
+#             break
+#     else:
+#         print(f"{string_} is pure.")
