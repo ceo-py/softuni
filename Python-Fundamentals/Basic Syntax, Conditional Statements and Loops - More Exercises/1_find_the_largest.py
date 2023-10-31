f2f5b4b827a53dd8d7b75f6870928eac70b343d0
ceo-py <sfmf@abv.bg>
2023-10-31 09:58:20 +0200

    added another solution

--- Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercises/1_find_the_largest.py
+++ Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercises/1_find_the_largest.py
@@ -1,7 +1,24 @@
 number_input = [n for n in input()]
-number_input.sort(reverse=True)
-for i in number_input:
-    print(i, end="")
+n = len(number_input)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if number_input[j] < number_input[j + 1]:
+            # Swap the elements
+            number_input[j], number_input[j + 1] = number_input[j + 1], number_input[j]
+
+sorted_number = ''.join(number_input)
+
+print(sorted_number)
+
+
+
+
+
+# number_input = [n for n in input()]
+# number_input.sort(reverse=True)
+# for i in number_input:
+#     print(i, end="")
 
 
 
