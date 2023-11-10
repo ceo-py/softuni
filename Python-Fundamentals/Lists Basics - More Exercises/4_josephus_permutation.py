eccd91a3b50bc2bf3c7492adfddea747bb959559
ceo-py <sfmf@abv.bg>
2023-11-10 20:08:33 +0200

    added another solution

--- Python-Fundamentals/Lists Basics - More Exercises/4_josephus_permutation.py
+++ Python-Fundamentals/Lists Basics - More Exercises/4_josephus_permutation.py
@@ -4,18 +4,13 @@ skip_number_k = int(input())
 final_numbers = list()
 pos = skip_number_k - 1
 index = 0
-len_list = (len(main_list))
 
-while len_list > 0:
-    index = (pos + index) % len_list
+while main_list:
+    index = (pos + index) % len(main_list)
     final_numbers.append(main_list.pop(index))
-    len_list -= 1
 
 
 print(f"[{','.join(final_numbers)}]")
-
-
-
 
 
 
