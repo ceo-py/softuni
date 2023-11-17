dcccec740c714437a5ee2abc5b153074f807977a
ceo-py <sfmf@abv.bg>
2023-11-17 13:15:01 +0200

    added another solution

--- Python-Fundamentals/Functions - More Exercises/2_center_point.py
+++ Python-Fundamentals/Functions - More Exercises/2_center_point.py
@@ -13,6 +13,33 @@ print(whats_closer(sum_x, sum_y))
 
 
 
+
+
+
+
+
+# import math
+#
+# x1 = math.floor(float(input()))
+# x2 = math.floor(float(input()))
+# y1 = math.floor(float(input()))
+# y2 = math.floor(float(input()))
+#
+# sum_x = math.floor(abs(x1) + abs(x2))
+# sum_y = math.floor(abs(y1) + abs(y2))
+#
+#
+# def whats_closer(arg1, arg2):
+#     return (x1, x2) if arg1 <= arg2 else (y1, y2)
+#
+#
+# print(whats_closer(sum_x, sum_y))
+
+
+
+
+
+
 #
 #
 #
