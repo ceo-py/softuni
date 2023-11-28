9032c8fa3f13b6a3b17a89a502a823e270dfb9f4
ceo-py <sfmf@abv.bg>
2023-11-28 22:35:59 +0200

    added another solution

--- PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/6_high_jump.py
+++ PB - Exams/Programming Basics Online Exam - 9 and 10 March 2019/6_high_jump.py
@@ -11,11 +11,12 @@ while unsuccessful_jumps < 3:
 
     if jump > current_high_jump:
         unsuccessful_jumps = 0
-        current_high_jump += 5
 
-        if current_high_jump > target_high_jump:
+        if current_high_jump >= target_high_jump:
             print(f"Tihomir succeeded, he jumped over {target_high_jump}cm after {jumps_counter} jumps.")
             break
+
+        current_high_jump += 5
 
     else:
         unsuccessful_jumps += 1
