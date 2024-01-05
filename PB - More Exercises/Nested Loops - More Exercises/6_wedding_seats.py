0528bf5f0bb8390679c19a096f46ce99185473e9
ceo-py <sfmf@abv.bg>
2024-01-05 20:39:27 +0200

    added another solution

--- PB - More Exercises/Nested Loops - More Exercises/6_wedding_seats.py
+++ PB - More Exercises/Nested Loops - More Exercises/6_wedding_seats.py
@@ -9,23 +9,51 @@ additional_row = 0
 additional_row_odd = 2
 total_seats = 0
 
-
 for sector in range(starting_sector, ending_sector + 1):
     additional_row += 1
 
     for row in range(1, number_rows_sector_one + additional_row):
 
-        if row % 2 != 0:
-            for place in range(starting_place, starting_place + number_space_odd_row):
-                print(f'{chr(sector)}{row}{chr(place)}')
-                total_seats += 1
-
-        else:
-            for place in range(starting_place, starting_place + number_space_odd_row + additional_row_odd):
+        for place in range(starting_place, starting_place + number_space_odd_row + (0 if row % 2 != 0 else additional_row_odd)):
                 print(f'{chr(sector)}{row}{chr(place)}')
                 total_seats += 1
 
 print(total_seats)
+
+
+
+
+
+
+
+# sector_simbols = input()
+# number_rows_sector_one = int(input())
+# number_space_odd_row = int(input())
+#
+# starting_sector = ord('A')
+# ending_sector = ord(sector_simbols)
+# starting_place = ord('a')
+# additional_row = 0
+# additional_row_odd = 2
+# total_seats = 0
+#
+#
+# for sector in range(starting_sector, ending_sector + 1):
+#     additional_row += 1
+#
+#     for row in range(1, number_rows_sector_one + additional_row):
+#
+#         if row % 2 != 0:
+#             for place in range(starting_place, starting_place + number_space_odd_row):
+#                 print(f'{chr(sector)}{row}{chr(place)}')
+#                 total_seats += 1
+#
+#         else:
+#             for place in range(starting_place, starting_place + number_space_odd_row + additional_row_odd):
+#                 print(f'{chr(sector)}{row}{chr(place)}')
+#                 total_seats += 1
+#
+# print(total_seats)
 
 
 
