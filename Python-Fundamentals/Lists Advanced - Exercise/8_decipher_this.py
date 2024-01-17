4aec90f80a2f5b5abe4f0a2bde07a59b5644ddf3
ceo-py <sfmf@abv.bg>
2024-01-17 10:48:47 +0200

    added another solution

--- Python-Fundamentals/Lists Advanced - Exercise/8_decipher_this.py
+++ Python-Fundamentals/Lists Advanced - Exercise/8_decipher_this.py
@@ -1,6 +1,6 @@
 message = input().split()
 
-words, numbers = [], []
+words = []
 for word in message:
     num, let = "", ""
     for symbol in word:
@@ -8,10 +8,29 @@ for word in message:
             num += symbol
         else:
             let += symbol
-    numbers.append(int(num))
     if len(let) != 1:
         let = f"{let[-1]}{let[1:-1]}{let[0]}"
-    words.append(let)
+    words.append(f"{chr(int(num))}{let}")
 
-for n, w in zip(numbers, words):
-    print(f"{chr(n)}{w}", end=" ")
+print(*words, end=' ')
+
+
+
+
+# message = input().split()
+#
+# words, numbers = [], []
+# for word in message:
+#     num, let = "", ""
+#     for symbol in word:
+#         if symbol.isdigit():
+#             num += symbol
+#         else:
+#             let += symbol
+#     numbers.append(int(num))
+#     if len(let) != 1:
+#         let = f"{let[-1]}{let[1:-1]}{let[0]}"
+#     words.append(let)
+#
+# for n, w in zip(numbers, words):
+#     print(f"{chr(n)}{w}", end=" ")
