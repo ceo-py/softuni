5e03a4f453a5b36084bb4234ae90110a4aa22300
ceo-py <sfmf@abv.bg>
2024-01-21 16:49:48 +0200

    added another solution

--- Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/6_orders.py
+++ Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/6_orders.py
@@ -6,8 +6,8 @@ for _ in range(number_orders):
     price_per_capsule = float(input())
     days = int(input())
     count_capsule = int(input())
-    single_order = price_per_capsule * days * count_capsule
     if 0.01 <= price_per_capsule <= 100.00 and 1 <= days <= 31 and 1 <= count_capsule <= 2000:
+        single_order = price_per_capsule * days * count_capsule
         total += single_order
         print(f"The price for the coffee is: ${single_order:.2f}")
 
