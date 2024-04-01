722b0e0d8debe280d7d120d5d9d018aa4a2023f0
ceo-py <sfmf@abv.bg>
2024-04-01 15:08:04 +0300

    added another solution

--- Fundamentals-Exams/Exam Preparation - 20 March 2024/2_destination_mapper.py
+++ Fundamentals-Exams/Exam Preparation - 20 March 2024/2_destination_mapper.py
@@ -1,12 +1,24 @@
 import re
 
 initial_string = input()
-
 pattern = r'([=/])(?P<place>[A-Z]{1}[a-zA-Z]{2,})\1'
-
-found_destinations = []
-for match in re.finditer(pattern, initial_string):
-    found_destinations.append(match.group('place'))
+found_destinations = [match.group('place') for match in re.finditer(pattern, initial_string)]
 
 print(f'Destinations: {", ".join(found_destinations)}')
 print(f'Travel Points: {sum(len(x) for x in found_destinations)}')
+
+
+
+
+# import re
+#
+# initial_string = input()
+#
+# pattern = r'([=/])(?P<place>[A-Z]{1}[a-zA-Z]{2,})\1'
+#
+# found_destinations = []
+# for match in re.finditer(pattern, initial_string):
+#     found_destinations.append(match.group('place'))
+#
+# print(f'Destinations: {", ".join(found_destinations)}')
+# print(f'Travel Points: {sum(len(x) for x in found_destinations)}')
