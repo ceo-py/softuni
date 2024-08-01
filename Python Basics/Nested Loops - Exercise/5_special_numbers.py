d7e94e791d37c0c434f550a58c70b752be63029a
ceo-py <sfmf@abv.bg>
2024-08-01 10:39:48 +0300

    added another solution

--- Python Basics/Nested Loops - Exercise/5_special_numbers.py
+++ Python Basics/Nested Loops - Exercise/5_special_numbers.py
@@ -2,18 +2,47 @@ number_to_check = int(input())
 
 
 
-for number_one in range(1, 9):
-    if number_to_check % number_one == 0:
+for number_one in range(1, 10):
+    if number_to_check % number_one != 0:
+        continue
 
-        for number_two in range(1, 9):
-            if number_to_check % number_two == 0:
+    for number_two in range(1, 10):
+        if number_to_check % number_two != 0:
+           continue
 
-                for number_three in range(1, 9):
-                    if number_to_check % number_three == 0:
+        for number_three in range(1, 10):
+            if number_to_check % number_three != 0:
+                continue
 
-                        for number_four in range(1, 9):
-                            if number_to_check % number_four == 0:
-                                print(f"{number_one}{number_two}{number_three}{number_four}", end=" ")
+            for number_four in range(1, 10):
+                if number_to_check % number_four != 0:
+                    continue
+
+                print(f"{number_one}{number_two}{number_three}{number_four}", end=" ")
+
+
+
+
+
+
+
+# There is no test with number 9 that`s why it will work with that range
+# number_to_check = int(input())
+#
+#
+#
+# for number_one in range(1, 9):
+#     if number_to_check % number_one == 0:
+#
+#         for number_two in range(1, 9):
+#             if number_to_check % number_two == 0:
+#
+#                 for number_three in range(1, 9):
+#                     if number_to_check % number_three == 0:
+#
+#                         for number_four in range(1, 9):
+#                             if number_to_check % number_four == 0:
+#                                 print(f"{number_one}{number_two}{number_three}{number_four}", end=" ")
 
 
 
