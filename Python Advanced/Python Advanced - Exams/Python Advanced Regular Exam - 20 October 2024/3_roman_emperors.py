b53ec63b599e8435fa15050b9264251e3006dded
ceo-py <sfmf@abv.bg>
2024-10-25 18:41:59 +0300

    added another exam Python Advanced Regular Exam - 20 October 2024

--- Python Advanced/Python Advanced - Exams/Python Advanced Regular Exam - 20 October 2024/3_roman_emperors.py
+++ Python Advanced/Python Advanced - Exams/Python Advanced Regular Exam - 20 October 2024/3_roman_emperors.py
@@ -5,10 +5,8 @@ def list_roman_emperors(*emperors, **emperors_dict):
 
     for emperor_name, emperor_success in emperors:
         emperor_length = emperors_dict.get(emperor_name)
-        if emperor_success:
-            successful_emperors[emperor_name] = emperor_length
-        else:
-            unsuccessful_emperors[emperor_name] = emperor_length
+        target_dict = successful_emperors if emperor_success else unsuccessful_emperors
+        target_dict[emperor_name] = emperor_length
 
     output.append(
         f"Total number of emperors: {len(successful_emperors) + len(unsuccessful_emperors)}")
@@ -21,6 +19,41 @@ def list_roman_emperors(*emperors, **emperors_dict):
         output += [f"****{emperor_name}: {length}" for emperor_name, length in sorted(unsuccessful_emperors.items(), key=lambda x: (x[1], x[0]))]
 
     return "\n".join(output)
+
+
+
+
+
+
+
+
+
+
+
+
+# def list_roman_emperors(*emperors, **emperors_dict):
+#     output = []
+#     successful_emperors = {}
+#     unsuccessful_emperors = {}
+#
+#     for emperor_name, emperor_success in emperors:
+#         emperor_length = emperors_dict.get(emperor_name)
+#         if emperor_success:
+#             successful_emperors[emperor_name] = emperor_length
+#         else:
+#             unsuccessful_emperors[emperor_name] = emperor_length
+#
+#     output.append(
+#         f"Total number of emperors: {len(successful_emperors) + len(unsuccessful_emperors)}")
+#
+#     if successful_emperors:
+#         output.append("Successful emperors:")
+#         output += [f"****{emperor_name}: {length}" for emperor_name, length in sorted(successful_emperors.items(), key=lambda x: (-x[1], x[0]))]
+#     if unsuccessful_emperors:
+#         output.append("Unsuccessful emperors:")
+#         output += [f"****{emperor_name}: {length}" for emperor_name, length in sorted(unsuccessful_emperors.items(), key=lambda x: (x[1], x[0]))]
+#
+#     return "\n".join(output)
 
 
 
