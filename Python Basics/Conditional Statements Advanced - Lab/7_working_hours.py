7f7437f4ab5daeceb8a28863df8a729a4ffb94e0
ceo-py <sfmf@abv.bg>
2024-11-01 11:59:00 +0200

    added few more exam on task working hours

--- Python Basics/Conditional Statements Advanced - Lab/7_working_hours.py
+++ Python Basics/Conditional Statements Advanced - Lab/7_working_hours.py
@@ -8,9 +8,43 @@ else:
 
 
 
+# hour = int(input())
+# day_of_week = input()
+# result = "closed"
+#
+# if 10 <= hour <= 18 and (day_of_week == "Monday" or
+#             day_of_week == "Tuesday" or
+#             day_of_week == "Wednesday" or
+#             day_of_week == "Thursday" or
+#             day_of_week == "Friday" or
+#             day_of_week == "Saturday"):
+#         result = "open"
+#
+# print(result)
 
+# # Преход от това
+# hour = int(input())
+# day = input()
+# ouput = "closed"
 #
+# if 10 <= hour <= 18 and day != "Sunday":
+#     ouput = "open"
 #
+# print(ouput)
+
+
+# # към това
+# hour = int(input())
+# day = input()
+# ouput = "closed" if 10 <= hour <= 18 and day != "Sunday" else "closed"
+# print(ouput)
+
+
+
+# # до това
+# print("closed" if 10 <= int(input()) <= 18 and input() != "Sunday" else "closed")
+
+
 # hour = int(input())
 # day_of_the_week = input()
 #
